653fdcde21b6376d4b0915eb0d6a155b56c29c26
Ary Borenszweig <ary@esperanto.org.ar>
2012-09-04 22:07:05 -0300

    Easier require of crystal

--- lib/crystal.rb
+++ lib/crystal.rb
@@ -1,15 +1,3 @@
-[
-  'ast',
-  'lexer',
-  'parser',
-  'to_s',
-  'token',
-  'visitor',
-  'core_ext/false_class',
-  'core_ext/fixnum',
-  'core_ext/float',
-  'core_ext/string',
-  'core_ext/true_class',
-].each do |filename|
-  require(File.expand_path("../../lib/crystal/#{filename}",  __FILE__))
+Dir["#{File.expand_path('../',  __FILE__)}/**/*.rb"].each do |filename|
+  require filename
 end
