1c0de26029836f0a57f4ee56b77e52d7dbdb66c4
Ary Borenszweig <ary@esperanto.org.ar>
2012-09-18 20:56:01 -0300

    Spec for if inside if

--- spec/codegen/if_spec.rb
+++ spec/codegen/if_spec.rb
@@ -20,4 +20,8 @@ describe 'Code gen: if' do
   it 'codegens if inside def without an else with true' do
     run('def foo; a = 1; if true; a = 2; end; a; end; foo').to_i.should eq(2)
   end
+
+  it 'codegen if inside if' do
+    run('a = 1; if false; a = 1; elsif false; a = 2; else; a = 3; end; a').to_i.should eq(3)
+  end
 end
