260f4afe389b27172f1b086132750b6e2240285b
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-09-24 23:55:37 -0300

    Create graphs from left to right. Use different shapes for variables, classes and primitive types

--- lib/crystal/graph.rb
+++ lib/crystal/graph.rb
@@ -8,12 +8,11 @@ module Crystal
     node.accept visitor
 
     visitor.graphviz.output :png => "#{output}.png"
-    visitor.graphviz.to_s
   end
 
   class GraphVisitor < Visitor
     def initialize
-      @g = GraphViz.new(:G, :type => :digraph)
+      @g = GraphViz.new(:G, :type => :digraph, :rankdir => 'LR')
       @vars = []
     end
 
@@ -31,7 +30,7 @@ module Crystal
 
     def visit_var(node)
       unless @vars.include? node.name
-        var = @g.add_nodes node.name
+        var = @g.add_nodes node.name, :shape => :note
         @g.add_edges var, type_node(node.type)
         @vars << node.name
       end
@@ -40,11 +39,13 @@ module Crystal
     def type_node(type)
       node = @g.get_node(type.object_id.to_s)
       unless node
-        node = @g.add_nodes type.object_id.to_s, :label => type.name
         if type.is_a? ObjectType
+          node = @g.add_nodes type.object_id.to_s, :shape => 'record', :label => type.name
           type.instance_vars.each do |ivar, type|
             @g.add_edges node, type_node(type), :label => ivar
           end
+        else
+          node = @g.add_nodes type.object_id.to_s, :label => type.name
         end
       end
       node
