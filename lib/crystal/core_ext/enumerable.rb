a59f0ccff4f3ee1c32f616708ec24d0b6eeb6d57
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-10-13 13:55:40 -0300

    Removed unused code

--- lib/crystal/core_ext/enumerable.rb
+++
@@ -1,5 +0,0 @@
-module Enumerable
-  def at(index)
-    each_with_index.find{ |obj, i| i == index}.first
-  end
-end
--- spec/core_ext/enumerable_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Enumerable do
-  it "gets element at position" do
-    [1, 2, 3].at(1).should eq(2)
-  end
-end
