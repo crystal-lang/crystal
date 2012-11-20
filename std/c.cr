3fb9bb9d5433670b8dc26fb07288918757d7fa4a
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-11-20 20:12:44 -0300

    Added C externs

---
+++ std/c.cr
@@ -0,0 +1,8 @@
+extern putchar(c : Char) : Char
+extern getchar : Char
+extern strlen(str : String) : Int
+extern puts(str : String) : Int
+extern atoi(str : String) : Int
+extern srand(seed : Int)
+extern rand : Int
+extern time(t : Long) : Long
