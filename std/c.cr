252219356d1385f3ef749b237bf75c55fb2b50be
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-11-22 18:35:42 -0300

    Revert "Added C externs"
    This reverts commit 3fb9bb9d5433670b8dc26fb07288918757d7fa4a.

--- std/c.cr
+++
@@ -1,8 +0,0 @@
-extern putchar(c : Char) : Char
-extern getchar : Char
-extern strlen(str : String) : Int
-extern puts(str : String) : Int
-extern atoi(str : String) : Int
-extern srand(seed : Int)
-extern rand : Int
-extern time(t : Long) : Long
