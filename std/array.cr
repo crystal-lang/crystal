67dbf7cd22d368991a8baef56d594ab251949ec6
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-11-27 00:04:29 -0300

    Added Array#to_s

--- std/array.cr
+++ std/array.cr
@@ -11,4 +11,13 @@ class Array
   def to_a
     self
   end
+
+  def to_s
+    str = ""
+    each_with_index do |elem, i|
+      str += "\n" if i > 0
+      str += elem.to_s
+    end
+    str
+  end
 end
