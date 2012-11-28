b371bb1d056461607bfc8394dced34cdf1100235
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-11-28 16:48:07 -0300

    Added partial Complex to std

---
+++ std/complex.cr
@@ -0,0 +1,26 @@
+class Complex
+  def initialize(real, imag)
+    @real = real
+    @imag = imag
+  end
+
+  def real
+    @real
+  end
+
+  def imag
+    @imag
+  end
+
+  def +(other)
+    Complex.new(real + other.real, imag + other.imag)
+  end
+
+  def *(other)
+    Complex.new(real * other.real - imag * other.imag, real * other.imag + imag * other.real)
+  end
+
+  def abs
+    Math.sqrt(real * real + imag * imag)
+  end
+end
