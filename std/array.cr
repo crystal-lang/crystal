f0538bc909dfd46e5ab2826f0dbd7def00516893
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-11-27 22:00:04 -0300

    Array#sort!

--- std/array.cr
+++ std/array.cr
@@ -30,4 +30,40 @@ class Array
     str += "]"
     str
   end
+
+  def sort!
+    quicksort 0, length - 1
+    self
+  end
+
+  # private
+
+  def swap(i, j)
+    temp = self[i]
+    self[i] = self[j]
+    self[j] = temp
+  end
+
+  def partition(left, right, pivot_index)
+    pivot_value = self[pivot_index]
+    swap pivot_index, right
+    store_index = left
+    (left...right).each do |i|
+      if self[i] < pivot_value
+        swap i, store_index
+        store_index += 1
+      end
+    end
+    swap store_index, right
+    store_index
+  end
+
+  def quicksort(left, right)
+    if left < right
+      pivot_index = (left + right) / 2
+      pivot_new_index = partition left, right, pivot_index
+      quicksort left, pivot_new_index - 1
+      quicksort pivot_new_index + 1, right
+    end
+  end
 end
