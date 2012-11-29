805eff84474533acfbc6ae99d31c9cf4b864a4f8
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-11-28 23:37:42 -0300

    Fixed global specs. Added another one

--- spec/codegen/global_spec.rb
+++ spec/codegen/global_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe 'Code gen: global' do
   it "codegens global" do
-    run("$foo = 1; def foo; $foo = 2.5; end; foo; $foo").to_f.should eq(2.5)
+    run("$foo = 1; def foo; $foo = 2; end; foo; $foo").to_i.should eq(2)
+  end
+
+  it "codegens global with union" do
+    run("$foo = 1; def foo; $foo = 2.5; end; foo; $foo.to_f").to_f.should eq(2.5)
   end
 end
