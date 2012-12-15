64aa1072abb01cc7e00df1318844475810a5531c
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-12-15 15:22:47 -0300

    Regexp allocates enough space for all captures

--- std/regexp.cr
+++ std/regexp.cr
@@ -2,6 +2,9 @@ lib PCRE("pcre")
   fun pcre_compile(pattern : Char*, options : Int, errptr : Char**, erroffset : Int*, tableptr : Long) : Long
   fun pcre_exec(code : Long, extra : Long, subject : Char*, length : Int, offset : Int, options : Int,
                 ovector : Int*, ovecsize : Int) : Int
+  fun pcre_fullinfo(code : Long, extra : Long, what : Int, where : Void*) : Int
+
+  INFO_CAPTURECOUNT = 2
 end
 
 class Regexp
@@ -16,11 +19,14 @@ class Regexp
       puts "#{errptr.as(String)} at #{erroffset}"
       exit 1
     end
+    @captures = 0
+    PCRE.pcre_fullinfo(@re, 0L, PCRE::INFO_CAPTURECOUNT, @captures.ptr.as(Void))
   end
 
   def match(str, pos = 0, options = 0)
-    ovector = Pointer.malloc(3 * 4).as(Int)
-    ret = PCRE.pcre_exec(@re, 0L, str.cstr, str.length, pos, options, ovector, 3)
+    ovector_size = (@captures + 1) * 3
+    ovector = Pointer.malloc(ovector_size * 4).as(Int)
+    ret = PCRE.pcre_exec(@re, 0L, str.cstr, str.length, pos, options, ovector, ovector_size)
     return nil unless ret > 0
     MatchData.new(self, str, pos, ovector)
   end
