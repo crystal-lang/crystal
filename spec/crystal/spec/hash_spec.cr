c917aa5e6b0923a4b187ac6d112ca0ad777612c6
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-12-28 21:05:56 -0300

    Some hash and string specs in crystal

---
+++ spec/crystal/spec/hash_spec.cr
@@ -0,0 +1,23 @@
+describe "Hash" do
+  describe "empty" do
+    it "length should be zero" do
+      {}.length.should eq(0)
+    end
+  end
+
+  it "sets and gets" do
+    a = {}
+    a[1] = 2
+    a[1].should eq(2)
+  end
+
+  it "gets from literal" do
+    a = {1 => 2}
+    a[1].should eq(2)
+  end
+
+  it "gets from union" do
+    a = {1 => 2, :foo => 1.1}
+    a[1].should eq(2)
+  end
+end
---
+++ spec/crystal/spec/string_spec.cr
@@ -0,0 +1,18 @@
+describe "String" do
+  it "compares strings: different length" do
+    "foo".should_not eq("fo")
+  end
+
+  it "compares strings: same object" do
+    f = "foo"
+    f.should eq(f)
+  end
+
+  it "compares strings: same length, same string" do
+    "foo".should eq("fo" + "o")
+  end
+
+  it "compares strings: same length, different string" do
+    "foo".should_not eq("bar")
+  end
+end
