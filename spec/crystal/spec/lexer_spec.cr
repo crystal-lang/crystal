0c4e7cc6904bdff7b77e7326586bff5bcdf94ba0
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-12-29 20:18:37 -0300

    Finished basic lexer spec in crystal

--- spec/crystal/spec/lexer_spec.cr
+++ spec/crystal/spec/lexer_spec.cr
@@ -1,27 +1,32 @@
+#!/usr/bin/env bin/crystal -run
 require "spec"
 require "../../../bootstrap/crystal/lexer"
 
-def it_lexes(string, token_type)
+def it_lexes(string, type)
   it "lexes #{string}" do
     lexer = Crystal::Lexer.new string
     token = lexer.next_token
-    token.type.should eq(token_type)
+    token.type.should eq(type)
   end
 end
 
-def it_lexes(string, token_type, token_value)
+def it_lexes(string, type, value)
   it "lexes #{string}" do
     lexer = Crystal::Lexer.new string
     token = lexer.next_token
-    token.type.should eq(token_type)
-    token.value.should eq(token_value)
+    token.type.should eq(type)
+    token.value.should eq(value)
+  end
+end
+
+def it_lexes_many(values, type)
+  values.each do |value|
+    it_lexes value, type, value
   end
 end
 
 def it_lexes_keywords(keywords)
-  keywords.each do |keyword|
-    it_lexes keyword, :IDENT, keyword
-  end
+  it_lexes_many keywords, :IDENT
 end
 
 def it_lexes_idents(keywords)
@@ -74,8 +79,36 @@ def it_lexes_char(string, value)
 end
 
 def it_lexes_operators(ops)
-  ops.each do |op|
-    it_lexes op, :TOKEN, op
+  it_lexes_many ops, :TOKEN
+end
+
+def it_lexes_const(value)
+  it_lexes value, :CONST, value
+end
+
+def it_lexes_instance_var(value)
+  it_lexes value, :INSTANCE_VAR, value
+end
+
+def it_lexes_globals(globals)
+  it_lexes_many globals, :GLOBAL
+end
+
+def it_lexes_symbols(symbols)
+  symbols.each do |symbol|
+    value = symbol[1, symbol.length - 1]
+    value = value[1, value.length - 2] if value.starts_with?("\"")
+    it_lexes symbol, :SYMBOL, value
+  end
+end
+
+def it_lexes_regex(regex)
+  it_lexes regex, :REGEXP, regex[1, regex.length - 2]
+end
+
+def it_lexes_global_match(globals)
+  globals.each do |global|
+    it_lexes global, :GLOBAL_MATCH, global[1, global.length - 1].to_i
   end
 end
 
@@ -83,7 +116,7 @@ describe "Lexer" do
   it_lexes " ", :SPACE
   it_lexes "\n", :NEWLINE
   it_lexes "\n\n\n", :NEWLINE
-  it_lexes_keywords ["def", "if", "else", "elsif", "end", "true", "false", "class", "module", "include", "while", "nil", "do", "yield", "return", "unless", "next", "break", "begin", "lib", "fun", "type", "struct", "macro", "ptr", "out", "require", "case", "when"]
+  it_lexes_keywords ["def", "if", "else", "elsif", "end", "true", "false", "class", "module", "include", "while", "nil", "do", "yield", "return", "unless", "next", "break", "begin", "lib", "fun", "type", "struct", "macro", "ptr", "out", "require", "case", "when", "generic"]
   it_lexes_idents ["ident", "something", "with_underscores", "with_1", "foo?", "bar!"]
   it_lexes_idents ["def?", "if?", "else?", "elsif?", "end?", "true?", "false?", "class?", "while?", "nil?", "do?", "yield?", "return?", "unless?", "next?", "break?", "begin?"]
   it_lexes_idents ["def!", "if!", "else!", "elsif!", "end!", "true!", "false!", "class!", "while!", "nil!", "do!", "yield!", "return!", "unless!", "next!", "break!", "begin!"]
@@ -95,4 +128,50 @@ describe "Lexer" do
   it_lexes_char "'\\t'", '\t'
   it_lexes_char "'\\0'", '\0'
   it_lexes_operators ["=", "<", "<=", ">", ">=", "+", "-", "*", "/", "(", ")", "==", "!=", "=~", "!", ",", ".", "..", "...", "!@", "+@", "-@", "&&", "||", "|", "{", "}", "?", ":", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "**=", "<<", ">>", "%", "&", "|", "^", "**", "<<=", ">>=", "~", "~@", "[]", "[", "]", "::", "<=>", "=>", "||=", "&&=", "==="]
+  it_lexes_const "Foo"
+  it_lexes_instance_var "@foo"
+  it_lexes_globals ["$foo", "$FOO", "$_foo", "$foo123", "$~"]
+  it_lexes_symbols [":foo", ":foo!", ":foo?", ":\"foo\""]
+  it_lexes_regex "/foo/"
+  it_lexes_global_match ["$1", "$10"]
+
+  it "lexes not instance var" do
+    lexer = Crystal::Lexer.new "!@foo"
+    token = lexer.next_token
+    token.type.should eq(:TOKEN)
+    token.value.should eq("!")
+    token = lexer.next_token
+    token.type.should eq(:INSTANCE_VAR)
+    token.value.should eq("@foo")
+  end
+
+  it "lexes comment and token" do
+    lexer = Crystal::Lexer.new "# comment\n1"
+    token = lexer.next_token
+    token.type.should eq(:NEWLINE)
+    token = lexer.next_token
+    token.type.should eq(:INT)
+    token.value.should eq("1")
+  end
+
+  it "lexes comment at the end" do
+    lexer = Crystal::Lexer.new "# comment"
+    token = lexer.next_token
+    token.type.should eq(:EOF)
+  end
+
+  it "lexes __LINE__" do
+    lexer = Crystal::Lexer.new "__LINE__"
+    token = lexer.next_token
+    token.type.should eq(:INT)
+    token.value.should eq(1)
+  end
+
+  it "lexes __FILE__" do
+    lexer = Crystal::Lexer.new "__FILE__"
+    lexer.filename = "foo"
+    token = lexer.next_token
+    token.type.should eq(:STRING)
+    token.value.should eq("foo")
+  end
 end
