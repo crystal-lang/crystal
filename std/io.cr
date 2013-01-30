194672e79a21086679428a51e403d724307d964c
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-01-30 00:51:47 -0300

    Make puts and print return nil

--- std/io.cr
+++ std/io.cr
@@ -9,54 +9,67 @@ end
 
 def print(obj : Char)
   C.putchar obj
+  nil
 end
 
 def print(obj : Int)
   C.printf "%d", obj
+  nil
 end
 
 def print(obj : Long)
   C.printf "%ld", obj
+  nil
 end
 
 def print(obj : Float)
   C.printf "%f", obj
+  nil
 end
 
 def print(obj : Double)
   C.printf "%g", obj
+  nil
 end
 
 def print(obj)
   C.printf obj.to_s
+  nil
 end
 
 def puts(obj : Char)
   C.printf "%c\n", obj
+  nil
 end
 
 def puts(obj : Int)
   C.printf "%d\n", obj
+  nil
 end
 
 def puts(obj : Long)
   C.printf "%ld\n", obj
+  nil
 end
 
 def puts(obj : Float)
   C.printf "%f\n", obj
+  nil
 end
 
 def puts(obj : Double)
   C.printf "%g\n", obj
+  nil
 end
 
 def puts(obj = "")
   C.puts obj.to_s
+  nil
 end
 
 def p(obj)
   puts obj.inspect
+  obj
 end
 
 def system(command)
