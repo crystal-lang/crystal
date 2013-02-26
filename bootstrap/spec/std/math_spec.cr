74cdd05c4c5b39801db18c619ee3871ebb87cdac
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-02-26 14:40:51 -0300

    Math#min and Math#max

--- bootstrap/spec/std/math_spec.cr
+++ bootstrap/spec/std/math_spec.cr
@@ -7,4 +7,14 @@ describe "Math" do
     assert { Math.sqrt(81.0).should eq(9.0) }
     assert { Math.sqrt(81.0f).should eq(9.0) }
   end
+
+  describe "min" do
+    assert { Math.min(1, 2).should eq(1) }
+    assert { Math.min(2, 1).should eq(1) }
+  end
+
+  describe "max" do
+    assert { Math.max(1, 2).should eq(2) }
+    assert { Math.max(2, 1).should eq(2) }
+  end
 end
--- std/math.cr
+++ std/math.cr
@@ -4,4 +4,12 @@ module Math
   def self.sqrt(value : Int)
     sqrt value.to_d
   end
+
+  def self.min(value1, value2)
+    value1 <= value2 ? value1 : value2
+  end
+
+  def self.max(value1, value2)
+    value1 >= value2 ? value1 : value2
+  end
 end
