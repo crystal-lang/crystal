dec9bf064c5b7de5801c6ab9d0e370309721845b
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-06-05 07:58:58 -0300

    Error when using return at the top level

--- spec/type_inference/return_spec.rb
+++ spec/type_inference/return_spec.rb
@@ -8,4 +8,9 @@ describe 'Type inference: return' do
   it "infers return type with many returns" do
     assert_type("def foo; if true; return 1; end; 2.5 end; foo") { union_of(int, double) }
   end
+
+  it "errors on return in top level" do
+    assert_error "return",
+      "can't return from top level"
+  end
 end
