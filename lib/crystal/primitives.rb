d9b11970d07428c3e6d9fb4e444ec0a70dfa964b
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-06-22 11:11:28 -0300

    Added ">>" method for integers

--- lib/crystal/primitives.rb
+++ lib/crystal/primitives.rb
@@ -88,8 +88,8 @@ module Crystal
       singleton(math, 'sqrt', {'other' => float64}, float64) { |b, f, llvm_mod| b.call(sqrt(llvm_mod), f.params[1]) }
     end
 
-    INT_CALC_OP_MAP = { :+ => :add, :- => :sub, :* => :mul, :/ => :sdiv, :% => :srem, :<< => :shl, :| => :or, :& => :and, :"^" => :xor }
-    UINT_CALC_OP_MAP = { :+ => :add, :- => :sub, :* => :mul, :/ => :udiv, :% => :urem, :<< => :shl, :| => :or, :& => :and, :"^" => :xor }
+    INT_CALC_OP_MAP = { :+ => :add, :- => :sub, :* => :mul, :/ => :sdiv, :% => :srem, :<< => :shl, :>> => :ashr, :| => :or, :& => :and, :"^" => :xor }
+    UINT_CALC_OP_MAP = { :+ => :add, :- => :sub, :* => :mul, :/ => :udiv, :% => :urem, :<< => :shl, :>> => :lshr, :| => :or, :& => :and, :"^" => :xor }
     FLOAT_CALC_OP_MAP = { :+ => :fadd, :- => :fsub, :* => :fmul, :/ => :fdiv }
 
     INT_CMP_OP_FUN = :icmp
@@ -228,7 +228,7 @@ module Crystal
       end
 
       [uint8, uint16, uint32, uint64, int8, int16, int32, int64].repeated_permutation(2) do |type1, type2|
-        [:%, :<<, :|, :&, :"^"].each do |op|
+        [:%, :<<, :>>, :|, :&, :"^"].each do |op|
           ret_type = greatest_type(type1, type2)
           singleton(type1, op, {'other' => type2}, type1) do |b, f|
             arg1 = adjust_calc_type(b, ret_type, type1, f.params[0])
