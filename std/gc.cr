2356ec5f1e2db5a0e6edb0d35295aaaccf200e62
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-14 10:58:40 -0300

    Use class vars for GC

--- std/gc.cr
+++ std/gc.cr
@@ -3,25 +3,35 @@ lib C
   fun realloc(pointer : Void*, size : Int32) : Void*
 end
 
-$gc_table_size = 1024
-$gc_table_entry = 0
-$gc_table = C.malloc($gc_table_size * 8).as(Pointer(Pointer(Void)))
+class GC
+  @@table_size = 1024
+  @@table_entry = 0
+  @@table = C.malloc(@@table_size * 8).as(Pointer(Pointer(Void)))
 
-fun __crystal_malloc(size : Int32) : Void*
-  pointer = C.malloc(size + 1)
+  def self.malloc(size : Int32)
+    pointer = C.malloc(size + 1)
 
-  $gc_table[$gc_table_entry] = pointer
-  $gc_table_entry += 1
+    @@table[@@table_entry] = pointer
+    @@table_entry += 1
 
-  if $gc_table_entry == $gc_table_size
-    $gc_table_size += 1
-    $gc_table = C.realloc($gc_table.as(Void), $gc_table_size * 8).as(Pointer(Pointer(Void)))
+    if @@table_entry == @@table_size
+      @@table_size *= 2
+      @@table = C.realloc(@@table.as(Void), @@table_size * 8).as(Pointer(Pointer(Void)))
+    end
+
+    pointer + 1
   end
 
-  pointer + 1
+  def self.realloc(pointer : Pointer(Void), size : Int32)
+    pointer = C.realloc(pointer - 1, size + 1)
+    pointer + 1
+  end
+end
+
+fun __crystal_malloc(size : Int32) : Void*
+  GC.malloc size
 end
 
 fun __crystal_realloc(pointer : Void*, size : Int32) : Void*
-  pointer = C.realloc(pointer - 1, size + 1)
-  pointer + 1
+  GC.realloc pointer, size
 end
