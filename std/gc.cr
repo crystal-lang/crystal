d86630c1d2a24ae6325d6564efcb508bec3e05fb
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-13 21:55:11 -0300

    Fixed malloc size for $gc_table

--- std/gc.cr
+++ std/gc.cr
@@ -3,9 +3,9 @@ lib C
   fun realloc(pointer : Void*, size : Int32) : Void*
 end
 
-$gc_table_size = 1
+$gc_table_size = 1024
 $gc_table_entry = 0
-$gc_table = C.malloc($gc_table_size).as(Pointer(Pointer(Void)))
+$gc_table = C.malloc($gc_table_size * 8).as(Pointer(Pointer(Void)))
 
 fun __crystal_malloc(size : Int32) : Void*
   pointer = C.malloc(size + 1)
@@ -15,7 +15,7 @@ fun __crystal_malloc(size : Int32) : Void*
 
   if $gc_table_entry == $gc_table_size
     $gc_table_size += 1
-    $gc_table = C.realloc($gc_table.as(Void), $gc_table_size).as(Pointer(Pointer(Void)))
+    $gc_table = C.realloc($gc_table.as(Void), $gc_table_size * 8).as(Pointer(Pointer(Void)))
   end
 
   pointer + 1
