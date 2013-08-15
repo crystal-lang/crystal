e112e83367ffe68e7cb0d0fdec4b3457fe33e29b
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-15 20:22:19 -0300

    Initial exception backtrace implementation

--- std/exception.cr
+++ std/exception.cr
@@ -1,10 +1,21 @@
+lib C
+  fun backtrace(array : Void**, size : Int32) : Int32
+  fun backtrace_symbols(array : Void**, size : Int32) : Char**
+end
+
 class Exception
   getter :message
   getter :inner_exception
+  getter :backtrace
 
   def initialize(message = nil, inner_exception = nil)
     @message = message
     @inner_exception = inner_exception
+
+    callstack = Pointer(Pointer(Void)).malloc(128)
+    frames = C.backtrace(callstack, 128)
+    strs = C.backtrace_symbols(callstack, frames)
+    @backtrace = strs.map(frames) { |c_str| String.from_cstr(c_str) }
   end
 
   def to_s
