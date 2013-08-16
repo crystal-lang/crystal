fcc3ab94d013cb834c5611378593cd0b79fe73b5
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-16 07:13:45 -0300

    Put all Int classes and Float classes into a single file respectively

--- std/float.cr
+++ std/float.cr
@@ -1,6 +1,3 @@
-require "float32"
-require "float64"
-
 class Float
   def +@
     self
@@ -16,5 +13,50 @@ class Float
 
   def round
     (self + 0.5).to_i32
+  end
+end
+
+class Float32
+  MIN = -INFINITY
+  MAX =  INFINITY
+
+  def ==(other)
+    false
+  end
+
+  def -@
+    0.0_f32 - self
+  end
+
+  def **(other)
+    self ** other.to_f32
+  end
+
+  def to_s
+    to_f64.to_s
+  end
+end
+
+class Float64
+  MIN = -INFINITY
+  MAX =  INFINITY
+
+  def ==(other)
+    false
+  end
+
+  def -@
+    0.0 - self
+  end
+
+  def **(other)
+    self ** other.to_f64
+  end
+
+  def to_s
+    str = String.new_with_capacity(12) do |buffer|
+      C.sprintf(buffer, "%g", self)
+    end
+    str
   end
 end
