16699d59d75403fbce4abfaf32dbcc6cc27a21f5
Brian J. Cardiff <bcardiff@manas.com.ar>
2013-08-17 21:13:11 -0300

    initial classes for tcl_tk

--- samples/tk_gui.cr
+++ samples/tk_gui.cr
@@ -3,6 +3,14 @@ require "tcl_tk"
 LibTcl.get_version out major, out minor, out patch_level, out type
 puts "Version: #{major}.#{minor} (patch level: #{patch_level})"
 
-interp = LibTcl.create_interp
-LibTk.init interp
-LibTk.main_loop
+win = Tk::Window.new
+# win.interpreter.eval "PopupMessage \"Hello, World!\""
+# win.interpreter.eval "label .l -text \"Hello, World\""
+# win.interpreter.eval "pack .l"
+
+label = Tk::Label.new(win, "1")
+label.text = "Hello, World"
+label.pack
+
+win.main_loop
+C.getchar
--- std/tcl_tk.cr
+++ std/tcl_tk.cr
@@ -7,9 +7,67 @@ lib LibTcl("tcl")
 
   fun get_version = Tcl_GetVersion(major : Int32*, minor : Int32*, patch_level : Int32*, type : Void*)
   fun create_interp = Tcl_CreateInterp() : Interp*
+  fun eval = Tcl_Eval(interp : Interp*, code : Char*) : Int32
 end
 
 lib LibTk("tk")
   fun init = Tk_Init(interp : LibTcl::Interp*) : Int32
   fun main_loop = Tk_MainLoop()
+# EXTERN Tk_Window  Tk_MainWindow(Tcl_Interp *interp);
+# EXTERN void   Tk_MoveWindow(Tk_Window tkwin, int x, int y);
+# EXTERN Tcl_Interp * Tk_Interp(Tk_Window tkwin);
+
+end
+
+module Tcl
+  class Interpreter
+    getter :tcl_interp
+
+    def initialize
+      @tcl_interp = LibTcl.create_interp
+    end
+
+    def eval(command)
+      LibTcl.eval(@tcl_interp, command.cstr)
+    end
+  end
+end
+
+# c = 0
+
+module Tk
+  class Label
+    def initialize(master, name)
+      # c = c + 1
+      @name = ".l#{name}"
+      @master = master
+      interpreter.eval "label #{@name}"
+    end
+
+    def interpreter
+      @master.interpreter
+    end
+
+    def text=(value)
+      interpreter.eval "#{@name} configure -text \"#{value}\""
+    end
+
+    def pack
+      interpreter.eval "pack #{@name}"
+    end
+  end
+
+  class Window
+    getter :interpreter
+    getter :tk
+
+    def initialize
+      @interpreter = Tcl::Interpreter.new
+      @tk = LibTk.init @interpreter.tcl_interp
+    end
+
+    def main_loop
+      LibTk.main_loop
+    end
+  end
 end
