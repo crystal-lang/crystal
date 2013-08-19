820f5243d23f5a2609031938306f0715b1c68129
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-19 13:17:00 -0300

    Missing file

---
+++ bootstrap/crystal/exception.cr
@@ -0,0 +1,45 @@
+module Crystal
+  class Exception < ::Exception
+  end
+
+  class SyntaxException < Exception
+    def initialize(message, line_number, column_number, filename)
+      super(message)
+      @line_number = line_number
+      @column_number = column_number
+      @filename = filename
+    end
+
+    def to_s
+      str = StringBuilder.new
+      if @filename
+        str << "Syntax error in #{@filename}:#{@line_number}: #{@message}"
+      else
+        str << "Syntax error in line #{@line_number}: #{@message}"
+      end
+
+      if @filename && File.exists?(@filename)
+        source = File.read(@filename)
+      end
+
+      if source
+        lines = source.lines
+        if @line_number - 1 < lines.length
+          line = lines[@line_number - 1]
+          if line
+            str << "\n\n"
+            str << line.chomp
+            str << "\n"
+            (@column_number - 1).times do
+              str << " "
+            end
+            str << "^"
+            str << "\n"
+          end
+        end
+      end
+
+      str.to_s
+    end
+  end
+end
