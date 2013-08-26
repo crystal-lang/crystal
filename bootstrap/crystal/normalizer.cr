b0273bf18760fafd917769fda3cc86b99807880c
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-08-26 09:32:47 -0300

    Bootstrap: normalize Or and RangeLiteral

--- bootstrap/crystal/normalizer.cr
+++ bootstrap/crystal/normalizer.cr
@@ -34,6 +34,24 @@ module Crystal
       new_node.transform(self)
     end
 
+    def transform(node : Or)
+      left = node.left
+      new_node = if left.is_a?(Var)
+                   If.new(left, left.clone, node.right)
+                 else
+                   temp_var = new_temp_var
+                   If.new(Assign.new(temp_var, left), temp_var, node.right)
+                 end
+      new_node.binary = :or
+      new_node.transform(self)
+    end
+
+    def transform(node : RangeLiteral)
+      super
+
+      Call.new(Ident.new(["Range"], true), "new", [node.from, node.to, BoolLiteral.new(node.exclusive)])
+    end
+
     def new_temp_var
       program.new_temp_var
     end
