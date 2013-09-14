6a217ff134afafa88f0a57bff22c06916569d148
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-09-14 00:34:02 -0300

    sleep

--- std/c.cr
+++ std/c.cr
@@ -4,8 +4,13 @@ lib C
   fun time(t : Int64) : Int64
   fun fork : Int32
   fun exit(status : Int32) : NoReturn
+  fun sleep(seconds : UInt32) : UInt32
 end
 
 def exit(status = 0)
   C.exit status
+end
+
+def sleep(seconds)
+  C.sleep seconds.to_u32
 end
