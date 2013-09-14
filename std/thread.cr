1f7b08b60da981b95af4dd564e0b7b30d2e6a81c
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-09-14 01:07:54 -0300

    Added simple implementation of threads

---
+++ std/thread.cr
@@ -0,0 +1,34 @@
+lib PThread
+  type Thread : Void*
+  fun create = pthread_create(thread : Thread*, attr : Void*, start : Void* ->, arg : Void*)
+  fun exit = pthread_exit(value : Void*) : Void
+  fun join = pthread_join(thread : Thread, value : Void**) : Int32
+end
+
+class Thread(T, R)
+  def self.new(arg : T, &block : T -> R)
+    Thread(T, R).new(arg, block)
+  end
+
+  def self.new(&block : Nil -> R)
+    Thread(Nil, R).new(nil, block)
+  end
+
+  def initialize(arg : T, func)
+    @func = func
+    @arg = arg
+    PThread.create(out @th, nil, ->self.start(Pointer(Void)), nil)
+  end
+
+  def start(x)
+    ret = Pointer(R).malloc(1)
+    ret.value = @func.call(@arg)
+    PThread.exit(ret.as(Void))
+    nil
+  end
+
+  def join
+    PThread.join(@th, out ret)
+    ret.as(R).value
+  end
+end
