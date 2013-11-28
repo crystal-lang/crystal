47bf09edfe8451a42c749cc27a6910d22ce2c1e7
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-11-27 21:58:23 -0300

    Pthread mutex

--- std/thread.cr
+++ std/thread.cr
@@ -1,8 +1,21 @@
 lib PThread
   type Thread : Void*
+  struct Mutex
+    a : Int64; b : Int64; c : Int64; d : Int64; e : Int64; f : Int64; g : Int64; h : Int64
+  end
+
+  struct MutexAttr
+  end
+
   fun create = pthread_create(thread : Thread*, attr : Void*, start : Void* ->, arg : Void*)
   fun exit = pthread_exit(value : Void*)
   fun join = pthread_join(thread : Thread, value : Void**) : Int32
+
+  fun mutex_init = pthread_mutex_init(mutex : Mutex*, mutex_attr : MutexAttr*) : Int32
+  fun mutex_lock = pthread_mutex_lock(mutex : Mutex*) : Int32
+  fun mutex_trylock = pthread_mutex_trylock(mutex : Mutex*) : Int32
+  fun mutex_unlock = pthread_mutex_unlock(mutex : Mutex*) : Int32
+  fun mutex_destroy = pthread_mutex_destroy(mutex : Mutex*) : Int32
 end
 
 class Thread(T, R)
@@ -29,5 +42,34 @@ class Thread(T, R)
   def join
     PThread.join(@th, out ret)
     ret.as(R).value
+  end
+end
+
+class Mutex
+  def initialize
+    PThread.mutex_init(out @mutex, nil)
+  end
+
+  def lock
+    PThread.mutex_lock(@mutex.ptr)
+  end
+
+  def try_lock
+    PThread.mutex_trylock(@mutex.ptr)
+  end
+
+  def unlock
+    PThread.mutex_unlock(@mutex.ptr)
+  end
+
+  def synchronize
+    lock
+    yield
+  ensure
+    unlock
+  end
+
+  def destroy
+    PThread.mutex_destroy(@mutex.ptr)
   end
 end
