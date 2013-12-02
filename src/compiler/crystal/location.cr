0e0a3b8a45c13e75fa0dd7dce48aa28b6d347f57
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-12-02 19:37:08 -0300

    Removed Location#clone (self is ok because it’s immutable)

--- src/compiler/crystal/location.cr
+++ src/compiler/crystal/location.cr
@@ -7,10 +7,6 @@ module Crystal
     def initialize(@line_number, @column_number, @filename)
     end
 
-    def clone
-      Location.new(@line_number, @column_number, @filename)
-    end
-
     def to_s
       "#{filename}:#{line_number}:#{column_number}"
     end
