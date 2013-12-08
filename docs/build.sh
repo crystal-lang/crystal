88f3aee0e1dfbe76f93d0902ce68c9064a8491bb
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-12-08 17:50:50 -0300

    Started writing some docs using AsciiDoc

---
+++ docs/build.sh
@@ -0,0 +1,1 @@
+asciidoc -b html5 -a icons -a toc2 -a iconsdir=`brew --prefix asciidoc`/etc/asciidoc/images/icons -a data-uri -a theme=flask -a source-highlighter=pygments -o ../docs.html index.asciidoc
