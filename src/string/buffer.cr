3f6f2022d414677878e36e0ae3bec0ed24fa024d
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-12-10 10:17:54 -0300

    Faster String::Builder

--- src/string/buffer.cr
+++ src/string/buffer.cr
@@ -1,28 +1,56 @@
 class String::Buffer
   def initialize(capacity)
-    @array = Array(Char).new(capacity)
+    @buffer = Pointer(Char).malloc(capacity)
+    @length = 0
+    @capacity = capacity
   end
 
   def <<(c : Char)
-    @array << c
+    check_needs_resize
+    @buffer[@length] = c
+    @length += 1
+    self
+  end
+
+  def <<(obj : String)
+    obj_length = obj.length
+    new_length = length + obj_length
+    if new_length > @capacity
+      cap2 = Math.log2(new_length).ceil
+      new_capacity = 2 ** cap2
+      resize_to_capacity(new_capacity)
+    end
+
+    (@buffer + @length).memcpy(obj.cstr, obj_length)
+    @length += obj_length
+
+    self
   end
 
   def <<(obj)
-    obj.to_s.each_char do |char|
-      @array << char
-    end
+    self << obj.to_s
   end
 
   def clear
-    @array.clear
+    @length = 0
   end
 
   def length
-    @array.length
+    @length
   end
 
   def to_s
-    String.new @array.buffer, length
+    String.new @buffer, length
+  end
+
+  # private
+
+  def check_needs_resize
+    resize_to_capacity(@capacity * 2) if @length == @capacity
+  end
+
+  def resize_to_capacity(capacity)
+    @capacity = capacity
+    @buffer = @buffer.realloc(@capacity)
   end
 end
-
