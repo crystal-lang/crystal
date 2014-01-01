2051ea3871bf1fe6cf549c803265809d590c4999
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-01-01 13:54:43 -0300

    at_exit

--- src/main.cr
+++ src/main.cr
@@ -2,6 +2,13 @@ lib CrystalMain
   fun __crystal_main(argc : Int32, argv : Char**)
 end
 
+$at_exit_handlers = nil
+
+def at_exit(handler)
+  handlers = $at_exit_handlers ||= [] of (-> Nil)
+  handlers << handler
+end
+
 fun main(argc : Int32, argv : Char**) : Int32
   GC.init
   CrystalMain.__crystal_main(argc, argv)
@@ -9,5 +16,7 @@ fun main(argc : Int32, argv : Char**) : Int32
 rescue ex
   puts ex
   1
+ensure
+  $at_exit_handlers.try &.each &.call
 end
 
