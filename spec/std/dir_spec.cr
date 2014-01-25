355bd84d3cf970dbbee205790e66fc3fec7f5f63
prep <maurice@codeninja.nl>
2014-01-25 22:32:22 +0100

    Add a ModeT, Dir#mkdir and Dir#rmdir

--- spec/std/dir_spec.cr
+++ spec/std/dir_spec.cr
@@ -13,4 +13,36 @@ describe "Dir" do
   it "tests exists? on nonexistent directory" do
     Dir.exists?(File.join([__DIR__, "/foo/bar/"])).should be_false
   end
+
+  it "tests mkdir and rmdir with a new path" do
+    path = "/tmp/crystal_mkdir_test_#{Process.pid}/"
+    Dir.mkdir(path, 0700).should eq(0)
+    Dir.exists?(path).should be_true
+    Dir.rmdir(path).should eq(0)
+  end
+
+  it "tests mkdir with an existing path" do
+    begin
+      Dir.mkdir(File.join([__DIR__, "../"]), 0700)
+      fail "Expected Errno to be raised"
+    rescue Errno
+    end
+  end
+
+  it "tests rmdir with an nonexistent path" do
+    path = "/tmp/crystal_mkdir_test_#{Process.pid}/"
+    begin
+      Dir.rmdir(path)
+      fail "Expected Errno to be raised"
+    rescue Errno
+    end
+  end
+
+  it "tests rmdir with a path that cannot be removed" do
+    begin
+      Dir.rmdir(File.join([__DIR__, "../"]))
+      fail "Expected Errno to be raised"
+    rescue Errno
+    end
+  end
 end
