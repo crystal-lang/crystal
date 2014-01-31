2944399fb1503e47d6ebe2be976d44ee5ab1f680
Craig Buchek <craig@boochtek.com>
2014-01-31 15:37:09 -0600

    Implement Date#to_s.

--- spec/std/date_spec.cr
+++ spec/std/date_spec.cr
@@ -6,4 +6,9 @@ describe "Date" do
   it "initializes from year, month, day" do
     date = Date.new(2014, 1, 31)
   end
+
+  it "prints in yyyy-mm-dd format" do
+    date = Date.new(2014, 1, 31)
+    date.to_s.should eq("2014-01-31")
+  end
 end
--- src/date.cr
+++ src/date.cr
@@ -1,6 +1,10 @@
 class Date
   def initialize(@year, @month, @day, @calendar = Date::Calendar::DEFAULT)
   end
+
+  def to_s
+    "%04d-%02d-%02d" % [@year, @month, @day]
+  end
 end
 
 
