00fd76b18c21f15508dd30977aabe7b95a844e60
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-02-18 23:42:49 -0300

    Added levenshtein algorithm

---
+++ spec/std/levenshtein_spec.cr
@@ -0,0 +1,17 @@
+#!/usr/bin/env bin/crystal --run
+require "spec"
+require "levenshtein"
+
+describe "levenshtein" do
+  assert { levenshtein("algorithm", "altruistic").should eq(6) }
+  assert { levenshtein("1638452297", "444488444").should eq(9) }
+  assert { levenshtein("", "").should eq(0) }
+  assert { levenshtein("", "a").should eq(1) }
+  assert { levenshtein("aaapppp", "").should eq(7) }
+  assert { levenshtein("frog", "fog").should eq(1) }
+  assert { levenshtein("fly", "ant").should eq(3) }
+  assert { levenshtein("elephant", "hippo").should eq(7) }
+  assert { levenshtein("hippo", "elephant").should eq(7) }
+  assert { levenshtein("hippo", "zzzzzzzz").should eq(8) }
+  assert { levenshtein("hello", "hallo").should eq(1) }
+end
---
+++ src/levenshtein.cr
@@ -0,0 +1,35 @@
+def levenshtein(s, t)
+  return 0 if s == t
+
+  s_len = s.length
+  t_len = t.length
+
+  return t_len if s_len == 0
+  return s_len if t_len == 0
+
+  # This is to allocate less memory
+  if t_len > s_len
+    t, s = s, t
+    t_len, s_len = s_len, t_len
+  end
+
+  s = s.cstr
+  t = t.cstr
+
+  v0 = Pointer(Int32).malloc(t_len + 1) { |i| i }
+  v1 = Pointer(Int32).malloc(t_len + 1)
+
+  s_len.times do |i|
+    v1[0] = i + 1
+
+    0.upto(t_len - 1) do |j|
+      cost = s[i] == t[j] ? 0 : 1
+      v1[j + 1] = Math.min(Math.min(v1[j] + 1, v0[j + 1] + 1), v0[j] + cost)
+    end
+
+    v0.memcpy(v1, t_len + 1)
+  end
+
+  v1[t_len]
+end
+
