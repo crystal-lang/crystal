5a59b6a71fc3d3f66c3000a169b3fc1ad434aeba
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-03-20 09:07:44 -0300

    Added missing FileUtils specs

---
+++ spec/std/file_utils_spec.cr
@@ -0,0 +1,14 @@
+#!/usr/bin/env bin/crystal --run
+require "spec"
+
+describe "FileUtils" do
+  describe "cmp" do
+    it "compares two equal files" do
+      FileUtils.cmp("#{__DIR__}/data/test_file.txt", "#{__DIR__}/data/test_file.txt").should be_true
+    end
+
+    it "compares two different files" do
+      FileUtils.cmp("#{__DIR__}/data/test_file.txt", "#{__DIR__}/data/test_file.ini").should be_false
+    end
+  end
+end
