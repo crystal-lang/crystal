02b320b54d7d16a766d1264f5dede22852fb731c
Konstantin Makarchev <kostya27@gmail.com>
2014-05-12 21:10:26 +0400

    use tag and patchlevel

--- src/compiler/crystal/version.cr
+++ src/compiler/crystal/version.cr
@@ -1,14 +1,17 @@
 module Crystal
-  VERSION = "0.1"
+  def self.version_tag; "unknown"; end
   def self.version_sha; "."; end
+  def self.version_patch; "0"; end
 
   def self.dump_version
-    rev = system2("git rev-parse --short HEAD 2>/dev/null")
-    sha = rev[0]? || "-"
+    describe = system2("git describe --tags 2>/dev/null")[0]? || "."
+    tag, patch, sha = describe.split("-")
     
     File.open(__DIR__ + "/version_generated.cr", "w") do |f|
       f.puts "module Crystal"
+      f.puts "  def self.version_tag; \"#{tag}\"; end"
       f.puts "  def self.version_sha; \"#{sha}\"; end"
+      f.puts "  def self.version_patch; \"#{patch}\"; end"
       f.puts "end"
     end
   end
@@ -16,6 +19,6 @@ module Crystal
   def self.version_string
     machine = system2("uname -m 2>/dev/null")[0]? || "-"
     system = system2("uname -s 2>/dev/null")[0]? || "-"
-    "v#{VERSION} #{version_sha} [#{system} #{machine}]"
+    "v#{version_tag}-p#{version_patch} #{version_sha} [#{system} #{machine}]"
   end
 end
