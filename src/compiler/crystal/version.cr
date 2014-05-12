bd8187e3f71c5cadd54f2fe8b45a0c0f9eda26e5
Konstantin Makarchev <kostya27@gmail.com>
2014-05-12 18:53:29 +0400

    fix __DIR__

--- src/compiler/crystal/version.cr
+++ src/compiler/crystal/version.cr
@@ -6,7 +6,7 @@ module Crystal
     rev = system2("git rev-parse --short HEAD 2>/dev/null")
     sha = rev[0]? || "-"
     
-    File.open(File.dirname(__FILE__) + "/version_generated.cr", "w") do |f|
+    File.open(__DIR__ + "/version_generated.cr", "w") do |f|
       f.puts "module Crystal"
       f.puts "  def self.version_sha; \"#{sha}\"; end"
       f.puts "end"
