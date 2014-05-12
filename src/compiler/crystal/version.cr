cdcfdfd5843f10ee9d842d52aafe56a321d17804
Konstantin Makarchev <kostya27@gmail.com>
2014-05-12 23:57:34 +0400

    not -

--- src/compiler/crystal/version.cr
+++ src/compiler/crystal/version.cr
@@ -19,6 +19,6 @@ module Crystal
 
   def self.version_string
     uname = system2("uname -s -m 2>/dev/null")[0]? || ""
-    "v#{version_tag}-p#{version_patch} #{version_sha} [#{uname}]"
+    "v#{version_tag}p#{version_patch} #{version_sha} [#{uname}]"
   end
 end
