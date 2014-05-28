5b001159ef94309b3588d1a6540063579e4b570d
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-05-28 17:32:49 -0300

    Cleaner C types for Mutex

--- src/thread.cr
+++ src/thread.cr
@@ -1,17 +1,13 @@
 lib PThread
   type Thread : Void*
-  struct Mutex
-    a : Int64; b : Int64; c : Int64; d : Int64; e : Int64; f : Int64; g : Int64; h : Int64
-  end
-
-  struct MutexAttr
-  end
+  type Mutex : Int64[8]
+  type MutexAttr : Void*
 
   fun create = pthread_create(thread : Thread*, attr : Void*, start : Void* ->, arg : Void*)
   fun exit = pthread_exit(value : Void*)
   fun join = pthread_join(thread : Thread, value : Void**) : Int32
 
-  fun mutex_init = pthread_mutex_init(mutex : Mutex*, mutex_attr : MutexAttr*) : Int32
+  fun mutex_init = pthread_mutex_init(mutex : Mutex*, mutex_attr : MutexAttr) : Int32
   fun mutex_lock = pthread_mutex_lock(mutex : Mutex*) : Int32
   fun mutex_trylock = pthread_mutex_trylock(mutex : Mutex*) : Int32
   fun mutex_unlock = pthread_mutex_unlock(mutex : Mutex*) : Int32
