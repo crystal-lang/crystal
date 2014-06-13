9ae36cfbe5563480c4a52bded1ee93cd5d5f3179
Brian J. Cardiff <bcardiff@manas.com.ar>
2014-06-13 01:28:42 -0300

    [lib/objc] remove instance map from export macros

--- libs/objc/export.cr
+++ libs/objc/export.cr
@@ -3,18 +3,16 @@ macro objc_class(class_name)
     property :obj
   end
 
-  $x_{{class_name}}_obj_map = {} of UInt8* => {{class_name}}
-
   $x_{{class_name}}_objc_class = ObjCClass.new(LibObjC.allocateClassPair(ObjCClass.new("NSObject").obj, "{{class_name}}", 0_u32))
 
   class {{class_name}}
     def self.mapped_class
+      # the registered class is not been able to lookup by name
       $x_{{class_name}}_objc_class.obj
     end
 
     def initialize
       @obj = initialize_using "init"
-      $x_{{class_name}}_obj_map[@obj] = self
     end
 
     {{yield}}
@@ -23,8 +21,7 @@ end
 
 macro objc_export(class_name, method_name)
   $x_{{class_name}}_{{method_name}}_imp = ->(_self : UInt8*, _cmd : LibObjC::SEL) {
-    o = $x_{{class_name}}_obj_map[_self] as {{class_name}}
-    o.{{method_name}}
+    {{class_name}}.new(_self).{{method_name}}
   }
   LibObjC.class_addMethod($x_{{class_name}}_objc_class.obj, "{{method_name}}".to_sel, $x_{{class_name}}_{{method_name}}_imp, "v@:")
 end
