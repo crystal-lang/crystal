3a88bd0eb9ec2e243645c05081929a30a8b34211
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-06-29 22:06:32 -0300

    Moved hook specs to their own files, and added specs for wrong number of arguments

---
+++ spec/compiler/codegen/hooks_spec.cr
@@ -0,0 +1,51 @@
+#!/usr/bin/env bin/crystal --run
+require "../../spec_helper"
+
+describe "Code gen: hooks" do
+  it "does inherited macro" do
+    run("
+      class Foo
+        macro inherited
+          $x = 1
+        end
+      end
+
+      class Bar < Foo
+      end
+
+      $x
+      ").to_i.should eq(1)
+  end
+
+  it "does included macro" do
+    run("
+      module Foo
+        macro included
+          $x = 1
+        end
+      end
+
+      class Bar
+        include Foo
+      end
+
+      $x
+      ").to_i.should eq(1)
+  end
+
+  it "does extended macro" do
+    run("
+      module Foo
+        macro extended
+          $x = 1
+        end
+      end
+
+      class Bar
+        extend Foo
+      end
+
+      $x
+      ").to_i.should eq(1)
+  end
+end
---
+++ spec/compiler/type_inference/hooks_spec.cr
@@ -0,0 +1,84 @@
+#!/usr/bin/env bin/crystal --run
+require "../../spec_helper"
+
+describe "Type inference: hooks" do
+  it "does inherited macro" do
+    assert_type("
+      class Foo
+        macro inherited
+          def self.{{@name.downcase.id}}
+            1
+          end
+        end
+      end
+
+      class Bar < Foo
+      end
+
+      Bar.bar
+      ") { int32 }
+  end
+
+  it "does included macro" do
+    assert_type("
+      module Foo
+        macro included
+          def self.{{@name.downcase.id}}
+            1
+          end
+        end
+      end
+
+      class Bar
+        include Foo
+      end
+
+      Bar.bar
+      ") { int32 }
+  end
+
+  it "does extended macro" do
+    assert_type("
+      module Foo
+        macro extended
+          def self.{{@name.downcase.id}}
+            1
+          end
+        end
+      end
+
+      class Bar
+        extend Foo
+      end
+
+      Bar.bar
+      ") { int32 }
+  end
+
+  it "errors if wrong inherited args length" do
+    assert_error %(
+      class Foo
+        macro inherited(x)
+        end
+      end
+      ), "macro 'inherited' must not have arguments"
+  end
+
+  it "errors if wrong included args length" do
+    assert_error %(
+      module Foo
+        macro included(x)
+        end
+      end
+      ), "macro 'included' must not have arguments"
+  end
+
+  it "errors if wrong extended args length" do
+    assert_error %(
+      module Foo
+        macro extended(x)
+        end
+      end
+      ), "macro 'extended' must not have arguments"
+  end
+end
