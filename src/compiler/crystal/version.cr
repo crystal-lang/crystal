cb42ed2245538320da7285627d70a390f3b0d3ea
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-07-10 19:34:47 -0300

    Removed patch info from release version (we won't use that for now)

--- src/compiler/crystal/version.cr
+++ src/compiler/crystal/version.cr
@@ -4,8 +4,8 @@ module Crystal
     build_date = {{ system("date -u").stringify }}
     a = str.split("-")
     tag = a[0]? || "?"
-    patch = a[1]? || "0"
+    # patch = a[1]? || "0"
     sha = a[2]? ? a[2][1..-1] : "-"
-    "#{tag}-p#{patch} [#{sha}] (#{build_date})"
+    "#{tag} [#{sha}] (#{build_date})"
   end
 end
