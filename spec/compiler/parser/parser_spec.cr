0ff6dac07625a8921742525e67b3ffaf18a15d69
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-07-14 17:40:31 -0300

    Fixed: parser didn't parse "def self.[]?"

--- spec/compiler/parser/parser_spec.cr
+++ spec/compiler/parser/parser_spec.cr
@@ -453,6 +453,7 @@ describe "Parser" do
   it_parses "def []?; end", Def.new("[]?", [] of Arg, nil)
   it_parses "def []=(value); end", Def.new("[]=", ["value".arg], nil)
   it_parses "def self.[]; end", Def.new("[]", [] of Arg, nil, "self".var)
+  it_parses "def self.[]?; end", Def.new("[]?", [] of Arg, nil, "self".var)
 
   it_parses "Int[8]", Call.new("Int".path, "[]", [8.int32] of ASTNode)
   it_parses "Int[8, 4]", Call.new("Int".path, "[]", [8.int32, 4.int32] of ASTNode)
