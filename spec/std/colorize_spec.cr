0a24914e5ecee288abce21e8d3462b6dfa15f7cb
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-07-24 02:32:31 -0300

    Umm... now added colorize :-P

---
+++ spec/std/colorize_spec.cr
@@ -0,0 +1,110 @@
+#!/usr/bin/env bin/crystal --run
+require "spec"
+require "colorize"
+
+describe "colorize" do
+  it "colorizes without change" do
+    "hello".colorize.to_s.should eq("hello")
+  end
+
+  it "colorizes foreground" do
+    "hello".colorize.black.to_s.should eq("\e[30mhello\e[0m")
+    "hello".colorize.red.to_s.should eq("\e[31mhello\e[0m")
+    "hello".colorize.green.to_s.should eq("\e[32mhello\e[0m")
+    "hello".colorize.yellow.to_s.should eq("\e[33mhello\e[0m")
+    "hello".colorize.blue.to_s.should eq("\e[34mhello\e[0m")
+    "hello".colorize.magenta.to_s.should eq("\e[35mhello\e[0m")
+    "hello".colorize.cyan.to_s.should eq("\e[36mhello\e[0m")
+    "hello".colorize.light_gray.to_s.should eq("\e[37mhello\e[0m")
+    "hello".colorize.dark_gray.to_s.should eq("\e[90mhello\e[0m")
+    "hello".colorize.light_red.to_s.should eq("\e[91mhello\e[0m")
+    "hello".colorize.light_green.to_s.should eq("\e[92mhello\e[0m")
+    "hello".colorize.light_yellow.to_s.should eq("\e[93mhello\e[0m")
+    "hello".colorize.light_blue.to_s.should eq("\e[94mhello\e[0m")
+    "hello".colorize.light_magenta.to_s.should eq("\e[95mhello\e[0m")
+    "hello".colorize.light_cyan.to_s.should eq("\e[96mhello\e[0m")
+    "hello".colorize.white.to_s.should eq("\e[97mhello\e[0m")
+  end
+
+  it "colorizes background" do
+    "hello".colorize.on_black.to_s.should eq("\e[40mhello\e[0m")
+    "hello".colorize.on_red.to_s.should eq("\e[41mhello\e[0m")
+    "hello".colorize.on_green.to_s.should eq("\e[42mhello\e[0m")
+    "hello".colorize.on_yellow.to_s.should eq("\e[43mhello\e[0m")
+    "hello".colorize.on_blue.to_s.should eq("\e[44mhello\e[0m")
+    "hello".colorize.on_magenta.to_s.should eq("\e[45mhello\e[0m")
+    "hello".colorize.on_cyan.to_s.should eq("\e[46mhello\e[0m")
+    "hello".colorize.on_light_gray.to_s.should eq("\e[47mhello\e[0m")
+    "hello".colorize.on_dark_gray.to_s.should eq("\e[100mhello\e[0m")
+    "hello".colorize.on_light_red.to_s.should eq("\e[101mhello\e[0m")
+    "hello".colorize.on_light_green.to_s.should eq("\e[102mhello\e[0m")
+    "hello".colorize.on_light_yellow.to_s.should eq("\e[103mhello\e[0m")
+    "hello".colorize.on_light_blue.to_s.should eq("\e[104mhello\e[0m")
+    "hello".colorize.on_light_magenta.to_s.should eq("\e[105mhello\e[0m")
+    "hello".colorize.on_light_cyan.to_s.should eq("\e[106mhello\e[0m")
+    "hello".colorize.on_white.to_s.should eq("\e[107mhello\e[0m")
+  end
+
+  it "colorizes mode" do
+    "hello".colorize.bold.to_s.should eq("\e[1mhello\e[0m")
+    "hello".colorize.bright.to_s.should eq("\e[1mhello\e[0m")
+    "hello".colorize.dim.to_s.should eq("\e[2mhello\e[0m")
+    "hello".colorize.underline.to_s.should eq("\e[4mhello\e[0m")
+    "hello".colorize.blink.to_s.should eq("\e[5mhello\e[0m")
+    "hello".colorize.reverse.to_s.should eq("\e[7mhello\e[0m")
+    "hello".colorize.hidden.to_s.should eq("\e[8mhello\e[0m")
+  end
+
+  it "colorizes foreground with background" do
+    "hello".colorize.blue.on_green.to_s.should eq("\e[34;42mhello\e[0m")
+  end
+
+  it "colorizes foreground with background with mode" do
+    "hello".colorize.blue.on_green.bold.to_s.should eq("\e[34;42;1mhello\e[0m")
+  end
+
+  it "colorized foreground with symbol" do
+    "hello".colorize(:red).to_s.should eq("\e[31mhello\e[0m")
+    "hello".colorize.fore(:red).to_s.should eq("\e[31mhello\e[0m")
+  end
+
+  it "colorizes io with method" do
+    io = StringIO.new
+    with_color.red.surround(io) do
+      io << "hello"
+    end
+    io.to_s.should eq("\e[31mhello\e[0m")
+  end
+
+  it "colorizes io with symbol" do
+    io = StringIO.new
+    with_color(:red).surround(io) do
+      io << "hello"
+    end
+    io.to_s.should eq("\e[31mhello\e[0m")
+  end
+
+  it "colorizes with push and pop" do
+    io = StringIO.new
+    with_color.red.push(io) do
+      io << "hello"
+      with_color.green.push(io) do
+        io << "world"
+      end
+      io << "bye"
+    end
+    io.to_s.should eq("\e[31mhello\e[0;32mworld\e[0;31mbye\e[0m")
+  end
+
+  it "colorizes with push and pop resets" do
+    io = StringIO.new
+    with_color.red.push(io) do
+      io << "hello"
+      with_color.green.bold.push(io) do
+        io << "world"
+      end
+      io << "bye"
+    end
+    io.to_s.should eq("\e[31mhello\e[0;32;1mworld\e[0;31mbye\e[0m")
+  end
+end
---
+++ src/colorize.cr
@@ -0,0 +1,194 @@
+class Object
+  def colorize
+    ColorizedObject.new(self)
+  end
+
+  def colorize(fore)
+    ColorizedObject.new(self).fore(fore)
+  end
+end
+
+def with_color
+  "".colorize
+end
+
+def with_color(color : Symbol)
+  "".colorize(color)
+end
+
+struct ColorizedObject(T)
+  FORE_DEFAULT       =  "39"
+  FORE_BLACK         =  "30"
+  FORE_RED           =  "31"
+  FORE_GREEN         =  "32"
+  FORE_YELLOW        =  "33"
+  FORE_BLUE          =  "34"
+  FORE_MAGENTA       =  "35"
+  FORE_CYAN          =  "36"
+  FORE_LIGHT_GRAY    =  "37"
+  FORE_DARK_GRAY     =  "90"
+  FORE_LIGHT_RED     =  "91"
+  FORE_LIGHT_GREEN   =  "92"
+  FORE_LIGHT_YELLOW  =  "93"
+  FORE_LIGHT_BLUE    =  "94"
+  FORE_LIGHT_MAGENTA =  "95"
+  FORE_LIGHT_CYAN    =  "96"
+  FORE_WHITE         =  "97"
+
+  BACK_DEFAULT       =  "49"
+  BACK_BLACK         =  "40"
+  BACK_RED           =  "41"
+  BACK_GREEN         =  "42"
+  BACK_YELLOW        =  "43"
+  BACK_BLUE          =  "44"
+  BACK_MAGENTA       =  "45"
+  BACK_CYAN          =  "46"
+  BACK_LIGHT_GRAY    =  "47"
+  BACK_DARK_GRAY     = "100"
+  BACK_LIGHT_RED     = "101"
+  BACK_LIGHT_GREEN   = "102"
+  BACK_LIGHT_YELLOW  = "103"
+  BACK_LIGHT_BLUE    = "104"
+  BACK_LIGHT_MAGENTA = "105"
+  BACK_LIGHT_CYAN    = "106"
+  BACK_WHITE         = "107"
+
+  MODE_DEFAULT       =   "0"
+  MODE_BOLD          =   "1"
+  MODE_DIM           =   "2"
+  MODE_UNDERLINE     =   "4"
+  MODE_BLINK         =   "5"
+  MODE_REVERSE       =   "7"
+  MODE_HIDDEN        =   "8"
+
+  def initialize(@object : T)
+    @fore = FORE_DEFAULT
+    @back = BACK_DEFAULT
+    @mode = MODE_DEFAULT
+  end
+
+  {% if true %}
+    {{ colors = %w(black red green yellow blue magenta cyan light_gray dark_gray light_red light_green light_yellow light_blue light_magenta light_cyan white) }}
+
+    {% for name in colors %}
+      def {{name.id}}
+        @fore = FORE_{{name.upcase.id}}
+        self
+      end
+
+      def on_{{name.id}}
+        @back = BACK_{{name.upcase.id}}
+        self
+      end
+    {% end %}
+
+    def fore(color : Symbol)
+      case color
+      {% for name in colors %}
+        when :{{name.id}} then @fore = FORE_{{name.upcase.id}}
+      {% end %}
+      else raise "unknown color: #{color}"
+      end
+      self
+    end
+
+    def back(color : Symbol)
+      case color
+      {% for name in colors %}
+        when :{{name.id}} then @back = BACK_{{name.upcase.id}}
+      {% end %}
+      else raise "unknown color: #{color}"
+      end
+      self
+    end
+  {% end %}
+
+  {% for name in %w(bold dim underline blink reverse hidden) %}
+    def {{name.id}}
+      @mode = MODE_{{name.upcase.id}}
+      self
+    end
+  {% end %}
+
+  def on(color : Symbol)
+    back color
+  end
+
+  def bright
+    bold
+  end
+
+  def to_s(io)
+    surround(io) do
+      io << @object
+    end
+  end
+
+  def surround(io = STDOUT)
+    must_append_end = append_start(io)
+    yield io
+    append_end(io) if must_append_end
+  end
+
+  $colorize_stack = [] of ColorizedObject(String)
+
+  def push(io = STDOUT)
+    last_color = $colorize_stack.last?
+
+    append_start(io, !!last_color)
+
+    $colorize_stack.push self
+    yield io
+    $colorize_stack.pop
+
+    if last_color
+      last_color.append_start(io, true)
+    else
+      append_end(io)
+    end
+  end
+
+  def append_start(io, reset = false)
+    fore_is_default = @fore == FORE_DEFAULT
+    back_is_default = @back == BACK_DEFAULT
+    mode_is_default = @mode == MODE_DEFAULT
+
+    if fore_is_default && back_is_default && mode_is_default && !reset
+      false
+    else
+      io << "\e["
+
+      printed = false
+
+      if reset
+        io << MODE_DEFAULT
+        printed = true
+      end
+
+      unless fore_is_default
+        io << ";" if printed
+        io << @fore
+        printed = true
+      end
+
+      unless back_is_default
+        io << ";" if printed
+        io << @back
+        printed = true
+      end
+
+      unless mode_is_default
+        io << ";" if printed
+        io << @mode
+      end
+
+      io << "m"
+
+      true
+    end
+  end
+
+  def append_end(io)
+    io << "\e[0m"
+  end
+end
