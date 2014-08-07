94c68f787177f075d157d60ae3facde6888564c1
Jonne Haß <me@mrzyx.de>
2014-08-07 21:50:10 +0200

    Ignore EAGAIN in read_nonblock
    Instead just return what we could successfully read
    Also adapt 2048 samples read_keypress to the new behaviour

--- src/io/console.cr
+++ src/io/console.cr
@@ -230,8 +230,10 @@ struct CFileIO
     begin
       String.new_with_capacity_and_length(length) do |buffer|
         read_length = read Slice.new(buffer, length)
-        if read_length == 0 || C.errno == C::EWOULDBLOCK || C.errno == C::EAGAIN
-          raise "exception in read_nonblock"
+        if read_length == 0
+          raise "read_nonblock: read nothing"
+        elsif C.errno == C::EWOULDBLOCK
+          raise Errno.new "exception in read_nonblock"
         else
           read_length.to_i
         end
