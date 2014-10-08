8755a2b1092b503af2fbeb5178a623f62a0e65fe
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-10-08 13:43:15 -0300

    Added missing specs

---
+++ spec/compiler/codegen/array_literal_spec.cr
@@ -0,0 +1,134 @@
+#!/usr/bin/env bin/crystal --run
+require "../../spec_helper"
+
+describe "Code gen: array literal spec" do
+  it "creates custom non-generic array" do
+    run(%(
+      class Custom
+        def initialize
+          @value = 0
+        end
+
+        def <<(element)
+          @value += element
+        end
+
+        def value
+          @value
+        end
+      end
+
+      custom = Custom {1, 2, 3}
+      custom.value
+      )).to_i.should eq(6)
+  end
+
+  it "creates custom generic array" do
+    run(%(
+      class Custom(T)
+        def initialize
+          @value = 0
+        end
+
+        def <<(element : T)
+          @value += element
+        end
+
+        def value
+          @value
+        end
+      end
+
+      custom = Custom {1, 2, 3}
+      custom.value
+      )).to_i.should eq(6)
+  end
+
+  it "creates custom generic array with type var" do
+    run(%(
+      class Custom(T)
+        def initialize
+          @value = 0
+        end
+
+        def <<(element : T)
+          @value += element
+        end
+
+        def value
+          @value
+        end
+      end
+
+      custom = Custom(Int32) {1, 2, 3}
+      custom.value
+      )).to_i.should eq(6)
+  end
+
+  it "creates custom generic array via alias" do
+    run(%(
+      class Custom(T)
+        def initialize
+          @value = 0
+        end
+
+        def <<(element : T)
+          @value += element
+        end
+
+        def value
+          @value
+        end
+      end
+
+      alias MyCustom = Custom
+
+      custom = MyCustom {1, 2, 3}
+      custom.value
+      )).to_i.should eq(6)
+  end
+
+  it "creates custom generic array via alias (2)" do
+    run(%(
+      class Custom(T)
+        def initialize
+          @value = 0
+        end
+
+        def <<(element : T)
+          @value += element
+        end
+
+        def value
+          @value
+        end
+      end
+
+      alias MyCustom = Custom(Int32)
+
+      custom = MyCustom {1, 2, 3}
+      custom.value
+      )).to_i.should eq(6)
+  end
+
+  it "creates custom non-generic array in nested module" do
+    run(%(
+      class Foo::Custom
+        def initialize
+          @value = 0
+        end
+
+        def <<(element)
+          @value += element
+        end
+
+        def value
+          @value
+        end
+      end
+
+      custom = Foo::Custom {1, 2, 3}
+      custom.value
+      )).to_i.should eq(6)
+  end
+end
---
+++ spec/compiler/codegen/hash_literal_spec.cr
@@ -0,0 +1,143 @@
+#!/usr/bin/env bin/crystal --run
+require "../../spec_helper"
+
+describe "Code gen: hash literal spec" do
+  it "creates custom non-generic hash" do
+    run(%(
+      class Custom
+        def initialize
+          @keys = 0
+          @values = 0
+        end
+
+        def []=(key, value)
+          @keys += key
+          @values += value
+        end
+
+        def keys
+          @keys
+        end
+
+        def values
+          @values
+        end
+      end
+
+      custom = Custom {1 => 10, 2 => 20}
+      custom.keys * custom.values
+      )).to_i.should eq(90)
+  end
+
+  it "creates custom generic hash" do
+    run(%(
+      class Custom(K, V)
+        def initialize
+          @keys = 0
+          @values = 0
+        end
+
+        def []=(key, value)
+          @keys += key
+          @values += value
+        end
+
+        def keys
+          @keys
+        end
+
+        def values
+          @values
+        end
+      end
+
+      custom = Custom {1 => 10, 2 => 20}
+      custom.keys * custom.values
+      )).to_i.should eq(90)
+  end
+
+  it "creates custom generic hash with type vars" do
+    run(%(
+      class Custom(K, V)
+        def initialize
+          @keys = 0
+          @values = 0
+        end
+
+        def []=(key, value)
+          @keys += key
+          @values += value
+        end
+
+        def keys
+          @keys
+        end
+
+        def values
+          @values
+        end
+      end
+
+      custom = Custom(Int32, Int32) {1 => 10, 2 => 20}
+      custom.keys * custom.values
+      )).to_i.should eq(90)
+  end
+
+  it "creates custom generic hash via alias (1)" do
+    run(%(
+      class Custom(K, V)
+        def initialize
+          @keys = 0
+          @values = 0
+        end
+
+        def []=(key, value)
+          @keys += key
+          @values += value
+        end
+
+        def keys
+          @keys
+        end
+
+        def values
+          @values
+        end
+      end
+
+      alias MyCustom = Custom
+
+      custom = MyCustom {1 => 10, 2 => 20}
+      custom.keys * custom.values
+      )).to_i.should eq(90)
+  end
+
+  it "creates custom generic hash via alias (2)" do
+    run(%(
+      class Custom(K, V)
+        def initialize
+          @keys = 0
+          @values = 0
+        end
+
+        def []=(key, value)
+          @keys += key
+          @values += value
+        end
+
+        def keys
+          @keys
+        end
+
+        def values
+          @values
+        end
+      end
+
+      alias MyCustom = Custom(Int32, Int32)
+
+      custom = MyCustom {1 => 10, 2 => 20}
+      custom.keys * custom.values
+      )).to_i.should eq(90)
+  end
+end
