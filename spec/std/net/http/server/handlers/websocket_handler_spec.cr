270ea94bdb1541a60545a2210064cad68056d514
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-11-05 01:31:27 -0300

    Fix errors in HTTP::WebSocketHandler

---
+++ spec/std/net/http/server/handlers/websocket_handler_spec.cr
@@ -0,0 +1,27 @@
+require "spec"
+require "net/http/server"
+
+describe HTTP::WebSocketHandler do
+  it "returns not found if the request is not an websocket upgrade" do
+    handler = HTTP::WebSocketHandler.new {}
+    response = handler.call HTTP::Request.new("GET", "/")
+    response.status_code.should eq(404)
+    response.upgrade_handler.should be_nil
+  end
+
+  it "gives upgrade response for websocket upgrade request" do
+    handler = HTTP::WebSocketHandler.new {}
+    headers = HTTP::Headers {
+      "Upgrade": "websocket",
+      "Connection": "Upgrade",
+      "Sec-WebSocket-Key": "dGhlIHNhbXBsZSBub25jZQ=="
+    }
+    request = HTTP::Request.new("GET", "/", headers)
+    response = handler.call request
+    response.status_code.should eq(101)
+    response.headers["Upgrade"].should eq("websocket")
+    response.headers["Connection"].should eq("Upgrade")
+    response.headers["Sec-WebSocket-Accept"].should eq("s3pPLMBiTxaQ9kYGzzhZRbK+xOo=")
+    response.upgrade_handler.should_not be_nil
+  end
+end
--- src/net/http/server/handlers/websocket_handler.cr
+++ src/net/http/server/handlers/websocket_handler.cr
@@ -10,9 +10,9 @@ class HTTP::WebSocketHandler < HTTP::Handler
     if request.headers["Upgrade"]? == "websocket" && request.headers["Connection"]? == "Upgrade"
       key = request.headers["Sec-WebSocket-Key"]
       accept_code = Base64.strict_encode64(OpenSSL::SHA1.hash("#{key}258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))
-      response_headers = {
+      response_headers = HTTP::Headers {
         "Upgrade" => "websocket",
-        "Connection" => "upgrade"
+        "Connection" => "Upgrade"
         "Sec-WebSocket-Accept" => accept_code
       }
       response = Response.new(101, headers: response_headers)
