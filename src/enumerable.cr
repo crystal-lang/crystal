6d1b0bc7c471e6620370c94558a191a1c7db81bc
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-11-09 18:22:35 -0300

    Reorganized Enumerable methods alphabetically

--- src/enumerable.cr
+++ src/enumerable.cr
@@ -1,25 +1,43 @@
 module Enumerable(T)
-  def first
-    each { |e| return e }
-    raise EmptyEnumerable.new
+  def all?
+    each { |e| return false unless yield e }
+    true
   end
 
-  def first?
-    each { |e| return e }
-    nil
+  def all?
+    all? { |o| o }
   end
 
-  def first(count : Int)
-    take(count)
+  def any?
+    each { |e| return true if yield e }
+    false
   end
 
-  def take(count : Int)
-    ary = [] of T
-    each_with_index do |e, i|
-      break if i == count
-      ary << e
+  def any?
+    any? { |e| e }
+  end
+
+  def compact_map(&block : T -> Nil | U)
+    ary = [] of U
+    each do |e|
+      v = yield e
+      ary << v if v
     end
     ary
+  end
+
+  def count
+    count = 0
+    each { |e| count += 1 if yield e }
+    count
+  end
+
+  def count
+    count { true }
+  end
+
+  def count(item)
+    count { |e| e == item }
   end
 
   def each_with_index
@@ -35,6 +53,73 @@ module Enumerable(T)
       yield elem, obj
     end
     obj
+  end
+
+  def find(if_none = nil)
+    each do |elem|
+      return elem if yield elem
+    end
+    if_none
+  end
+
+  def first
+    each { |e| return e }
+    raise EmptyEnumerable.new
+  end
+
+  def first(count : Int)
+    take(count)
+  end
+
+  def first?
+    each { |e| return e }
+    nil
+  end
+
+  def flat_map(&block : T -> Array(U))
+    ary = [] of U
+    each { |e| ary.concat(yield e) }
+    ary
+  end
+
+  def grep(pattern)
+    select { |elem| pattern === elem }
+  end
+
+  def group_by(&block : T -> U)
+    h = Hash(U, Array(T)).new
+    each do |e|
+      v = yield e
+      if h.has_key?(v)
+        h[v].push(e)
+      else
+        h[v] = [e]
+      end
+    end
+    h
+  end
+
+  def includes?(obj)
+    any? { |e| e == obj }
+  end
+
+  def index
+    each_with_index do |e, i|
+      return i if yield e
+    end
+    nil
+  end
+
+  def index(obj)
+    index { |e| e == obj }
+  end
+
+  def index_by(&block : T -> U)
+    hash = {} of U => T
+    each do |elem|
+      hash[yield elem] = elem
+    end
+    hash
   end
 
   def inject
@@ -56,45 +141,6 @@ module Enumerable(T)
     memo
   end
 
-  def map(&block : T -> U)
-    ary = [] of U
-    each { |e| ary << yield e }
-    ary
-  end
-
-  def map_with_index(&block : T, Int32 -> U)
-    ary = [] of U
-    each_with_index { |e, i| ary << yield e, i }
-    ary
-  end
-
-  def flat_map(&block : T -> Array(U))
-    ary = [] of U
-    each { |e| ary.concat(yield e) }
-    ary
-  end
-
-  def compact_map(&block : T -> Nil | U)
-    ary = [] of U
-    each do |e|
-      v = yield e
-      ary << v if v
-    end
-    ary
-  end
-
-  def select(&block : T ->)
-    ary = [] of T
-    each { |e| ary << e if yield e }
-    ary
-  end
-
-  def reject(&block : T ->)
-    ary = [] of T
-    each { |e| ary << e unless yield e }
-    ary
-  end
-
   def join(separator = "")
     String.build do |io|
       join separator, io
@@ -114,97 +160,20 @@ module Enumerable(T)
     end
   end
 
-  def to_a
-    ary = [] of T
-    each { |e| ary << e }
+  def map(&block : T -> U)
+    ary = [] of U
+    each { |e| ary << yield e }
     ary
   end
 
-  def count
-    count = 0
-    each { |e| count += 1 if yield e }
-    count
-  end
-
-  def count
-    count { true }
-  end
-
-  def count(item)
-    count { |e| e == item }
-  end
-
-  def find(if_none = nil)
-    each do |elem|
-      return elem if yield elem
-    end
-    if_none
-  end
-
-  def any?
-    each { |e| return true if yield e }
-    false
-  end
-
-  def any?
-    any? { |e| e }
-  end
-
-  def all?
-    each { |e| return false unless yield e }
-    true
-  end
-
-  def all?
-    all? { |o| o }
-  end
-
-  def includes?(obj)
-    any? { |e| e == obj }
-  end
-
-  def index(obj)
-    index { |e| e == obj }
-  end
-
-  def index
-    each_with_index do |e, i|
-      return i if yield e
-    end
-    nil
-  end
-
-  def grep(pattern)
-    select { |elem| pattern === elem }
-  end
-
-  def min
-    min_by { |x| x }
+  def map_with_index(&block : T, Int32 -> U)
+    ary = [] of U
+    each_with_index { |e, i| ary << yield e, i }
+    ary
   end
 
   def max
     max_by { |x| x }
-  end
-
-  def minmax
-    minmax_by { |x| x }
-  end
-
-  def min_by(&block : T -> U)
-    min :: U
-    obj :: T
-    found = false
-
-    each_with_index do |elem, i|
-      value = yield elem
-      if i == 0 || value < min
-        min = value
-        obj = elem
-      end
-      found = true
-    end
-
-    found ? obj : raise EmptyEnumerable.new
   end
 
   def max_by(&block : T -> U)
@@ -222,6 +191,14 @@ module Enumerable(T)
     end
 
     found ? obj : raise EmptyEnumerable.new
+  end
+
+  def min
+    min_by { |x| x }
+  end
+
+  def minmax
+    minmax_by { |x| x }
   end
 
   def minmax_by(&block : T -> U)
@@ -247,26 +224,26 @@ module Enumerable(T)
     found ? {objmin, objmax} : raise EmptyEnumerable.new
   end
 
-  def partition(&block : T -> U)
-    a, b = [] of T, [] of T
-    each do |e|
-      value = yield(e)
-      value ? a.push(e) : b.push(e)
+  def min_by(&block : T -> U)
+    min :: U
+    obj :: T
+    found = false
+
+    each_with_index do |elem, i|
+      value = yield elem
+      if i == 0 || value < min
+        min = value
+        obj = elem
+      end
+      found = true
     end
-    {a, b}
+
+    found ? obj : raise EmptyEnumerable.new
   end
 
-  def group_by(&block : T -> U)
-    h = Hash(U, Array(T)).new
-    each do |e|
-      v = yield e
-      if h.has_key?(v)
-        h[v].push(e)
-      else
-        h[v] = [e]
-      end
-    end
-    h
+  def none?(&block : T -> U)
+    each { |e| return false if yield(e) }
+    true
   end
 
   def one?(&block : T -> U)
@@ -278,9 +255,25 @@ module Enumerable(T)
     c == 1
   end
 
-  def none?(&block : T -> U)
-    each { |e| return false if yield(e) }
-    true
+  def partition(&block : T -> U)
+    a, b = [] of T, [] of T
+    each do |e|
+      value = yield(e)
+      value ? a.push(e) : b.push(e)
+    end
+    {a, b}
+  end
+
+  def reject(&block : T ->)
+    ary = [] of T
+    each { |e| ary << e unless yield e }
+    ary
+  end
+
+  def select(&block : T ->)
+    ary = [] of T
+    each { |e| ary << e if yield e }
+    ary
   end
 
   def sum(initial = 0)
@@ -291,11 +284,18 @@ module Enumerable(T)
     inject(initial) { |memo, e| memo + (yield e) }
   end
 
-  def index_by(&block : T -> U)
-    hash = {} of U => T
-    each do |elem|
-      hash[yield elem] = elem
+  def take(count : Int)
+    ary = [] of T
+    each_with_index do |e, i|
+      break if i == count
+      ary << e
     end
-    hash
+    ary
+  end
+
+  def to_a
+    ary = [] of T
+    each { |e| ary << e }
+    ary
   end
 end
