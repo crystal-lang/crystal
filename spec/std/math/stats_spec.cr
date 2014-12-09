4146f08e566b80bf509ead4570078e681949d97a
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-12-09 16:36:57 +0000

    Fixed typo (parenthesis) in stats_spec

--- spec/std/math/stats_spec.cr
+++ spec/std/math/stats_spec.cr
@@ -2,6 +2,7 @@ require "spec"
 
 describe "Stats" do
   it "mean" do
-    Math.mean([1, 3.5, 9].should eq(4.5))
+    Math.mean([1, 3.5, 9]).should eq(4.5)
+    Math.mean({1, 3.5, 9}).should eq(4.5)
   end
 end
