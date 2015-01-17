6f85c8653dbcf635f12d2655c446574b5b7be839
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-01-17 12:08:59 -0300

    Added conway and wordcount to examples (based on http://arthurtw.github.io/2015/01/12/quick-comparison-nim-vs-rust.html )

---
+++ samples/conway.cr
@@ -0,0 +1,102 @@
+# Ported from http://arthurtw.github.io/2015/01/12/quick-comparison-nim-vs-rust.html
+
+struct ANSI
+  def initialize(@io)
+  end
+
+  def clear
+    @io << "\e[2J"
+  end
+
+  def pos(x, y)
+    @io << "\e[" << x << ';' << y << 'H'
+  end
+end
+
+module IO
+  def ansi
+    ANSI.new self
+  end
+end
+
+struct ConwayMap
+  WIDTH = 40
+  HEIGHT = 30
+
+  include Math
+
+  def initialize(pattern)
+    @map = Array.new(HEIGHT) { Array.new(WIDTH, false) }
+
+    ix = min WIDTH, pattern.map(&.length).max
+    iy = min HEIGHT, pattern.length
+    dx = (WIDTH - ix) / 2
+    dy = (HEIGHT - iy) / 2
+
+    iy.times do |y|
+      ix.times do |x|
+        if x < pattern[y].length && !pattern[y][x].whitespace?
+          @map[y + dy][x + dx] = true
+        end
+      end
+    end
+  end
+
+  def next
+    old_map = @map.clone
+
+    HEIGHT.times do |i|
+      WIDTH.times do |j|
+        nlive = 0
+
+        max(i-1, 0).upto(min(i+1, HEIGHT-1)) do |i2|
+          max(j-1, 0).upto(min(j+1, WIDTH-1)) do |j2|
+            nlive += 1 if old_map[i2][j2] && (i2 != i || j2 != j)
+          end
+        end
+
+        if @map[i][j]
+          @map[i][j] = 2 <= nlive <= 3
+        else
+          @map[i][j] = nlive == 3
+        end
+      end
+    end
+  end
+
+  def to_s(io)
+    io.ansi.clear
+    io.ansi.pos 1, 1
+    @map.each do |row|
+      row.each do |cell|
+        io << (cell ? "()" : ". ")
+      end
+      io.puts
+    end
+  end
+end
+
+PAUSE_MILLIS = 20
+DEFAULT_COUNT = 300
+INITIAL_MAP = [
+    "                        1           ",
+    "                      1 1           ",
+    "            11      11            11",
+    "           1   1    11            11",
+    "11        1     1   11              ",
+    "11        1   1 11    1 1           ",
+    "          1     1       1           ",
+    "           1   1                    ",
+    "            11                      ",
+  ]
+
+map = ConwayMap.new INITIAL_MAP
+
+Thread.new { gets; exit }
+
+1.upto(DEFAULT_COUNT) do |i|
+  puts map
+  puts "n = #{i}\tPress ENTER to exit"
+  sleep PAUSE_MILLIS * 0.001
+  map.next
+end
---
+++ samples/wordcount.cr
@@ -0,0 +1,54 @@
+# Ported from http://arthurtw.github.io/2015/01/12/quick-comparison-nim-vs-rust.html
+
+require "option_parser"
+
+def do_work(in_filenames, output_filename, ignore_case)
+  if in_filenames.empty?
+    in_files = [STDIN]
+  else
+    in_files = in_filenames.map { |name| BufferedIO.new(File.open(name, "r")) }
+  end
+
+  if output_filename
+    out_file = File.open(output_filename, "w")
+  else
+    out_file = STDOUT
+  end
+
+  counts = Hash(String, Int32).new { |h, k| h[k] = 0 }
+
+  in_files.each do |in_file|
+    in_file.each_line do |line|
+      line = line.downcase if ignore_case
+      line.scan(/\w+/) do |match|
+        counts[match[0]] += 1
+      end
+    end
+  end
+
+  entries = counts.to_a.sort_by! &.[0]
+  entries.each do |entry|
+    word, count = entry
+    out_file.puts "#{count}\t#{word}"
+  end
+end
+
+output_filename = nil
+ignore_case = false
+
+OptionParser.parse! do |opts|
+  opts.banner = "Usage: wordcount [OPTIONS] [FILES]"
+  opts.on("-o NAME", "set output filename") do |filename|
+    output_filename = filename
+  end
+  opts.on("-i", "--ignore-case", "ignore case") do
+    ignore_case = true
+  end
+  opts.on("-h", "--help", "print this help menu") do
+    puts opts
+  end
+end
+
+in_filenames = ARGV
+
+do_work ARGV, output_filename, ignore_case
