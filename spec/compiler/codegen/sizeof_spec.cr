5941a46a074fd079c4e0833ca0b65ede1903c053
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-01-25 16:24:27 -0300

    Fixed sizeof specs for 32 bits.

--- spec/compiler/codegen/sizeof_spec.cr
+++ spec/compiler/codegen/sizeof_spec.cr
@@ -19,6 +19,7 @@ describe "Code gen: sizeof" do
   end
 
   it "gets sizeof class" do
+    # A class is represented as a pointer to its data
     run("
       class Foo
         def initialize(@x, @y, @z)
@@ -28,26 +29,30 @@ describe "Code gen: sizeof" do
       Foo.new(1, 2, 3)
 
       sizeof(Foo)
-      ").to_i.should eq(8)
+      ").to_i.should eq(sizeof(Void*))
   end
 
   it "gets sizeof union" do
-    run("
+    size = run("
       sizeof(Int32 | Float64)
-      ").to_i.should eq(16)
-  end
+      ").to_i
 
-  it "gets sizeof class" do
-    run("
-      class Foo
-        def initialize(@x, @y, @z)
-        end
-      end
-
-      Foo.new(1, 2, 3)
-
-      sizeof(Foo)
-      ").to_i.should eq(8)
+    # This union is represented as:
+    #
+    #   struct {
+    #      4 bytes, # for the type id
+    #      8 bytes, # for the largest size between Int32 and Float64
+    #   }
+    #
+    # But in 64 bits structs are aligned to 8 bytes, so it'll actually
+    # be struct { 8 bytes, 8 bytes }.
+    #
+    # In 32 bits structs are aligned to 4 bytes, so it remains the same.
+    ifdef x86_64
+      size.should eq(16)
+    else
+      size.should eq(12)
+    end
   end
 
   it "gets instance_sizeof class" do
