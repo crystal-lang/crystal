31fc87841e69d2eb98442433f9acd76e7469e47d
Zamith <zamith.28@gmail.com>
2015-01-31 14:53:56 +0000

    Fixes target dir in deps
    target_dir was not being called, resulting in strange behaviour.

--- src/crystal/project/github_dependency.cr
+++ src/crystal/project/github_dependency.cr
@@ -15,18 +15,18 @@ module Crystal
     end
 
     def target_dir
-      ".deps/#{@author}-#{@repository}"
+      @_target_dir ||= ".deps/#{@author}-#{@repository}"
     end
 
     def install
-      unless Dir.exists?(@target_dir)
-        exec "git clone git://github.com/#{@author}/#{@repository}.git #{@target_dir}"
+      unless Dir.exists?(target_dir)
+        exec "git clone git://github.com/#{@author}/#{@repository}.git #{target_dir}"
       end
-      exec "ln -sf ../#{@target_dir}/src libs/#{name}"
+      exec "ln -sf ../#{target_dir}/src libs/#{name}"
 
       if @locked_version
         if current_version != @locked_version
-          exec "git -C #{@target_dir} checkout -q #{@locked_version}"
+          exec "git -C #{target_dir} checkout -q #{@locked_version}"
         end
       else
         @locked_version = current_version
@@ -34,13 +34,13 @@ module Crystal
     end
 
     def update
-      exec "rm -rf #{@target_dir}"
+      exec "rm -rf #{target_dir}"
       @locked_version = nil
       install
     end
 
     def current_version
-      exec("git -C #{@target_dir} rev-parse HEAD").chomp
+      exec("git -C #{target_dir} rev-parse HEAD").chomp
     end
 
     private def exec(cmd)
