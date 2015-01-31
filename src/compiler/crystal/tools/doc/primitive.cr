81f191e90b341b7157f9a1e702878fdf504bc55c
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-01-31 11:30:49 -0300

    [Docs] Added more primitives

--- src/compiler/crystal/tools/doc/primitive.cr
+++ src/compiler/crystal/tools/doc/primitive.cr
@@ -14,22 +14,52 @@ ref2 = pointer as Reference
 ref2.object_id == ref.object_id #=> true
 ```
 )
+    when :cast
+      case a_def.name
+      when "ord"
+%(
+Returns this Char's Unicode codepoint.
+
+```
+'a'.ord  #=> 97
+'あ'.ord #=> 12354
+```
+)
+      else
+        ""
+      end
     when :binary
       case a_def.name
       when "=="
-        "Returns true if this object is equal to other."
+        "Returns true if this #{a_def.owner} is equal to `other`."
       when "!="
-        "Returns true if this object is not equal to other."
+        "Returns true if this #{a_def.owner} is not equal to `other`."
       when "<"
-        "Returns true if this object is less than other."
+        if a_def.owner.is_a?(CharType)
+          "Returns true if this #{a_def.owner}'s codepoint is less than `other`'s."
+        else
+          "Returns true if this #{a_def.owner} is less than `other`."
+        end
       when "<="
-        "Returns true if this object is less than or equal to other."
+        if a_def.owner.is_a?(CharType)
+          "Returns true if this #{a_def.owner}'s codepoint is less than or equal to `other`'s."
+        else
+          "Returns true if this #{a_def.owner} is less than or equal to `other`."
+        end
       when ">"
-        "Returns true if this object is greater than other."
+        if a_def.owner.is_a?(CharType)
+          "Returns true if this #{a_def.owner}'s codepoint is greater than `other`'s."
+        else
+          "Returns true if this #{a_def.owner} is greater than `other`."
+        end
       when ">="
-        "Returns true if this object is greater than or equal to other."
+        if a_def.owner.is_a?(CharType)
+          "Returns true if this #{a_def.owner}'s codepoint is greater than or equal to `other`'s."
+        else
+          "Returns true if this #{a_def.owner} is greater than or equal to `other`."
+        end
       when "*"
-        "Returns the result of multipling this object by other."
+        "Returns the result of multipling this #{a_def.owner} by `other`."
       else
         ""
         # raise "Bug: missing binary doc: #{a_def.name}"
