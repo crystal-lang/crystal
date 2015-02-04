d745e039f6f9481b077bd64693c3029090e8e1cf
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-02-03 22:02:36 -0300

    Small fix in @LInk attachment logic. Fixed #381. Fixed #388.

--- spec/compiler/type_inference/lib_spec.cr
+++ spec/compiler/type_inference/lib_spec.cr
@@ -346,4 +346,22 @@ it "errors if unknown named arg" do
       Foo.foo(a)
       )) { int64 }
   end
+
+  it "correctly attached link flags if there's an ifdef" do
+    result = infer_type(%(
+      @[Link("SDL")]
+      @[Link("SDLMain")]
+      @[Link(framework: "Cocoa")] ifdef some_flag
+      lib LibSDL
+        fun init = SDL_Init(flags : UInt32) : Int32
+      end
+
+      LibSDL.init(0_u32)
+      ))
+    sdl = result.program.types["LibSDL"] as LibType
+    attrs = sdl.link_attributes.not_nil!
+    attrs.length.should eq(2)
+    attrs[0].lib.should eq("SDL")
+    attrs[1].lib.should eq("SDLMain")
+  end
 end
