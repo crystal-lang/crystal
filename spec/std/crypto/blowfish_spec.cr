a512012cbeed8fa5affff6a6165f80ed4e232786
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-02-14 19:17:49 -0300

    Refactored and optimized blowfish. Related to #411

--- spec/std/crypto/blowfish_spec.cr
+++ spec/std/crypto/blowfish_spec.cr
@@ -9,9 +9,21 @@ describe "Blowfish" do
     l, r = bf.encrypt_pair(orig_l, orig_r)
     l.should eq(0xcc91732b)
     r.should eq(0x8022f684)
-    
+
     l, r = bf.decrypt_pair(l, r)
     l.should eq(orig_l)
     r.should eq(orig_r)
+  end
+
+  it "raises if the key is empty" do
+    expect_raises ArgumentError, /Invalid key size/ do
+      Crypto::Blowfish.new("")
+    end
+  end
+
+  it "raises if the key length is bigger than 56" do
+    expect_raises ArgumentError, /Invalid key size/ do
+      Crypto::Blowfish.new("a" * 57)
+    end
   end
 end
