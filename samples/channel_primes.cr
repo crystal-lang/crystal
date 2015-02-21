33ae99e9a666af6e8190f197c6078613546c7fb3
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-02-21 15:45:19 -0300

    Added sample with coroutines and channels (ported from http://dancallahan.info/journal/go-concurrency/#How+do+channels+and+goroutines+work+together?)

---
+++ samples/channel_primes.cr
@@ -0,0 +1,33 @@
+# Ported from Go sample from this page: http://dancallahan.info/journal/go-concurrency/#How+do+channels+and+goroutines+work+together?
+
+def generate(chan)
+  i = 2
+  loop do
+    chan.send(i)
+    i += 1
+  end
+end
+
+def filter(in_chan, out_chan, prime)
+  loop do
+    i = in_chan.receive
+    if i % prime != 0
+      out_chan.send(i)
+    end
+  end
+end
+
+def run_filter(in_chan, out_chan, prime)
+  spawn { filter(in_chan, out_chan, prime) }
+end
+
+ch = Channel(Int32).new
+spawn { generate(ch) }
+
+100.times do
+  prime = ch.receive
+  puts prime
+  ch1 = Channel(Int32).new
+  run_filter(ch, ch1, prime)
+  ch = ch1
+end
