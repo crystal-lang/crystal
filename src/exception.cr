85a0476eac48a35b373ecab0820dd87512ebd557
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-03-09 22:23:52 -0300

    Improved `caller` performance

--- src/exception.cr
+++ src/exception.cr
@@ -1,62 +1,65 @@
 ifdef darwin
   lib Unwind
-    CURSOR_SIZE = 140
-    CONTEXT_SIZE = 128
+    type Cursor = LibC::SizeT[140]
+    type Context = LibC::SizeT[128]
 
     REG_IP = -1
 
-    fun get_context = unw_getcontext(context : LibC::SizeT*) : Int32
-    fun init_local = unw_init_local(cursor : LibC::SizeT*, context : LibC::SizeT*) : Int32
-    fun step = unw_step(cursor : LibC::SizeT*) : Int32
-    fun get_reg = unw_get_reg(cursor : LibC::SizeT*, regnum : Int32, reg : LibC::SizeT*) : Int32
-    fun get_proc_name = unw_get_proc_name(cursor : LibC::SizeT*, name : UInt8*, size : Int32, offset : LibC::SizeT*) : Int32
+    fun get_context = unw_getcontext(context : Context*) : Int32
+    fun init_local = unw_init_local(cursor : Cursor*, context : Context*) : Int32
+    fun step = unw_step(cursor : Cursor*) : Int32
+    fun get_reg = unw_get_reg(cursor : Cursor*, regnum : Int32, reg : LibC::SizeT*) : Int32
+    fun get_proc_name = unw_get_proc_name(cursor : Cursor*, name : UInt8*, size : Int32, offset : LibC::SizeT*) : Int32
   end
 elsif linux
   ifdef x86_64
     @[Link("unwind")]
     lib Unwind
-      CURSOR_SIZE = 140
-      CONTEXT_SIZE = 128
+      type Cursor = LibC::SizeT[140]
+      type Context = LibC::SizeT[128]
 
       REG_IP = -1
 
-      fun get_context = _Ux86_64_getcontext(context : LibC::SizeT*) : Int32
-      fun init_local = _ULx86_64_init_local(cursor : LibC::SizeT*, context : LibC::SizeT*) : Int32
-      fun step = _ULx86_64_step(cursor : LibC::SizeT*) : Int32
-      fun get_reg = _ULx86_64_get_reg(cursor : LibC::SizeT*, regnum : Int32, reg : LibC::SizeT*) : Int32
-      fun get_proc_name = _ULx86_64_get_proc_name(cursor : LibC::SizeT*, name : UInt8*, size : Int32, offset : LibC::SizeT*) : Int32
+      fun get_context = _Ux86_64_getcontext(context : Context*) : Int32
+      fun init_local = _ULx86_64_init_local(cursor : Cursor*, context : Context*) : Int32
+      fun step = _ULx86_64_step(cursor : Cursor*) : Int32
+      fun get_reg = _ULx86_64_get_reg(cursor : Cursor*, regnum : Int32, reg : LibC::SizeT*) : Int32
+      fun get_proc_name = _ULx86_64_get_proc_name(cursor : Cursor*, name : UInt8*, size : Int32, offset : LibC::SizeT*) : Int32
     end
   else
     @[Link("unwind")]
     lib Unwind
-      CURSOR_SIZE = 127
-      CONTEXT_SIZE = 87
+      type Cursor = LibC::SizeT[127]
+      type Context = LibC::SizeT[87]
 
       REG_IP = -1
 
-      fun get_context = getcontext(context : LibC::SizeT*) : Int32
-      fun init_local = _ULx86_init_local(cursor : LibC::SizeT*, context : LibC::SizeT*) : Int32
-      fun step = _ULx86_step(cursor : LibC::SizeT*) : Int32
-      fun get_reg = _ULx86_get_reg(cursor : LibC::SizeT*, regnum : Int32, reg : LibC::SizeT*) : Int32
-      fun get_proc_name = _ULx86_get_proc_name(cursor : LibC::SizeT*, name : UInt8*, size : Int32, offset : LibC::SizeT*) : Int32
+      fun get_context = getcontext(context : Context*) : Int32
+      fun init_local = _ULx86_init_local(cursor : Cursor*, context : Context*) : Int32
+      fun step = _ULx86_step(cursor : Cursor*) : Int32
+      fun get_reg = _ULx86_get_reg(cursor : Cursor*, regnum : Int32, reg : LibC::SizeT*) : Int32
+      fun get_proc_name = _ULx86_get_proc_name(cursor : Cursor*, name : UInt8*, size : Int32, offset : LibC::SizeT*) : Int32
     end
   end
 end
 
 def caller
-  cursor = Pointer(LibC::SizeT).malloc(Unwind::CURSOR_SIZE)
-  context = Pointer(LibC::SizeT).malloc(Unwind::CONTEXT_SIZE)
+  cursor :: Unwind::Cursor
+  context :: Unwind::Context
 
-  Unwind.get_context(context)
-  Unwind.init_local(cursor, context)
+  cursor_ptr = pointerof(cursor)
+  context_ptr = pointerof(context)
+
+  Unwind.get_context(context_ptr)
+  Unwind.init_local(cursor_ptr, context_ptr)
   fname_size = 64
   fname_buffer = Pointer(UInt8).malloc(fname_size)
 
   backtrace = [] of String
-  while Unwind.step(cursor) > 0
-    Unwind.get_reg(cursor, Unwind::REG_IP, out pc)
+  while Unwind.step(cursor_ptr) > 0
+    Unwind.get_reg(cursor_ptr, Unwind::REG_IP, out pc)
     while true
-      Unwind.get_proc_name(cursor, fname_buffer, fname_size, out offset)
+      Unwind.get_proc_name(cursor_ptr, fname_buffer, fname_size, out offset)
       fname = String.new(fname_buffer)
       break if fname.length < fname_size - 1
 
