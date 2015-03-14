f31cdb6653418397b9d1a2a6cfe925062b8b2a91
Jacob <jacob.ub.bl@gmail.com>
2015-03-14 01:59:15 +0000

    More symbols without quotes and include Comparable

--- src/symbol.cr
+++ src/symbol.cr
@@ -1,4 +1,10 @@
 struct Symbol
+  include Comparable(Symbol)
+
+  def <=>(other : Symbol)
+    to_s <=> other.to_s
+  end
+  
   def inspect(io : IO)
     io << ":"
 
@@ -16,12 +22,19 @@ struct Symbol
 
   # Determines if a string needs to be quoted to be used for a symbol.
   def self.needs_quotes?(string)
-    string.each_char do |char|
-      case char
-      when '0'..'9', 'A'..'Z', 'a'..'z', '_'
-        # Nothing
-      else
-        return true
+    case string
+    when "+", "-", "*", "/", "==", "<", "<=", ">", ">=", "!", "!=", "=~", "!~"
+      # Nothing
+    when "&", "|", "^", "~", "**", ">>", "<<", "%", "[]", "<=>", "===", "[]?", "[]="
+      # Nothing
+    else
+      string.each_char do |char|
+        case char
+        when '0'..'9', 'A'..'Z', 'a'..'z', '_'
+          # Nothing
+        else
+          return true
+        end
       end
     end
     false
