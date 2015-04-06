216f163a6e724e174b09645a31dccaeb2556b7cf
Xyz10 <cassyblanca@icloud.com>
2015-04-05 21:01:11 -0700

    Fixed GitDependency spec (forgot to add .git extension!).

--- spec/std/crystal/git_dependency_spec.cr
+++ spec/std/crystal/git_dependency_spec.cr
@@ -5,13 +5,13 @@ module Crystal
   describe "GitDependency" do
     describe "#initialize" do
       it "uses the repository's name as the dependency name" do
-        dependency = GitDependency.new("https://example.com/owner/repo")
+        dependency = GitDependency.new("https://example.com/owner/repo.git")
 
         dependency.name.should eq("repo")
       end
 
       it "customizes Git dependency name" do
-        dependency = GitDependency.new("https://example.com/owner/repo", "name")
+        dependency = GitDependency.new("https://example.com/owner/repo.git", "name")
 
         dependency.name.should eq("name")
       end
@@ -24,20 +24,20 @@ module Crystal
 
       %w(crystal_repo crystal-repo repo.cr repo_crystal repo-crystal).each do |repo_name|
         it "guesses name from project name like #{repo_name}" do
-          dependency = GitDependency.new("https://example.com/owner/#{repo_name}")
+          dependency = GitDependency.new("https://example.com/owner/#{repo_name}.git")
 
           dependency.name.should eq("repo")
         end
 
         it "doesn't guess name from project name when specifying name" do
-          dependency = GitDependency.new("https://example.com/owner/#{repo_name}", "name")
+          dependency = GitDependency.new("https://example.com/owner/#{repo_name}.git", "name")
 
           dependency.name.should eq("name")
         end
       end
 
       it "gets the target_dir" do
-        dependency = GitDependency.new("https://example.com/owner/repo")
+        dependency = GitDependency.new("https://example.com/owner/repo.git")
         dependency.target_dir.should eq(".deps/owner-repo")
       end
     end
