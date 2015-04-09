2adc0dd85c417f0a70069e8b78c25d7affbf749f
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-04-08 21:46:36 -0300

    Use CC environment variable if available. Fixes #525

--- src/compiler/crystal/compiler.cr
+++ src/compiler/crystal/compiler.cr
@@ -11,6 +11,8 @@ module Crystal
     DataLayout32 = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32-n8:16:32"
     DataLayout64 = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64"
 
+    CC = ENV["CC"]? || "cc"
+
     record Source, filename, code
     record Result, program, node, original_node
 
@@ -180,7 +182,7 @@ module Crystal
 
       target_machine.emit_obj_to_file llvm_mod, o_name
 
-      puts "cc #{o_name} -o #{output_filename} #{@link_flags} #{lib_flags}"
+      puts "#{CC} #{o_name} -o #{output_filename} #{@link_flags} #{lib_flags}"
     end
 
     private def codegen(units, lib_flags, output_filename)
@@ -212,7 +214,7 @@ module Crystal
       end
 
       timing("Codegen (clang)") do
-        system "cc -o #{output_filename} #{object_names.join " "} #{@link_flags} #{lib_flags}"
+        system "#{CC} -o #{output_filename} #{object_names.join " "} #{@link_flags} #{lib_flags}"
       end
     end
 
