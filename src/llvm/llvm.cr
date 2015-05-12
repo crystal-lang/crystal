95432ca57c791181267e1bfb77ca104a63d36f6f
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2015-05-12 21:27:36 +0300

    Autodetect triple if the default triple is not compatible with the rootfs
    This allows to inject a statically built x86_64 Crystal compiler into
    a 32-bit x86 Linux rootfs and successfully use it there. The compiler
    will see that the x86_64 dynamic linker is not present in the system.
    And automatically override the triple configuration with the tuple
    string obtained by probing the (hopefully) installed GCC or Clang.
    
    It also makes porting Crystal to ARM, MIPS or other non-x86 systems
    much easier. Because the statically built x86_64 Crystal compiler
    binary can work nicely in the QEMU user chroot and also pick the
    right target triple automatically.
    
    More info:
      https://wiki.debian.org/Multiarch/Tuples
      https://wiki.debian.org/Multiarch/LibraryPathOverview

--- src/llvm/llvm.cr
+++ src/llvm/llvm.cr
@@ -65,9 +65,39 @@ module LLVM
     LibLLVM.is_multithreaded != 0
   end
 
+  private def self.default_target_triple_from_cc?
+    cc = ENV["CC"]? || "cc"
+    `#{cc} -v 2>&1`.split("\n").each do |line|
+      return $1.strip if line =~ /^Target: (.*)/
+    end
+    nil
+  end
+
   def self.default_target_triple
     chars = LibLLVM.get_default_target_triple
-    String.new(chars).tap { LibLLVM.dispose_message(chars) }
+    triple = String.new(chars).tap { LibLLVM.dispose_message(chars) }
+
+    # Check if there is a correct dynamic linker present for the target triple
+    # in the rootfs (see https://wiki.debian.org/Multiarch/LibraryPathOverview).
+    # If not, then we are probably running a static Crystal compiler binary
+    # (natively or emulated via QEMU & binfmt_misc) in an "alien" chroot and it
+    # makes sense to guess a better triple by probing the installed C compiler.
+    case triple
+    when /^x86_64\-.*\-linux\-gnu$/
+      unless File.exists?("/lib/ld-linux-x86-64.so.2")
+        triple = default_target_triple_from_cc? || triple
+      end
+    when /^i\d86\-.*\-linux\-gnu$/
+      unless File.exists?("/lib/ld-linux.so.2")
+        triple = default_target_triple_from_cc? || triple
+      end
+    when /^arm.*\-linux\-gnueabihf$/
+      unless File.exists?("/lib/ld-linux-armhf.so.3")
+        triple = default_target_triple_from_cc? || triple
+      end
+    end
+
+    triple
   end
 
   def self.to_io(chars, io)
