a92ac27b9179d8f42ef8f5bd2a0d77713355d597
phortx <benny@itws.de>
2015-05-25 19:35:39 +0200

    More time_format examples, beginning with 0

--- spec/std/time/time_format.cr
+++ spec/std/time/time_format.cr
@@ -6,12 +6,15 @@ describe TimeFormat do
       it "should print the week number" do
         time_format = TimeFormat.new("%W")
 
-        time_format.format(Time.new(2015,  5, 22)).should eq("21")
-        time_format.format(Time.new(2015,  3, 24)).should eq("13")
-        time_format.format(Time.new(1989, 10, 26)).should eq("43")
-        time_format.format(Time.new(2012,  4, 15)).should eq("16")
-        time_format.format(Time.new(2014,  9,  2)).should eq("36")
-        time_format.format(Time.new(2000,  1,  1)).should eq("1")
+        time_format.format(Time.new(2015,  5, 22)).should eq("20")
+        time_format.format(Time.new(2015,  3, 24)).should eq("12")
+        time_format.format(Time.new(1989, 10, 26)).should eq("42")
+        time_format.format(Time.new(2012,  4, 15)).should eq("14")
+        time_format.format(Time.new(2014,  9,  2)).should eq("35")
+        time_format.format(Time.new(1991, 11, 23)).should eq("46")
+        time_format.format(Time.new(2000,  1,  1)).should eq("51")
+        time_format.format(Time.new(2000, 12, 31)).should eq("51")
+        time_format.format(Time.new(2001, 12, 31)).should eq("0")
       end
     end
   end
