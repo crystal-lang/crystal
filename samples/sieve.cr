29e1680eb4dc001df4251fbebfa9346c16bfecfd
John Benediktsson <mrjbq7@gmail.com>
2015-06-07 10:54:37 -0700

    adding the sieve of eratosthenes example.

---
+++ samples/sieve.cr
@@ -0,0 +1,20 @@
+# Compute prime numbers up to 100 with the Sieve of Eratosthenes
+max = 100
+
+sieve = Array.new(max + 1, true)
+sieve[0] = false
+sieve[1] = false
+
+2.step(Math.sqrt(max)) do |i|
+  if sieve[i]
+    (i * i).step(max, i) do |j|
+      sieve[j] = false
+    end
+  end
+end
+
+sieve.each_with_index do |prime, number|
+  if prime
+    puts number
+  end
+end
