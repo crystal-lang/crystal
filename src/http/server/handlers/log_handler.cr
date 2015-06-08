782f4998213de973bcf237cb70f05bb938eb29d1
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-06-08 11:37:31 -0300

    HTTP::LogHandler: show elapsed time

--- src/http/server/handlers/log_handler.cr
+++ src/http/server/handlers/log_handler.cr
@@ -1,7 +1,24 @@
 class HTTP::LogHandler < HTTP::Handler
   def call(request)
-    call_next(request).tap do |response|
-      puts "#{request.method} #{request.path} - #{response.status_code}"
-    end
+    time = Time.now
+    response = call_next(request)
+    elapsed = Time.now - time
+    elapsed_text = elapsed_text(elapsed)
+
+    puts "#{request.method} #{request.path} - #{response.status_code} (#{elapsed_text})"
+    response
+  end
+
+  private def elapsed_text(elapsed)
+    minutes = elapsed.total_minutes
+    return "#{minutes.round(2)}m" if minutes >= 1
+
+    seconds = elapsed.total_seconds
+    return "#{seconds.round(2)}s" if seconds >= 1
+
+    millis = elapsed.total_milliseconds
+    return "#{millis.round(2)}ms" if millis >= 1
+
+    "#{(millis * 1000).round(2)}ns"
   end
 end
