6d6ca9d2f8e2ff72da2068f50fe54a9f59e0dc2e
Konstantin Makarchev <kostya27@gmail.com>
2015-06-19 22:03:11 +0400

    faster base64 encode

--- src/base64.cr
+++ src/base64.cr
@@ -84,19 +84,19 @@ module Base64
     bytes = chars.cstr
     len = data.length
     cstr = data.pointer(len)
-    i = 0
-    while i < len - len % 3
-      n = (cstr[i].to_u32 << 16) | (cstr[i + 1].to_u32 << 8) | (cstr[i + 2].to_u32)
-      yield bytes[(n >> 18) & 63]
-      yield bytes[(n >> 12) & 63]
-      yield bytes[(n >> 6) & 63]
-      yield bytes[(n) & 63]
-      i += 3
+    endcstr = cstr + len - len % 3
+    while cstr < endcstr
+      n = Intrinsics.bswap32((cstr as UInt32*).value)
+      yield bytes[(n >> 26) & 63]
+      yield bytes[(n >> 20) & 63]
+      yield bytes[(n >> 14) & 63]
+      yield bytes[(n >> 8) & 63]
+      cstr += 3
     end
 
     pd = len % 3
     if pd == 1
-      n = (cstr[i].to_u32 << 16)
+      n = (cstr.value.to_u32 << 16)
       yield bytes[(n >> 18) & 63]
       yield bytes[(n >> 12) & 63]
       if pad
@@ -104,7 +104,7 @@ module Base64
         yield PAD
       end
     elsif pd == 2
-      n = (cstr[i].to_u32 << 16) | (cstr[i + 1].to_u32 << 8)
+      n = (cstr.value.to_u32 << 16) | ((cstr + 1).value.to_u32 << 8)
       yield bytes[(n >> 18) & 63]
       yield bytes[(n >> 12) & 63]
       yield bytes[(n >> 6) & 63]
