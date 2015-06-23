0ffb3853186b2b1f7e98ab81b89d309b1321273e
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-06-23 10:59:31 -0300

    Finished documenting the Benchmark module

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -1,14 +1,8 @@
-# A Performance Benchmarking Library
-# Overview
-#
 # The Benchmark module provides methods for benchmarking Crystal code, giving
 # detailed reports on the time taken for each task.
 #
-# * Measure the time to construct the string given by the expression
-# ```
-# "a"*1_000_000_000
-# ```
-# 
+# ### Measure the time to construct the string given by the expression: `"a"*1_000_000_000`
+#
 # ```
 # require "benchmark"
 #
@@ -16,14 +10,16 @@
 # ```
 #
 # This generates the following output:
-# 
+#
+# ```text
 #  0.190000   0.220000   0.410000 (  0.420185)
-# 
+# ```
+#
 # This report shows the user CPU time, system CPU time, the sum of
 # the user and system CPU times, and the elapsed real time. The unit
 # of time is seconds.
 #
-# * Do some experiments sequentially using the #bm method:
+# ### Do some experiments sequentially using the `#bm` method:
 #
 # ```
 # require "benchmark"
@@ -34,53 +30,69 @@
 #  x.report("upto:") { 1.upto(n) do ; a = "1"; end }
 # end
 # ```
-# 
+#
 # The result:
 #
+# ```text
 #            user     system      total        real
 # times:   0.010000   0.000000   0.010000 (  0.008976)
 # upto:    0.010000   0.000000   0.010000 (  0.010466)
+# ```
 #
-# Note: By default the generated executables are not fully optimized. To turn optimizations on during your benchmarks, use the --release flag
-# $ crystal build some_program.cr --release
-# 
-
+# Make sure to always benchmark code by compiling with the `--release` flag.
 module Benchmark
   extend self
 
-  # :nodoc:
+  # A data object, representing the times associated with a benchmark measurement.
   class Tms
+    # User CPU time
     getter utime
+
+    # System CPU time
     getter stime
+
+    # User CPU time of children
     getter cutime
+
+    # System CPU time of children
     getter cstime
+
+    # Elapsed real time
     getter real
+
+    # The label associated with this measure
     getter label
 
+    # :nodoc:
     def initialize(@utime, @stime, @cutime, @cstime, @real, @label)
     end
 
+    # Total time, that is utime + stime + cutime + cstime
     def total
       utime + stime + cutime + cstime
     end
 
+    # Prints *utime*, *stime*, *total* and *real* to the given IO.
     def to_s(io : IO)
       io.printf "  %.6f   %.6f   %.6f (  %.6f)", utime, stime, total, real
     end
   end
 
-  # :nodoc:
-  class Report
+  # Yielded by `Benchmark#bm`, use `#report` to report benchmarks.
+  class Job
+    # :nodoc:
     def initialize
       @reports = [] of {String, ->}
       @label_width = 0
     end
 
+    # Reports a single benchmark unit.
     def report(label = " ", &block)
       @label_width = label.length if label.length > @label_width
       @reports << {label, block}
     end
 
+    # :nodoc:
     def execute
       if @label_width > 0
         print " " * @label_width
@@ -100,7 +112,8 @@ module Benchmark
     end
   end
 
-  def measure(label = "")
+  # Returns the time used to execute the given block.
+  def measure(label = "") : Tms
     t0, r0 = Process.times, Time.now
     yield
     t1, r1 = Process.times, Time.now
@@ -112,14 +125,21 @@ module Benchmark
                      label)
   end
 
-  def realtime
+  # Returns the elapsed real time used to execute the given block.
+  #
+  # ```
+  # Benchmark.realtime { "a" * 100_000 } #=> 00:00:00.0005840
+  # ```
+  def realtime : TimeSpan
     r0 = Time.now
     yield
     Time.now - r0
   end
 
+  # Main interface of the `Benchmark` module. Yields a `Job` to which
+  # one can report the benchmarks. See the module's description.
   def bm
-    report = Report.new
+    report = Job.new
     yield report
     report.execute
     report
