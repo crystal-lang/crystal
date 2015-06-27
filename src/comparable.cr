6518ec3ad0af7ccde136e3ce92067c438a1fd17a
yui-knk <spiketeika@gmail.com>
2015-06-28 01:12:27 +0900

    [ci skip] Add docs for Comparable (referenced Ruby doc)

--- src/comparable.cr
+++ src/comparable.cr
@@ -1,3 +1,8 @@
+# The Comparable mixin is used by classes whose objects may be ordered.
+#
+# Including types must provide an `<=>` method, which compares the receiver against
+# another object, returning -1, 0, or +1 depending on whether the receiver is less than,
+# equal to, or greater than the other object.
 module Comparable(T)
   def <(other : T)
     (self <=> other) < 0
