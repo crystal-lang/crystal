b6fea6a0a405fe71c81720c621df94e7e0665788
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-07-22 15:29:05 -0300

    refactor implementations spec with unicode chars to mark positions

--- spec/compiler/crystal/tools/implementations_spec.cr
+++ spec/compiler/crystal/tools/implementations_spec.cr
@@ -4,33 +4,91 @@ require "../../../../src/compiler/crystal/**"
 
 include Crystal
 
-def implementations(code)
+def assert_implementations(code)
+  cursor_location = nil
+  expected_locations = [] of Location
+
   code.lines.each_with_index do |line, line_number_0|
     if column_number = line.index('‸')
-      code = code.gsub('‸', "")
+      cursor_location = Location.new(line_number_0+1, column_number+1, ".")
+    end
 
-      compiler = Compiler.new
-      compiler.no_build = true
-      result = compiler.compile(Compiler::Source.new(".", code), "fake-no-build")
-
-      visitor = ImplementationsVisitor.new(Location.new(line_number_0+1, column_number, "."))
-      visitor.process(result)
-
-      return visitor.locations.map(&.to_s).sort
+    if column_number = line.index('༓')
+      expected_locations << Location.new(line_number_0+1, column_number+1, ".")
     end
   end
 
-  raise "no cursor found in spec"
+  code = code.gsub('‸', "").gsub('༓', "")
+
+  if cursor_location
+    compiler = Compiler.new
+    compiler.no_build = true
+    result = compiler.compile(Compiler::Source.new(".", code), "fake-no-build")
+
+    visitor = ImplementationsVisitor.new(cursor_location)
+    visitor.process(result)
+
+    visitor.locations.map(&.to_s).sort.should eq(expected_locations.map(&.to_s))
+  else
+    raise "no cursor found in spec"
+  end
 end
 
+# References
+#
+#   ༓ marks the expected implementations to be found
+#   ‸ marks the method call which implementations wants to be found
+#
 describe "implementations" do
   it "find top level method calls" do
-    implementations("
-def foo
-  1
-end
+    assert_implementations %(
+      ༓def foo
+        1
+      end
 
-puts f‸oo
-").should eq([".:2:1"])
+      puts f‸oo
+    )
+  end
+
+  it "find implementors of different classes" do
+    assert_implementations %(
+      class A
+        ༓def foo
+        end
+      end
+
+      class B
+        ༓def foo
+        end
+      end
+
+      def bar(o)
+        o.f‸oo
+      end
+
+      bar(A.new)
+      bar(B.new)
+    )
+  end
+
+  it "find implementors of classes that are only used" do
+    assert_implementations %(
+      class A
+        ༓def foo
+        end
+      end
+
+      class B
+        def foo
+        end
+      end
+
+      def bar(o)
+        o.f‸oo
+      end
+
+      bar(A.new)
+      B.new
+    )
   end
 end
