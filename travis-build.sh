c22571f56d9a44955ce0ac37901b33f63207aba5
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-08-06 16:15:27 -0300

    use travis to keep generated docs up to date

---
+++ travis-build.sh
@@ -0,0 +1,14 @@
+rev=$(git rev-parse --short HEAD)
+
+rake docs docs:tidy
+
+git config --global user.email "bcardiff@manas.com.ar"
+git config --global user.name "Travis on behalf Brian J. Cardiff"
+
+git remote add upstream "https://$GH_TOKEN@github.com/manastech/crystal"
+git fetch upstream
+git reset upstream/gh-pages
+
+git add -A .
+git commit -m "rebuild docs at ${rev}"
+git push -q upstream HEAD:gh-pages
