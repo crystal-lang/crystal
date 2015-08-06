bf301be96faa1744595a3bca5048dd254b4240be
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-08-06 17:07:48 -0300

    ignore vendor/bundle (for travis) include username in remote to check if travis is able to commit

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ rake docs docs:tidy
 git config --global user.email "bcardiff@manas.com.ar"
 git config --global user.name "Travis on behalf Brian J. Cardiff"
 
-git remote add upstream "https://$GH_TOKEN@github.com/manastech/crystal"
+git remote add upstream "https://bcardiff:$GH_TOKEN@github.com/manastech/crystal"
 git fetch upstream
 git reset upstream/gh-pages
 
