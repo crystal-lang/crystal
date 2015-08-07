74b031bc63bbc39b7b867e5685a8a0175022032e
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-08-07 11:07:34 -0300

    Signal numbers are different in Darwin and Linux (fixes #1114)

--- src/signal.cr
+++ src/signal.cr
@@ -2,42 +2,84 @@ lib LibC
   fun signal(sig : Int32, handler : Int32 ->)
 end
 
-enum Signal
-  EXIT   =  0
-  HUP    =  1
-  INT    =  2
-  QUIT   =  3
-  ILL    =  4
-  TRAP   =  5
-  IOT    =  6
-  ABRT   =  6
-  EMT    =  7
-  FPE    =  8
-  KILL   =  9
-  BUS    = 10
-  SEGV   = 11
-  SYS    = 12
-  PIPE   = 13
-  ALRM   = 15
-  TERM   = 15
-  URG    = 16
-  STOP   = 17
-  TSTP   = 18
-  CONT   = 19
-  CHLD   = 20
-  CLD    = 20
-  TTIN   = 21
-  TTOU   = 22
-  IO     = 23
-  XCPU   = 24
-  XFSZ   = 25
-  VTLARM = 26
-  PROF   = 27
-  WINCH  = 28
-  INFO   = 29
-  USR1   = 30
-  USR2   = 31
 
+ifdef darwin
+  enum Signal
+    HUP    =  1
+    INT    =  2
+    QUIT   =  3
+    ILL    =  4
+    TRAP   =  5
+    IOT    =  6
+    ABRT   =  6
+    EMT    =  7
+    FPE    =  8
+    KILL   =  9
+    BUS    = 10
+    SEGV   = 11
+    SYS    = 12
+    PIPE   = 13
+    ALRM   = 15
+    TERM   = 15
+    URG    = 16
+    STOP   = 17
+    TSTP   = 18
+    CONT   = 19
+    CHLD   = 20
+    CLD    = 20
+    TTIN   = 21
+    TTOU   = 22
+    IO     = 23
+    XCPU   = 24
+    XFSZ   = 25
+    VTALRM = 26
+    PROF   = 27
+    WINCH  = 28
+    INFO   = 29
+    USR1   = 30
+    USR2   = 31
+  end
+else
+  enum Signal
+    HUP    = 1
+    INT    = 2
+    QUIT   = 3
+    ILL    = 4
+    TRAP   = 5
+    ABRT   = 6
+    IOT    = 6
+    BUS    = 7
+    FPE    = 8
+    KILL   = 9
+    USR1   = 10
+    SEGV   = 11
+    USR2   = 12
+    PIPE   = 13
+    ALRM   = 14
+    TERM   = 15
+    STKFLT = 16
+    CLD    = 17
+    CHLD   = 17
+    CONT   = 18
+    STOP   = 19
+    TSTP   = 20
+    TTIN   = 21
+    TTOU   = 22
+    URG    = 23
+    XCPU   = 24
+    XFSZ   = 25
+    VTALRM = 26
+    PROF   = 27
+    WINCH  = 28
+    POLL   = 29
+    IO     = 29
+    PWR    = 30
+    SYS    = 31
+    UNUSED = 31
+  end
+end
+
+enum Signal
   def trap(block : Int32 ->)
     trap &block
   end
