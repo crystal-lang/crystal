5f825a4005caf8b74fe198926bec5133ed9b9218
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-08-09 21:37:56 -0300

    Simplified types_to_s spec by using `with ... yield`

--- spec/compiler/crystal/types_spec.cr
+++ spec/compiler/crystal/types_spec.cr
@@ -1,55 +1,42 @@
 require "../../spec_helper"
 
 def assert_type_to_s(expected)
-  t = yield Program.new
+  p = Program.new
+  t = with p yield p
   t.to_s.should eq(expected)
 end
 
 describe "types to_s of" do
   it "array of simple types" do
-    assert_type_to_s "Array(Int32)" do |p|
-      p.array_of(p.int32)
-    end
+    assert_type_to_s "Array(Int32)" { array_of(int32) }
   end
 
   it "array of simple types" do
-    assert_type_to_s "(String | Int32)" do |p|
-      p.union_of(p.string, p.int32)
-    end
+    assert_type_to_s "(String | Int32)" { union_of(string, int32) }
   end
 
   describe "union types" do
     describe "should not have extra parens" do
       it "in arrays" do
-        assert_type_to_s "Array(String | Int32)" do |p|
-          p.array_of(p.union_of(p.string, p.int32))
-        end
+        assert_type_to_s "Array(String | Int32)" { array_of(union_of(string, int32)) }
       end
 
       it "in pointers" do
-        assert_type_to_s "Pointer(String | Int32)" do |p|
-          p.pointer_of(p.union_of(p.string, p.int32))
-        end
+        assert_type_to_s "Pointer(String | Int32)" { pointer_of(union_of(string, int32)) }
       end
 
       it "in tuples" do
-        assert_type_to_s "{String, String | Int32}" do |p|
-          p.tuple_of([p.string, p.union_of(p.string, p.int32)])
-        end
+        assert_type_to_s "{String, String | Int32}" { tuple_of [string, union_of(string, int32)]  }
       end
     end
 
     describe "should have parens" do
       it "as return type" do
-        assert_type_to_s "( -> (String | Int32))" do |p|
-          p.fun_of([p.union_of(p.string, p.int32)])
-        end
+        assert_type_to_s "( -> (String | Int32))" { fun_of union_of(string, int32) }
       end
 
       it "as arg type" do
-        assert_type_to_s "((String | Int32) -> Int32)" do |p|
-          p.fun_of([p.union_of(p.string, p.int32), p.int32])
-        end
+        assert_type_to_s "((String | Int32) -> Int32)" { fun_of union_of(string, int32), int32 }
       end
     end
   end
