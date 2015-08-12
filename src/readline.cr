4285f8fcbf46322582bcd4e8fd75521d0a36d6d7
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-08-12 20:05:57 -0300

    Fixed 1162: fixes readline autocomplete to show more than one completion

--- src/readline.cr
+++ src/readline.cr
@@ -10,6 +10,13 @@ lib LibReadline
   $rl_point : Int32
 end
 
+private def malloc_match(match)
+  match_ptr = LibC.malloc(LibC::SizeT.cast(match.bytesize) + 1) as UInt8*
+  match_ptr.copy_from(match.to_unsafe, match.bytesize)
+  match_ptr[match.bytesize] = 0_u8
+  match_ptr
+end
+
 module Readline
   extend self
 
@@ -39,6 +46,33 @@ module Readline
     LibReadline.rl_point
   end
 
+  # :nodoc:
+  def common_prefix_bytesize(str1 : String, str2 : String)
+    r1 = CharReader.new str1
+    r2 = CharReader.new str2
+
+    while r1.has_next? && r2.has_next?
+      break if r1.current_char != r2.current_char
+
+      r1.next_char
+      r2.next_char
+    end
+
+    r1.pos
+  end
+
+  # :nodoc :
+  def common_prefix_bytesize(strings : Array)
+    str1 = strings[0]
+    low = str1.bytesize
+    1.upto(strings.length - 1).each do |i|
+      str2 = strings[i]
+      low2 = common_prefix_bytesize(str1, str2)
+      low = low2 if low2 < low
+    end
+    low
+  end
+
   LibReadline.rl_attempted_completion_function = ->(text_ptr, start, finish) {
     completion_proc = @@completion_proc
     return Pointer(UInt8*).null unless completion_proc
@@ -49,14 +83,23 @@ module Readline
     return Pointer(UInt8*).null unless matches
     return Pointer(UInt8*).null if matches.empty?
 
-    result = LibC.malloc(LibC::SizeT.cast(sizeof(UInt8*)) * (matches.length + 1)) as UInt8**
+    # We *must* to create the results using malloc (readline later frees that).
+    # We create an extra result for the first element.
+    result = LibC.malloc(LibC::SizeT.cast(sizeof(UInt8*)) * (matches.length + 2)) as UInt8**
     matches.each_with_index do |match, i|
-      match_ptr = LibC.malloc(LibC::SizeT.cast(match.bytesize) + 1) as UInt8*
-      match_ptr.copy_from(match.to_unsafe, match.bytesize)
-      match_ptr[match.bytesize] = 0_u8
-      result[i] = match_ptr
+      result[i + 1] = malloc_match(match)
     end
-    result[matches.length] = Pointer(UInt8).null
+    result[matches.length + 1] = Pointer(UInt8).null
+
+    # The first element is the completion if it's oe
+    if matches.length == 1
+      result[0] = malloc_match(matches[0])
+    else
+      # Otherwise, we compute the common prefix of all matches
+      low = Readline.common_prefix_bytesize(matches)
+      sub = matches[0].byte_slice(0, low)
+      result[0] = malloc_match(sub)
+    end
     result
   }
 end
