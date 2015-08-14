7c3239ee505e07544ec372839efed527801d210a
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-08-14 20:29:09 -0300

    Removed the `alias_method` macro

--- src/object.cr
+++ src/object.cr
@@ -547,31 +547,4 @@ class Object
       {{delegate}}.\{{name.id}}(\{{*args}}) \{{block}}
     end
   end
-
-  # Defines new_method as an alias of old_method.
-  #
-  # This creates a new method new_method that invokes old_method.
-  #
-  # Note that due to current language limitations this is only useful
-  # when neither named arguments nor blocks are involved.
-  #
-  # ```
-  # class Person
-  #   getter name
-  #
-  #   def initialize(@name)
-  #   end
-  #
-  #   alias_method full_name, name
-  # end
-  #
-  # person = Person.new "John"
-  # person.name #=> "John"
-  # person.full_name #=> "John"
-  # ```
-  macro alias_method(new_method, old_method)
-    def {{new_method.id}}(*args)
-      {{old_method.id}}(*args)
-    end
-  end
 end
