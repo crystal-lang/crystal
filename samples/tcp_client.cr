53efdab7934d01748f6ab9b61ffb0c2a3cac4ede
Konstantin Makarchev <kostya27@gmail.com>
2015-08-19 21:24:17 +0400

    fix

--- samples/tcp_client.cr
+++ samples/tcp_client.cr
@@ -5,6 +5,6 @@ require "socket"
 socket = TCPSocket.new "127.0.0.1", 9000
 10.times do |i|
   socket.puts "#{i}"
-  puts "server responce #{socket.gets}"
+  puts "server response #{socket.gets}"
   sleep 0.5
 end
