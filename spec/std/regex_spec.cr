b6f9372c745b1f57c9fea79dd63421ad34ea9b6b
Ben Miller <bjmllr@gmail.com>
2015-08-22 23:10:41 -0400

    regex: sort specs

--- spec/std/regex_spec.cr
+++ spec/std/regex_spec.cr
@@ -1,6 +1,48 @@
 require "spec"
 
 describe "Regex" do
+  it "compare to other instances" do
+    Regex.new("foo").should eq(Regex.new("foo"))
+    Regex.new("foo").should_not eq(Regex.new("bar"))
+  end
+
+  it "does =~" do
+    (/foo/ =~ "bar foo baz").should eq(4)
+    $~.length.should eq(0)
+  end
+
+  it "does inspect" do
+    /foo/.inspect.should eq("/foo/")
+  end
+
+  it "does to_s" do
+    /foo/.to_s.should eq("/foo/")
+    /foo/imx.to_s.should eq("/foo/imx")
+  end
+
+  it "doesn't crash when PCRE tries to free some memory (#771)" do
+    expect_raises(ArgumentError) { Regex.new("foo)") }
+  end
+
+  it "escapes" do
+    Regex.escape(" .\\+*?[^]$(){}=!<>|:-hello").should eq("\\ \\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-hello")
+  end
+
+  it "matches ignore case" do
+    ("HeLlO" =~ /hello/).should be_nil
+    ("HeLlO" =~ /hello/i).should eq(0)
+  end
+
+  it "matches lines beginnings on ^ in multiline mode" do
+    ("foo\nbar" =~ /^bar/).should be_nil
+    ("foo\nbar" =~ /^bar/m).should eq(4)
+  end
+
+  it "matches multiline" do
+    ("foo\n<bar\n>baz" =~ /<bar.*?>/).should be_nil
+    ("foo\n<bar\n>baz" =~ /<bar.*?>/m).should eq(4)
+  end
+
   it "matches with =~ and captures" do
     ("fooba" =~ /f(o+)(bar?)/).should eq(0)
     $~.length.should eq(2)
@@ -21,16 +63,6 @@ describe "Regex" do
     $2.should eq("ba")
   end
 
-  it "does =~" do
-    (/foo/ =~ "bar foo baz").should eq(4)
-    $~.length.should eq(0)
-  end
-
-  it "raises if outside match range with []" do
-    "foo" =~ /foo/
-    expect_raises(IndexError) { $1 }
-  end
-
   describe "name_table" do
     it "is a map of capture group number to name" do
       table = (/(?<date> (?<year>(\d\d)?\d\d) - (?<month>\d\d) - (?<day>\d\d) )/x).name_table
@@ -42,44 +74,12 @@ describe "Regex" do
     end
   end
 
-  it "matches multiline" do
-    ("foo\n<bar\n>baz" =~ /<bar.*?>/).should be_nil
-    ("foo\n<bar\n>baz" =~ /<bar.*?>/m).should eq(4)
-  end
-
-  it "matches ignore case" do
-    ("HeLlO" =~ /hello/).should be_nil
-    ("HeLlO" =~ /hello/i).should eq(0)
-  end
-
-  it "does to_s" do
-    /foo/.to_s.should eq("/foo/")
-    /foo/imx.to_s.should eq("/foo/imx")
-  end
-
-  it "does inspect" do
-    /foo/.inspect.should eq("/foo/")
-  end
-
   it "raises exception with invalid regex" do
     expect_raises(ArgumentError) { Regex.new("+") }
   end
 
-  it "escapes" do
-    Regex.escape(" .\\+*?[^]$(){}=!<>|:-hello").should eq("\\ \\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-hello")
-  end
-
-  it "doesn't crash when PCRE tries to free some memory (#771)" do
-    expect_raises(ArgumentError) { Regex.new("foo)") }
-  end
-
-  it "compare to other instances" do
-    Regex.new("foo").should eq(Regex.new("foo"))
-    Regex.new("foo").should_not eq(Regex.new("bar"))
-  end
-
-  it "matches lines beginnings on ^ in multiline mode" do
-    ("foo\nbar" =~ /^bar/).should be_nil
-    ("foo\nbar" =~ /^bar/m).should eq(4)
+  it "raises if outside match range with []" do
+    "foo" =~ /foo/
+    expect_raises(IndexError) { $1 }
   end
 end
