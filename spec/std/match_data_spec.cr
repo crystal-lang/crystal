673f84b09179704cde043940e9f9589d4f7405f9
Ben Miller <bjmllr@gmail.com>
2015-08-22 23:07:08 -0400

    MatchData: move specs to separate file from Regex

---
+++ spec/std/match_data_spec.cr
@@ -0,0 +1,103 @@
+require "spec"
+
+describe "MatchData" do
+  it "does inspect" do
+    /f(o)(x)/.match("the fox").inspect.should eq(%(#<MatchData "fox" 1:"o" 2:"x">))
+    /f(o)(x)?/.match("the fort").inspect.should eq(%(#<MatchData "fo" 1:"o" 2:nil>))
+    /fox/.match("the fox").inspect.should eq(%(#<MatchData "fox">))
+  end
+
+  it "does to_s" do
+    /f(o)(x)/.match("the fox").to_s.should eq(%(#<MatchData "fox" 1:"o" 2:"x">))
+    /f(?<lettero>o)(?<letterx>x)/.match("the fox").to_s.should eq(%(#<MatchData "fox" lettero:"o" letterx:"x">))
+    /fox/.match("the fox").to_s.should eq(%(#<MatchData "fox">))
+  end
+
+  describe "#[]" do
+    it "raises if outside match range with []" do
+      "foo" =~ /foo/
+      expect_raises(IndexError) { $~[1] }
+    end
+
+    it "capture named group" do
+      ("fooba" =~ /f(?<g1>o+)(?<g2>bar?)/).should eq(0)
+      $~["g1"].should eq("oo")
+      $~["g2"].should eq("ba")
+    end
+
+    it "captures empty group" do
+      ("foo" =~ /(?<g1>z?)foo/).should eq(0)
+      $~[1].should eq("")
+      $~["g1"].should eq("")
+    end
+
+    it "raises exception on optional empty group" do
+      ("foo" =~ /(?<g1>z)?foo/).should eq(0)
+      expect_raises(Exception) { $~[1] }
+      expect_raises(Exception) { $~["g1"] }
+    end
+
+    it "raises exception when named group doesn't exist" do
+      ("foo" =~ /foo/).should eq(0)
+      expect_raises(ArgumentError) { $~["group"] }
+    end
+  end
+
+  describe "#[]?" do
+    it "returns nil if outside match range with []" do
+      "foo" =~ /foo/
+      $~[1]?.should be_nil
+    end
+
+    it "capture named group" do
+      ("fooba" =~ /f(?<g1>o+)(?<g2>bar?)/).should eq(0)
+      $~["g1"]?.should eq("oo")
+      $~["g2"]?.should eq("ba")
+    end
+
+    it "capture empty group" do
+      ("foo" =~ /(?<g1>z?)foo/).should eq(0)
+      $~[1]?.should eq("")
+      $~["g1"]?.should eq("")
+    end
+
+    it "capture optional empty group" do
+      ("foo" =~ /(?<g1>z)?foo/).should eq(0)
+      $~[1]?.should be_nil
+      $~["g1"]?.should be_nil
+    end
+
+    it "returns nil exception when named group doesn't exist" do
+      ("foo" =~ /foo/).should eq(0)
+      $~["group"]?.should be_nil
+    end
+  end
+
+  describe "#pre_match" do
+    it "returns the part of the string before the match" do
+      "Crystal".match(/yst/) { |md| md.pre_match.should eq "Cr" }
+    end
+
+    it "returns an empty string when there's nothing before" do
+      "Crystal".match(/Cryst/) { |md| md.pre_match.should eq "" }
+    end
+
+    it "works with unicode" do
+      "há日本語".match(/本/) { |md| md.pre_match.should eq "há日" }
+    end
+  end
+
+  describe "#post_match" do
+    it "returns the part of the string after the match" do
+      "Crystal".match(/yst/) { |md| md.post_match.should eq "al" }
+    end
+
+    it "returns an empty string when there's nothing after" do
+      "Crystal".match(/ystal/) { |md| md.post_match.should eq "" }
+    end
+
+    it "works with unicode" do
+      "há日本語".match(/本/) { |md| md.post_match.should eq "語" }
+    end
+  end
+end
--- spec/std/regex_spec.cr
+++ spec/std/regex_spec.cr
@@ -42,94 +42,6 @@ describe "Regex" do
     end
   end
 
-  describe "MatchData#[]" do
-    it "raises if outside match range with []" do
-      "foo" =~ /foo/
-      expect_raises(IndexError) { $~[1] }
-    end
-
-    it "capture named group" do
-      ("fooba" =~ /f(?<g1>o+)(?<g2>bar?)/).should eq(0)
-      $~["g1"].should eq("oo")
-      $~["g2"].should eq("ba")
-    end
-
-    it "captures empty group" do
-      ("foo" =~ /(?<g1>z?)foo/).should eq(0)
-      $~[1].should eq("")
-      $~["g1"].should eq("")
-    end
-
-    it "raises exception on optional empty group" do
-      ("foo" =~ /(?<g1>z)?foo/).should eq(0)
-      expect_raises(Exception) { $~[1] }
-      expect_raises(Exception) { $~["g1"] }
-    end
-
-    it "raises exception when named group doesn't exist" do
-      ("foo" =~ /foo/).should eq(0)
-      expect_raises(ArgumentError) { $~["group"] }
-    end
-  end
-
-  describe "MatchData#[]?" do
-    it "returns nil if outside match range with []" do
-      "foo" =~ /foo/
-      $~[1]?.should be_nil
-    end
-
-    it "capture named group" do
-      ("fooba" =~ /f(?<g1>o+)(?<g2>bar?)/).should eq(0)
-      $~["g1"]?.should eq("oo")
-      $~["g2"]?.should eq("ba")
-    end
-
-    it "capture empty group" do
-      ("foo" =~ /(?<g1>z?)foo/).should eq(0)
-      $~[1]?.should eq("")
-      $~["g1"]?.should eq("")
-    end
-
-    it "capture optional empty group" do
-      ("foo" =~ /(?<g1>z)?foo/).should eq(0)
-      $~[1]?.should be_nil
-      $~["g1"]?.should be_nil
-    end
-
-    it "returns nil exception when named group doesn't exist" do
-      ("foo" =~ /foo/).should eq(0)
-      $~["group"]?.should be_nil
-    end
-  end
-
-  describe "MatchData#pre_match" do
-    it "returns the part of the string before the match" do
-      "Crystal".match(/yst/) { |md| md.pre_match.should eq "Cr" }
-    end
-
-    it "returns an empty string when there's nothing before" do
-      "Crystal".match(/Cryst/) { |md| md.pre_match.should eq "" }
-    end
-
-    it "works with unicode" do
-      "há日本語".match(/本/) { |md| md.pre_match.should eq "há日" }
-    end
-  end
-
-  describe "MatchData#post_match" do
-    it "returns the part of the string after the match" do
-      "Crystal".match(/yst/) { |md| md.post_match.should eq "al" }
-    end
-
-    it "returns an empty string when there's nothing after" do
-      "Crystal".match(/ystal/) { |md| md.post_match.should eq "" }
-    end
-
-    it "works with unicode" do
-      "há日本語".match(/本/) { |md| md.post_match.should eq "語" }
-    end
-  end
-
   it "matches multiline" do
     ("foo\n<bar\n>baz" =~ /<bar.*?>/).should be_nil
     ("foo\n<bar\n>baz" =~ /<bar.*?>/m).should eq(4)
@@ -143,13 +55,6 @@ describe "Regex" do
   it "does to_s" do
     /foo/.to_s.should eq("/foo/")
     /foo/imx.to_s.should eq("/foo/imx")
-
-    /f(o)(x)/.match("the fox").to_s.should eq(%(#<MatchData "fox" 1:"o" 2:"x">))
-    /f(?<lettero>o)(?<letterx>x)/.match("the fox").to_s.should eq(%(#<MatchData "fox" lettero:"o" letterx:"x">))
-    /fox/.match("the fox").to_s.should eq(%(#<MatchData "fox">))
-    /f(o)(x)/.match("the fox").inspect.should eq(%(#<MatchData "fox" 1:"o" 2:"x">))
-    /f(o)(x)?/.match("the fort").inspect.should eq(%(#<MatchData "fo" 1:"o" 2:nil>))
-    /fox/.match("the fox").inspect.should eq(%(#<MatchData "fox">))
   end
 
   it "does inspect" do
