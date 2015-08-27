e9e8fd902691adf1f06f131194639cc7fccd4fce
Konstantin Makarchev <kostya27@gmail.com>
2015-08-27 22:21:18 +0300

    char blank

--- src/char.cr
+++ src/char.cr
@@ -183,6 +183,17 @@ struct Char
     digit? 16
   end
 
+  def blank?
+    case ord
+      when 9, 0xa, 0xb, 0xc, 0xd, 0x20, 0x85, 0xa0, 0x1680, 0x180e,
+          0x2000, 0x2001, 0x2002, 0x2003, 0x2004, 0x2005, 0x2006,
+          0x2007, 0x2008, 0x2009, 0x200a, 0x2028, 0x2029, 0x202f,
+          0x205f, 0x3000 then true
+    else
+      false
+    end
+  end
+
   # Returns `true` if this char is matched by the given *sets*.
   #
   # Each parameter defines a set, the character is matched against
