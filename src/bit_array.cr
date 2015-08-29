fd073c375c61f2010111febd40b34f0eaa091e98
tbpgr <tbpgr@tbpgr.jp>
2015-08-30 06:55:40 +0900

    Fix typo [ci skip]

--- src/bit_array.cr
+++ src/bit_array.cr
@@ -11,7 +11,7 @@
 #     require "bit_array"
 #     ba = BitArray.new(12) # => "BitArray[000000000000]"
 #     ba[2]                 # => false
-#     0.upto(5) { |i| a[i*2] = true }
+#     0.upto(5) { |i| ba[i*2] = true }
 #     ba                    # => "BitArray[101010101010]"
 #     ba[2]                 # => true
 struct BitArray
