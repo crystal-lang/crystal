705c951c295da39c6a7e45c41e52f22e43d94557
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-09-07 16:13:32 -0300

    Simpler code for previous commit

--- src/compiler/crystal/semantic/call_error.cr
+++ src/compiler/crystal/semantic/call_error.cr
@@ -226,28 +226,25 @@ class Crystal::Call
   end
 
   def check_abstract_def_error(owner, matches, defs, def_name)
-    if !matches || (matches.try &.empty?)
-      signature = CallSignature.new(def_name, args.map(&.type), block, named_args)
-      if defs.all? &.abstract
-        defs.each do |a_def|
-          if a_def.abstract
-            context = MatchContext.new(owner, a_def.owner)
-            match = MatchesLookup.match_def(signature, DefWithMetadata.new(a_def), context)
-            if match
-              if a_def.owner == owner
-                owner.all_subclasses.each do |subclass|
-                  submatches = subclass.lookup_matches(signature)
-                  if submatches.empty?
-                    raise "abstract `def #{def_full_name(a_def.owner, a_def)}` must be implemented by #{subclass}"
-                  end
-                end
-                raise "abstract `def #{def_full_name(a_def.owner, a_def)}` must be implemented by #{owner}"
-              else
-                raise "abstract `def #{def_full_name(a_def.owner, a_def)}` must be implemented by #{owner}"
-              end
-            end
+    return unless !matches || (matches.try &.empty?)
+    return unless defs.all? &.abstract
+
+    signature = CallSignature.new(def_name, args.map(&.type), block, named_args)
+    defs.each do |a_def|
+      context = MatchContext.new(owner, a_def.owner)
+      match = MatchesLookup.match_def(signature, DefWithMetadata.new(a_def), context)
+      next unless match
+
+      if a_def.owner == owner
+        owner.all_subclasses.each do |subclass|
+          submatches = subclass.lookup_matches(signature)
+          if submatches.empty?
+            raise "abstract `def #{def_full_name(a_def.owner, a_def)}` must be implemented by #{subclass}"
           end
         end
+        raise "abstract `def #{def_full_name(a_def.owner, a_def)}` must be implemented by #{owner}"
+      else
+        raise "abstract `def #{def_full_name(a_def.owner, a_def)}` must be implemented by #{owner}"
       end
     end
   end
