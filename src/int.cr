4ddc1156f367cdeb4f3a1480d07dc1f63ec4cc19
Piotr Szotkowski <chastell@chastell.net>
2015-09-20 00:30:49 +0200

    Document Int#bit

--- src/int.cr
+++ src/int.cr
@@ -171,6 +171,15 @@ struct Int
     self === char.ord
   end
 
+  # Returns this number's *bit*th bit, starting with the least-significant.
+  #
+  # ```
+  # 11.bit(0) #=> 1
+  # 11.bit(1) #=> 1
+  # 11.bit(2) #=> 0
+  # 11.bit(3) #=> 1
+  # 11.bit(4) #=> 0
+  # ```
   def bit(bit)
     self >> bit & 1
   end
