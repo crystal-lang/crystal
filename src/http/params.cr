9a3ee4e4a2979bab7fd3a19e4f39060a59955405
Oleksii Fedorov <waterlink000@gmail.com>
2015-09-23 14:44:16 +0200

    Add HTTP::Params class - convenient wrapper over Hash(String, Array(String)) http params

--- src/http/params.cr
+++ src/http/params.cr
@@ -1,8 +1,8 @@
 require "uri"
 
 module HTTP
-  module Params
-    # Parses an HTTP query string into a `Hash(String, Array(String))`
+  class Params
+    # Parses an HTTP query string into a `HTTP::Params`
     #
     #     HTTP::Params.parse("foo=bar&foo=baz&qux=zoo") #=> {"foo" => ["bar", "baz"], "qux" => ["zoo"]}
     def self.parse(query : String)
@@ -11,7 +11,7 @@ module HTTP
         ary = parsed[key] ||= [] of String
         ary.push value
       end
-      parsed
+      Params.new(parsed)
     end
 
     # Parses an HTTP query and yields each key-value pair
@@ -78,6 +78,96 @@ module HTTP
       form_builder.to_s
     end
 
+    protected getter raw_params
+    def initialize(@raw_params)
+    end
+
+    def ==(other : self)
+      self.raw_params == other.raw_params
+    end
+
+    def ==(other)
+      false
+    end
+
+    # Returns first value for specified param name.
+    def [](name)
+      raw_params[name].first
+    end
+
+    # Sets first value for specified param name.
+    def []=(name, value)
+      raw_params[name] ||= [""]
+      raw_params[name][0] = value
+    end
+
+    # Returns all values for specified param name.
+    def fetch_all(name)
+      raw_params.fetch(name) { [] of String }
+    end
+
+    # Returns first value for specified param name.
+    def fetch(name)
+      raw_params.fetch(name).first
+    end
+
+    # Returns first value for specified param name. Fallbacks to provided
+    # default value when there is no such param.
+    def fetch(name, default)
+      raw_params.fetch(name, [default]).first
+    end
+
+    # Returns first value for specified param name. Fallbacks to return value
+    # of provided block.
+    def fetch(name, &block : -> String)
+      raw_params.fetch(name) { [block.call] }.first
+    end
+
+    # Appends new value for specified param name. Creates param when there was
+    # no such param.
+    def add(name, value)
+      raw_params[name] ||= [] of String
+      raw_params[name] = [] of String if raw_params[name] == [""]
+      raw_params[name] << value
+    end
+
+    # Sets all values for specified param name at once.
+    def set_all(name, values)
+      raw_params[name] = values
+    end
+
+    # Allows to iterate over all name-value pairs.
+    def each
+      raw_params.each do |name, values|
+        values.each do |value|
+          yield(name, value)
+        end
+      end
+    end
+
+    # Deletes first value for provided param name. If there are no values left,
+    # deletes param itself. Returns deleted value.
+    def delete(name)
+      value = raw_params[name].shift
+      raw_params.delete(name) if raw_params[name].size == 0
+      value
+    end
+
+    # Deletes all values for provided param name. Returns array of deleted
+    # values.
+    def delete_all(name)
+      raw_params.delete(name)
+    end
+
+    # Serializes to string representation as http url encoded form
+    def to_s(io)
+      io << HTTP::Params.build do |builder|
+        each do |name, value|
+          builder.add(name, value)
+        end
+      end
+    end
+
     # :nodoc:
     struct Builder
       def initialize
@@ -92,8 +182,8 @@ module HTTP
         self
       end
 
-      def to_s
-        @string.to_s
+      def to_s(io)
+        io << @string.to_s
       end
     end
   end
