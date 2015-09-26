9409279779b6b54bc662cca28f06298793870424
Joakim Reinert <mail@jreinert.com>
2015-09-26 20:05:47 +0200

    updated byte_format specs to use renamed write method

--- spec/std/io/byte_format_spec.cr
+++ spec/std/io/byte_format_spec.cr
@@ -49,43 +49,43 @@ describe IO::ByteFormat do
     describe "encode" do
       it "writes int8" do
         io = StringIO.new
-        io.write_bytes 0x12_i8, IO::ByteFormat::LittleEndian
+        io.write_object 0x12_i8, IO::ByteFormat::LittleEndian
         assert_bytes io, 0x12
       end
 
       it "writes int16" do
         io = StringIO.new
-        io.write_bytes 0x1234_i16, IO::ByteFormat::LittleEndian
+        io.write_object 0x1234_i16, IO::ByteFormat::LittleEndian
         assert_bytes io, 0x34, 0x12
       end
 
       it "writes uint16" do
         io = StringIO.new
-        io.write_bytes 0x1234_u16, IO::ByteFormat::LittleEndian
+        io.write_object 0x1234_u16, IO::ByteFormat::LittleEndian
         assert_bytes io, 0x34, 0x12
       end
 
       it "writes int32" do
         io = StringIO.new
-        io.write_bytes 0x12345678_i32, IO::ByteFormat::LittleEndian
+        io.write_object 0x12345678_i32, IO::ByteFormat::LittleEndian
         assert_bytes io, 0x78, 0x56, 0x34, 0x12
       end
 
       it "writes int64" do
         io = StringIO.new
-        io.write_bytes 0x123456789ABCDEF0_i64, IO::ByteFormat::LittleEndian
+        io.write_object 0x123456789ABCDEF0_i64, IO::ByteFormat::LittleEndian
         assert_bytes io, 0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12
       end
 
       it "writes float32" do
         io = StringIO.new
-        io.write_bytes 1.234_f32, IO::ByteFormat::LittleEndian
+        io.write_object 1.234_f32, IO::ByteFormat::LittleEndian
         assert_bytes io, 0xB6, 0xF3, 0x9D, 0x3F
       end
 
       it "writes float64" do
         io = StringIO.new
-        io.write_bytes 1.234, IO::ByteFormat::LittleEndian
+        io.write_object 1.234, IO::ByteFormat::LittleEndian
         assert_bytes io, 0x58, 0x39, 0xB4, 0xC8, 0x76, 0xBE, 0xF3, 0x3F
       end
     end
@@ -139,37 +139,37 @@ describe IO::ByteFormat do
     describe "encode" do
       it "writes int8" do
         io = StringIO.new
-        io.write_bytes 0x12_i8, IO::ByteFormat::BigEndian
+        io.write_object 0x12_i8, IO::ByteFormat::BigEndian
         assert_bytes io, 0x12
       end
 
       it "writes int16" do
         io = StringIO.new
-        io.write_bytes 0x1234_i16, IO::ByteFormat::BigEndian
+        io.write_object 0x1234_i16, IO::ByteFormat::BigEndian
         assert_bytes_reversed io, 0x34, 0x12
       end
 
       it "writes int32" do
         io = StringIO.new
-        io.write_bytes 0x12345678_i32, IO::ByteFormat::BigEndian
+        io.write_object 0x12345678_i32, IO::ByteFormat::BigEndian
         assert_bytes_reversed io, 0x78, 0x56, 0x34, 0x12
       end
 
       it "writes int64" do
         io = StringIO.new
-        io.write_bytes 0x123456789ABCDEF0_i64, IO::ByteFormat::BigEndian
+        io.write_object 0x123456789ABCDEF0_i64, IO::ByteFormat::BigEndian
         assert_bytes_reversed io, 0xF0, 0xDE, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12
       end
 
       it "writes float32" do
         io = StringIO.new
-        io.write_bytes 1.234_f32, IO::ByteFormat::BigEndian
+        io.write_object 1.234_f32, IO::ByteFormat::BigEndian
         assert_bytes_reversed io, 0xB6, 0xF3, 0x9D, 0x3F
       end
 
       it "writes float64" do
         io = StringIO.new
-        io.write_bytes 1.234, IO::ByteFormat::BigEndian
+        io.write_object 1.234, IO::ByteFormat::BigEndian
         assert_bytes_reversed io, 0x58, 0x39, 0xB4, 0xC8, 0x76, 0xBE, 0xF3, 0x3F
       end
     end
