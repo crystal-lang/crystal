9ed0434fb140763aa6d986d67e9e42e6271bae6d
Technorama Ltd <oss-crystal@technorama.net>
2015-09-27 09:02:32 -0400

    ConditionVariable:   * Check return values.   * New method "broadcast".   * Add spec.

---
+++ spec/std/thread/condition_variable_spec.cr
@@ -0,0 +1,63 @@
+require "spec"
+
+describe ConditionVariable do
+  it "signals" do
+    mutex = Mutex.new
+    cond = ConditionVariable.new
+    pcond = ConditionVariable.new
+    waiting = 0
+    signaled = 0
+
+    threads = 3.times.map do
+      Thread.new do
+        mutex.synchronize do
+          waiting += 1
+          pcond.signal
+          cond.wait mutex
+        end
+      end
+    end.to_a
+
+    while signaled < 3
+      mutex.synchronize do
+       if waiting > 0
+         waiting -= 1
+         signaled += 1
+         cond.signal
+       end
+      end
+    end
+    threads.map &.join
+  end
+
+  it "broadcasts" do
+    mutex = Mutex.new
+    cond = ConditionVariable.new
+    pcond = ConditionVariable.new
+    waiting = 0
+    signaled = false
+
+    threads = 3.times.map do
+      Thread.new do
+        mutex.synchronize do
+          waiting += 1
+          pcond.signal
+          cond.wait mutex
+        end
+      end
+    end.to_a
+
+    until signaled
+      mutex.synchronize do
+        if waiting >= 3
+          cond.broadcast
+          signaled = true
+        else
+          pcond.wait mutex
+        end
+      end
+    end
+
+    threads.map &.join
+  end
+end
