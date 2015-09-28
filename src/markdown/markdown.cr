04e487da7eabebdc1e15b6ea1b4e31efdf4ee477
Adler <nkj20932@hotmail.com>
2015-09-28 15:56:29 +0800

    Fix documentation format for the doc parser. - Remove extra gaps between doc and class definition in Markdown & ECR library definition. - Attach doc right above Time struct.

--- src/markdown/markdown.cr
+++ src/markdown/markdown.cr
@@ -5,13 +5,11 @@
 # 
 #     require "markdown"
 #     
-#     text = "## This is title \n" \ 
-#            "This is a [link](http://crystal-lang.org)"
+#     text = "## This is title \n This is a [link](http://crystal-lang.org)"
 #
 #     Markdown.to_html(text)
 #     #=><h2>This is title</h2>
 #     #=><p>This is a <a href="http://crystal-lang.org">link</a></p>
-
 class Markdown
   def self.parse(text, renderer)
     parser = Parser.new(text, renderer)
