ad47e983e615a3abe1f129af52c1db643c13f809
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-10-16 15:33:52 -0300

    Logger uses separate fiber to serialize writing the messages (fixes #1748)

--- src/logger.cr
+++ src/logger.cr
@@ -61,21 +61,22 @@ class Logger(T)
     io << severity.rjust(5) << " -- " << progname << ": " << message
   end
 
+  # :nodoc:
+  record Message, severity, datetime, progname, message
+
   def initialize(@io : T)
     @level = Severity::INFO
     @formatter = DEFAULT_FORMATTER
     @progname = ""
-  end
-
-  def <<(message)
-    @io << message
+    @channel = Channel(Message).new(100)
+    spawn write_messages
   end
 
   def close
-    @io.close
+    @channel.close
   end
 
-  macro log_level(name)
+  {% for name in Severity.constants %}
     {{name.id}} = Severity::{{name.id}}
 
     def {{name.id.downcase}}?
@@ -89,31 +90,29 @@ class Logger(T)
     def {{name.id.downcase}}(progname = nil)
       log(Severity::{{name.id}}, progname) { yield }
     end
-  end
-
-  log_level UNKNOWN
-  log_level FATAL
-  log_level ERROR
-  log_level WARN
-  log_level INFO
-  log_level DEBUG
+  {% end %}
 
   def log(severity, message, progname = nil)
     return if severity < level
-    format(severity, Time.now, progname || @progname, message, @io)
-    @io.puts
-    @io.flush
+    enqueue(severity, Time.now, progname || @progname, message)
   end
 
   def log(severity, progname = nil)
     return if severity < level
-    format(severity, Time.now, progname || @progname, yield, @io)
-    @io.puts
-    @io.flush
+    enqueue(severity, Time.now, progname || @progname, yield)
   end
 
-  def format(severity, datetime, progname, message, io)
-    label = severity == Severity::UNKNOWN ? "ANY" : severity.to_s
-    formatter.call(label, Time.now, progname.to_s, message.to_s, io)
+  private def enqueue(severity, datetime, progname, message)
+    @channel.send Message.new(severity, datetime, progname, message)
+  end
+
+  private def write_messages
+    while msg = @channel.receive?
+      label = msg.severity == Severity::UNKNOWN ? "ANY" : msg.severity.to_s
+      formatter.call(label, msg.datetime, msg.progname.to_s, msg.message.to_s, @io)
+      @io.puts
+      @io.flush
+    end
+    @io.close
   end
 end
