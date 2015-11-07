6221bd98ee58b145281e62d1901c1442fad7394c
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-11-07 15:27:24 +0900

    Refactor
    Add some comments
    Rename from `int_to_float`/`float_to_int` to `int_as_float`/`float_as_int`, which is same as original
    Ruby's name.

--- src/range/bsearch.cr
+++ src/range/bsearch.cr
@@ -1,26 +1,10 @@
-private macro bsearch_search(exclusive, mid)
-  saved_to = to
-  satisfied = nil
-  while from < to
-    mid = {{ mid.id }}
-
-    if yield mid
-      satisfied = mid
-      to = mid
-    else
-      from = mid + 1
-    end
-  end
-
-  if ! {{ exclusive }} && from == saved_to && yield from
-    satisfied = from
-  end
-
-  satisfied
-end
-
 {% for p in [64, 32] %}
-  private def int_to_float(i : Int{{ p }})
+  # Cast integer as floating number value with keeping binary structure.
+  private def int_as_float(i : Int{{ p }})
+    # Integer uses two's complement to represent signed number, but
+    # floating number value uses sign bit. This difference causes a
+    # problem that it reproduce incorrect value when you sum positive
+    # number and negative number. It fixes this problem.
     if i < 0
       i = -i
       -(pointerof(i) as Pointer(Float{{ p }})).value
@@ -29,7 +13,8 @@ end
     end
   end
 
-  private def float_to_int(f : Float{{ p }})
+  # Cast floating number value as integer with keeping binary structure.
+  private def float_as_int(f : Float{{ p }})
     if f < 0
       f = -f
       -(pointerof(f) as Pointer(Int{{ p }})).value
@@ -51,18 +36,34 @@ end
   end
 
   private def bsearch_internal(from : Float{{ p }}, to : Float{{ p }}, exclusive)
-    from = float_to_int from
-    to = float_to_int to
+    from = float_as_int from
+    to = float_as_int to
     to -= 1 if exclusive
 
-    bsearch_internal(from, to, false){ |i| yield int_to_float i }
-      .try{ |i| int_to_float i }
+    bsearch_internal(from, to, false){ |i| yield int_as_float i }
+      .try{ |i| int_as_float i }
   end
 
-  private def bsearch_internal(from : Int{{ p }}, to : Int{{ p }}, exclusive, &block)
-    bsearch_search(exclusive,
-      "(from < 0) == (to < 0) ? from + (to - from) / 2
-      : (from < -to) ? -((- from - to - 1) / 2 + 1) : (from + to) / 2")
+  private def bsearch_internal(from : Int{{ p }}, to : Int{{ p }}, exclusive)
+    saved_to = to
+    satisfied = nil
+    while from < to
+      mid = (from < 0) == (to < 0) ? from + (to - from) / 2
+          : (from < -to) ? -((- from - to - 1) / 2 + 1) : (from + to) / 2
+
+      if yield mid
+        satisfied = mid
+        to = mid
+      else
+        from = mid + 1
+      end
+    end
+
+    if !exclusive && from == saved_to && yield from
+      satisfied = from
+    end
+
+    satisfied
   end
 {% end %}
 
@@ -80,10 +81,16 @@ struct Range(B, E)
   # ```
   #
   # Returns `nil` if the block didn't return `true` for any value.
-  def bsearch(&block)
+  def bsearch
     from = self.begin
     to = self.end
 
+    # If the range consists of floating value,
+    # it uses specialized implementation for floating value.
+    # This implementation is very fast. For example,
+    # `(1..1e300).bsearch{ false }` loops over 2000 times in
+    # popular implementation, but in this implementation loops 65 times
+    # at most.
     {% for v in %w(from to) %}
       if {{ v.id }}.is_a?(Float::Primitive)
         return bsearch_internal from, to, self.excludes_end? do |value|
@@ -92,6 +99,23 @@ struct Range(B, E)
       end
     {% end %}
 
-    bsearch_search(self.excludes_end?, "from + (to - from) / 2")
+    saved_to = to
+    satisfied = nil
+    while from < to
+      mid = from + (to - from) / 2
+
+      if yield mid
+        satisfied = mid
+        to = mid
+      else
+        from = mid + 1
+      end
+    end
+
+    if !self.excludes_end? && from == saved_to && yield from
+      satisfied = from
+    end
+
+    satisfied
   end
 end
