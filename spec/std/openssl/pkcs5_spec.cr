a95543574b5dc6a065313a7112da9f8943ca96c7
Benoist Claassen <benoist.claassen@gmail.com>
2015-11-13 12:20:39 +0100

    added pkcs5-pbkdf2-hmac-sha1

---
+++ spec/std/openssl/pkcs5_spec.cr
@@ -0,0 +1,15 @@
+require "spec"
+require "openssl/pkcs5"
+
+describe OpenSSL::PKCS5 do
+  it "computes pbkdf2_hmac_sha1" do
+    [
+      {1, 16, "0c60c80f961f0e71f3a9b524af601206"},
+      {1, 32, "0c60c80f961f0e71f3a9b524af6012062fe037a6e0f0eb94fe8fc46bdc637164"},
+      {2**16, 16, "1b345dd55f62a35aecdb9229bc7ae95b"},
+      {2**16, 32, "1b345dd55f62a35aecdb9229bc7ae95b305a8d538940134627e46f82d3a41e5e"}
+    ].each do |tuple|
+      OpenSSL::PKCS5.pbkdf2_hmac_sha1("password", "salt", tuple[0], tuple[1]).hexstring.should eq tuple[2]
+    end
+  end
+end
