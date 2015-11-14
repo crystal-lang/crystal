7b1923408166f44af66045dd6ba4d41767eaadf7
Benoist Claassen <benoist.claassen@gmail.com>
2015-11-14 16:47:58 +0100

    change pkcs5 to a module

--- src/openssl/pkcs5.cr
+++ src/openssl/pkcs5.cr
@@ -1,6 +1,6 @@
 require "./lib_crypto"
 
-class OpenSSL::PKCS5
+module OpenSSL::PKCS5
   def self.pbkdf2_hmac_sha1(secret, salt, iterations = 2**16, key_size = 64)
     buffer = Slice(UInt8).new(key_size)
     LibCrypto.pkcs5_pbkdf2_hmac_sha1(secret, secret.size, salt, salt.size, iterations, key_size, buffer)
