379275fff47ef0267c56fd6daf1211f93d2de34b
Ivan Palamarchuk <i.delef@gmail.com>
2015-12-03 21:09:50 +0300

    add types to the html builder

--- src/html/builder.cr
+++ src/html/builder.cr
@@ -37,9 +37,23 @@ struct HTML::Builder
   #
   # ```
   # HTML::Builder.new.build { doctype } # => <doctype/>
+  # HTML::Builder.new.build { doctype "html4" } # =>
+  #   <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
   # ```
-  def doctype
-    @str << "<!DOCTYPE html>"
+  def doctype(type = nil : String)
+    @str << case type
+    when 'html4'
+      '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"' + 
+      ' "http://www.w3.org/TR/html4/strict.dtd">'
+    when 'xhtml1.0'
+      '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"' +
+      '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
+    when 'xhtml1.1'
+      '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"' +
+      ' "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
+    else
+      '<!DOCTYPE html>'
+    end
   end
 
   # Renders `BR` html tag.
