d38c68197ff533ce8c649d53357aaa56a1e30f94
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-01-21 11:46:56 -0300

    Added docs for Proc and Box

--- src/box.cr
+++ src/box.cr
@@ -1,13 +1,26 @@
+# A Box allows turning any object to a `Void*` and back.
+#
+# A Box's purpose is passing data to C as a `Void*` and then converting that
+# back to the original data type.
+#
+# For an example usage, see `Proc`'s explanation about sending Procs to C.
 class Box(T)
+  # Returns the original object
   getter object
 
+  # Creates a Box with the given object.
+  #
+  # This method isn't usually used directly. Instead, `Box.box` is used.
   def initialize(@object : T)
   end
 
+  # Creates a Box for an object and returns it as a Void*.
   def self.box(object)
     new(object) as Void*
   end
 
+  # Unboxes a Void* into an object of type T. Note that for this you must
+  # specify T: `Box(T).unbox(data)`.
   def self.unbox(pointer : Void*)
     (pointer as self).object
   end
