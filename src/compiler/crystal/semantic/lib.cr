5d7b3bfc2dc872bba818dd03cf0ad6e1d84514d8
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-01-25 00:17:53 -0300

    Fixed #2064: give error when using `out` at varargs position

--- src/compiler/crystal/semantic/lib.cr
+++ src/compiler/crystal/semantic/lib.cr
@@ -61,6 +61,16 @@ class Crystal::Call
         end
       end
     end
+
+    # Check that there are no out args more then the number of arguments in the fun
+    if untyped_def.varargs
+      untyped_def.args.size.upto(self.args.size - 1) do |i|
+        self_arg = self.args[i]
+        if self_arg.is_a?(Out)
+          self_arg.raise "can't use out at varargs position: declare the variable with `#{self_arg.exp} = uninitialized ...` and pass it with `pointerof(#{self_arg.exp})`"
+        end
+      end
+    end
   end
 
   def check_fun_args_types_match(obj_type, typed_def)
