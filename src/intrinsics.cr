f1042057404827bc4655802a97d16729b9fd0b16
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-01-29 16:09:03 -0300

    Fixed #2077: use appropiate definition of memove according to the word size

--- src/intrinsics.cr
+++ src/intrinsics.cr
@@ -1,8 +1,14 @@
 lib Intrinsics
   fun debugtrap = "llvm.debugtrap"
-  fun memcpy = "llvm.memcpy.p0i8.p0i8.i32"(dest : Void*, src : Void*, len : UInt32, align : UInt32, is_volatile : Bool)
-  fun memmove = "llvm.memmove.p0i8.p0i8.i32"(dest : Void*, src : Void*, len : UInt32, align : UInt32, is_volatile : Bool)
-  fun memset = "llvm.memset.p0i8.i32"(dest : Void*, val : UInt8, len : UInt32, align : UInt32, is_volatile : Bool)
+  ifdef x86_64
+    fun memcpy = "llvm.memcpy.p0i8.p0i8.i64"(dest : Void*, src : Void*, len : UInt64, align : UInt32, is_volatile : Bool)
+    fun memmove = "llvm.memmove.p0i8.p0i8.i64"(dest : Void*, src : Void*, len : UInt64, align : UInt32, is_volatile : Bool)
+    fun memset = "llvm.memset.p0i8.i64"(dest : Void*, val : UInt8, len : UInt64, align : UInt32, is_volatile : Bool)
+  else
+    fun memcpy = "llvm.memcpy.p0i8.p0i8.i32"(dest : Void*, src : Void*, len : UInt32, align : UInt32, is_volatile : Bool)
+    fun memmove = "llvm.memmove.p0i8.p0i8.i32"(dest : Void*, src : Void*, len : UInt32, align : UInt32, is_volatile : Bool)
+    fun memset = "llvm.memset.p0i8.i32"(dest : Void*, val : UInt8, len : UInt32, align : UInt32, is_volatile : Bool)
+  end
   fun read_cycle_counter = "llvm.readcyclecounter" : UInt64
   fun bswap32 = "llvm.bswap.i32"(id : UInt32) : UInt32
 
