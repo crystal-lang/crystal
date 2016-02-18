facb21d4f5deb1fce844dcbd052f3cefbfd52ef9
Jonathan <jonathan.lecointe@gmail.com>
2016-02-18 12:56:10 +0100

    Added more escaping characters for HTML
    Improvement of the escaped characters list and addition of a test related to the modifications
    
    Fixed typo error in test
    
    Changed quote and nonbreaking space html escaping to avoid confusion
    
    Fix typo on nbsp
    
    Fixed indentation in html_spec.cr
    
    Fixed test to match the typo fixed for nbsp

--- spec/std/html_spec.cr
+++ spec/std/html_spec.cr
@@ -14,5 +14,17 @@ describe "HTML" do
 
       str.should eq("&lt; &amp; &gt;")
     end
+
+    it "escapes javascript exemple from a string" do
+      str = HTML.escape("<script>alert('You are being hacked')</script>")
+
+      str.should eq("&lt;script&gt;alert&#40;&#39;You are being hacked&#39;&#41;&lt;/script&gt;")
+    end
+
+    it "escapes nonbreakable space but not normal space" do
+      str = HTML.escape("nbsp space ")
+
+      str.should eq("nbsp&nbsp;space ")
+    end
   end
 end
