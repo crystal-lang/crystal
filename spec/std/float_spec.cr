a73c6a7e8eef77255bde9ed960971806ed20ec84
Julien Portalier <julien@portalier.com>
2016-03-01 11:16:51 +0000

    Fix ** specs on musl-libc

--- spec/std/float_spec.cr
+++ spec/std/float_spec.cr
@@ -2,14 +2,14 @@ require "spec"
 
 describe "Float" do
   describe "**" do
-    assert { (2.5_f32 ** 2_i32).should eq(6.25_f32) }
-    assert { (2.5_f32 ** 2).should eq(6.25_f32) }
-    assert { (2.5_f32 ** 2.5_f32).should eq(9.882117688026186_f32) }
-    assert { (2.5_f32 ** 2.5).should eq(9.882117688026186_f32) }
-    assert { (2.5_f64 ** 2_i32).should eq(6.25_f64) }
-    assert { (2.5_f64 ** 2).should eq(6.25_f64) }
-    assert { (2.5_f64 ** 2.5_f64).should eq(9.882117688026186_f64) }
-    assert { (2.5_f64 ** 2.5).should eq(9.882117688026186_f64) }
+    assert { (2.5_f32 ** 2_i32).should be_close(6.25_f32, 0.0001) }
+    assert { (2.5_f32 ** 2).should be_close(6.25_f32, 0.0001) }
+    assert { (2.5_f32 ** 2.5_f32).should be_close(9.882117688026186_f32, 0.0001) }
+    assert { (2.5_f32 ** 2.5).should be_close(9.882117688026186_f32, 0.0001) }
+    assert { (2.5_f64 ** 2_i32).should be_close(6.25_f64, 0.0001) }
+    assert { (2.5_f64 ** 2).should be_close(6.25_f64, 0.0001) }
+    assert { (2.5_f64 ** 2.5_f64).should be_close(9.882117688026186_f64, 0.0001) }
+    assert { (2.5_f64 ** 2.5).should be_close(9.882117688026186_f64, 0.001) }
   end
 
   describe "%" do
