253b461b9cba664684b1ab1a30f664ea39aa97e2
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-03-03 13:18:46 -0300

    Compiler: show stats label first, then time after execution

--- src/compiler/crystal/compiler.cr
+++ src/compiler/crystal/compiler.cr
@@ -437,12 +437,13 @@ module Crystal
 
   def self.timing(label, stats)
     if stats
+      print "%-34s" % "#{label}:"
       time = Time.now
       value = yield
       elapsed_time = Time.now - time
       LibGC.get_heap_usage_safe(out heap_size, out free_bytes, out unmapped_bytes, out bytes_since_gc, out total_bytes)
       mb = heap_size / 1024.0 / 1024.0
-      puts "%-34s %s (%7.2fMB)" % {"#{label}:", elapsed_time, mb}
+      puts " %s (%7.2fMB)" % {elapsed_time, mb}
       value
     else
       yield
