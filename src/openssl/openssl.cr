7b2b9b0e9f668c12f2a124bc46869eb128c43d53
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-03-14 18:20:39 -0300

    OpenSSL: passing `-D without_openssl` doesn't link against ssl and crypto

--- src/openssl/openssl.cr
+++ src/openssl/openssl.cr
@@ -1,26 +1,28 @@
-require "./lib_ssl"
+ifdef !without_openssl
+  require "./lib_ssl"
 
-module OpenSSL
-  class Error < Exception
-    getter err : LibCrypto::ULong
-    getter err_msg : String?
+  module OpenSSL
+    class Error < Exception
+      getter err : LibCrypto::ULong
+      getter err_msg : String?
 
-    def initialize(msg = nil)
-      unless (err = @err = LibCrypto.err_get_error) == 0
-        @err_msg = String.new(LibCrypto.err_error_string(err, nil))
-        msg = msg ? "#{msg}: #{@err_msg}" : @err_msg
+      def initialize(msg = nil)
+        unless (err = @err = LibCrypto.err_get_error) == 0
+          @err_msg = String.new(LibCrypto.err_error_string(err, nil))
+          msg = msg ? "#{msg}: #{@err_msg}" : @err_msg
+        end
+        super(msg)
       end
-      super(msg)
     end
+
+    LibSSL.ssl_library_init
+    LibSSL.ssl_load_error_strings
+    LibCrypto.openssl_add_all_algorithms
+    LibCrypto.err_load_crypto_strings
   end
 
-  LibSSL.ssl_library_init
-  LibSSL.ssl_load_error_strings
-  LibCrypto.openssl_add_all_algorithms
-  LibCrypto.err_load_crypto_strings
+  require "./bio"
+  require "./ssl/*"
+  require "./digest/*"
+  require "./md5"
 end
-
-require "./bio"
-require "./ssl/*"
-require "./digest/*"
-require "./md5"
