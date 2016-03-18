2feb4ea01dfbacf37020330c79a0237f92641dac
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-03-18 15:21:14 -0300

    turn off compiler colors

--- src/compiler/crystal/tools/playground/server.cr
+++ src/compiler/crystal/tools/playground/server.cr
@@ -40,6 +40,7 @@ module Crystal::Playground
       ]
       output_filename = tempfile "play-#{@session_key}-#{tag}"
       compiler = Compiler.new
+      compiler.color = false
       begin
         @logger.info "Instrumented code compilation started (session=#{@session_key}, tag=#{tag})."
         result = compiler.compile sources, output_filename
