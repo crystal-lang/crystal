48f9cf2b556ee97bfd7676f7aad5efa9127ac59c
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-03-19 09:21:22 -0300

    Add `crystal env` command

--- src/compiler/crystal/command.cr
+++ src/compiler/crystal/command.cr
@@ -14,10 +14,11 @@ class Crystal::Command
     Command:
         init                     generate a new project
         build                    compile program
-        play                     starts crystal playground server
         deps                     install project dependencies
         docs                     generate documentation
+        env                      print Crystal environment information
         eval                     eval code from args or standard input
+        play                     starts crystal playground server
         run (default)            compile and run program
         spec                     compile and run specs (in spec directory)
         tool                     run a tool
@@ -73,7 +74,10 @@ class Crystal::Command
       when "docs".starts_with?(command)
         options.shift
         docs
-      when "eval".starts_with?(command)
+      when command == "env"
+        options.shift
+        env
+      when command == "eval"
         options.shift
         eval
       when "run".starts_with?(command)
@@ -157,6 +161,37 @@ class Crystal::Command
   private def build
     config = create_compiler "build"
     config.compile
+  end
+
+  private def env
+    if ARGV.size == 1 && {"--help", "-h"}.includes?(ARGV[0])
+      puts <<-USAGE
+      Usage: crystal env [var ...]
+
+      Prints Crystal environment information.
+
+      By default it prints information as a shell script.
+      If one or more variable names is given as arguments,
+      it prints the value of each named variable on its own line.
+      USAGE
+
+      exit
+    end
+
+    vars = {
+      "CRYSTAL_PATH":    Config::PATH || "",
+      "CRYSTAL_VERSION": Config::VERSION || "",
+    }
+
+    if ARGV.empty?
+      vars.each do |key, value|
+        puts "#{key}=#{value.inspect}"
+      end
+    else
+      ARGV.each do |key|
+        puts vars[key]?
+      end
+    end
   end
 
   private def eval
