926d2fb34b82f139be3fad39bbedb011de587970
Vitalii Elenhaupt <velenhaupt@gmail.com>
2016-03-21 14:19:50 +0200

    Add query parameter to api search

--- api/js/doc.js
+++ api/js/doc.js
@@ -12,6 +12,16 @@ document.addEventListener('DOMContentLoaded', function() {
   var typesList = document.getElementById('types-list');
   var searchInput = document.getElementById('search-input');
   var parents = document.querySelectorAll('#types-list li.parent');
+  var typesLinks = document.querySelectorAll('#types-list li a');
+  var query = function queryParams() {
+    var str = window.location.search;
+    var params = {};
+    str.replace(
+      new RegExp('([^?=&]+)=([^&]*)', 'g'),
+      function($0, $1, $2) { params[$1] = $2; }
+    );
+    return params;
+  }()['q'] || '';
 
   for(var i = 0; i < parents.length; i++) {
     var _parent = parents[i];
@@ -36,6 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   };
 
+  for (var i = 0; i < typesLinks.length; i ++) {
+    typesLinks[i].addEventListener("click", function(e) {
+      if (query) {
+        e.preventDefault();
+        window.location.href = this.href + '?q=' + query;
+      }
+    }, false);
+  }
+
   var childMatch = function(type, regexp){
     var types = type.querySelectorAll("ul li");
     for (var j = 0; j < types.length; j ++) {
@@ -45,36 +64,43 @@ document.addEventListener('DOMContentLoaded', function() {
     return false;
   };
 
+  var search = function(text) {
+    var types = document.querySelectorAll('#types-list li');
+    var words = text.toLowerCase().split(/\s+/).filter(function(word) {
+      return word.length > 0;
+    });
+    var regexp = new RegExp(words.join('|'));
+
+    for(var i = 0; i < types.length; i++) {
+      var type = types[i];
+      if(words.length == 0 || regexp.exec(type.getAttribute('data-name')) || childMatch(type, regexp)) {
+        type.className = type.className.replace(/ +hide/g, '');
+        var is_parent     =   new RegExp("parent").exec(type.className);
+        var is_not_opened = !(new RegExp("open").exec(type.className));
+        if(childMatch(type,regexp) && is_parent && is_not_opened){
+          type.className += " open";
+        };
+      } else {
+        if(type.className.indexOf('hide') == -1) {
+          type.className += ' hide';
+        };
+      };
+      if(words.length == 0){
+        type.className = type.className.replace(/ +open/g, '');
+      };
+    }
+  }
+
+  if (query) {
+    searchInput.value = query;
+    search(query);
+  }
+
   var searchTimeout;
   searchInput.addEventListener('keyup', function() {
+    query = searchInput.value;
     clearTimeout(searchTimeout);
-    searchTimeout = setTimeout(function() {
-      var text = searchInput.value;
-      var types = document.querySelectorAll('#types-list li');
-      var words = text.toLowerCase().split(/\s+/).filter(function(word) {
-        return word.length > 0;
-      });
-      var regexp = new RegExp(words.join('|'));
-
-      for(var i = 0; i < types.length; i++) {
-        var type = types[i];
-        if(words.length == 0 || regexp.exec(type.getAttribute('data-name')) || childMatch(type, regexp)) {
-          type.className = type.className.replace(/ +hide/g, '');
-          var is_parent     =   new RegExp("parent").exec(type.className);
-          var is_not_opened = !(new RegExp("open").exec(type.className));
-          if(childMatch(type,regexp) && is_parent && is_not_opened){
-            type.className += " open";
-          };
-        } else {
-          if(type.className.indexOf('hide') == -1) {
-            type.className += ' hide';
-          };
-        };
-        if(words.length == 0){
-          type.className = type.className.replace(/ +open/g, '');
-        };
-      }
-    }, 200);
+    searchTimeout = setTimeout(function() { search(query); }, 200);
   });
 
   typesList.onscroll = function() {
