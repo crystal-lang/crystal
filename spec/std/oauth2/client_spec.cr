192a5e08b490aabc81f526ea0fe4ffeeaf948bc9
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-03-21 13:02:31 -0300

    Wrap URI.escape and URI.unescape_one method for a www_form_component compliant alternative. Use these in HTTP::Params parse and build methods

--- spec/std/oauth2/client_spec.cr
+++ spec/std/oauth2/client_spec.cr
@@ -6,19 +6,19 @@ describe OAuth2::Client do
     it "gets with default endpoint" do
       client = OAuth2::Client.new "localhost", "client_id", "client_secret", redirect_uri: "uri"
       uri = client.get_authorize_uri(scope: "foo bar")
-      uri.should eq("https://localhost/oauth2/authorize?client_id=client_id&redirect_uri=uri&response_type=code&scope=foo%20bar")
+      uri.should eq("https://localhost/oauth2/authorize?client_id=client_id&redirect_uri=uri&response_type=code&scope=foo+bar")
     end
 
     it "gets with custom endpoint" do
       client = OAuth2::Client.new "localhost", "client_id", "client_secret", redirect_uri: "uri", authorize_uri: "/baz"
       uri = client.get_authorize_uri(scope: "foo bar")
-      uri.should eq("https://localhost/baz?client_id=client_id&redirect_uri=uri&response_type=code&scope=foo%20bar")
+      uri.should eq("https://localhost/baz?client_id=client_id&redirect_uri=uri&response_type=code&scope=foo+bar")
     end
 
     it "gets with state" do
       client = OAuth2::Client.new "localhost", "client_id", "client_secret", redirect_uri: "uri"
       uri = client.get_authorize_uri(scope: "foo bar", state: "xyz")
-      uri.should eq("https://localhost/oauth2/authorize?client_id=client_id&redirect_uri=uri&response_type=code&scope=foo%20bar&state=xyz")
+      uri.should eq("https://localhost/oauth2/authorize?client_id=client_id&redirect_uri=uri&response_type=code&scope=foo+bar&state=xyz")
     end
   end
 
