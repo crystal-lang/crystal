32c39d46700a86b623a105f0d6315bccd0077bfb
Doug Everly <Doug@Everly.org>
2016-03-24 09:46:37 -0400

    # This is a combination of 3 commits. # The first commit's message is: Added Process.daemonize, samples and spec.
    # This is the 2nd commit message:
    
    Removed duplicate samples/tcp_client.cr
    
    # This is the 3rd commit message:
    
    Added Process.daemonize, samples and spec.

---
+++ samples/daemonize/tcp_server_daemon.cr
@@ -0,0 +1,25 @@
+require "socket"
+
+# goes with tcp_client.cr
+
+Process.daemonize
+
+def process(client)
+  client_addr = client.remote_address
+  puts "#{client_addr} connected"
+
+  while msg = client.read_line
+    puts "#{client_addr} msg '#{msg.chop}'"
+    client << msg
+  end
+rescue IO::EOFError
+  puts "#{client_addr} dissconnected"
+ensure
+  client.close
+end
+
+server = TCPServer.new "127.0.0.1", 9000
+puts "listen on 127.0.0.1:9000"
+loop { spawn process(server.accept) }
+
+
--- spec/std/process_spec.cr
+++ spec/std/process_spec.cr
@@ -1,5 +1,6 @@
 require "spec"
 require "process"
+require "tempfile"
 
 describe Process do
   it "runs true" do
@@ -168,4 +169,16 @@ describe Process do
     end
     buffer.to_s.lines.size.should eq(1000)
   end
+	
+	it "daemonizes and has parent pid == 1" do
+		tmp = Tempfile.new("it_daemonizes")
+		fork {
+			Process.daemonize
+			File.write(tmp.path, Process.ppid.to_s)
+		}
+		sleep 1
+		res = File.read(tmp.path)
+		res.should eq("1")
+		tmp.delete
+	end
 end
