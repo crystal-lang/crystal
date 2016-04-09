3c0ab38cc52058f25b096b82df6862980e98df17
John Bolliger <johnbolliger@gmail.com>
2016-04-08 19:44:18 -0600

    Changed Crypto::Subtle.constant_time_compare to return a boolean, and accept inputs that respond to to_slice.

--- spec/std/crypto/subtle_spec.cr
+++ spec/std/crypto/subtle_spec.cr
@@ -4,10 +4,10 @@ require "crypto/subtle"
 describe "Subtle" do
   it "compares constant times" do
     data = [
-      {"a" => Slice.new(1, 0x11), "b" => Slice.new(1, 0x11), "result" => 1},
-      {"a" => Slice.new(1, 0x12), "b" => Slice.new(1, 0x11), "result" => 0},
-      {"a" => Slice.new(1, 0x11), "b" => Slice.new(2) { |i| 0x11 + i }, "result" => 0},
-      {"a" => Slice.new(2) { |i| 0x11 + i }, "b" => Slice.new(1, 0x11), "result" => 0},
+      {"a" => Slice.new(1, 0x11), "b" => Slice.new(1, 0x11), "result" => true},
+      {"a" => Slice.new(1, 0x12), "b" => Slice.new(1, 0x11), "result" => false},
+      {"a" => Slice.new(1, 0x11), "b" => Slice.new(2) { |i| 0x11 + i }, "result" => false},
+      {"a" => Slice.new(2) { |i| 0x11 + i }, "b" => Slice.new(1, 0x11), "result" => false},
     ]
 
     data.each do |test|
@@ -32,6 +32,14 @@ describe "Subtle" do
   it "compares constant time bytes bug" do
     h1 = "$2a$05$LEC1XBXgXECzKUO2LBDhKOa9lH9zigNKnksVaDwViFNgPU4WkrD53J"
     h2 = "$2a$05$LEC1XBXgXECzKUO2LBDhKOaHlSGFuDDwMuVg6gOzdxQ0xN4rFOwMUn"
-    Crypto::Subtle.constant_time_compare(h1.to_slice, h2.to_slice).should eq(0)
+    Crypto::Subtle.constant_time_compare(h1, h2).should eq(false)
+  end
+
+  it "compares constant time and slices strings" do
+    h1 = "$2a$05$LEC1XBXgXECzKUO2LBDhKOa9lH9zigNKnksVaDwViFNgPU4WkrD53J"
+    h2 = "$2a$05$LEC1XBXgXECzKUO2LBDhKOaHlSGFuDDwMuVg6gOzdxQ0xN4rFOwMUn"
+
+    slice_result = Crypto::Subtle.constant_time_compare(h1.to_slice, h2.to_slice)
+    Crypto::Subtle.constant_time_compare(h1, h2).should eq(slice_result)
   end
 end
