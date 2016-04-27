2471fa9d544eb7e451f32d02d057e707e17cd374
Julien Portalier <julien@portalier.com>
2016-04-27 15:39:20 +0200

    C bindings for x86_64-linux-gnu

---
+++ src/lib_c/x86_64-linux-gnu/c/stdio.cr
@@ -0,0 +1,8 @@
+require "./sys/types"
+require "./stddef"
+
+lib LibC
+  fun printf(format : Char*, ...) : Int
+  fun rename(old : Char*, new : Char*) : Int
+  fun snprintf(s : Char*, maxlen : SizeT, format : Char*, ...) : Int
+end
