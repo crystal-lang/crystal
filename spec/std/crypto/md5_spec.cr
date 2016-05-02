c0dee6f185e35ef13185f9c5b33f1a1d16b35f3d
Will Leinweber <will@bitfission.com>
2016-05-02 16:50:01 -0700

    Crypto::MD5 use UInt8.slice in specs

--- spec/std/crypto/md5_spec.cr
+++ spec/std/crypto/md5_spec.cr
@@ -7,21 +7,14 @@ describe Crypto::MD5 do
   end
 
   it "calculates hash from UInt8 slices" do
-    s = Slice(UInt8).new(3)
-    s[0] = 0x66_u8 # 'f'
-    s[1] = 0x6f_u8 # 'o'
-    s[2] = 0x6f_u8 # 'o'
+    s = UInt8.slice(0x66, 0x6f, 0x6f) # f,o,o
     Crypto::MD5.hex_digest(s).should eq("acbd18db4cc2f85cedef654fccc4a4d8")
   end
 
   it "can take a block" do
-    s = Slice(UInt8).new(2)
-    s[0] = 0x6f_u8 # 'o'
-    s[1] = 0x6f_u8 # 'o'
-
     Crypto::MD5.hex_digest do |ctx|
       ctx.update "f"
-      ctx.update s
+      ctx.update UInt8.slice(0x6f, 0x6f)
     end.should eq("acbd18db4cc2f85cedef654fccc4a4d8")
   end
 end
