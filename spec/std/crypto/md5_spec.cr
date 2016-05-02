c0de01bedfb9576f848aa6a5a10525fed32a41c0
Will Leinweber <will@bitfission.com>
2016-05-02 16:07:15 -0700

    Crypto::MD5 add block form and documentation

--- spec/std/crypto/md5_spec.cr
+++ spec/std/crypto/md5_spec.cr
@@ -13,4 +13,15 @@ describe Crypto::MD5 do
     s[2] = 0x6f_u8 # 'o'
     Crypto::MD5.hex_digest(s).should eq("acbd18db4cc2f85cedef654fccc4a4d8")
   end
+
+  it "can take a block" do
+    s = Slice(UInt8).new(2)
+    s[0] = 0x6f_u8 # 'o'
+    s[1] = 0x6f_u8 # 'o'
+
+    Crypto::MD5.hex_digest do |ctx|
+      ctx.update "f"
+      ctx.update s
+    end.should eq("acbd18db4cc2f85cedef654fccc4a4d8")
+  end
 end
