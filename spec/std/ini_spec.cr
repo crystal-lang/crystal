faa73b1bd29e1b210408c43eb1d883afe8246dfa
Jonne Haß <me@jhass.eu>
2016-05-28 11:30:19 +0200

    Rename IniFile to INI, closes #2450, ref #279

--- spec/std/ini_spec.cr
+++ spec/std/ini_spec.cr
@@ -1,26 +1,26 @@
 require "spec"
-require "inifile"
+require "ini"
 
-describe "IniFile" do
+describe "INI" do
   describe "parse from string" do
     it "parses key = value" do
-      IniFile.load("key = value").should eq({"" => {"key" => "value"}})
+      INI.parse("key = value").should eq({"" => {"key" => "value"}})
     end
 
     it "ignores whitespaces" do
-      IniFile.load("   key   =   value  ").should eq({"" => {"key" => "value"}})
+      INI.parse("   key   =   value  ").should eq({"" => {"key" => "value"}})
     end
 
     it "parses sections" do
-      IniFile.load("[section]\na = 1").should eq({"section" => {"a" => "1"}})
+      INI.parse("[section]\na = 1").should eq({"section" => {"a" => "1"}})
     end
 
     it "empty section" do
-      IniFile.load("[section]").should eq({"section" => {} of String => String})
+      INI.parse("[section]").should eq({"section" => {} of String => String})
     end
 
     it "parse file" do
-      IniFile.load(File.read "#{__DIR__}/data/test_file.ini").should eq({
+      INI.parse(File.read "#{__DIR__}/data/test_file.ini").should eq({
         "general" => {
           "log_level" => "DEBUG",
         },
