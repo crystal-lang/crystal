a2f01cae7c8c8e3c3cd1efb67f8fb4f5527e7042
Jonne Haß <me@jhass.eu>
2016-05-29 11:44:34 -0400

    HTTP::ErrorHandler does not tell the client which exception occured by default

--- src/http/server/handlers/error_handler.cr
+++ src/http/server/handlers/error_handler.cr
@@ -1,15 +1,26 @@
 # A handler that invokes the next handler and, if that next handler raises
-# an exception, returns with a 500 (Internal Server Error) status code and
-# prints the exception with its backtrace to the response's body.
+# an exception, returns with a 500 (Internal Server Error) status code.
+#
+#  In verbose mode prints the exception with its backtrace to the response,
+#  else a generic error message is returned to the client. Use the LogHandler before this
+#  to log the exception on the server side.
 class HTTP::ErrorHandler < HTTP::Handler
+  def initialize(@verbose : Bool = false)
+  end
+
   def call(context)
     begin
       call_next(context)
     rescue ex : Exception
-      context.response.status_code = 500
-      context.response.content_type = "text/plain"
-      context.response.print("ERROR: ")
-      ex.inspect_with_backtrace(context.response)
+      if @verbose
+        context.response.reset
+        context.response.status_code = 500
+        context.response.content_type = "text/plain"
+        context.response.print("ERROR: ")
+        ex.inspect_with_backtrace(context.response)
+      else
+        context.response.respond_with_error
+      end
     end
   end
 end
