a9019b6a6cbd656ad55da016d8353726f2aabff9
Mirek Rusin <mirek@me.com>
2016-06-03 18:11:25 -0300

    Updates.

--- spec/std/uuid_spec.cr
+++ spec/std/uuid_spec.cr
@@ -1,6 +1,19 @@
 require "spec"
 
 describe "UUID" do
+  it "has working zero UUID" do
+    UUID.empty.should eq UUID.empty
+    UUID.empty.to_s.should eq "00000000-0000-0000-0000-000000000000"
+    UUID.empty.variant.should eq UUID::Variant::NCS
+  end
+
+  it "doesn't overwrite empty" do
+    empty = UUID.empty
+    empty.should eq empty
+    empty.decode "a01a5a94-7b52-4ca8-b310-382436650336"
+    UUID.empty.should_not eq empty
+  end
+
   it "can be built from strings" do
     UUID.new("c20335c3-7f46-4126-aae9-f665434ad12b").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
     UUID.new("c20335c37f464126aae9f665434ad12b").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
@@ -8,11 +21,24 @@ describe "UUID" do
     UUID.new("C20335C37F464126AAE9F665434AD12B").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
   end
 
+  it "should have correct variant and version" do
+    UUID.new("C20335C37F464126AAE9F665434AD12B").variant.should eq UUID::Variant::RFC4122
+    UUID.new("C20335C37F464126AAE9F665434AD12B").version.should eq UUID::Version::V4
+  end
+
+  it "supports different string formats" do
+    UUID.new("ee843b2656d8472bb3430b94ed9077ff").to_s.should eq "ee843b26-56d8-472b-b343-0b94ed9077ff"
+    UUID.new("3e806983-eca4-4fc5-b581-f30fb03ec9e5").to_s(UUID::Format::Hexstring).should eq "3e806983eca44fc5b581f30fb03ec9e5"
+    UUID.new("1ed1ee2f-ef9a-4f9c-9615-ab14d8ef2892").to_s(UUID::Format::URN).should eq "urn:uuid:1ed1ee2f-ef9a-4f9c-9615-ab14d8ef2892"
+  end
+
   it "compares to strings" do
     uuid = UUID.new "c3b46146eb794e18877b4d46a10d1517"
     ->{ uuid == "c3b46146eb794e18877b4d46a10d1517" }.call.should eq(true)
     ->{ uuid == "c3b46146-eb79-4e18-877b-4d46a10d1517" }.call.should eq(true)
     ->{ uuid == "C3B46146-EB79-4E18-877B-4D46A10D1517" }.call.should eq(true)
+    ->{ uuid == "urn:uuid:C3B46146-EB79-4E18-877B-4D46A10D1517" }.call.should eq(true)
+    ->{ uuid == "urn:uuid:c3b46146-eb79-4e18-877b-4d46a10d1517" }.call.should eq(true)
     ->{ UUID.new == "C3B46146-EB79-4E18-877B-4D46A10D1517" }.call.should eq(false)
   end
 
@@ -32,5 +58,23 @@ describe "UUID" do
     expect_raises(ArgumentError) { UUID.new == "2ab8ffc8f58749e197eda3e3d14e0 6c" }
     expect_raises(ArgumentError) { UUID.new == "2ab8ffc8f58749e197eda3e3d14e 06c" }
     expect_raises(ArgumentError) { UUID.new == "2ab8ffc8f58749e197eda3e3d14e-76c" }
+  end
+
+  it "should handle variant" do
+    uuid = UUID.new
+    expect_raises(ArgumentError) { uuid.variant = UUID::Variant::Unknown }
+    {% for variant in %w(NCS RFC4122 Microsoft Future) %}
+      uuid.variant = UUID::Variant::{{ variant.id }}
+      uuid.variant.should eq UUID::Variant::{{ variant.id }}
+    {% end %}
+  end
+
+  it "should handle version" do
+    uuid = UUID.new
+    expect_raises(ArgumentError) { uuid.version = UUID::Version::Unknown }
+    {% for version in %w(1 2 3 4 5) %}
+      uuid.version = UUID::Version::V{{ version.id }}
+      uuid.version.should eq UUID::Version::V{{ version.id }}
+    {% end %}
   end
 end
--- src/uuid.cr
+++ src/uuid.cr
@@ -5,19 +5,58 @@ require "./uuid/*"
 #
 # Supports custom variants with arbitrary 16 bytes as well as (RFC 4122)[https://www.ietf.org/rfc/rfc4122.txt] variant
 # versions.
-module UUID
-  # Raises `ArgumentError` if string `value` at index `i` doesn't contain hex digit followed by another hex digit.
-  # TODO: Move to String#digit?(...)
-  def self.string_has_hex_pair_at!(value : String, i)
-    unless value[i].hex? && value[i + 1].hex?
-      raise ArgumentError.new [
-        "Invalid hex character at position #{i * 2} or #{i * 2 + 1}",
-        "expected '0' to '9', 'a' to 'f' or 'A' to 'F'.",
-      ].join(", ")
-    end
+struct UUID
+  # Internal representation.
+  @data = StaticArray(UInt8, 16).new
+
+  # Generates RFC 4122 v4 UUID.
+  def initialize
+    initialize Version::V4
   end
 
-  def self.new(*args)
-    ::UUID::UUID.new *args
+  # Generates UUID from static 16-`bytes`.
+  def initialize(bytes : StaticArray(UInt8, 16))
+    @data = bytes
+  end
+
+  # Creates UUID from 16-`bytes` slice.
+  def initialize(bytes : Slice(UInt8))
+    raise ArgumentError.new "Invalid bytes length #{bytes.size}, expected 16." if bytes.size != 16
+    @data.to_unsafe.copy_from bytes
+  end
+
+  # Creates UUID from string `value`. See `UUID#decode(value : String)` for details on supported string formats.
+  def initialize(value : String)
+    decode value
+  end
+
+  # Returns 16-byte slice.
+  def to_slice
+    Slice(UInt8).new to_unsafe, 16
+  end
+
+  # Returns unsafe pointer to 16-bytes.
+  def to_unsafe
+    @data.to_unsafe
+  end
+
+  # Writes hyphenated format string to `io`.
+  def to_s(io : IO)
+    io << to_s
+  end
+
+  # Returns `true` if `other` string represents the same UUID, `false` otherwise.
+  def ==(other : String)
+    self == UUID.new other
+  end
+
+  # Returns `true` if `other` 16-byte slice represents the same UUID, `false` otherwise.
+  def ==(other : Slice(UInt8))
+    to_slice == other
+  end
+
+  # Returns `true` if `other` static 16 bytes represent the same UUID, `false` otherwise.
+  def ==(other : StaticArray(UInt8, 16))
+    self.==(Slice(UInt8).new other.to_unsafe, 16)
   end
 end
