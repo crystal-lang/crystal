093e71b503e4d5e6003b4ab8ef3ed66e061ca8fa
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-06-09 19:47:37 -0300

    Implement the new path search algorithm

--- spec/compiler/crystal_path/crystal_path_spec.cr
+++ spec/compiler/crystal_path/crystal_path_spec.cr
@@ -1,114 +1,93 @@
 require "../../spec_helper"
 
+private def assert_finds(search, results, relative_to = nil, path = __DIR__, file = __FILE__, line = __LINE__)
+  it "finds #{search.inspect}", file, line do
+    crystal_path = Crystal::CrystalPath.new(path)
+    relative_to = "#{__DIR__}/#{relative_to}" if relative_to
+    results = results.map { |result| "#{__DIR__}/#{result}" }
+    matches = crystal_path.find search, relative_to: relative_to
+    matches.should eq(results)
+  end
+end
+
+private def assert_doesnt_find(search, relative_to = nil, path = __DIR__, file = __FILE__, line = __LINE__)
+  it "doesn't finds #{search.inspect}", file, line do
+    crystal_path = Crystal::CrystalPath.new(path)
+    relative_to = "#{__DIR__}/#{relative_to}" if relative_to
+    expect_raises Exception, /can't find file/ do
+      crystal_path.find search, relative_to: relative_to
+    end
+  end
+end
+
 describe Crystal::CrystalPath do
-  it "finds file with .cr extension" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "test_files/file_one.cr"
-    matches.should eq(["#{__DIR__}/test_files/file_one.cr"])
-  end
+  assert_finds "test_files/file_one.cr", ["test_files/file_one.cr"]
+  assert_finds "test_files/file_one", ["test_files/file_one.cr"]
+  assert_finds "test_files/*", [
+    "test_files/file_one.cr",
+    "test_files/file_two.cr",
+  ]
+  assert_finds "test_files/**", [
+    "test_files/file_one.cr",
+    "test_files/file_two.cr",
+    "test_files/src/file_three.cr",
+    "test_files/src/test_files.cr",
+    "test_files/src/test_files/file_four.cr",
+    "test_files/src/test_files/another/another.cr",
+    "test_files/src/yet_another/yet_another.cr",
+    "test_files/test_folder/file_three.cr",
+    "test_files/test_folder/test_folder.cr",
+  ]
+  assert_finds "./file_two.cr", relative_to: "test_files/file_one.cr", results: [
+    "test_files/file_two.cr",
+  ]
+  assert_finds "./test_folder/file_three.cr", relative_to: "test_files/file_one.cr", results: [
+    "test_files/test_folder/file_three.cr",
+  ]
+  assert_finds "./test_folder/*", relative_to: "test_files/file_one.cr", results: [
+    "test_files/test_folder/file_three.cr",
+    "test_files/test_folder/test_folder.cr",
+  ]
+  assert_finds "../**", relative_to: "test_files/test_folder/file_three.cr", results: [
+    "test_files/file_one.cr",
+    "test_files/file_two.cr",
+    "test_files/src/file_three.cr",
+    "test_files/src/test_files.cr",
+    "test_files/src/test_files/file_four.cr",
+    "test_files/src/test_files/another/another.cr",
+    "test_files/src/yet_another/yet_another.cr",
+    "test_files/test_folder/file_three.cr",
+    "test_files/test_folder/test_folder.cr",
+  ]
+  assert_finds "../test_folder", relative_to: "test_files/test_folder/file_three.cr", results: [
+    "test_files/test_folder/test_folder.cr",
+  ]
 
-  it "finds file without .cr extension" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "test_files/file_one"
-    matches.should eq(["#{__DIR__}/test_files/file_one.cr"])
-  end
+  # For `require "foo"`:
+  # 1. foo.cr (to find something in the standard library)
+  assert_finds "crystal_path_spec", ["crystal_path_spec.cr"]
+  # 2. foo/src/foo.cr (to find something in a shard)
+  assert_finds "test_files", ["test_files/src/test_files.cr"]
 
-  it "finds all files with *" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "test_files/*"
-    matches.should eq([
-      "#{__DIR__}/test_files/file_one.cr",
-      "#{__DIR__}/test_files/file_two.cr",
-    ])
-  end
+  # For `require "foo/bar"`:
+  # 1. foo/bar.cr (to find something in the standard library)
+  assert_finds "test_files/file_one", ["test_files/file_one.cr"]
+  # 2. foo/src/bar.cr (to find something in a shard, non-namespaced structure)
+  assert_finds "test_files/file_three", ["test_files/src/file_three.cr"]
+  # 3. foo/src/foo/bar.cr (to find something in a shard, namespaced structure)
+  assert_finds "test_files/file_four", ["test_files/src/test_files/file_four.cr"]
 
-  it "finds all files with **" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "test_files/**"
-    matches.should eq([
-      "#{__DIR__}/test_files/file_one.cr",
-      "#{__DIR__}/test_files/file_two.cr",
-      "#{__DIR__}/test_files/test_folder/file_three.cr",
-      "#{__DIR__}/test_files/test_folder/test_folder.cr",
-    ])
-  end
+  # Nested searches
+  # a/1. foo.cr (to find something in the standard library (nested))
+  assert_finds "other_test_files", ["other_test_files/other_test_files.cr"]
+  # b/2. foo/src/bar/bar.cr (to find something in a shard, non-namespaced structure, nested)
+  assert_finds "test_files/yet_another", ["test_files/src/yet_another/yet_another.cr"]
+  # b/3. foo/src/foo/bar/bar.cr (to find something in a shard, namespaced structure, nested)
+  assert_finds "test_files/another", ["test_files/src/test_files/another/another.cr"]
 
-  it "finds file in directory with its basename" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "test_files/test_folder"
-    matches.should eq([
-      "#{__DIR__}/test_files/test_folder/test_folder.cr",
-    ])
-  end
-
-  it "doesn't find file relative to another one if not using ./" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    expect_raises Exception, /can't find file/ do
-      path.find "file_two.cr", relative_to: "#{__DIR__}/test_files/file_one.cr"
-    end
-  end
-
-  it "finds file relative to another one if using ./" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "./file_two.cr", relative_to: "#{__DIR__}/test_files/file_one.cr"
-    matches.should eq([
-      "#{__DIR__}/test_files/file_two.cr",
-    ])
-  end
-
-  it "doesn't find file relative to another one with directory if not using ./" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    expect_raises Exception, /can't find file/ do
-      path.find "test_folder/file_three.cr", relative_to: "#{__DIR__}/test_files/file_one.cr"
-    end
-  end
-
-  it "finds file relative to another one with directory if using ./" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "./test_folder/file_three.cr", relative_to: "#{__DIR__}/test_files/file_one.cr"
-    matches.should eq([
-      "#{__DIR__}/test_files/test_folder/file_three.cr",
-    ])
-  end
-
-  it "doesn't inds files with * relative to another one if not using ./" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    expect_raises Exception, /can't find file/ do
-      path.find "test_folder/*", relative_to: "#{__DIR__}/test_files/file_one.cr"
-    end
-  end
-
-  it "finds files with * relative to another one if using ./" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "./test_folder/*", relative_to: "#{__DIR__}/test_files/file_one.cr"
-    matches.should eq([
-      "#{__DIR__}/test_files/test_folder/file_three.cr",
-      "#{__DIR__}/test_files/test_folder/test_folder.cr",
-    ])
-  end
-
-  it "finds files with ** relative to another one" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    matches = path.find "../**", relative_to: "#{__DIR__}/test_files/test_folder/file_three.cr"
-    matches.should eq([
-      "#{__DIR__}/test_files/file_one.cr",
-      "#{__DIR__}/test_files/file_two.cr",
-      "#{__DIR__}/test_files/test_folder/file_three.cr",
-      "#{__DIR__}/test_files/test_folder/test_folder.cr",
-    ])
-  end
-
-  it "doesn't find file with .cr extension" do
-    path = Crystal::CrystalPath.new(__DIR__)
-    expect_raises Exception, /can't find file/ do
-      path.find "test_files/missing_file.cr"
-    end
-  end
-
-  it "ignore an empty directory path" do
-    path = Crystal::CrystalPath.new(":")
-    expect_raises Exception, /can't find file/ do
-      path.find __FILE__[1..-1]
-    end
-  end
+  assert_doesnt_find "file_two.cr"
+  assert_doesnt_find "test_folder/file_three.cr"
+  assert_doesnt_find "test_folder/*", relative_to: "#{__DIR__}/test_files/file_one.cr"
+  assert_doesnt_find "test_files/missing_file.cr"
+  assert_doesnt_find __FILE__[1..-1], path: ":"
 end
