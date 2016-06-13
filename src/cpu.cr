eb1e42a9be3beb5795fcbdc8e410f25cfdb6c1ab
Nax <max.bacoux@gmail.com>
2016-06-13 20:44:28 +0200

    Add checks for x86_64 and i686

--- src/cpu.cr
+++ src/cpu.cr
@@ -17,23 +17,27 @@ module System::CPU
     AVX2
   end
 
-  @@vendor_string = ""
+  @@vendor_string = "\x00" * 12
   @@vendor = Vendor::Unknown
   @@features = Features::None
   @@initialized = false
 
   def self.cpuid(fn : Int32, subfn : Int32 = 0)
-    buf = StaticArray(UInt32, 4).new
-    ptr = buf.to_unsafe.address
-    asm(%(
-      movl $1, %eax
-      movl $2, %ecx
-      cpuid
-      movl %eax, 0x0($0)
-      movl %ebx, 0x4($0)
-      movl %ecx, 0x8($0)
-      movl %edx, 0xc($0)) :: "r"(ptr), "r"(fn), "r"(subfn): "eax", "ebx", "ecx", "edx", "memory")
-    buf
+    ifdef x86_64 || i686
+      buf = StaticArray(UInt32, 4).new
+      ptr = buf.to_unsafe.address
+      asm(%(
+        movl $1, %eax
+        movl $2, %ecx
+        cpuid
+        movl %eax, 0x0($0)
+        movl %ebx, 0x4($0)
+        movl %ecx, 0x8($0)
+        movl %edx, 0xc($0)) :: "r"(ptr), "r"(fn), "r"(subfn): "eax", "ebx", "ecx", "edx", "memory")
+      buf
+    else
+      {{ raise "Unsupported platform, only x86_64 and i686 are supported. " }}
+    end
   end
 
   def self.vendor_string
@@ -93,45 +97,47 @@ module System::CPU
   end
 
   private def self.detect_cpu_features : Void
-    return if @@initialized
+    ifdef x86_64 || i686
+      return if @@initialized
 
-    vendor_slice = Slice(UInt8).new(12)
-    id0 = cpuid(0)
+      vendor_slice = Slice(UInt8).new(12)
+      id0 = cpuid(0)
 
-    vendor_slice[0] = (id0[1] >> 0).to_u8
-    vendor_slice[1] = (id0[1] >> 8).to_u8
-    vendor_slice[2] = (id0[1] >> 16).to_u8
-    vendor_slice[3] = (id0[1] >> 24).to_u8
-    vendor_slice[4] = (id0[3] >> 0).to_u8
-    vendor_slice[5] = (id0[3] >> 8).to_u8
-    vendor_slice[6] = (id0[3] >> 16).to_u8
-    vendor_slice[7] = (id0[3] >> 24).to_u8
-    vendor_slice[8] = (id0[2] >> 0).to_u8
-    vendor_slice[9] = (id0[2] >> 8).to_u8
-    vendor_slice[10] = (id0[2] >> 16).to_u8
-    vendor_slice[11] = (id0[2] >> 24).to_u8
+      vendor_slice[0] = (id0[1] >> 0).to_u8
+      vendor_slice[1] = (id0[1] >> 8).to_u8
+      vendor_slice[2] = (id0[1] >> 16).to_u8
+      vendor_slice[3] = (id0[1] >> 24).to_u8
+      vendor_slice[4] = (id0[3] >> 0).to_u8
+      vendor_slice[5] = (id0[3] >> 8).to_u8
+      vendor_slice[6] = (id0[3] >> 16).to_u8
+      vendor_slice[7] = (id0[3] >> 24).to_u8
+      vendor_slice[8] = (id0[2] >> 0).to_u8
+      vendor_slice[9] = (id0[2] >> 8).to_u8
+      vendor_slice[10] = (id0[2] >> 16).to_u8
+      vendor_slice[11] = (id0[2] >> 24).to_u8
 
-    @@vendor_string = String.new(vendor_slice)
+      @@vendor_string = String.new(vendor_slice)
 
-    if @@vendor_string == "GenuineIntel"
-      @@vendor = Vendor::Intel
-    elsif @@vendor_string == "AuthenticAMD"
-      @@vendor = Vendor::AMD
+      if @@vendor_string == "GenuineIntel"
+        @@vendor = Vendor::Intel
+      elsif @@vendor_string == "AuthenticAMD"
+        @@vendor = Vendor::AMD
+      end
+
+      id1 = cpuid(1)
+      id7 = cpuid(7)
+
+      add_feature Features::SSE, id1[3], 25
+      add_feature Features::SSE2, id1[3], 26
+      add_feature Features::SSE3, id1[2], 0
+      add_feature Features::SSSE3, id1[2], 9
+      add_feature Features::SSE4_1, id1[2], 19
+      add_feature Features::SSE4_2, id1[2], 20
+      add_feature Features::AVX, id1[2], 28
+      add_feature Features::AVX2, id7[1], 5
+
+      @@initialized = true
     end
-
-    id1 = cpuid(1)
-    id7 = cpuid(7)
-
-    add_feature Features::SSE, id1[3], 25
-    add_feature Features::SSE2, id1[3], 26
-    add_feature Features::SSE3, id1[2], 0
-    add_feature Features::SSSE3, id1[2], 9
-    add_feature Features::SSE4_1, id1[2], 19
-    add_feature Features::SSE4_2, id1[2], 20
-    add_feature Features::AVX, id1[2], 28
-    add_feature Features::AVX2, id7[1], 5
-
-    @@initialized = true
   end
 
   private def self.add_feature(feature : Features, id : UInt32, bit : Int32)
