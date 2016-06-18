2b947350ed165d279644fb11aaade0af2d2b4174
Jonne Haß <me@jhass.eu>
2016-06-18 15:37:25 +0200

    Replace two more echo -n usages with printf
    related to #2869

--- src/openssl/lib_crypto.cr
+++ src/openssl/lib_crypto.cr
@@ -237,7 +237,7 @@ lib LibCrypto
   fun x509v3_ext_print = X509V3_EXT_print(out : Bio*, ext : X509_EXTENSION, flag : Int, indent : Int) : Int
 end
 
-{% if `(pkg-config --atleast-version=1.0.2 libcrypto && echo -n true) || echo -n false` == "true" %}
+{% if `(pkg-config --atleast-version=1.0.2 libcrypto && printf %s true) || printf %s false` == "true" %}
 lib LibCrypto
   OPENSSL_102 = true
 end
