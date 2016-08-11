3e80dbcebc8e84dc252c70e94325b00be1908a34
Kenichi Kamiya <kachick1@gmail.com>
2016-08-12 05:06:43 +0900

    Improve a spec fixture

--- spec/std/hash_spec.cr
+++ spec/std/hash_spec.cr
@@ -206,13 +206,13 @@ describe "Hash" do
 
   describe "has_value?" do
     it "returns true if contains the value" do
-      a = {1 => 2}
-      a.has_value?(2).should be_true
+      a = {1 => 2, 3 => 4, 5 => 6}
+      a.has_value?(4).should be_true
     end
 
     it "returns false if does not contain the value" do
-      a = {1 => 2}
-      a.has_value?(1).should be_false
+      a = {1 => 2, 3 => 4, 5 => 6}
+      a.has_value?(3).should be_false
     end
   end
 
