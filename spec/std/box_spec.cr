4d78a81bb0f980fa23ff7daa7173e6a06312550d
Konstantin Makarchev <kostya27@gmail.com>
2016-09-04 21:14:46 +0300

    add Box#box

--- spec/std/box_spec.cr
+++ spec/std/box_spec.cr
@@ -6,4 +6,10 @@ describe "Box" do
     box = Box.box(a)
     Box(Int32).unbox(box).should eq(1)
   end
+
+  it "boxed and unboxed" do
+    a = 1
+    box = Box(Int32).new(a)
+    Box(Int32).unbox(box.box).should eq(1)
+  end
 end
