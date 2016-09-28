3f8435814bccf881f0d6a0abbd547b336470c8ea
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-09-28 19:11:45 -0300

    Added docs to OAuth2

--- src/oauth2/client.cr
+++ src/oauth2/client.cr
@@ -1,3 +1,57 @@
+# An OAuth2 client.
+#
+# For a quick example of how to authenticate an `HTTP::Client` with OAuth2 if
+# you already have an access token, check the `OAuth2` module description.
+#
+# This class also provides methods to build authorize URIs
+# and get access tokens with different methods, as specified by
+# [RFC6749](https://tools.ietf.org/html/rfc6749).
+#
+# ### Example
+#
+# ```
+# require "oauth2"
+#
+# client_id = "some_client_id"
+# client_secret = "some_client_secret"
+# redirect_uri = "http://some.callback"
+#
+# # Create oauth client, optionally pass custom URIs if needed,
+# # if the authorize or token URIs are not the standard ones
+# oauth2_client = OAuth2::Client.new("api.example.com", client_id, client_secret,
+#   redirect_uri: redirect_uri)
+#
+# # Build an authorize URI
+# authorize_uri = oauth2_client.get_authorize_uri
+#
+# # Redirect the user to `authorize_uri`...
+# #
+# # ...
+# #
+# # When http://some.callback is hit, once the user authorized the access,
+# # we resume our logic to finally get an access token. The callback URL
+# # should receive an `authorization_code` parameter that we need to use.
+# authorization_code = request.params["code"]
+#
+# # Get the access token
+# access_token = oauth2_client.get_access_token_using_authorization_code(authorization_code)
+#
+# # Probably save the access token for reuse... This can be done
+# # with `to_json` and `from_json`.
+#
+# # Use the token to authenticate an HTTP::Client
+# client = HTTP::Client.new("api.example.com", tls: true)
+# access_token.authenticate(client)
+#
+# # And do requests as usual
+# client.get "/some_path"
+#
+# # If the token expires, we can refresh it
+# new_access_token = oauth2_client.get_access_token_using_refresh_token(access_token.refresh_token)
+# ```
+#
+# You can also use an `OAuth2::Session` to automatically refresh expired
+# tokens before each request.
 class OAuth2::Client
   def initialize(@host : String, @client_id : String, @client_secret : String,
                  @port = 443,
@@ -7,7 +61,9 @@ class OAuth2::Client
                  @redirect_uri : String? = nil)
   end
 
-  def get_authorize_uri(scope = nil, state = nil)
+  # Builds an authorize URI, as specified by
+  # [RFC6749, Section 4.1.1](https://tools.ietf.org/html/rfc6749#section-4.1.1)
+  def get_authorize_uri(scope = nil, state = nil) : String
     query = HTTP::Params.build do |form|
       form.add "client_id", @client_id
       form.add "redirect_uri", @redirect_uri
@@ -19,7 +75,9 @@ class OAuth2::Client
     URI.new(@scheme, @host, @port, @authorize_uri, query).to_s
   end
 
-  def get_access_token_using_authorization_code(authorization_code)
+  # Gets an access token using an authorization code, as specified by
+  # [RFC6749, Section 4.1.1](https://tools.ietf.org/html/rfc6749#section-4.1.3)
+  def get_access_token_using_authorization_code(authorization_code) : AccessToken
     get_access_token do |form|
       form.add("redirect_uri", @redirect_uri)
       form.add("grant_type", "authorization_code")
@@ -27,7 +85,9 @@ class OAuth2::Client
     end
   end
 
-  def get_access_token_using_refresh_token(refresh_token, scope = nil)
+  # Gets an access token using a refresh token, as specified by
+  # [RFC6749, Section 6](https://tools.ietf.org/html/rfc6749#section-6)
+  def get_access_token_using_refresh_token(refresh_token, scope = nil) : AccessToken
     get_access_token do |form|
       form.add("grant_type", "refresh_token")
       form.add("refresh_token", refresh_token)
@@ -35,7 +95,9 @@ class OAuth2::Client
     end
   end
 
-  def get_access_token_using_client_credentials(scope = nil)
+  # Gets an access token using client credentials, as specified by
+  # [RFC 6749, Section 4.4.2](https://tools.ietf.org/html/rfc6749#section-4.4.2)
+  def get_access_token_using_client_credentials(scope = nil) : AccessToken
     get_access_token do |form|
       form.add("grant_type", "client_credentials")
       form.add("scope", scope) unless scope.nil?
