a27bda901fcf324233d8723357f5b2bd85c7ffd3
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-10-04 19:04:58 -0300

    Mutex is now thread safe

--- src/mutex.cr
+++ src/mutex.cr
@@ -1,45 +1,54 @@
 # A fiber-safe mutex.
-#
-# TODO: this isn't thread-safe yet.
 class Mutex
   @mutex_fiber : Fiber?
+  @thread_mutex = Thread::Mutex.new
 
   def initialize
     @lock_count = 0
   end
 
   def lock
+    @thread_mutex.lock
     mutex_fiber = @mutex_fiber
     current_fiber = Fiber.current
 
     if !mutex_fiber
       @mutex_fiber = current_fiber
+      @thread_mutex.unlock
     elsif mutex_fiber == current_fiber
       @lock_count += 1 # recursive lock
+      @thread_mutex.unlock
     else
       queue = @queue ||= Deque(Fiber).new
       queue << current_fiber
-      Scheduler.reschedule
+      @thread_mutex.unlock
+      Scheduler.current.reschedule
     end
 
     nil
   end
 
   def unlock
+    @thread_mutex.lock
+
     unless @mutex_fiber == Fiber.current
+      @thread_mutex.unlock
       raise "Attempt to unlock a mutex which is not locked"
     end
 
     if @lock_count > 0
       @lock_count -= 1
+      @thread_mutex.unlock
       return
     end
 
     if fiber = @queue.try &.shift?
       @mutex_fiber = fiber
-      Scheduler.enqueue fiber
+      @thread_mutex.unlock
+      Scheduler.current.enqueue fiber
     else
       @mutex_fiber = nil
+      @thread_mutex.unlock
     end
 
     nil
