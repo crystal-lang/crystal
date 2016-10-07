39859999e0bfc9c0e257c2738e4bc9ae406610fa
Carlos Gajardo <cjgajard@gmail.com>
2016-10-06 22:32:10 -0300

    Implement no-copy subarray iteration

--- spec/std/indexable_spec.cr
+++ spec/std/indexable_spec.cr
@@ -3,7 +3,7 @@ require "spec"
 class SafeIndexable
   include Indexable(Int32)
 
-  getter size
+  property size
 
   def initialize(@size : Int32)
   end
@@ -33,5 +33,67 @@ describe Indexable do
   it "does rindex with big offset" do
     indexable = SafeIndexable.new(3)
     indexable.rindex(0, 100).should be_nil
+  end
+
+  it "iterates throught a subset of its elements (#3386)" do
+    indexable = SafeIndexable.new(5)
+    last_element = nil
+
+    return_value = indexable.each(start: 2, count: 3) do |elem|
+      last_element = elem
+    end
+
+    return_value.should eq(indexable)
+    last_element.should eq(4)
+  end
+
+  it "iterates until its size (#3386)" do
+    indexable = SafeIndexable.new(5)
+    last_element = nil
+
+    indexable.each(start: 3, count: 999) do |elem|
+      last_element = elem
+    end
+
+    last_element.should eq(4)
+  end
+
+  it "iterates until its size, having mutated (#3386)" do
+    indexable = SafeIndexable.new(10)
+    last_element = nil
+
+    indexable.each(start: 3, count: 999) do |elem|
+      indexable.size += 1 if elem <= 5
+      # size is incremented 3 times
+      last_element = elem
+    end
+
+    # last was 9, but now is 12.
+    last_element.should eq(12)
+  end
+
+  it "iterates until its size, having mutated (#3386)" do
+    indexable = SafeIndexable.new(10)
+    last_element = nil
+
+    indexable.each(start: 3, count: 5) do |elem|
+      indexable.size += 1
+      last_element = elem
+    end
+
+    # last element iterated is still 7.
+    last_element.should eq(7)
+  end
+
+  it "iterates within a range of indices (#3386)" do
+    indexable = SafeIndexable.new(5)
+    last_element = nil
+
+    return_value = indexable.each(within: 2..3) do |elem|
+      last_element = elem
+    end
+
+    return_value.should eq(indexable)
+    last_element.should eq(3)
   end
 end
