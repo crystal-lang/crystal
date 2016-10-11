416b0e74ff655e3a69194c881bd75bf0bbdd4b78
Laurent Vallar <val@zbla.net>
2016-10-11 22:28:09 +0200

    Always invoke gets_to_end, thx @asterite & @waj.

--- src/file.cr
+++ src/file.cr
@@ -417,20 +417,8 @@ class File < IO::FileDescriptor
   # ```
   def self.read(filename, encoding = nil, invalid = nil) : String
     File.open(filename, "r") do |file|
-      if encoding
-        file.set_encoding(encoding, invalid: invalid)
-        file.gets_to_end
-      else
-        {% if flag?(:linux) %}
-        return file.gets_to_end if File.expand_path(filename).starts_with?("/proc")
-        {% end %}
-
-        size = file.size.to_i
-        String.new(size) do |buffer|
-          file.read Slice.new(buffer, size)
-          {size.to_i, 0}
-        end
-      end
+      file.set_encoding(encoding, invalid: invalid) if encoding
+      file.gets_to_end
     end
   end
 
