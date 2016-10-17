5c3ba1f267965caf253e5f4823f363b3854db28d
chocolateboy <chocolate@cpan.org>
2016-10-17 19:43:46 +0100

    fix doc typo for Number#divmod

--- src/number.cr
+++ src/number.cr
@@ -155,7 +155,7 @@ struct Number
   #
   # ```
   # 11.divmod(3)  # => {3, 2}
-  # 11.divmod(-3) # => {-4, 1}
+  # 11.divmod(-3) # => {-4, -1}
   # ```
   def divmod(number)
     {(self / number).floor, self % number}
