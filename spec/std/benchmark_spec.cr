d4d0bab9e92605cbf0968c0f1108386ee79c05c5
RX14 <chris@rx14.co.uk>
2016-10-29 13:54:57 +0100

    Fix IPS::Entry#human_mean for ips < 0.1
    Also cleans up the code to be more readable by using variable
    assignment instead of yielding a tuple.

--- spec/std/benchmark_spec.cr
+++ spec/std/benchmark_spec.cr
@@ -68,6 +68,7 @@ private def h_mean(mean)
 end
 
 describe Benchmark::IPS::Entry, "#human_mean" do
+  assert { h_mean(0.01234567890123).should eq("  0.01 ") }
   assert { h_mean(0.12345678901234).should eq("  0.12 ") }
 
   assert { h_mean(1.23456789012345).should eq("  1.23 ") }
