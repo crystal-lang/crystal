582b4533d378082c8a033e813b1769de6a259e16
Bryan Mulvihill <mulvihill@informz.com>
2016-11-06 20:43:04 -0500

    Update from_value when Flags

--- spec/std/enum_spec.cr
+++ spec/std/enum_spec.cr
@@ -90,11 +90,20 @@ describe Enum do
     SpecEnum.from_value?(3).should be_nil
   end
 
-  it "does from_value" do
-    SpecEnum.from_value(0).should eq(SpecEnum::One)
-    SpecEnum.from_value(1).should eq(SpecEnum::Two)
-    SpecEnum.from_value(2).should eq(SpecEnum::Three)
-    expect_raises { SpecEnum.from_value(3) }
+  describe "from_value" do
+    it "does from_value" do
+      SpecEnum.from_value(0).should eq(SpecEnum::One)
+      SpecEnum.from_value(1).should eq(SpecEnum::Two)
+      SpecEnum.from_value(2).should eq(SpecEnum::Three)
+      expect_raises { SpecEnum.from_value(3) }
+    end
+
+    it "for flags enum" do
+      SpecEnumFlags.from_value(0).should eq(SpecEnumFlags::None)
+      SpecEnumFlags.from_value(1).should eq(SpecEnumFlags::One)
+      SpecEnumFlags.from_value(2).should eq(SpecEnumFlags::Two)
+      SpecEnumFlags.from_value(3).should eq(SpecEnumFlags::One | SpecEnumFlags::Two)
+    end
   end
 
   it "has hash" do
