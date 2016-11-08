c40e7359dd2528edf9b04dbb80243a2cb442d80b
Guilherme Bernal <dev@lbguilherme.com>
2016-11-07 21:57:12 -0300

    Raises on failure to run a process (Fixes #3517)

--- src/process.cr
+++ src/process.cr
@@ -228,8 +228,12 @@ class Process
       end
     end
 
+    parent_sock, child_sock = IO.pipe
+
     @pid = Process.fork_internal(run_hooks: false) do
       begin
+        parent_sock.close
+        child_sock.close_on_exec = true
         Process.exec_internal(
           command,
           argv,
@@ -240,12 +244,22 @@ class Process
           fork_error || error,
           chdir
         )
+      rescue ex : Errno
+        value = ex.errno
+        child_sock.write(Bytes.new(pointerof(value).as(UInt8*), 4))
       rescue ex
         ex.inspect_with_backtrace STDERR
-        LibC._exit 127 # TODO: remove after 0.19
       ensure
         LibC._exit 127
       end
+    end
+
+    child_sock.close
+    slice = Bytes.new(4)
+    if parent_sock.read(slice) == 4
+      parent_sock.close
+      Errno.value = slice.pointer(4).as(Int32*).value
+      raise Errno.new("execvp")
     end
 
     @waitpid_future = Event::SignalChildHandler.instance.waitpid(pid)
