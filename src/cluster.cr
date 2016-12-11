c5cc9ba27b8d305a1b6549c9802b2b23feb478ff
Adrien THIERRY <adrien-thierry@users.noreply.github.com>
2016-12-12 00:19:34 +0100

    Update cluster.cr
    Removed redundant return

--- src/cluster.cr
+++ src/cluster.cr
@@ -2,12 +2,13 @@ class Cluster
 
     def self.fork (env : Hash)
         env["FORKED"] = "1"
-        return Process.fork { Process.run(PROGRAM_NAME, nil, env, true, false, true, true, true, nil ) }
+        Process.fork { Process.run(PROGRAM_NAME, nil, env, true, false, true, true, true, nil ) }
     end
 
     def self.master?
      (ENV["FORKED"]? || "0") == "0"
     end
+  
     def self.worker?
      (ENV["FORKED"]? || "0") == "1"
     end
