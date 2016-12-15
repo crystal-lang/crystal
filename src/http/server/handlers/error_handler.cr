dd1dca4d17e15a098b917e0bad9299f144b3986b
Andrew Hamon <andrew@hamon.cc>
2016-12-15 16:11:09 -0500

    Make HTTP::Handler a module
    Because HTTP::Handler was just an interface, it is less restrictive on
    applications to be an includeable module rather than in inheritable
    class.

--- src/http/server/handlers/error_handler.cr
+++ src/http/server/handlers/error_handler.cr
@@ -4,7 +4,9 @@
 #  In verbose mode prints the exception with its backtrace to the response,
 #  else a generic error message is returned to the client. Use the LogHandler before this
 #  to log the exception on the server side.
-class HTTP::ErrorHandler < HTTP::Handler
+class HTTP::ErrorHandler
+  include HTTP::Handler
+
   def initialize(@verbose : Bool = false)
   end
 
