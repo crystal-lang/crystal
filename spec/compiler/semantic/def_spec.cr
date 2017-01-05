34fd7438d02411b8ee862487b6ae4e62d6c445c0
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-01-05 09:34:39 +0900

    Add spec for failed type restriction

--- spec/compiler/semantic/def_spec.cr
+++ spec/compiler/semantic/def_spec.cr
@@ -171,6 +171,16 @@ describe "Semantic: def" do
       "undefined method"
   end
 
+  it "errors when type restriction is incompatible with default value" do
+    assert_error "
+      def foo(x : Int64 = 1)
+      end
+
+      foo
+      ",
+      "can't restrict Int32 to Int64"
+  end
+
   it "types call with global scope" do
     assert_type("
       def bar
