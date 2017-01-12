2e077a3f4d06524f6051ce9690c0f650c3ecd6e6
David Kuo <s50407s@gmail.com>
2017-01-12 20:34:33 +0800

    [Compiler] Get all ancestors of a type in macro

--- src/compiler/crystal/macros/methods.cr
+++ src/compiler/crystal/macros/methods.cr
@@ -1305,6 +1305,8 @@ module Crystal
         interpret_argless_method(method, args) { TypeNode.type_vars(type) }
       when "instance_vars"
         interpret_argless_method(method, args) { TypeNode.instance_vars(type) }
+      when "ancestors"
+        interpret_argless_method(method, args) { TypeNode.ancestors(type) }
       when "superclass"
         interpret_argless_method(method, args) { TypeNode.superclass(type) }
       when "subclasses"
@@ -1455,6 +1457,10 @@ module Crystal
       else
         ArrayLiteral.new
       end
+    end
+
+    def self.ancestors(type)
+      ArrayLiteral.map(type.ancestors) { |ancestor| TypeNode.new(ancestor) }
     end
 
     def self.superclass(type)
