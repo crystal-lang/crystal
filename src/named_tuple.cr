b5b3d88baee1714d15d2cb504e1ca152f0d3bfba
mjago <martyn.jago@btinternet.com>
2017-01-17 19:26:20 +0000

    Update example with bcardiff's suggestions

--- src/named_tuple.cr
+++ src/named_tuple.cr
@@ -41,12 +41,11 @@ struct NamedTuple
   # Here the Int32 | String union is cast to Int32.
   #
   # ```
-  # num_or_str : Int32 | String
-  # num_or_str = 42
+  # num_or_str = 42.as(Int32 | String)
   # NamedTuple(name: String, val: Int32).from({"name" => "number", "val" => num_or_str}) # => {name: "number", val: 42}
   #
-  # num_or_str = "a string"
-  # NamedTuple(name: String, val: Int32).from({"name" => "number", "val" => num_or_str}) # => can't cast String to Int32
+  # num_or_str = "a string".as(Int32 | String)
+  # NamedTuple(name: String, val: Int32).from({"name" => "number", "val" => num_or_str}) # raises TypeCastError (cast from String to Int32 failed)
   # ```
   # See also: `#from`.
   def self.from(hash : Hash)
