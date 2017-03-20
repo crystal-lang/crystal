c907c9c544ec9949b417b9b30165a5438cc0894a
Sijawusz Pur Rahnama <sija@sija.pl>
2017-03-20 02:43:32 +0100

    Print sigfaults and exceptions to STDERR
    Bind `LibC.dprintf` function

--- src/lib_c/i686-linux-musl/c/stdio.cr
+++ src/lib_c/i686-linux-musl/c/stdio.cr
@@ -3,6 +3,7 @@ require "./stddef"
 
 lib LibC
   fun printf(x0 : Char*, ...) : Int
+  fun dprintf(fd : Int, format : Char*, ...) : Int
   fun rename(x0 : Char*, x1 : Char*) : Int
   fun snprintf(x0 : Char*, x1 : SizeT, x2 : Char*, ...) : Int
 end
