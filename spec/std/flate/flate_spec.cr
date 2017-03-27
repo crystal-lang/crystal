6a45e084db8f00ea6ce56ebe7bb8be0136cef446
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-03-28 00:52:39 +0900

    Flate: try to inflate even if @stream.avail_in is 0 (#4193)
    Fix #4192
    
    Probably LibZ::Error::STREAM_END is only way to detect the end of a stream.

--- spec/std/flate/flate_spec.cr
+++ spec/std/flate/flate_spec.cr
@@ -2,6 +2,26 @@ require "spec"
 require "flate"
 
 module Flate
+  describe Reader do
+    it "should read byte by byte (#4192)" do
+      io = IO::Memory.new
+      "cbc9cc4b350402ae1c20c30808b800".scan(/../).each do |match|
+        io.write_byte match[0].to_u8(16)
+      end
+      io.rewind
+
+      reader = Reader.new(io)
+
+      str = String::Builder.build do |builder|
+        while b = reader.read_byte
+          builder.write_byte b
+        end
+      end
+
+      str.should eq("line1111\nline2222\n")
+    end
+  end
+
   describe Writer do
     it "should be able to write" do
       message = "this is a test string !!!!\n"
