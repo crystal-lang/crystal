9265d52c94312b05ee304b63242b180deb65c616
Vesa Kaihlavirta <vegai@iki.fi>
2017-03-31 15:43:54 +0300

    Remove rest of ... specs

--- spec/compiler/normalize/range_literal_spec.cr
+++ spec/compiler/normalize/range_literal_spec.cr
@@ -4,8 +4,4 @@ describe "Normalize: range literal" do
   it "normalizes not exclusive" do
     assert_expand "1..2", "::Range.new(1, 2, false)"
   end
-
-  it "normalizes exclusive" do
-    assert_expand "1...2", "::Range.new(1, 2, true)"
-  end
 end
