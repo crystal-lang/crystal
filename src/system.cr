6e19fbd12d3230b83bca03d5933765e38191bbca
Wesley Moxam <wes@4ormat.com>
2017-05-26 09:50:18 -0400

    Use sysctl to return the number of CPUs on BSD systems
    See:
      http://man.openbsd.org/sysctl.3
      https://www.freebsd.org/cgi/man.cgi?query=sysctl&sektion=3

--- src/system.cr
+++ src/system.cr
@@ -1,4 +1,7 @@
 require "c/unistd"
+{% if flag?(:freebsd) || flag?(:openbsd) %}
+  require "c/sysctl"
+{% end %}
 
 module System
   # Returns the hostname.
@@ -24,7 +27,21 @@ module System
   # ```
   # System.cpu_count # => 4
   # ```
-  def self.cpu_count
-    LibC.sysconf(LibC::SC_NPROCESSORS_ONLN)
-  end
+  {% if flag?(:freebsd) || flag?(:openbsd) %}
+    def self.cpu_count
+      mib = Int32[LibC::CTL_HW, LibC::HW_NCPU]
+      ncpus = 0
+      size = sizeof(Int32).to_u64
+
+      if LibC.sysctl(mib, 2, pointerof(ncpus), pointerof(size), nil, 0) == 0
+        ncpus
+      else
+        -1
+      end
+    end
+  {% else %} # linux, etc
+    def self.cpu_count
+      LibC.sysconf(LibC::SC_NPROCESSORS_ONLN)
+    end
+  {% end %}
 end
