83b10d7bfb306aef7574039efced964c2399189d
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2017-05-30 18:34:54 +0300

    It is absolutely superfluous to assume that we may get another exception when it's rescued.

--- spec/std/raise_spec.cr
+++ spec/std/raise_spec.cr
@@ -7,19 +7,19 @@ end
 
 describe "raise" do
   it "should set exception's callstack" do
-    new_ex = expect_raises Exception, "without callstack" do
+    ex = expect_raises Exception, "without callstack" do
       raise Exception.new "without callstack"
     end
-    new_ex.callstack.should_not be_nil
+    ex.callstack.should_not be_nil
   end
 
   it "shouldn't overwrite the callstack" do
     ex = Exception.new "with callstack"
     ex.callstack = CallStack.new
     callstack_to_match = ex.callstack.clone
-    new_ex = expect_raises Exception, "with callstack" do
+    expect_raises Exception, "with callstack" do
       raise ex
     end
-    new_ex.callstack.should eq(callstack_to_match)
+    ex.callstack.should eq(callstack_to_match)
   end
 end
