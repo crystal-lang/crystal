f3f87cec3d39f1f0d3cf2d2a73ae2669b5d6364e
Wes <me@wmoxam.com>
2017-06-04 20:49:25 -0400

    Adds Crystal::System::System namespace

--- src/system.cr
+++ src/system.cr
@@ -1,7 +1,4 @@
-require "c/unistd"
-{% if flag?(:freebsd) || flag?(:openbsd) %}
-  require "c/sysctl"
-{% end %}
+require "crystal/system/system"
 
 module System
   # Returns the hostname.
@@ -13,13 +10,7 @@ module System
   # System.hostname # => "host.example.org"
   # ```
   def self.hostname
-    String.new(255) do |buffer|
-      unless LibC.gethostname(buffer, LibC::SizeT.new(255)) == 0
-        raise Errno.new("Could not get hostname")
-      end
-      len = LibC.strlen(buffer)
-      {len, len}
-    end
+    Crystal::System::System.hostname
   end
 
   # Returns the number of logical processors available to the system.
@@ -27,21 +18,7 @@ module System
   # ```
   # System.cpu_count # => 4
   # ```
-  {% if flag?(:freebsd) || flag?(:openbsd) %}
-    def self.cpu_count
-      mib = Int32[LibC::CTL_HW, LibC::HW_NCPU]
-      ncpus = 0
-      size = sizeof(Int32).to_u64
-
-      if LibC.sysctl(mib, 2, pointerof(ncpus), pointerof(size), nil, 0) == 0
-        ncpus
-      else
-        -1
-      end
-    end
-  {% else %} # linux, etc
-    def self.cpu_count
-      LibC.sysconf(LibC::SC_NPROCESSORS_ONLN)
-    end
-  {% end %}
+  def self.cpu_count
+    Crystal::System::System.cpu_count
+  end
 end
