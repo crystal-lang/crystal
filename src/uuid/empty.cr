0f7d36f4b81e41d32b0b18afbfe6849544dbca5f
Jack Thorne <jack@fastly.com>
2017-06-07 10:17:45 -0500

    cleanup some code left over from wip

--- src/uuid/empty.cr
+++ src/uuid/empty.cr
@@ -1,6 +1,5 @@
 struct UUID
   def self.empty
-    #initialize UUID::Version::V4, StaticArray(UInt8, 16).new(0_u8)
     new StaticArray(UInt8, 16).new(0_u8)
   end
 end
--- src/uuid/rfc4122.cr
+++ src/uuid/rfc4122.cr
@@ -21,19 +21,6 @@ struct UUID
     V5 = 5
   end
 
-  # Generates RFC 4122 UUID `variant` with specified `version`.
-#  def initialize(@bytes : StaticArray(UInt8, 16), version = Version::V4)
-#    #@bytes.to_unsafe.copy_from SecureRandom.random_bytes(16).pointer(16), 16
-#
-#    case version
-#    when Version::V4
-#      variant = Variant::RFC4122
-#      version = Version::V4
-#    else
-#      raise ArgumentError.new "Creating #{version} not supported."
-#    end
-#  end
-
   # Returns version based on provided 6th `byte` (0-indexed).
   def self.byte_version(byte : UInt8)
     case byte >> 4
