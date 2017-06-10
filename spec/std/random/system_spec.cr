6e3727a054b4d41a9aeeb525d29c6ccb8093a9d6
Oleh Prypin <oleh@pryp.in>
2017-06-11 00:12:33 +0300

    Change Random::System to a module
    Fixes #4505

--- spec/std/random/system_spec.cr
+++ spec/std/random/system_spec.cr
@@ -2,15 +2,13 @@ require "spec"
 require "random/system"
 
 describe "Random::System" do
-  rng = Random::System.new
-
   it "returns random number from the secure system source" do
-    rng.next_u.should be_a(Int::Unsigned)
+    Random::System.next_u.should be_a(Int::Unsigned)
 
-    x = rng.rand(123456...654321)
+    x = Random::System.rand(123456...654321)
     x.should be >= 123456
     x.should be < 654321
 
-    rng.rand(Int64::MAX / 2).should be <= (Int64::MAX / 2)
+    Random::System.rand(Int64::MAX / 2).should be <= (Int64::MAX / 2)
   end
 end
