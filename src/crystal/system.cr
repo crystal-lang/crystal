11e76e49f2f48f370c9e82c48c59cd8343e25838
Wesley Moxam <wes@4ormat.com>
2017-06-14 22:03:44 -0400

    Fix BSD cpu count (#4466)
    * Use sysctl to return the number of CPUs on BSD systems
    
    See:
      http://man.openbsd.org/sysctl.3
      https://www.freebsd.org/cgi/man.cgi?query=sysctl&sektion=3

---
+++ src/crystal/system.cr
@@ -0,0 +1,20 @@
+module Crystal
+  # :nodoc
+  module System
+    # Returns the hostname
+    # def self.hostname
+
+    # Returns the number of logical processors available to the system.
+    #
+    # def self.cpu_count
+  end
+end
+
+require "./system/unix/hostname"
+
+{% if flag?(:freebsd) || flag?(:openbsd) %}
+  require "./system/unix/sysctl_cpucount"
+{% else %}
+  # TODO: restrict on flag?(:unix) after crystal > 0.22.0 is released
+  require "./system/unix/sysconf_cpucount"
+{% end %}
