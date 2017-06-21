e0b9fa2b9de89b8566c6052a173c0c8c26749c80
Brian J. Cardiff <bcardiff@manas.tech>
2017-06-21 00:48:15 -0300

    Update docs

--- src/crystal/system/time.cr
+++ src/crystal/system/time.cr
@@ -4,10 +4,11 @@ module Crystal
     # :nodoc:
     module Time
       # Returns the number of seconds that you must add to UTC to get local time.
-      # *seconds* are absolutes.
+      # *seconds* are measured from `0001-01-01 00:00:00`.
       # def self.compute_utc_offset(seconds)
 
-      # Returns the current utc time meassured in absolute `{seconds, tenth_microsecond}`
+      # Returns the current UTC time measured in `{seconds, tenth_microsecond}`
+      # since `0001-01-01 00:00:00`.
       # def self.compute_utc_second_and_tenth_microsecond
     end
   end
