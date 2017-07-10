06c202f27284bf6228eea23e4f0f97a9ac0ea0e4
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-07-10 19:10:11 +0900

    Format: raise an error if @paren_count > 0 on finish

--- src/compiler/crystal/tools/formatter.cr
+++ src/compiler/crystal/tools/formatter.cr
@@ -4330,6 +4330,8 @@ module Crystal
     end
 
     def finish
+      raise "BUG: unclosed parenthesis" if @paren_count > 0
+
       skip_space
       write_line
       skip_space_or_newline last: true
