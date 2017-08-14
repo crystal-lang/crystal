cb07bffa53fdb83041ab3889a1ce01b7dca62305
Mike Fiedler <miketheman@gmail.com>
2017-08-14 19:18:04 -0400

    Adds Enumerable#reduce? for when input is empty
    Resolves #2155
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- spec/std/enumerable_spec.cr
+++ spec/std/enumerable_spec.cr
@@ -563,6 +563,20 @@ describe "Enumerable" do
     end
   end
 
+  describe "reduce?" do
+    it { [1, 2, 3].reduce? { |memo, i| memo + i }.should eq(6) }
+    it { [1, 2, 3].reduce?(10) { |memo, i| memo + i }.should eq(16) }
+
+    it "returns nil if empty" do
+      ([] of Int32).reduce? { |memo, i| memo + i }.should be_nil
+    end
+
+    it "does not raise if empty if there is a memo argument" do
+      result = ([] of Int32).reduce?(10) { |memo, i| memo + i }
+      result.should eq 10
+    end
+  end
+
   describe "join" do
     it "joins with separator and block" do
       str = [1, 2, 3].join(", ") { |x| x + 1 }
--- src/enumerable.cr
+++ src/enumerable.cr
@@ -609,6 +609,37 @@ module Enumerable(T)
     memo
   end
 
+  # Similar to `reduce`, but instead of raising when the input is empty,
+  # return `nil`
+  #
+  # ```
+  # ([] of Int32).reduce? { |acc, i| acc + i } # => nil
+  # ```
+  def reduce?
+    memo = uninitialized T
+    found = false
+
+    each do |elem|
+      memo = found ? (yield memo, elem) : elem
+      found = true
+    end
+
+    found ? memo : nil
+  end
+
+  # Similar to `reduce?`, however instead of returning `nil` when the input is empty,
+  # return the initial value of the accumulator.
+  #
+  # ```
+  # ([] of Int32).reduce?(10) { |acc, i| acc + i } # => 10
+  # ```
+  def reduce?(memo)
+    each do |elem|
+      memo = yield memo, elem
+    end
+    memo
+  end
+
   # Returns a `String` created by concatenating the elements in the collection,
   # separated by *separator* (defaults to none).
   #
