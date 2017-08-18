af8c4f6866d24d21e45520c4e76a524712b2f026
Benoit de Chezelles <lesell_b@epitech.eu>
2017-08-18 20:38:20 +0200

    Remove backtrace from ECR errors
    Rescure only Errno::ENOENT & Errno::EISDIR errors, as thoses are
    the most common errors in this case.

--- src/ecr/process.cr
+++ src/ecr/process.cr
@@ -1,2 +1,15 @@
 require "ecr/processor"
-puts ECR.process_file(ARGV[0], ARGV[1])
+
+filename = ARGV[0]
+buffer_name = ARGV[1]
+
+begin
+  puts ECR.process_file(filename, buffer_name)
+rescue ex : Errno
+  if {Errno::ENOENT, Errno::EISDIR}.includes?(ex.errno)
+    STDERR.puts ex.message
+    exit 1
+  else
+    raise ex
+  end
+end
