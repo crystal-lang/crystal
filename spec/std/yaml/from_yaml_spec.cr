de2a1b7cb7fb0ed1f05155d7de410678ff586a53
jwaldrip <jason@waldrip.net>
2017-08-30 14:43:02 -0600

    add date to test case

--- spec/std/yaml/from_yaml_spec.cr
+++ spec/std/yaml/from_yaml_spec.cr
@@ -214,7 +214,7 @@ describe "String.from_yaml" do
       "+1", "1", "-1",
       "1.1", "+1.1", "-1.1",
       "0x_0A_74_AE", "0b1010_0111_0100_1010_1110", "02472256",
-      "2001-12-15T02:59:43.1Z"
+      "2001-12-15T02:59:43.1Z",
     }
     values.each do |value|
       it %(should raise if a reserved value "#{value}") do
