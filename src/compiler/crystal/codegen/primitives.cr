4df8ead569252c925f7209e71a7b661e9ea34171
Brian J. Cardiff <bcardiff@manas.tech>
2017-09-13 20:32:27 -0300

    Allow named tuple metaclass to be indexed
    mimic behavior from tuple

--- src/compiler/crystal/codegen/primitives.cr
+++ src/compiler/crystal/codegen/primitives.cr
@@ -783,8 +783,15 @@ class Crystal::CodeGenVisitor
       ptr = aggregate_index value, index
       to_lhs ptr, type.entries[index].type
     else
-      type = (type.instance_type.as(TupleInstanceType))
-      type_id(type.tuple_types[index].as(Type).metaclass)
+      type = type.instance_type
+      case type
+      when TupleInstanceType
+        type_id(type.tuple_types[index].as(Type).metaclass)
+      when NamedTupleInstanceType
+        type_id(type.entries[index].type.as(Type).metaclass)
+      else
+        raise "BUG: unsupported codegen for tuple_indexer"
+      end
     end
   end
 
