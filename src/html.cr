8e71b0507018bae174e8aff2099e11e1c64e0f93
Ary Borenszweig <asterite@gmail.com>
2017-09-28 17:18:09 -0300

    Let HTML.unescape unescape all HTML5 entities

--- src/html.cr
+++ src/html.cr
@@ -1,3 +1,5 @@
+require "./html/entities"
+
 # Provides HTML escaping and unescaping methods.
 module HTML
   private SUBSTITUTIONS = {
@@ -34,43 +36,57 @@ module HTML
     end
   end
 
-  # Returns a string where some named and all numeric character references
+  # Returns a string where named and numeric character references
   # (e.g. &gt;, &#62;, &x3e;) in *string* are replaced with the corresponding
-  # unicode characters. Only these named entities are replaced:
-  # apos, amp, quot, gt, lt and nbsp.
+  # unicode characters.
   #
   # ```
   # HTML.unescape("Crystal &amp; You") # => "Crystal & You"
   # ```
   def self.unescape(string : String) : String
-    return string unless string.includes? '&'
+    string.gsub(/&(?:([a-zA-Z]{2,32};?)|\#([0-9]+);?|\#[xX]([0-9A-Fa-f]+);?)/) do |string, match|
+      if code = match[1]?
+        # Try to find the code
+        value = named_entity(code)
+        if value
+          value
+        elsif !code.ends_with?(';')
+          # If we can't find it and it doesn't end with ';',
+          # we need to find each prefix of it.
+          # We start from the largest prefix.
+          removed = 0
+          until code.empty?
+            code = code.rchop
+            removed += 1
 
-    string.gsub(/&(apos|amp|quot|gt|lt|nbsp|\#[0-9]+|\#[xX][0-9A-Fa-f]+);/) do |string, _match|
-      match = _match[1]
-      case match
-      when "apos" then "'"
-      when "amp"  then "&"
-      when "quot" then "\""
-      when "gt"   then ">"
-      when "lt"   then "<"
-      when "nbsp" then " "
-      when /\A#0*(\d+)\z/
-        n = $1.to_i
-        if n <= Char::MAX_CODEPOINT
-          n.unsafe_chr
-        else
-          "&##{$1};"
+            value = named_entity(code)
+            if value
+              # If we find it, we need to append the part that
+              # isn't part of the matched code
+              value += string[-removed..-1]
+              break
+            end
+          end
+
+          # We either found the code or not,
+          # in which case we need to return the original string
+          value || string
         end
-      when /\A#x([0-9a-f]+)\z/i
-        n = $1.to_i(16)
-        if n <= Char::MAX_CODEPOINT
-          n.unsafe_chr
-        else
-          "&#x#{$1};"
-        end
+      elsif code = match[2]?
+        # Find by decimal code
+        n = code.to_i
+        n <= Char::MAX_CODEPOINT ? n.unsafe_chr : string
+      elsif code = match[3]?
+        # Find by hexadecimal code
+        n = code.to_i(16)
+        n <= Char::MAX_CODEPOINT ? n.unsafe_chr : string
       else
-        "&#{match};"
+        string
       end
     end
+  end
+
+  private def self.named_entity(code)
+    HTML::SINGLE_CHAR_ENTITIES[code]? || HTML::DOUBLE_CHAR_ENTITIES[code]?
   end
 end
