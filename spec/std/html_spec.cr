a149f70907cba7edc79e3ff1685a41e74a0874da
Johannes Müller <straightshoota@gmail.com>
2017-09-30 16:25:56 +0200

    Improve decoding of numerical character references according to HTML5 spec

--- spec/std/html_spec.cr
+++ spec/std/html_spec.cr
@@ -53,18 +53,33 @@ describe "HTML" do
       str.should eq("3 + 2 = 5")
     end
 
+    it "unescapes decimal encoded chars" do
+      str = HTML.unescape("3 &#00043; 2 &#00061 5")
+
+      str.should eq("3 + 2 = 5")
+    end
+
     it "unescapes &nbsp;" do
       str = HTML.unescape("nbsp&nbsp;space ")
 
       str.should eq("nbsp\u{0000A0}space ")
     end
 
-    it "unescapes Char::MAX_CODEPOINT" do
+    it "does not unescape Char::MAX_CODEPOINT" do
+      # Char::MAX_CODEPOINT is actually a noncharacter and is not replaced
       str = HTML.unescape("limit &#x10FFFF;")
-      str.should eq("limit 􏿿")
+      str.should eq("limit &#x10FFFF;")
 
       str = HTML.unescape("limit &#1114111;")
-      str.should eq("limit 􏿿")
+      str.should eq("limit &#1114111;")
+    end
+
+    it "unescapes characters above Char::MAX_CODEPOINT" do
+      str = HTML.unescape("limit &#x110000;")
+      str.should eq("limit \uFFFD")
+
+      str = HTML.unescape("limit &#1114112;")
+      str.should eq("limit \uFFFD")
     end
 
     it "unescapes &NotSquareSuperset;" do
@@ -81,6 +96,32 @@ describe "HTML" do
     it "unescapes named character reference with numerical characters" do
       str = HTML.unescape("&frac34;")
       str.should eq("\u00BE")
+    end
+
+    it "does not escape unicode control charachters except space characters" do
+      string = "&#x0001;-&#x001F; &#x000D; &#x007F;"
+      HTML.unescape(string).should eq(string)
+
+      string = HTML.unescape("&#x0080;-&#x009F;")
+      string.should eq("\u20AC-\u0178")
+
+      HTML.unescape("&#x000;").should eq("\uFFFD")
+    end
+
+    it "escapes space characters" do
+      string = HTML.unescape("&#x0020;&#32;&#x0009;&#x000A;&#x000C;")
+      string.should eq("  \t\n\f")
+    end
+
+    it "does not escape noncharachter codepoints" do
+      # noncharacters http://www.unicode.org/faq/private_use.html
+      string = "&#xFDD0;-&#xFDEF; &#xFFFE; &#FFFF; &#x1FFFE; &#x1FFFF; &#x2FFFE; &#x10FFFF;"
+      HTML.unescape(string).should eq(string)
+    end
+
+    it "does not escape unicode surrogate characters" do
+      string = "&#xD800;-&#xDFFF;"
+      HTML.unescape(string).should eq("\uFFFD-\uFFFD")
     end
   end
 end
--- src/html.cr
+++ src/html.cr
@@ -36,6 +36,46 @@ module HTML
     end
   end
 
+  # These replacements permit compatibility with old numeric entities that
+  # assumed Windows-1252 encoding.
+  # http://www.whatwg.org/specs/web-apps/current-work/multipage/tokenization.html#consume-a-character-reference
+  private CHARACTER_REPLACEMENTS = {
+    '\u20AC', # First entry is what 0x80 should be replaced with.
+    '\u0081',
+    '\u201A',
+    '\u0192',
+    '\u201E',
+    '\u2026',
+    '\u2020',
+    '\u2021',
+    '\u02C6',
+    '\u2030',
+    '\u0160',
+    '\u2039',
+    '\u0152',
+    '\u008D',
+    '\u017D',
+    '\u008F',
+    '\u0090',
+    '\u2018',
+    '\u2019',
+    '\u201C',
+    '\u201D',
+    '\u2022',
+    '\u2013',
+    '\u2014',
+    '\u02DC',
+    '\u2122',
+    '\u0161',
+    '\u203A',
+    '\u0153',
+    '\u009D',
+    '\u017E',
+    '\u0178', # Last entry is 0x9F.
+    # 0x00->'\uFFFD' is handled programmatically.
+    # 0x0D->'\u000D' is a no-op.
+  }
+
   # Returns a string where named and numeric character references
   # (e.g. &gt;, &#62;, &x3e;) in *string* are replaced with the corresponding
   # unicode characters. This method decodes all HTML5 entities including those
@@ -78,12 +118,10 @@ module HTML
         end
       elsif code = match[2]?
         # Find by decimal code
-        n = code.to_i
-        n <= Char::MAX_CODEPOINT ? n.unsafe_chr : string
+        decode_codepoint(code.to_i) || string
       elsif code = match[3]?
         # Find by hexadecimal code
-        n = code.to_i(16)
-        n <= Char::MAX_CODEPOINT ? n.unsafe_chr : string
+        decode_codepoint(code.to_i(16)) || string
       else
         string
       end
@@ -92,5 +130,26 @@ module HTML
 
   private def self.named_entity(code)
     HTML::SINGLE_CHAR_ENTITIES[code]? || HTML::DOUBLE_CHAR_ENTITIES[code]?
+  end
+
+  # see https://html.spec.whatwg.org/multipage/parsing.html#numeric-character-reference-end-state
+  private def self.decode_codepoint(codepoint)
+    if 0x80 <= codepoint <= 0x9F
+      # Replace characters from Windows-1252 with UTF-8 equivalents.
+      CHARACTER_REPLACEMENTS[codepoint - 0x80].to_s
+    elsif codepoint == 0 ||
+          codepoint > Char::MAX_CODEPOINT ||
+          0xD800 <= codepoint <= 0xDFFF # unicode surrogat characters
+      # Replace invalid characters with replacement character.
+      "\uFFFD"
+    elsif 0xFDD0 <= codepoint <= 0xFDEF ||                                                    # unicode noncharacters
+ codepoint & 0xFFFF >= 0xFFFE ||                                                              # last two of each plane (nonchars) disallowed
+ (codepoint < 0x0020 && codepoint != 0x0009 && codepoint != 0x000A && codepoint != 0x000C) || # unicode control characters
+ codepoint == 0x007F
+      # these codepoints should not be replaced, therefore return nil
+      nil
+    else
+      codepoint.unsafe_chr
+    end
   end
 end
