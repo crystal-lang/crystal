49dd92e3c1fddd8876b8fec540965ed0994cde55
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2017-10-19 21:09:57 +0300

    use multiplicative HashDos proof function
    it is multiplicative function resilent to HashDos given following
    restriction is satisfied:
    - neither seed nor hash-value is exposed to atacker.

--- src/crystal/hasher.cr
+++ src/crystal/hasher.cr
@@ -9,10 +9,35 @@ struct Crystal::Hasher
   # TODO: the implementation is naive and should probably use
   # another algorithm like SipHash or FNV.
 
-  @@seed = uninitialized UInt64
-  Random::Secure.random_bytes(Slice.new(pointerof(@@seed).as(UInt8*), 8))
+  @@seed = uninitialized UInt64[2]
+  Random::Secure.random_bytes(Slice.new(pointerof(@@seed).as(UInt8*), sizeof(typeof(@@seed))))
 
-  property result : UInt64 = @@seed
+  @a : UInt64 = @@seed[0]
+  @b : UInt64 = @@seed[1]
+
+  private C1 = 0xacd5ad43274593b9_u64
+  private C2 = 0x6956abd6ed268a3d_u64
+
+  private def rotl32(v : UInt64)
+    v.unsafe_shl(32) | v.unsafe_shr(32)
+  end
+
+  private def permute(v : UInt64)
+    @a = rotl32(@a ^ v) * C1
+    @b = (rotl32(@b) ^ v) * C2
+    self
+  end
+
+  def result
+    a, b = @a, @b
+    a ^= a >> 33
+    b ^= b >> 32
+    a *= C1
+    b *= C2
+    a ^= a >> 32
+    b ^= b >> 33
+    a + b
+  end
 
   def nil
     self
@@ -23,13 +48,11 @@ struct Crystal::Hasher
   end
 
   def int(value)
-    @result = @result * 31 + value.to_u64
-    self
+    permute(value.to_u64)
   end
 
   def float(value)
-    @result = @result * 31 + value.to_f64.unsafe_as(UInt64)
-    self
+    permute(value.to_f64.unsafe_as(UInt64))
   end
 
   def char(value)
@@ -45,18 +68,36 @@ struct Crystal::Hasher
   end
 
   def reference(value)
-    @result = @result * 31 + value.object_id.to_u64
-    self
+    permute(value.object_id.to_u64)
   end
 
   def string(value)
     string(value.to_slice)
   end
 
+  @[NoInline]
   def string(value : Bytes)
-    value.each do |b|
-      @result = @result * 31  + b.to_u64
+    bsz = value.size
+    v = bsz.to_u64 << 56
+    u = value.to_unsafe
+    bsz.unsafe_div(8).downto(1) do
+      # force correct unaligned read
+      t8 = uninitialized UInt64
+      pointerof(t8).as(UInt8*).copy_from(u, 8)
+      permute(t8)
+      u += 8
     end
+    if (bsz & 4) != 0
+      # force correct unaligned read
+      t4 = uninitialized UInt32
+      pointerof(t4).as(UInt8*).copy_from(u, 4)
+      v |= t4 << 24
+      u += 4
+    end
+    if (r = bsz & 3) != 0
+      v |= u[0].to_u64 | (u[r/2].to_u64 << 8) | (u[r - 1].to_u64 << 16)
+    end
+    permute(v)
     self
   end
 
