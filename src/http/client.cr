1121401d539292c461675d990454a115d04c4192
Chris Larsen <clarsenipod@gmail.com>
2017-10-20 16:00:32 -0400

    Format client.cr

--- src/http/client.cr
+++ src/http/client.cr
@@ -23,7 +23,7 @@
 #
 # params = HTTP::Params.encode({"author" => "John Doe", "offset" => "20"}) # => author=John+Doe&offset=20
 # response = HTTP::Client.get "http://www.example.com?" + params
-# response.status_code      # => 200
+# response.status_code # => 200
 # ```
 #
 # ### Streaming
