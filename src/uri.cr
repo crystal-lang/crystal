a43adff3f7caf6f8b111c914e810378dbd1b1a7b
Lachlan Dowding <lachlan@permafro.st>
2017-11-05 09:10:07 +1000

    Add more well-known default ports sourced from IANA via Mahmoud's [scheme_port_map.json][1]
    [1]: https://gist.github.com/mahmoud/2fe281a8daaff26cfe9c15d2c5bf5c8b

--- src/uri.cr
+++ src/uri.cr
@@ -428,20 +428,57 @@ class URI
   end
 
   # A map of schemes and their respective default ports, seeded
-  # with some well-known schemes.
+  # with some well-known schemes sourced from the IANA
+  # [Uniform Resource Identifier (URI) Schemes][1] and
+  # [Service Name and Transport Protocol Port Number Registry][2]
+  # via Mahmoud's [scheme_port_map.json][3].
+  #
+  # [1]: <https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml>
+  # [2]: <https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml>
+  # [3]: <https://gist.github.com/mahmoud/2fe281a8daaff26cfe9c15d2c5bf5c8b>
   @@default_ports = {
-    "ftp"    => 21,
-    "ftps"   => 990,
-    "gopher" => 70,
-    "http"   => 80,
-    "https"  => 443,
-    "ldap"   => 389,
-    "ldaps"  => 636,
-    "nntp"   => 119,
-    "scp"    => 22,
-    "sftp"   => 22,
-    "ssh"    => 22,
-    "telnet" => 23,
+    "acap"     => 674,
+    "afp"      => 548,
+    "dict"     => 2628,
+    "dns"      => 53,
+    "ftp"      => 21,
+    "ftps"     => 990,
+    "git"      => 9418,
+    "gopher"   => 70,
+    "http"     => 80,
+    "https"    => 443,
+    "imap"     => 143,
+    "ipp"      => 631,
+    "ipps"     => 631,
+    "irc"      => 194,
+    "ircs"     => 6697,
+    "ldap"     => 389,
+    "ldaps"    => 636,
+    "mms"      => 1755,
+    "msrp"     => 2855,
+    "mtqp"     => 1038,
+    "nfs"      => 111,
+    "nntp"     => 119,
+    "nntps"    => 563,
+    "pop"      => 110,
+    "prospero" => 1525,
+    "redis"    => 6379,
+    "rsync"    => 873,
+    "rtsp"     => 554,
+    "rtsps"    => 322,
+    "rtspu"    => 5005,
+    "scp"      => 22,
+    "sftp"     => 22,
+    "smb"      => 445,
+    "snmp"     => 161,
+    "ssh"      => 22,
+    "svn"      => 3690,
+    "telnet"   => 23,
+    "ventrilo" => 3784,
+    "vnc"      => 5900,
+    "wais"     => 210,
+    "ws"       => 80,
+    "wss"      => 443,
   }
 
   # Returns the default port for the given *scheme* if known, otherwise
