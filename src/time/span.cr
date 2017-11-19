d192e384bd3b4fe72057a46840ddf90adb16b733
Michael Petö <michael@petoe.me>
2017-11-19 18:24:48 +0100

    Allow more ways to initialize Time::Span

--- src/time/span.cr
+++ src/time/span.cr
@@ -64,7 +64,7 @@ struct Time::Span
     )
   end
 
-  def initialize(*, seconds : Int8 | Int16 | Int32 | Int64 | UInt8 | UInt16 | UInt32, nanoseconds : Int)
+  def initialize(*, seconds : Int8 | Int16 | Int32 | Int64 | UInt8 | UInt16 | UInt32, nanoseconds : Int = 0)
     seconds = seconds.to_i64
     # Normalize nanoseconds in the range 0...1_000_000_000
     # check for possible overflow seconds could become too big
@@ -90,7 +90,7 @@ struct Time::Span
     @nanoseconds = nanoseconds.to_i32
   end
 
-  def self.new(*, seconds : Int, nanoseconds : Int)
+  def self.new(*, seconds : Int, nanoseconds : Int = 0)
     unless Int64::MIN <= seconds <= Int64::MAX
       raise ArgumentError.new "Time::Span too big or too small"
     end
@@ -100,7 +100,7 @@ struct Time::Span
     )
   end
 
-  def self.new(*, nanoseconds : Int)
+  def self.new(*, nanoseconds : Int = 0)
     new(
       seconds: 0_i64,
       nanoseconds: nanoseconds,
