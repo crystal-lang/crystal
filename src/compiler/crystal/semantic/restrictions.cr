b49696b575a245b03dfb277bb382a19a3955f079
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-11-28 13:22:01 +0900

    Sementic: prevent infinity recursion by alias and generics combination
    Fixed #5329

--- src/compiler/crystal/semantic/restrictions.cr
+++ src/compiler/crystal/semantic/restrictions.cr
@@ -667,7 +667,10 @@ module Crystal
       elsif context.strict?
         type_var == other_type_var
       else
-        type_var.restrict(other_type_var, context) == type_var
+        # To prevent infinity recursion, it checks equality between
+        # `type_var` and `other_type_var` directly before try to restrict
+        # `type_var` by `other_type_var`.
+        type_var == other_type_var || type_var.restrict(other_type_var, context) == type_var
       end
     end
   end
