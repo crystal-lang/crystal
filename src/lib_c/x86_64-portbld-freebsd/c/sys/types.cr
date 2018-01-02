a3f2ecc30d77b8146f49fdd3564f409495765dde
myfreeweb <greg@unrelenting.technology>
2018-01-02 19:03:47 +0300

    Support FreeBSD 12 (64-bit inodes) (#5199)

--- src/lib_c/x86_64-portbld-freebsd/c/sys/types.cr
+++ src/lib_c/x86_64-portbld-freebsd/c/sys/types.cr
@@ -9,9 +9,17 @@ lib LibC
   alias DevT = UInt
   alias GidT = UInt
   alias IdT = Long
-  alias InoT = UInt
+  {% if flag?(:"freebsd12.0") %}
+    alias InoT = ULong
+  {% else %}
+    alias InoT = UInt
+  {% end %}
   alias ModeT = UShort
-  alias NlinkT = UShort
+  {% if flag?(:"freebsd12.0") %}
+    alias NlinkT = ULong
+  {% else %}
+    alias NlinkT = UShort
+  {% end %}
   alias OffT = Long
   alias PidT = Int
   type PthreadAttrT = Void*
