f77b7475f734ee712ec5fc227f3557d7e0cd3eb2
Chris Huxtable <chris@huxtable.ca>
2018-01-05 16:40:15 -0500

    Add comments from time.h

--- src/lib_c/amd64-unknown-openbsd/c/time.cr
+++ src/lib_c/amd64-unknown-openbsd/c/time.cr
@@ -9,17 +9,17 @@ lib LibC
   CLOCK_BOOTTIME            = 6
 
   struct Tm
-    tm_sec : Int
-    tm_min : Int
-    tm_hour : Int
-    tm_mday : Int
-    tm_mon : Int
-    tm_year : Int
-    tm_wday : Int
-    tm_yday : Int
-    tm_isdst : Int
-    tm_gmtoff : Long
-    tm_zone : Char*
+    tm_sec : Int       # seconds after the minute [0-60]
+    tm_min : Int       # minutes after the hour [0-59]
+    tm_hour : Int      # hours since midnight [0-23]
+    tm_mday : Int      # day of the month [1-31]
+    tm_mon : Int       # months since January [0-11]
+    tm_year : Int      # years since 1900
+    tm_wday : Int      # days since Sunday [0-6]
+    tm_yday : Int      # days since January 1 [0-365]
+    tm_isdst : Int     # Daylight Saving Time flag
+    tm_gmtoff : Long   # offset from UTC in seconds
+    tm_zone : Char*    # timezone abbreviation
   end
 
   struct Timespec
