3408329cb81f848820b810794d536ab6eb5d0269
Chris Huxtable <chris@huxtable.ca>
2018-01-06 11:49:55 -0500

    Reorganize and expand Constants
    Based on fcntl.h

--- src/lib_c/amd64-unknown-openbsd/c/fcntl.cr
+++ src/lib_c/amd64-unknown-openbsd/c/fcntl.cr
@@ -3,21 +3,46 @@ require "./sys/stat"
 require "./unistd"
 
 lib LibC
-  F_GETFD    =       1
-  F_SETFD    =       2
-  F_GETFL    =       3
-  F_SETFL    =       4
-  FD_CLOEXEC =       1
-  O_CLOEXEC  = 0x10000
-  O_CREAT    =  0x0200
-  O_NOFOLLOW =  0x0100
-  O_TRUNC    =  0x0400
-  O_APPEND   =  0x0008
-  O_NONBLOCK =  0x0004
-  O_SYNC     =  0x0080
-  O_RDONLY   =  0x0000
-  O_RDWR     =  0x0002
-  O_WRONLY   =  0x0001
+  O_RDONLY    = 0x000000  # open for reading only
+  O_WRONLY    = 0x000001  # open for writing only
+  O_RDWR      = 0x000002  # open for reading and writing
+  O_ACCMODE   = 0x000003  # mask for above modes
+  O_NONBLOCK  = 0x000004  # no delay
+  O_APPEND    = 0x000008  # set append mode
+  O_SHLOCK    = 0x000010  # open with shared file lock
+  O_EXLOCK    = 0x000020  # open with exclusive file lock
+  O_ASYNC     = 0x000040  # signal pgrp when data ready
+  O_FSYNC     = 0x000080  # backwards compatibility
+  O_NOFOLLOW  = 0x000100  # if path is a symlink, don't follow
+  O_SYNC      = 0x000080  # synchronous writes
+  O_CREAT     = 0x000200  # create if nonexistent
+  O_TRUNC     = 0x000400  # truncate to zero length
+  O_EXCL      = 0x000800  # error if already exists
+  O_DSYNC     = O_SYNC    # synchronous data writes
+  O_RSYNC     = O_SYNC    # synchronous reads
+  O_NOCTTY    = 0x008000  # don't assign controlling terminal
+  O_CLOEXEC   = 0x010000  # atomically set FD_CLOEXEC
+  O_DIRECTORY = 0x020000  # fail if not a directory
+
+  F_DUPFD           =  0  # duplicate file descriptor
+  F_GETFD           =  1  # get file descriptor flags
+  F_SETFD           =  2  # set file descriptor flags
+  F_GETFL           =  3  # get file status flags
+  F_SETFL           =  4  # set file status flags
+  F_GETOWN          =  5  # get SIGIO/SIGURG proc/pgrp
+  F_SETOWN          =  6  # set SIGIO/SIGURG proc/pgrp
+  F_GETLK           =  7  # get record locking information
+  F_SETLK           =  8  # set record locking information
+  F_SETLKW          =  9  # F_SETLK; wait if blocked
+  F_DUPFD_CLOEXEC   = 10  # duplicate with FD_CLOEXEC set
+  F_ISATTY          = 11  # used by isatty(3)
+
+  FD_CLOEXEC        =  1	# close-on-exec flag
+
+  F_RDLCK           =  1  # shared or read lock
+  F_UNLCK           =  2  # unlock
+  F_WRLCK           =  3  # exclusive or write lock
+
 
   struct Flock
     l_start : OffT
