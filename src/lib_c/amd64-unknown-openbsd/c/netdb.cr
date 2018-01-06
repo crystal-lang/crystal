7d9d5c457415bb9fc03357c031726bd10d023e04
Chris Huxtable <chris@huxtable.ca>
2018-01-06 10:45:22 -0500

    Reorders, expands, and comments Constants
    Based on netdb.h

--- src/lib_c/amd64-unknown-openbsd/c/netdb.cr
+++ src/lib_c/amd64-unknown-openbsd/c/netdb.cr
@@ -3,21 +3,29 @@ require "./sys/socket"
 require "./stdint"
 
 lib LibC
-  AI_PASSIVE     =   1
-  AI_CANONNAME   =   2
-  AI_NUMERICHOST =   4
-  AI_NUMERICSERV =  16
-  AI_ADDRCONFIG  =  64
-  EAI_AGAIN      =  -3
-  EAI_BADFLAGS   =  -1
-  EAI_FAIL       =  -4
-  EAI_FAMILY     =  -6
-  EAI_MEMORY     = -10
-  EAI_NONAME     =  -2
-  EAI_SERVICE    =  -8
-  EAI_SOCKTYPE   =  -7
-  EAI_SYSTEM     = -11
-  EAI_OVERFLOW   = -14
+  AI_PASSIVE      =   1 # socket address is intended for bind()
+  AI_CANONNAME    =   2 # request for canonical name
+  AI_NUMERICHOST  =   4 # don't ever try hostname lookup
+  AI_EXT          =   8 # enable non-portable extensions
+  AI_NUMERICSERV  =  16 # don't ever try servname lookup
+  AI_FQDN         =  32 # return the FQDN that was resolved
+  AI_ADDRCONFIG   =  64 # return configured address families only
+  AI_MASK         = (AI_PASSIVE|AI_CANONNAME|AI_NUMERICHOST|AI_NUMERICSERV|AI_FQDN|AI_ADDRCONFIG)
+
+  EAI_BADFLAGS    =  -1 # invalid value for ai_flags
+  EAI_NONAME      =  -2 # name or service is not known
+  EAI_AGAIN       =  -3 # temporary failure in name resolution
+  EAI_FAIL        =  -4 # non-recoverable failure in name resolution
+  EAI_NODATA      =  -5 # no address associated with name
+  EAI_FAMILY      =  -6 # ai_family not supported
+  EAI_SOCKTYPE    =  -7 # ai_socktype not supported
+  EAI_SERVICE     =  -8 # service not supported for ai_socktype
+  EAI_ADDRFAMILY  =  -9 # address family for name not supported
+  EAI_MEMORY      = -10 # memory allocation failure
+  EAI_SYSTEM      = -11 # system error (code indicated in errno)
+  EAI_BADHINTS    = -12 # invalid value for hints
+  EAI_PROTOCOL    = -13 # resolved protocol is unknown
+  EAI_OVERFLOW    = -14 # argument buffer overflow
 
   struct Addrinfo
     ai_flags : Int
