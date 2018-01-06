cceb99ac63f4796788e41cf88791ab65c20d3481
Chris Huxtable <chris@huxtable.ca>
2018-01-06 08:57:35 -0500

    Corrects return type of strlen()

--- src/lib_c/amd64-unknown-openbsd/c/string.cr
+++ src/lib_c/amd64-unknown-openbsd/c/string.cr
@@ -5,5 +5,5 @@ lib LibC
   fun memcmp(x0 : Void*, x1 : Void*, x2 : SizeT) : Int
   fun strcmp(x0 : Char*, x1 : Char*) : Int
   fun strerror(x0 : Int) : Char*
-  fun strlen(x0 : Char*) : ULong
+  fun strlen(x0 : Char*) : SizeT
 end
