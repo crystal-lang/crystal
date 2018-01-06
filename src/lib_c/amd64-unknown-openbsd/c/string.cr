298ea45f1020666d7d8c6c0c889f70a709f60704
Chris Huxtable <chris@huxtable.ca>
2018-01-06 08:59:28 -0500

    Add safer string functions

--- src/lib_c/amd64-unknown-openbsd/c/string.cr
+++ src/lib_c/amd64-unknown-openbsd/c/string.cr
@@ -6,4 +6,6 @@ lib LibC
   fun strcmp(x0 : Char*, x1 : Char*) : Int
   fun strerror(x0 : Int) : Char*
   fun strlen(x0 : Char*) : SizeT
+  fun strncmp(s1 : Char*, s2 : Char*, len : SizeT) : Int
+  fun strnlen(s : Char*, maxlen SizeT) : SizeT
 end
