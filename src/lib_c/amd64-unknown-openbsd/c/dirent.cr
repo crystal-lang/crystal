e86cc8c30f5d6154e4ef4a21dd975768c541a1f5
Chris Huxtable <chris@huxtable.ca>
2018-01-06 12:20:48 -0500

    Add comments

--- src/lib_c/amd64-unknown-openbsd/c/dirent.cr
+++ src/lib_c/amd64-unknown-openbsd/c/dirent.cr
@@ -4,13 +4,13 @@ lib LibC
   type DIR = Void
 
   struct Dirent
-    d_fileno : InoT
-    d_off : OffT
-    d_reclen : UShort
-    d_type : Char
-    d_namlen : Char
-    __d_padding : StaticArray(Char, 4)
-    d_name : StaticArray(Char, 256)
+    d_fileno : InoT                         # file number of entry
+    d_off : OffT                            # offset after this entry
+    d_reclen : UShort                       # length of this record
+    d_type : Char                           # file type, see below
+    d_namlen : Char                         # length of string in d_name
+    __d_padding : StaticArray(Char, 4)      # suppress padding after d_name
+    d_name : StaticArray(Char, 256)         # name must be no longer than this
   end
 
   fun closedir(x0 : DIR*) : Int
