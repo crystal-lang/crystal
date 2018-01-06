d252b59cfbba5c5ed54aa2d061f56537779c1ffe
Chris Huxtable <chris@huxtable.ca>
2018-01-06 09:16:48 -0500

    Adds safer allocators/reallocators

--- src/lib_c/amd64-unknown-openbsd/c/stdlib.cr
+++ src/lib_c/amd64-unknown-openbsd/c/stdlib.cr
@@ -12,7 +12,10 @@ lib LibC
   fun exit(x0 : Int) : NoReturn
   fun free(x0 : Void*) : Void
   fun getenv(x0 : Char*) : Char*
+  fun calloc(x0 : SizeT, x0 : SizeT) : Void*
   fun malloc(x0 : SizeT) : Void*
+  fun reallocarray(x0 : Void*, x1 : SizeT, x2 : SizeT) : Void*
+  fun recallocarray(x0 : Void*, x1 : SizeT, x2 : SizeT, x3 : SizeT) : Void*
   fun realloc(x0 : Void*, x1 : SizeT) : Void*
   fun strtod(x0 : Char*, x1 : Char**) : Double
   fun strtof(x0 : Char*, x1 : Char**) : Float
