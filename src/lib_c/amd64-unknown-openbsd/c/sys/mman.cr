0a03af534c289c56777e9eb25800f9812fa286b9
Chris Huxtable <chris@huxtable.ca>
2018-01-06 17:27:25 -0500

    Reorganized constants and added comments

--- src/lib_c/amd64-unknown-openbsd/c/sys/mman.cr
+++ src/lib_c/amd64-unknown-openbsd/c/sys/mman.cr
@@ -1,29 +1,40 @@
 require "./types"
 
 lib LibC
-  PROT_EXEC             =   0x04
-  PROT_NONE             =   0x00
-  PROT_READ             =   0x01
-  PROT_WRITE            =   0x02
-  MAP_FIXED             = 0x0010
-  MAP_PRIVATE           = 0x0002
-  MAP_SHARED            = 0x0001
-  MAP_ANON              = 0x1000
-  MAP_ANONYMOUS         = LibC::MAP_ANON
-  MAP_FAILED            = Pointer(Void).new(-1)
-  POSIX_MADV_DONTNEED   = 4
-  POSIX_MADV_NORMAL     = 0
-  POSIX_MADV_RANDOM     = 1
-  POSIX_MADV_SEQUENTIAL = 2
-  POSIX_MADV_WILLNEED   = 3
-  MADV_DONTNEED         = LibC::POSIX_MADV_DONTNEED
-  MADV_NORMAL           = LibC::POSIX_MADV_NORMAL
-  MADV_RANDOM           = LibC::POSIX_MADV_RANDOM
-  MADV_SEQUENTIAL       = LibC::POSIX_MADV_SEQUENTIAL
-  MADV_WILLNEED         = LibC::POSIX_MADV_WILLNEED
 
   fun mmap(x0 : Void*, x1 : SizeT, x2 : Int, x3 : Int, x4 : Int, x5 : OffT) : Void*
   fun mprotect(x0 : Void*, x1 : SizeT, x2 : Int) : Int
   fun munmap(x0 : Void*, x1 : SizeT) : Int
   fun madvise(x0 : Void*, x1 : SizeT, x2 : Int) : Int
+  # Protections are chosen from these bits, or-ed together
+  PROT_NONE   = 0x00  # no permissions
+  PROT_READ   = 0x01  # pages can be read
+  PROT_WRITE  = 0x02  # pages can be written
+  PROT_EXEC   = 0x04  # pages can be executed
+
+  #Sharing types
+  MAP_SHARED    = 0x0001    # share changes
+  MAP_PRIVATE   = 0x0002    # changes are private
+  MAP_FIXED     = 0x0010    # map addr must be exactly as requested
+  MAP_ANON      = 0x1000    # allocated from memory, swap space
+  MAP_ANONYMOUS = MAP_ANON  # alternate POSIX spelling
+
+  MAP_FAILED    = Pointer(Void).new(-1)
+
+  # POSIX memory advisory values
+  POSIX_MADV_NORMAL     = 0  # no further special treatment
+  POSIX_MADV_RANDOM     = 1  # expect random page references
+  POSIX_MADV_SEQUENTIAL = 2  # expect sequential page references
+  POSIX_MADV_WILLNEED   = 3  # will need these pages
+  POSIX_MADV_DONTNEED   = 4  # don't need these pages
+
+  # Original advice values
+  MADV_NORMAL     = POSIX_MADV_NORMAL
+  MADV_RANDOM     = POSIX_MADV_RANDOM
+  MADV_SEQUENTIAL = POSIX_MADV_SEQUENTIAL
+  MADV_WILLNEED   = POSIX_MADV_WILLNEED
+  MADV_DONTNEED   = POSIX_MADV_DONTNEED
+  MADV_SPACEAVAIL = 5  # insure that resources are reserved
+  MADV_FREE       = 6  # pages are empty, free them
+
 end
