cf9d7f2440845c7ac4c56cf6956135d67a3c38e4
Chris Huxtable <chris@huxtable.ca>
2018-01-07 15:05:10 -0500

    Expands constants and reorders to match in.h

--- src/lib_c/amd64-unknown-openbsd/c/netinet/tcp.cr
+++ src/lib_c/amd64-unknown-openbsd/c/netinet/tcp.cr
@@ -1,3 +1,7 @@
 lib LibC
-  TCP_NODELAY = 0x01
+  TCP_NODELAY     = 0x01  # don't delay send to coalesce pkts
+  TCP_MAXSEG      = 0x02  # set maximum segment size
+  TCP_MD5SIG      = 0x04  # enable TCP MD5 signature option
+  TCP_SACK_ENABLE = 0x08  # enable SACKs (if disabled by def.)
+  TCP_NOPUSH      = 0x10  # don't push last block of write
 end
