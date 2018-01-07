45c6effdddfab52024696b1f5ebf40afbc377671
Chris Huxtable <chris@huxtable.ca>
2018-01-07 15:06:36 -0500

    Adds useful arg names

--- src/lib_c/amd64-unknown-openbsd/c/arpa/inet.cr
+++ src/lib_c/amd64-unknown-openbsd/c/arpa/inet.cr
@@ -2,8 +2,10 @@ require "../netinet/in"
 require "../stdint"
 
 lib LibC
-  fun htons(x0 : UInt16T) : UInt16T
-  fun ntohs(x0 : UInt16T) : UInt16T
-  fun inet_ntop(x0 : Int, x1 : Void*, x2 : Char*, x3 : SocklenT) : Char*
-  fun inet_pton(x0 : Int, x1 : Char*, x2 : Void*) : Int
+  fun htons(host16 : UInt16T) : UInt16T
+  fun htonl(host32 : UInt32T) : UInt32T
+  fun ntohs(net16 : UInt16T) : UInt16T
+  fun ntohl(net32 : UInt32T) : UInt32T
+  fun inet_ntop(af : Int, src : Void*, dst : Char*, size : SocklenT) : Char*
+  fun inet_pton(af : Int, src : Char*, dst : Void*) : Int
 end
