a395221ef8e71595dd8108ca10dd589e78605ce3
Johannes Müller <straightshoota@gmail.com>
2018-01-12 11:33:05 +0100

    Time zone implementation for win32
    This adds basic support for using the new time zone model on windows.
    * `Crystal::System::Time.zone_sources` returns an empty array because
      Windows does not include a copy of the tz database.
    * `Crystal::System::Time.load_localtime` creates a local time zone
      `Time::Location` based on data provided by `GetTimeZoneInformation`.
    * A mapping from Windows time zone names to identifiers used by the
      IANA timezone database is included as well as an automated generator
      for that file.

--- src/lib_c/x86_64-windows-msvc/c/winbase.cr
+++ src/lib_c/x86_64-windows-msvc/c/winbase.cr
@@ -41,6 +41,10 @@ lib LibC
     daylightBias : LONG
   end
 
+  TIME_ZONE_ID_UNKNOWN  = 0_u32
+  TIME_ZONE_ID_STANDARD = 1_u32
+  TIME_ZONE_ID_DAYLIGHT = 2_u32
+
   fun GetTimeZoneInformation(tz_info : TIME_ZONE_INFORMATION*) : DWORD
   fun GetSystemTimeAsFileTime(time : FILETIME*)
   fun GetSystemTimePreciseAsFileTime(time : FILETIME*)
