faf1eefb91a30b21dcca838f8909396ca5c83bec
r00ster91 <35064754+r00ster91@users.noreply.github.com>
2018-02-18 13:55:20 +0100

    Fix Random example

--- src/random.cr
+++ src/random.cr
@@ -4,7 +4,6 @@ require "random/pcg32"
 # `Random` provides an interface for random values generation, using a pseudo random number generator (PRNG).
 #
 # ```
-# Random.new_seed # => 112705036
 # Random.rand     # => 0.167595
 # Random.rand(5)  # => 2
 # ```
