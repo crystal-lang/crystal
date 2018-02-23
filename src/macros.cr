ac8dfdfef304977ac71cced0ff67047a936b1989
Chris Baynes <binaryexp+github@gmail.com>
2018-02-23 17:23:54 +0100

    Format macros source.

--- src/macros.cr
+++ src/macros.cr
@@ -53,7 +53,7 @@
 # record Point, x = 0, y = 0
 #
 # p = Point.new y: 2 # => #<Point(@x=0, @y=2)>
-# p.copy x: 3 # => #<Point(@x=3, @y=2)>
+# p.copy x: 3        # => #<Point(@x=3, @y=2)>
 # ```
 macro record(name, *properties)
   struct {{name.id}}
