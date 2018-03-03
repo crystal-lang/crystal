71166594bacc2c1bbb5641ec7d9737fd3ac5959d
Luis Lavena <luislavena@gmail.com>
2018-03-03 15:28:44 -0300

    Use Spec::CLI as new runner

--- src/spec.cr
+++ src/spec.cr
@@ -1,4 +1,5 @@
 require "./spec/dsl"
+require "./spec/cli"
 
 # Crystal's built-in testing library. It provides a structure for writing executable examples
 # of how your code should behave. A domain specific language allows you to write them in a way similar to natural language.
@@ -66,55 +67,4 @@ require "./spec/dsl"
 module Spec
 end
 
-OptionParser.parse! do |opts|
-  opts.banner = "crystal spec runner"
-  opts.on("-e ", "--example STRING", "run examples whose full nested names include STRING") do |pattern|
-    Spec.pattern = pattern
-  end
-  opts.on("-l ", "--line LINE", "run examples whose line matches LINE") do |line|
-    Spec.line = line.to_i
-  end
-  opts.on("-p", "--profile", "Print the 10 slowest specs") do
-    Spec.slowest = 10
-  end
-  opts.on("--fail-fast", "abort the run on first failure") do
-    Spec.fail_fast = true
-  end
-  opts.on("--location file:line", "run example at line 'line' in file 'file', multiple allowed") do |location|
-    if location =~ /\A(.+?)\:(\d+)\Z/
-      Spec.add_location $1, $2.to_i
-    else
-      STDERR.puts "location #{location} must be file:line"
-      exit 1
-    end
-  end
-  opts.on("--junit_output OUTPUT_DIR", "generate JUnit XML output") do |output_dir|
-    junit_formatter = Spec::JUnitFormatter.file(output_dir)
-    Spec.add_formatter(junit_formatter)
-  end
-  opts.on("--help", "show this help") do |pattern|
-    puts opts
-    exit
-  end
-  opts.on("-v", "--verbose", "verbose output") do
-    Spec.override_default_formatter(Spec::VerboseFormatter.new)
-  end
-  opts.on("--no-color", "Disable colored output") do
-    Spec.use_colors = false
-  end
-  opts.unknown_args do |args|
-  end
-end
-
-unless ARGV.empty?
-  STDERR.puts "Error: unknown argument '#{ARGV.first}'"
-  exit 1
-end
-
-if ENV["SPEC_VERBOSE"]? == "1"
-  Spec.override_default_formatter(Spec::VerboseFormatter.new)
-end
-
-Signal::INT.trap { Spec.abort! }
-
-Spec.run
+Spec::CLI.new(ARGV).run
