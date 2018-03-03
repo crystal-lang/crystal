63c722aa86141f750ce7ae61a2338fe47f536bcc
Luis Lavena <luislavena@gmail.com>
2018-03-03 15:28:28 -0300

    Refactor spec runner CLI for extensibility
    Introduce `Spec::CLI` class as new entry point of Spec runner. This
    new interface makes more easy to extend runner's behavior and options by
    extensions or formatters, overriding `#setup` and using previous
    definition:
    
        # TAP formatter extension (spec/tap.cr)
        require "spec/cli"
    
        class Spec::CLI
          def setup
            # important: include previous definition!
            previous_def
    
            # extend with new option
            parser.on("--tap", "use my own TAP formatter") do
              options.default_formatter = Spec::TAPFormatter.new
            end
          end
        end
    
        # my spec_helper.cr
        require "spec"
        require "spec/tap"
    
    With these changes will be possible to extend Spec runner with other
    formatters or extensions.
    
    It provides 1:1 behavior found in `spec.cr`, but it can be improved more
    easily moving forward (ie. register formatters, etc.)
    
    Ref #5662

---
+++ spec/std/spec/cli_spec.cr
@@ -0,0 +1,183 @@
+require "spec"
+require "spec/cli"
+
+private def prepare_cli(argv, **kargs)
+  cli = Spec::CLI.new(argv, **kargs)
+  cli.prepare
+
+  {cli, cli.options}
+end
+
+# override and capture exit calls
+module Spec
+  class CLI
+    property! exited : Int32
+    property? testing = false
+
+    def testing
+      @testing = true
+      yield
+    ensure
+      @testing = false
+    end
+
+    private def display(message)
+      stdout.puts message
+
+      if testing?
+        @exited = 0
+      else
+        exit
+      end
+    end
+
+    private def terminate(message, status = 1)
+      stderr.puts message
+
+      if testing?
+        @exited = status
+      else
+        exit status
+      end
+    end
+  end
+end
+
+describe Spec::CLI do
+  context "parses options" do
+    it "captures provided example pattern" do
+      _, options = prepare_cli %w(-e Foo)
+      options.pattern?.should be_truthy
+      options.pattern.should eq("Foo")
+
+      _, options = prepare_cli %w(--example Bar)
+      options.pattern?.should be_truthy
+      options.pattern.should eq("Bar")
+    end
+
+    it "captures provided line number" do
+      _, options = prepare_cli %w(-l 10)
+      options.line?.should be_truthy
+      options.line.should eq(10)
+
+      _, options = prepare_cli %w(--line 10)
+      options.line?.should be_truthy
+      options.line.should eq(10)
+    end
+
+    it "enables slowest mode" do
+      _, options = prepare_cli %w(-p)
+      options.slowest?.should be_truthy
+      options.slowest.should eq(10)
+
+      _, options = prepare_cli %w(--profile)
+      options.slowest?.should be_truthy
+      options.slowest.should eq(10)
+    end
+
+    it "enables fail-fast mode" do
+      _, options = prepare_cli %w(--fail-fast)
+      options.fail_fast?.should be_true
+    end
+
+    context "locations" do
+      it "captures single file:line location" do
+        _, options = prepare_cli %w(--location foo.cr:1)
+
+        options.locations.size.should eq(1)
+        options.locations.should eq([{"foo.cr", 1}])
+      end
+
+      it "captures multiple file:line locations" do
+        _, options = prepare_cli %w(--location foo.cr:10 --location bar.cr:50)
+
+        options.locations.size.should eq(2)
+        options.locations.should eq([{"foo.cr", 10}, {"bar.cr", 50}])
+      end
+
+      it "aborts on incorrect location format" do
+        io = IO::Memory.new
+
+        cli = Spec::CLI.new %w(--location missing.cr), stderr: io
+        cli.testing do
+          cli.prepare
+        end
+
+        cli.options.locations.size.should eq(0)
+        cli.exited?.should be_truthy
+        cli.exited.should eq(1)
+
+        io.rewind
+        io.gets_to_end.should contain("location missing.cr must be file:line")
+      end
+    end
+
+    it "adds JUnit format to output" do
+      _, options = prepare_cli %w(--junit_output tmp)
+
+      options.formatters.size.should eq(1)
+      options.formatters.first.should be_a(Spec::JUnitFormatter)
+    end
+
+    it "displays help options" do
+      io = IO::Memory.new
+
+      cli = Spec::CLI.new %w(--help), stdout: io
+      cli.testing do
+        cli.prepare
+      end
+
+      cli.exited?.should be_truthy
+      cli.exited.should eq(0)
+
+      io.rewind
+      output = io.gets_to_end
+      output.should contain("crystal spec runner")
+      output.should contain("show this help")
+    end
+
+    it "changes default formatter to verbose" do
+      _, options = prepare_cli %w()
+      options.default_formatter?.should be_falsey
+
+      _, options = prepare_cli %w(-v)
+      options.default_formatter.should be_truthy
+      options.default_formatter.should be_a(Spec::VerboseFormatter)
+
+      _, options = prepare_cli %w(--verbose)
+      options.default_formatter.should be_truthy
+      options.default_formatter.should be_a(Spec::VerboseFormatter)
+    end
+
+    it "changes default formatter using SPEC_VERBOSE" do
+      old_env = ENV["SPEC_VERBOSE"]?
+      ENV["SPEC_VERBOSE"] = "1"
+
+      _, options = prepare_cli %w()
+      options.default_formatter.should be_truthy
+      options.default_formatter.should be_a(Spec::VerboseFormatter)
+    ensure
+      ENV["SPEC_VERBOSE"] = old_env
+    end
+
+    it "disables color output" do
+      _, options = prepare_cli %w(--no-color)
+      options.no_color?.should be_true
+    end
+
+    it "aborts execution with unknown arguments" do
+      io = IO::Memory.new
+      cli = Spec::CLI.new %w(unknown), stderr: io
+
+      cli.testing do
+        cli.prepare
+      end
+
+      cli.exited?.should be_truthy
+      cli.exited.should eq(1)
+
+      io.rewind
+      io.gets_to_end.should contain("Error: unknown argument 'unknown'")
+    end
+  end
+end
---
+++ src/spec/cli.cr
@@ -0,0 +1,136 @@
+require "./formatter"
+require "option_parser"
+
+module Spec
+  # :nodoc:
+  class CLI
+    # :nodoc:
+    class Options
+      property formatters = Array(Spec::Formatter).new
+      property locations = Array({String, Int32}).new
+
+      property! default_formatter : Spec::Formatter
+      property! fail_fast : Bool
+      property! line : Int32
+      property! no_color : Bool
+      property! pattern : String
+      property! slowest : Int32
+    end
+
+    getter options : Options
+
+    private getter argv : Array(String)
+    private getter parser = OptionParser.new
+    private getter stderr : IO
+    private getter stdout : IO
+
+    private getter? prepared : Bool = false
+
+    def initialize(@argv, *, @options = Options.new, @stderr = STDERR, @stdout = STDOUT)
+      setup
+    end
+
+    def prepare
+      return if prepared?
+
+      parser.parse(argv)
+
+      unless argv.empty?
+        terminate "Error: unknown argument '#{argv.first}'"
+      end
+
+      if ENV["SPEC_VERBOSE"]? == "1"
+        options.default_formatter = Spec::VerboseFormatter.new
+      end
+    ensure
+      @prepared = true
+    end
+
+    def run
+      prepare unless prepared?
+      apply_options
+
+      Signal::INT.trap { Spec.abort! }
+
+      Spec.run
+    end
+
+    private def apply_options
+      Spec.pattern = options.pattern if options.pattern?
+      Spec.line = options.line if options.line?
+      Spec.slowest = options.slowest if options.slowest?
+      Spec.fail_fast = options.fail_fast if options.fail_fast?
+      Spec.use_colors = false if options.no_color?
+
+      options.locations.each do |file, line|
+        Spec.add_location file, line
+      end
+
+      options.formatters.each do |formatter|
+        Spec.add_formatter formatter
+      end
+
+      if formatter = options.default_formatter?
+        Spec.override_default_formatter formatter
+      end
+    end
+
+    private def setup
+      parser.banner = "crystal spec runner"
+
+      parser.on("-e ", "--example STRING", "run examples whose full nested names include STRING") do |pattern|
+        options.pattern = pattern
+      end
+
+      parser.on("-l ", "--line LINE", "run examples whose line matches LINE") do |line|
+        options.line = line.to_i
+      end
+
+      parser.on("-p", "--profile", "Print the 10 slowest specs") do
+        options.slowest = 10
+      end
+
+      parser.on("--fail-fast", "abort the run on first failure") do
+        options.fail_fast = true
+      end
+
+      parser.on("--location file:line", "run example at line 'line' in file 'file', multiple allowed") do |location|
+        if location =~ /\A(.+?)\:(\d+)\Z/
+          options.locations << {$1, $2.to_i}
+        else
+          terminate "location #{location} must be file:line"
+        end
+      end
+
+      parser.on("--junit_output OUTPUT_DIR", "generate JUnit XML output") do |output_dir|
+        junit_formatter = Spec::JUnitFormatter.file(output_dir)
+        options.formatters << junit_formatter
+      end
+
+      parser.on("--help", "show this help") do |pattern|
+        display parser
+      end
+
+      parser.on("-v", "--verbose", "verbose output") do
+        options.default_formatter = Spec::VerboseFormatter.new
+      end
+
+      parser.on("--no-color", "Disable colored output") do
+        options.no_color = true
+      end
+
+      parser.unknown_args do |args|
+      end
+    end
+
+    private def display(message)
+      stdout.puts message
+      exit
+    end
+
+    private def terminate(message, status = 1)
+      stderr.puts message
+      exit status
+    end
+  end
+end
