c3cf5643125667a0c7d1748318eada85905904f5
Johannes Müller <straightshoota@gmail.com>
2018-04-28 21:53:04 +0200

    Fix: Ensure TCPSocket is closed when HTTP::Websocket::Protocol.new raises

--- src/http/web_socket/protocol.cr
+++ src/http/web_socket/protocol.cr
@@ -245,38 +245,42 @@ class HTTP::WebSocket::Protocol
     port = port || (tls ? 443 : 80)
 
     socket = TCPSocket.new(host, port)
-
-    {% if !flag?(:without_openssl) %}
-      if tls
-        if tls.is_a?(Bool) # true, but we want to get rid of the union
-          context = OpenSSL::SSL::Context::Client.new
-        else
-          context = tls
+    begin
+      {% if !flag?(:without_openssl) %}
+        if tls
+          if tls.is_a?(Bool) # true, but we want to get rid of the union
+            context = OpenSSL::SSL::Context::Client.new
+          else
+            context = tls
+          end
+          socket = OpenSSL::SSL::Socket::Client.new(socket, context: context, sync_close: true, hostname: host)
         end
-        socket = OpenSSL::SSL::Socket::Client.new(socket, context: context, sync_close: true, hostname: host)
+      {% end %}
+
+      random_key = Base64.strict_encode(StaticArray(UInt8, 16).new { rand(256).to_u8 })
+
+      headers["Host"] = "#{host}:#{port}"
+      headers["Connection"] = "Upgrade"
+      headers["Upgrade"] = "websocket"
+      headers["Sec-WebSocket-Version"] = VERSION
+      headers["Sec-WebSocket-Key"] = random_key
+
+      path = "/" if path.empty?
+      handshake = HTTP::Request.new("GET", path, headers)
+      handshake.to_io(socket)
+      socket.flush
+      handshake_response = HTTP::Client::Response.from_io(socket)
+      unless handshake_response.status_code == 101
+        raise Socket::Error.new("Handshake got denied. Status code was #{handshake_response.status_code}.")
       end
-    {% end %}
 
-    random_key = Base64.strict_encode(StaticArray(UInt8, 16).new { rand(256).to_u8 })
-
-    headers["Host"] = "#{host}:#{port}"
-    headers["Connection"] = "Upgrade"
-    headers["Upgrade"] = "websocket"
-    headers["Sec-WebSocket-Version"] = VERSION
-    headers["Sec-WebSocket-Key"] = random_key
-
-    path = "/" if path.empty?
-    handshake = HTTP::Request.new("GET", path, headers)
-    handshake.to_io(socket)
-    socket.flush
-    handshake_response = HTTP::Client::Response.from_io(socket)
-    unless handshake_response.status_code == 101
-      raise Socket::Error.new("Handshake got denied. Status code was #{handshake_response.status_code}.")
-    end
-
-    challenge_response = Protocol.key_challenge(random_key)
-    unless handshake_response.headers["Sec-WebSocket-Accept"]? == challenge_response
-      raise Socket::Error.new("Handshake got denied. Server did not verify WebSocket challenge.")
+      challenge_response = Protocol.key_challenge(random_key)
+      unless handshake_response.headers["Sec-WebSocket-Accept"]? == challenge_response
+        raise Socket::Error.new("Handshake got denied. Server did not verify WebSocket challenge.")
+      end
+    rescue exc
+      socket.close
+      raise exc
     end
 
     new(socket, masked: true)
