7f6b999c020518d6e8ac50cff9158a22c5917854
Serdar Dogruyol <dogruyolserdar@gmail.com>
2018-04-29 12:20:52 +0300

    Remove SemanticVersion

--- spec/compiler/macro/macro_methods_spec.cr
+++ spec/compiler/macro/macro_methods_spec.cr
@@ -1665,8 +1665,4 @@ describe "macro methods" do
       assert_macro "", %({{flag?(:foo)}}), [] of ASTNode, %(false)
     end
   end
-
-  it "compares versions" do
-    assert_macro "", %({{compare_versions("1.10.3", "1.2.3")}}), [] of ASTNode, %(1)
-  end
 end
