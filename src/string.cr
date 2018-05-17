645afb052550e1cfc846dd36feaa97a88b71dbf4
r00ster <35064754+r00ster91@users.noreply.github.com>
2018-05-17 09:28:57 +0200

    Improve documentation of String#% (#6095)

--- src/string.cr
+++ src/string.cr
@@ -4054,7 +4054,10 @@ class String
   # Interpolates *other* into the string using `Kernel#sprintf`.
   #
   # ```
-  # "Party like it's %d!!!" % 1999 # => "Party like it's 1999!!!"
+  # "I have %d apples" % 5                                             # => "I have 5 apples"
+  # "%s, %s, %s, D" % ['A', 'B', 'C']                                  # => "A, B, C, D"
+  # "sum: %{one} + %{two} = %{three}" % {one: 1, two: 2, three: 1 + 2} # => "sum: 1 + 2 = 3"
+  # "I have %<apples>s apples" % {apples: 4}                           # => "I have 4 apples"
   # ```
   def %(other)
     sprintf self, other
