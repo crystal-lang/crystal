54c3f968faf0f8e96ce48cb2cacdd67fab987e17
BuildTools <unconfigured@null.spigotmc.org>
2018-06-11 14:37:07 +0200

    Finish the \a escape sequence

--- spec/compiler/lexer/lexer_spec.cr
+++ spec/compiler/lexer/lexer_spec.cr
@@ -231,8 +231,8 @@ describe "Lexer" do
   it_lexes_number :i8, ["0i8", "0"]
 
   it_lexes_char "'a'", 'a'
-  it_lexes_char "'\\a'", 7.chr
-  it_lexes_char "'\\b'", 8.chr
+  it_lexes_char "'\\a'", '\u{7}'
+  it_lexes_char "'\\b'", '\u{8}'
   it_lexes_char "'\\n'", '\n'
   it_lexes_char "'\\t'", '\t'
   it_lexes_char "'\\v'", '\v'
