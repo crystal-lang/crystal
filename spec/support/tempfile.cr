bfbf43dec341aef65da350c1d120fbf37eae32f4
Johannes Müller <straightshoota@gmail.com>
2018-06-12 19:31:59 +0200

    fixup! Add spec helper `with_tempfile`

--- spec/support/tempfile.cr
+++ spec/support/tempfile.cr
@@ -21,7 +21,7 @@ SPEC_TEMPFILE_CLEANUP = ENV["SPEC_TEMPFILE_CLEANUP"]? != "0"
 # If the environment variable `SPEC_TEMPFILE_CLEANUP` is set to `0`, no paths
 # will be cleaned up, enabling easier debugging.
 def with_tempfile(*paths, file = __FILE__)
-  calling_spec = File.basename(file).gsub("_spec.cr", "")
+  calling_spec = File.basename(file).rchop("_spec.cr")
   paths = paths.map { |path| File.join(SPEC_TEMPFILE_PATH, calling_spec, path) }
   FileUtils.mkdir_p(File.join(SPEC_TEMPFILE_PATH, calling_spec))
 
