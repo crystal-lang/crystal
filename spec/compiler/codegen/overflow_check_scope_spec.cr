e6cb6cb3fc5474975e61981cc74b11e3d144f39a
Brian J. Cardiff <bcardiff@gmail.com>
2018-06-18 19:40:49 +0200

    Code gen: Use llvm overflow intrinsics for checked int subtraction

--- spec/compiler/codegen/overflow_check_scope_spec.cr
+++ spec/compiler/codegen/overflow_check_scope_spec.cr
@@ -72,6 +72,69 @@ describe "Code gen: overflow check scope" do
     {% end %}
   end
 
+  describe "for sub" do
+    it "can be unchecked" do
+      checked_run(%(
+        unchecked { -2147483648_i32 - 1_i32 }
+      )).to_i.should eq(2147483647_i32)
+    end
+
+    it "can be checked " do
+      unchecked_run(%(
+        require "prelude"
+
+        x = 0
+        begin
+          checked { -2147483648_i32 - 1_i32 }
+          x = 1
+        rescue OverflowError
+          x = 2
+        end
+        x
+      )).to_i.should eq(2)
+    end
+
+    {% for type in [UInt8, UInt16, UInt32, UInt64, Int8, Int16, Int32, Int64] %}
+      it "wrap around if unchecked for {{type}}" do
+        unchecked_run(%(
+          require "prelude"
+          {{type}}::MIN - {{type}}.new(1) == {{type}}::MAX
+        )).to_b.should be_true
+      end
+
+      it "raises if checked for {{type}}" do
+        checked_run(%(
+          require "prelude"
+          begin
+            {{type}}::MIN - {{type}}.new(1)
+            0
+          rescue OverflowError
+            1
+          end
+        )).to_i.should eq(1)
+      end
+
+      it "wrap around if unchecked for {{type}} - Int64" do
+        unchecked_run(%(
+          require "prelude"
+          {{type}}::MIN - 1_i64 == {{type}}::MAX
+        )).to_b.should be_true
+      end
+
+      it "raises if checked for {{type}} - Int64" do
+        checked_run(%(
+          require "prelude"
+          begin
+            {{type}}::MIN - 1_i64
+            0
+          rescue OverflowError
+            1
+          end
+        )).to_i.should eq(1)
+      end
+    {% end %}
+  end
+
   it "obey default checked" do
     checked_run(%(
       require "prelude"
