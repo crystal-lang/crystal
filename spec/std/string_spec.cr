c23337ae4401cb6e6f6c3b3898c9aa8663bcad5e
r00ster91 <r00ster91@outlook.com>
2018-06-19 16:11:27 +0200

    Finish

--- spec/std/string_spec.cr
+++ spec/std/string_spec.cr
@@ -1448,7 +1448,7 @@ describe "String" do
     "a".dump.should eq %("a")
     "\\".dump.should eq %("\\\\")
     "\"".dump.should eq %("\\\"")
-    # TODO: "\a".dump.should eq %("\\a")
+    "\a".dump.should eq %("\\a")
     "\b".dump.should eq %("\\b")
     "\e".dump.should eq %("\\e")
     "\f".dump.should eq %("\\f")
@@ -1476,7 +1476,7 @@ describe "String" do
     "a".inspect.should eq %("a")
     "\\".inspect.should eq %("\\\\")
     "\"".inspect.should eq %("\\\"")
-    # TODO: "\a".inspect.should eq %("\\a")
+    "\a".inspect.should eq %("\\a")
     "\b".inspect.should eq %("\\b")
     "\e".inspect.should eq %("\\e")
     "\f".inspect.should eq %("\\f")
@@ -1655,7 +1655,7 @@ describe "String" do
   end
 
   it "escapes chars" do
-    # TODO: "\a"[0].should eq('\a')
+    "\a"[0].should eq('\a')
     "\b"[0].should eq('\b')
     "\t"[0].should eq('\t')
     "\n"[0].should eq('\n')
