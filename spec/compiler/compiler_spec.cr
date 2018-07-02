5f4c3697641654959a313a0332fbba59334f8ca3
Johannes Müller <straightshoota@gmail.com>
2018-07-02 12:39:25 +0200

    Fix: Rename datapath helper to compiler_datapath for compiler specs

--- spec/compiler/compiler_spec.cr
+++ spec/compiler/compiler_spec.cr
@@ -4,7 +4,7 @@ require "./spec_helper"
 describe "Compiler" do
   it "compiles a file" do
     with_tempfile "compiler_spec_output" do |path|
-      Crystal::Command.run ["build", datapath("compiler_sample"), "-o", path]
+      Crystal::Command.run ["build", compiler_datapath("compiler_sample"), "-o", path]
 
       File.exists?(path).should be_true
 
@@ -13,7 +13,7 @@ describe "Compiler" do
   end
 
   it "runs subcommand in preference to a filename " do
-    Dir.cd datapath do
+    Dir.cd compiler_datapath do
       with_tempfile "compiler_spec_output" do |path|
         Crystal::Command.run ["build", "compiler_sample", "-o", path]
 
--- spec/compiler/spec_helper.cr
+++ spec/compiler/spec_helper.cr
@@ -1,6 +1,6 @@
 require "../spec_helper"
 require "../support/tempfile"
 
-def datapath(*components)
+def compiler_datapath(*components)
   File.join("spec", "compiler", "data", *components)
 end
