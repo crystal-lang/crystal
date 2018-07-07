c331845c5d7df2c7a01a9badf2e842709ad867a1
Ary Borenszweig <asterite@gmail.com>
2018-07-07 17:45:19 -0300

    Encoding: compare with case insensitive UTF-8 or UTF8 to avoid using iconv when not necessary

--- src/io.cr
+++ src/io.cr
@@ -1023,7 +1023,10 @@ abstract class IO
   # String operations (`gets`, `gets_to_end`, `read_char`, `<<`, `print`, `puts`
   # `printf`) will use this encoding.
   def set_encoding(encoding : String, invalid : Symbol? = nil)
-    if (encoding == "UTF-8") && (invalid != :skip)
+    if invalid != :skip && (
+         encoding.compare("UTF-8", case_insensitive: true) == 0 ||
+         encoding.compare("UTF8", case_insensitive: true) == 0
+       )
       @encoding = nil
     else
       @encoding = EncodingOptions.new(encoding, invalid)
