c6f4fd9858bcc7acd29c43d77d06edd702067d45
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-07-16 12:29:05 -0500

    Fix missing comment inside code block example (#6387)

--- src/time.cr
+++ src/time.cr
@@ -37,12 +37,12 @@ require "crystal/system/time"
 #
 # ```
 # time = Time.utc(2016, 2, 15, 10, 20, 30)
-# time.to_s        # => 2016-02-15 10:20:30 UTC
+# time.to_s # => 2016-02-15 10:20:30 UTC
 # time = Time.new(2016, 2, 15, 10, 20, 30, location: Time::Location.load("Europe/Berlin"))
-# time.to_s        # => 2016-02-15 10:20:30 +01:00 Europe/Berlin
-# The time-of-day can be omitted and defaults to midnight (start of day):
+# time.to_s # => 2016-02-15 10:20:30 +01:00 Europe/Berlin
+# # The time-of-day can be omitted and defaults to midnight (start of day):
 # time = Time.utc(2016, 2, 15)
-# time.to_s        # => 2016-02-15 00:00:00 UTC
+# time.to_s # => 2016-02-15 00:00:00 UTC
 # ```
 #
 # ### Retrieving Time Information
