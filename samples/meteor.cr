0f12dfda3bd4a46ae8ecb6ddcea36dec3e7284f5
r00ster91 <r00ster91@outlook.com>
2018-07-28 17:26:01 +0200

    Re-add sudoku comments, add comment to meteor

--- samples/meteor.cr
+++ samples/meteor.cr
@@ -1,3 +1,5 @@
+# Translated from: https://gitlab.redox-os.org/redox-os/rust/blob/a59de37e99060162a2674e3ff45409ac73595c0e/src/test/bench/shootout-meteor.rs
+
 alias Masks = Array(Array(Array(UInt64)))
 alias Point = Tuple(Int32, Int32)
 
