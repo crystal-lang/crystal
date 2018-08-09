e78f9292ff92cbf45488a6c1897229644d4e82e4
Ludovic Lagouardette <10891360+Zenohate@users.noreply.github.com>
2018-08-09 18:17:43 +0200

    Added 128bit primitives to the range definition

--- src/compiler/crystal/types.cr
+++ src/compiler/crystal/types.cr
@@ -1275,6 +1275,8 @@ module Crystal
         {Int32::MIN, Int32::MAX}
       when :i64
         {Int64::MIN, Int64::MAX}
+      when :i128
+        {Int128::MIN, Int128::MAX}
       when :u8
         {UInt8::MIN, UInt8::MAX}
       when :u16
@@ -1283,6 +1285,8 @@ module Crystal
         {UInt32::MIN, UInt32::MAX}
       when :u64
         {UInt64::MIN, UInt64::MAX}
+      when :u128
+        {UInt128::MIN, UInt128::MAX}
       else
         raise "Bug: called 'range' for non-integer literal"
       end
