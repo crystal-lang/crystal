19bb0fe8a73db2e01b95ccfc2eb795da0c4b0f66
Konstantin Makarchev <kostya27@gmail.com>
2018-08-09 18:24:53 +0300

    rename args

--- src/json/serialization.cr
+++ src/json/serialization.cr
@@ -117,7 +117,7 @@ module JSON
 
       def self.new(pull : ::JSON::PullParser)
         instance = allocate
-        instance.initialize(pull_for_json_serializable: pull)
+        instance.initialize(__pull_for_json_serializable: pull)
         GC.add_finalizer(instance) if instance.responds_to?(:finalize)
         instance
       end
@@ -132,8 +132,7 @@ module JSON
       end
     end
 
-    def initialize(*, pull_for_json_serializable : ::JSON::PullParser)
-      pull = pull_for_json_serializable
+    def initialize(*, __pull_for_json_serializable pull : ::JSON::PullParser)
       {% begin %}
         {% properties = {} of Nil => Nil %}
         {% for ivar in @type.instance_vars %}
--- src/yaml/serialization.cr
+++ src/yaml/serialization.cr
@@ -123,7 +123,7 @@ module YAML
 
         ctx.record_anchor(node, instance)
 
-        instance.initialize(context_for_yaml_serializable: ctx, node_for_yaml_serializable: node)
+        instance.initialize(__context_for_yaml_serializable: ctx, __node_for_yaml_serializable: node)
         GC.add_finalizer(instance) if instance.responds_to?(:finalize)
         instance
       end
@@ -138,9 +138,7 @@ module YAML
       end
     end
 
-    def initialize(*, context_for_yaml_serializable : YAML::ParseContext, node_for_yaml_serializable : ::YAML::Nodes::Node)
-      ctx = context_for_yaml_serializable
-      node = node_for_yaml_serializable
+    def initialize(*, __context_for_yaml_serializable ctx : YAML::ParseContext, __node_for_yaml_serializable node : ::YAML::Nodes::Node)
       {% begin %}
         {% properties = {} of Nil => Nil %}
         {% for ivar in @type.instance_vars %}
