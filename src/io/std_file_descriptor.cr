8d563a368aaf17481c6a52b1ab0cbe4c66b7092f
Jarrod Funnell <jarrod.funnell@gmail.com>
2018-08-11 13:00:35 +1000

    Don't set std handles to nonblocking after fork
    Even setting NONBLOCK for a split second is dangerous.
    From what I can see the child's stdin/out/err is supposed to be
    blocking at all times, so just reopen the original FD's in
    blocking mode.
    
    There is still a problem with handling closed filehandles that needs
    to be solved. `crystal_program | fast_exiting_child` crashes.. but this
    is apparently not a regression.

--- src/io/std_file_descriptor.cr
+++ src/io/std_file_descriptor.cr
@@ -2,24 +2,26 @@ require "./syscall"
 require "./file_descriptor"
 
 class IO::StdFileDescriptor < IO::FileDescriptor
-  def initialize(@fd : Int32)
-    @closed = false
+  def initialize(@fd)
+    # XXX: This is -supposed- to work, but something (libevent?) is changing the FD before here.
+    # strace confirms it.
+    @closed = LibC.fcntl(@fd, LibC::F_GETFD) < 0
+    return if @closed
+
+    path = uninitialized UInt8[256]
 
     # If we have a TTY for stdin/out/err, it is a shared terminal.
     # We need to reopen it to use O_NONBLOCK without causing other programs to break
-    if tty?
-      # Figure out the terminal TTY name
-      path = uninitialized UInt8[256]
 
-      # XXX: Need to error somehow, if this doesn't return 0
-      LibC.ttyname_r(@fd, path, 256)
-
+    # Figure out the terminal TTY name
+    if LibC.ttyname_r(@fd, path, 256) == 0
       # Open a fresh handle to the TTY
       @fd = LibC.open(path, LibC::O_RDWR)
 
       self.close_on_exec = true
       self.blocking = false
     else
+      # We are accidentally trying to block on closed handles due to the fcntl issue above.
       self.blocking = true
     end
   end
