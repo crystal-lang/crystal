7c930aa21ca83dfef3ae6c7d4e3190a259c6cea8
Anton Maminov <anton.linux@gmail.com>
2018-08-29 19:17:35 +0300

    Spec: add expectation be_empty (#6614)

--- spec/std/spec/expectations_spec.cr
+++ spec/std/spec/expectations_spec.cr
@@ -18,4 +18,14 @@ describe "expectations" do
     it { "1-2-3".should_not end_with("-2") }
     it { "1-2-3".should_not end_with("0-1-2-3") }
   end
+
+  context "empty" do
+    it { "".should be_empty }
+    it { Array(String).new.should be_empty }
+    it { Hash(String, String).new.should be_empty }
+    it { "foo".should_not be_empty }
+    it { ["foo", "bar"].should_not be_empty }
+    it { {"foo" => "bar"}.should_not be_empty }
+    it { {"foo", "bar"}.should_not be_empty }
+  end
 end
