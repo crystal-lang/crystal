41045d65b0b635348314271f429e201f62d90fce
Julien Portalier <julien@portalier.com>
2018-10-14 16:25:50 +0200

    Add Thread::ConditionVariable#wait with timeout.
    Configures thread condition variables to use the monotonic clock.
    
    Adds an overload for `#wait` that takes a timeout and executes the
    block when the timeout is reached, but doesn't when the condition
    variable is normally resumed.
    
    Refactors & enables Thread::ConditionVariable specs so they don't
    randomly hang.

--- src/lib_c/amd64-unknown-openbsd/c/pthread.cr
+++ src/lib_c/amd64-unknown-openbsd/c/pthread.cr
@@ -4,10 +4,14 @@ require "./sys/types"
 lib LibC
   PTHREAD_MUTEX_ERRORCHECK = 1
 
+  fun pthread_condattr_destroy(x0 : PthreadCondattrT*) : Int
+  fun pthread_condattr_init(x0 : PthreadCondattrT*) : Int
+  fun pthread_condattr_setclock(x0 : PthreadCondattrT*, x1 : ClockidT) : Int
   fun pthread_cond_broadcast(x0 : PthreadCondT*) : Int
   fun pthread_cond_destroy(x0 : PthreadCondT*) : Int
   fun pthread_cond_init(x0 : PthreadCondT*, x1 : PthreadCondattrT*) : Int
   fun pthread_cond_signal(x0 : PthreadCondT*) : Int
+  fun pthread_cond_timedwait(x0 : PthreadCondT*, x1 : PthreadMutexT*, x2 : Timespec*) : Int
   fun pthread_cond_wait(x0 : PthreadCondT*, x1 : PthreadMutexT*) : Int
   fun pthread_create(x0 : PthreadT*, x1 : PthreadAttrT*, x2 : Void* -> Void*, x3 : Void*) : Int
   fun pthread_detach(x0 : PthreadT) : Int
--- src/lib_c/i686-linux-gnu/c/pthread.cr
+++ src/lib_c/i686-linux-gnu/c/pthread.cr
@@ -3,10 +3,14 @@ require "./sys/types"
 lib LibC
   PTHREAD_MUTEX_ERRORCHECK = 2
 
+  fun pthread_condattr_destroy(attr : PthreadCondattrT*) : Int
+  fun pthread_condattr_init(attr : PthreadCondattrT*) : Int
+  fun pthread_condattr_setclock(attr : PthreadCondattrT*, type : ClockidT) : Int
   fun pthread_cond_broadcast(cond : PthreadCondT*) : Int
   fun pthread_cond_destroy(cond : PthreadCondT*) : Int
   fun pthread_cond_init(cond : PthreadCondT*, cond_attr : PthreadCondattrT*) : Int
   fun pthread_cond_signal(cond : PthreadCondT*) : Int
+  fun pthread_cond_timedwait(cond : PthreadCondT*, mutex : PthreadMutexT*, abstime : Timespec*) : Int
   fun pthread_cond_wait(cond : PthreadCondT*, mutex : PthreadMutexT*) : Int
   fun pthread_create(newthread : PthreadT*, attr : PthreadAttrT*, start_routine : Void* -> Void*, arg : Void*) : Int
   fun pthread_detach(th : PthreadT) : Int
