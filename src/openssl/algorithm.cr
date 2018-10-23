d4d4c721bc8dd49a0aa8b17f62e637d9ee317766
Andre Soares <andregsilvv@gmail.com>
2018-10-23 11:34:27 +0000

    Skipping OpenSSL::Algorithm inside the enum converter method

--- src/openssl/algorithm.cr
+++ src/openssl/algorithm.cr
@@ -13,15 +13,15 @@ module OpenSSL
 
     def to_evp
       case self
-      when OpenSSL::Algorithm::MD4       then LibCrypto.evp_md4
-      when OpenSSL::Algorithm::MD5       then LibCrypto.evp_md5
-      when OpenSSL::Algorithm::RIPEMD160 then LibCrypto.evp_ripemd160
-      when OpenSSL::Algorithm::SHA1      then LibCrypto.evp_sha1
-      when OpenSSL::Algorithm::SHA224    then LibCrypto.evp_sha224
-      when OpenSSL::Algorithm::SHA256    then LibCrypto.evp_sha256
-      when OpenSSL::Algorithm::SHA384    then LibCrypto.evp_sha384
-      when OpenSSL::Algorithm::SHA512    then LibCrypto.evp_sha512
-      else                                    raise "Invalid algorithm: #{self}"
+      when MD4       then LibCrypto.evp_md4
+      when MD5       then LibCrypto.evp_md5
+      when RIPEMD160 then LibCrypto.evp_ripemd160
+      when SHA1      then LibCrypto.evp_sha1
+      when SHA224    then LibCrypto.evp_sha224
+      when SHA256    then LibCrypto.evp_sha256
+      when SHA384    then LibCrypto.evp_sha384
+      when SHA512    then LibCrypto.evp_sha512
+      else                raise "Invalid algorithm: #{self}"
       end
     end
   end
