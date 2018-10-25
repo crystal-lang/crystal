5af1b408dd2b59f4488ed414e895fa881e0a60ae
Andre Soares <andregsilv@gmail.com>
2018-10-25 19:01:04 -0300

    Indent

--- src/openssl/pkcs5.cr
+++ src/openssl/pkcs5.cr
@@ -10,13 +10,13 @@ module OpenSSL::PKCS5
   end
 
   {% if compare_versions(LibSSL::OPENSSL_VERSION, "1.0.0") >= 0 %}
-  def self.pbkdf2_hmac(secret, salt, iterations = 2**16, algorithm : OpenSSL::Algorithm = OpenSSL::Algorithm::SHA1, key_size = 64) : Bytes
-    evp = algorithm.to_evp
-    buffer = Bytes.new(key_size)
-    if LibCrypto.pkcs5_pbkdf2_hmac(secret, secret.bytesize, salt, salt.bytesize, iterations, evp, key_size, buffer) != 1
-      raise OpenSSL::Error.new "pkcs5_pbkdf2_hmac"
+    def self.pbkdf2_hmac(secret, salt, iterations = 2**16, algorithm : OpenSSL::Algorithm = OpenSSL::Algorithm::SHA1, key_size = 64) : Bytes
+      evp = algorithm.to_evp
+      buffer = Bytes.new(key_size)
+      if LibCrypto.pkcs5_pbkdf2_hmac(secret, secret.bytesize, salt, salt.bytesize, iterations, evp, key_size, buffer) != 1
+        raise OpenSSL::Error.new "pkcs5_pbkdf2_hmac"
+      end
+      buffer
     end
-    buffer
-  end
   {% end %}
 end
