ecdb8db244b1dfc8a96dc5728b72edc047c83978
Andre Soares <andregsilv@gmail.com>
2018-12-01 10:13:44 -0200

    Add --format option to docs command (#6982)
    * Add --format option
    
    * Remove type 'sss'
    
    * Reuse README reading
    
    * Use %w for the array
    
    * Do not return tuple in `read_readme`
    
    * Typo: missing w

--- src/compiler/crystal/tools/doc/generator.cr
+++ src/compiler/crystal/tools/doc/generator.cr
@@ -28,7 +28,7 @@ class Crystal::Doc::Generator
     },
   }
 
-  def initialize(@program : Program, @included_dirs : Array(String), @output_dir : String, @canonical_base_url : String?)
+  def initialize(@program : Program, @included_dirs : Array(String), @output_dir : String, @output_format : String, @canonical_base_url : String?)
     @base_dir = Dir.current.chomp
     @types = {} of Crystal::Type => Doc::Type
     @repo_name = ""
@@ -46,20 +46,18 @@ class Crystal::Doc::Generator
       types.insert 0, program_type
     end
 
-    generate_docs program_type, types
+    if @output_format == "json"
+      generate_docs_json program_type, types
+    else
+      generate_docs_html program_type, types
+    end
   end
 
   def program_type
     type(@program)
   end
 
-  def generate_docs(program_type, types)
-    copy_files
-    generate_types_docs types, @output_dir, types
-    generate_readme program_type, types
-  end
-
-  def generate_readme(program_type, types)
+  def read_readme
     if File.file?("README.md")
       filename = "README.md"
     elsif File.file?("Readme.md")
@@ -67,12 +65,29 @@ class Crystal::Doc::Generator
     end
 
     if filename
-      raw_body = File.read(filename)
-      body = doc(program_type, raw_body)
+      content = File.read(filename)
     else
-      raw_body = ""
-      body = ""
+      content = ""
     end
+
+    content
+  end
+
+  def generate_docs_json(program_type, types)
+    readme = read_readme
+    json = Main.new(readme, Type.new(self, @program), repository_name)
+    puts json
+  end
+
+  def generate_docs_html(program_type, types)
+    copy_files
+    generate_types_docs types, @output_dir, types
+    generate_readme program_type, types
+  end
+
+  def generate_readme(program_type, types)
+    raw_body = read_readme
+    body = doc(program_type, raw_body)
 
     File.write File.join(@output_dir, "index.html"), MainTemplate.new(body, types, repository_name, @canonical_base_url)
 
