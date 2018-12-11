a6e67376f77d2b75cdc800decdf102886e263b23
Mark Raymond Jr <markrjr@live.com>
2018-12-11 00:54:59 -0600

    Revert part of system_soec and change conditional block for system.

--- spec/std/system_spec.cr
+++ spec/std/system_spec.cr
@@ -4,7 +4,7 @@ require "system"
 describe System do
   describe "hostname" do
     it "returns current hostname" do
-      shell_hostname = {{ `hostname`.stringify }}.strip # Workaround for inability to execute shell commands on Windows.
+      shell_hostname = `hostname`.strip # Workaround for inability to execute shell commands on Windows.
       $?.success?.should be_true                        # The hostname command has to be available
       hostname = System.hostname
       hostname.should eq(shell_hostname)
@@ -13,10 +13,9 @@ describe System do
 
   describe "cpu_count" do
     it "returns current CPU count" do
-      shell_cpus = 0
       {% if flag?(:win32) %}
         shell_cpus = ENV["NUMBER_OF_PROCESSORS"].to_i
-      {% else %}
+      {% elsif flag?(:unix) %}
         shell_cpus = `getconf _NPROCESSORS_ONLN 2>/dev/null || nproc --all 2>/dev/null || grep -sc '^processor' /proc/cpuinfo || sysctl -n hw.ncpu 2>/dev/null`.to_i
       {% end %}
       cpu_count = System.cpu_count
