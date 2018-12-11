0d7683a44b613389e7b85dc77f03c557a36e254f
Mark Raymond Jr <markrjr@live.com>
2018-12-11 14:26:37 -0600

    Flag changes for system.

--- src/crystal/system.cr
+++ src/crystal/system.cr
@@ -8,12 +8,14 @@ module Crystal::System
   # def self.cpu_count
 end
 
-require "./system/unix/hostname"
+{% if flag?(:unix) %}
+  require "./system/unix/hostname"
 
-{% if flag?(:freebsd) || flag?(:openbsd) %}
+  {% if flag?(:freebsd) || flag?(:openbsd) %}
   require "./system/unix/sysctl_cpucount"
-{% elsif flag?(:unix) %}
-  require "./system/unix/sysconf_cpucount"
+  {% else %}
+    require "./system/unix/sysconf_cpucount"
+  {% end %}
 {% elsif flag?(:win32) %}
   require "./system/win32/hostname"
   require "./system/win32/cpucount"
