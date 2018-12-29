a81779e04619a0c34d8711a053d16cb706c094b2
Dwight Watson <dwightwatson@me.com>
2018-12-29 19:12:14 +1100

    Break out standalone StatusCode class

--- src/http/server/response.cr
+++ src/http/server/response.cr
@@ -122,7 +122,7 @@ class HTTP::Server
     end
 
     protected def write_headers
-      status_message = HTTP.default_status_message_for(@status_code)
+      status_message = HTTP::StatusCode.default_message_for(@status_code)
       @io << @version << ' ' << @status_code << ' ' << status_message << "\r\n"
       headers.each do |name, values|
         values.each do |value|
