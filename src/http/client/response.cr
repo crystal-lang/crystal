b63e0cbd095fc4fc55b8ecacff42cfc7eeb58b73
Dwight Watson <dwightwatson@me.com>
2018-12-31 01:27:55 +1100

    Flip methdos back to instance methods

--- src/http/client/response.cr
+++ src/http/client/response.cr
@@ -10,7 +10,7 @@ class HTTP::Client::Response
   @cookies : Cookies?
 
   def initialize(@status_code, @body : String? = nil, @headers : Headers = Headers.new, status_message = nil, @version = "HTTP/1.1", @body_io = nil)
-    @status_message = status_message || HTTP::Status.message(@status_code)
+    @status_message = status_message || HTTP::Status.new(@status_code).message
 
     if Response.mandatory_body?(@status_code)
       @body = "" unless @body || @body_io
@@ -31,7 +31,7 @@ class HTTP::Client::Response
 
   # Returns `true` if the response status code is between 200 and 299.
   def success?
-    HTTP::Status.success?(status_code)
+    HTTP::Status.new(status_code).success?
   end
 
   # Returns a convenience wrapper around querying and setting cookie related
--- src/http/server/response.cr
+++ src/http/server/response.cr
@@ -126,7 +126,7 @@ class HTTP::Server
     end
 
     protected def write_headers
-      status_message = HTTP::Status.message(@status_code)
+      status_message = HTTP::Status.new(@status_code).message
       @io << @version << ' ' << @status_code << ' ' << status_message << "\r\n"
       headers.each do |name, values|
         values.each do |value|
