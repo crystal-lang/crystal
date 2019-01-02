bcff3eba5062f05bb35af711b769e639efb69b69
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-01-02 10:32:27 +0900

    Add --show-backtrace option to show backtrace on bug
    Fix error message on bug
    Fix exit status code on some errors

--- src/compiler/crystal/command/format.cr
+++ src/compiler/crystal/command/format.cr
@@ -9,6 +9,7 @@ class Crystal::Command
       FORMAT
       SYNTAX
       INVALID_BYTE_SEQUENCE
+      BUG
     end
   end
 
@@ -16,14 +17,15 @@ class Crystal::Command
     @format = "text"
     excludes = ["lib"] of String
     includes = [] of String
-    check = nil
+    check = false
+    show_backtrace = false
 
     option_parser =
       OptionParser.parse(options) do |opts|
         opts.banner = "Usage: crystal tool format [options] [file or directory]\n\nOptions:"
 
         opts.on("--check", "Checks that formatting code produces no changes") do |f|
-          check = [] of FormatResult
+          check = true
         end
 
         opts.on("-f text|json", "--format text|json", "Output format text (default) or json") do |f|
@@ -46,15 +48,20 @@ class Crystal::Command
         opts.on("--no-color", "Disable colored output") do
           @color = false
         end
+
+        opts.on("--show-backtrace", "Show backtrace on a bug (it is only for report!)") do
+          show_backtrace = true
+        end
       end
 
     files = options
-    check_files = check
+    check_files = [] of FormatResult
 
     if files.size == 1
       file = files.first
       if file == "-"
-        return format_stdin(check_files)
+        format_stdin(check)
+        return
       end
     end
 
@@ -68,25 +75,24 @@ class Crystal::Command
       files = normalize_paths files
     end
 
-    format_many files, check_files, excludes
+    format_many files, check, check_files, excludes, show_backtrace
 
-    if check_files
-      if check_files.empty?
-        exit 0
-      else
-        check_files.each do |result|
-          case result.code
-          when .format?
-            error "formatting '#{result.filename}' produced changes", exit_code: nil
-          when .syntax?
-            error "'#{result.filename}' has syntax errors", exit_code: nil
-          when .invalid_byte_sequence?
-            error "'#{result.filename}' is not a valid Crystal source file", exit_code: nil
-          end
+    if check
+      check_files.each do |result|
+        case result.code
+        when .format?
+          error "formatting '#{result.filename}' produced changes", exit_code: nil
+        when .syntax?
+          error "'#{result.filename}' has syntax errors", exit_code: nil
+        when .invalid_byte_sequence?
+          error "'#{result.filename}' is not a valid Crystal source file", exit_code: nil
+        when .bug?
+          error "there's a bug formatting '#{result.filename}'", exit_code: nil
         end
-        exit 1
       end
     end
+
+    exit check_files.empty? ? 0 : 1
   end
 
   private def normalize_paths(paths)
@@ -97,12 +103,12 @@ class Crystal::Command
     end
   end
 
-  private def format_stdin(check_files)
+  private def format_stdin(check)
     source = STDIN.gets_to_end
 
     begin
       result = Crystal.format(source)
-      exit(result == source ? 0 : 1) if check_files
+      exit(result == source ? 0 : 1) if check
 
       print result
     rescue ex : InvalidByteSequenceError
@@ -124,62 +130,68 @@ class Crystal::Command
     end
   end
 
-  private def format_many(files, check_files, excludes)
+  private def format_many(files, check, check_files, excludes, show_backtrace)
     files.each do |filename|
-      format_file_or_directory filename, check_files, excludes
+      format_file_or_directory filename, check, check_files, excludes, show_backtrace
     end
   end
 
-  private def format_file_or_directory(filename, check_files, excludes)
+  private def format_file_or_directory(filename, check, check_files, excludes, show_backtrace)
     if File.file?(filename)
       unless excludes.any? { |exclude| filename.starts_with?(exclude) }
-        format_file filename, check_files
+        format_file filename, check, check_files, show_backtrace
       end
     elsif Dir.exists?(filename)
       filename = filename.chomp('/')
       filenames = Dir["#{filename}/**/*.cr"]
-      format_many filenames, check_files, excludes
+      format_many filenames, check, check_files, excludes, show_backtrace
     else
       error "file or directory does not exist: #{filename}"
     end
   end
 
-  private def format_file(filename, check_files)
+  private def format_file(filename, check, check_files, show_backtrace)
     source = File.read(filename)
 
     begin
       result = Crystal.format(source, filename: filename)
       return if result == source
 
-      if check_files
+      if check
         check_files << FormatResult.new(filename, FormatResult::Code::FORMAT)
       else
         File.write(filename, result)
         STDOUT << "Format".colorize(:green).toggle(@color) << ' ' << filename << '\n'
       end
     rescue ex : InvalidByteSequenceError
-      if check_files
-        check_files << FormatResult.new(filename, FormatResult::Code::INVALID_BYTE_SEQUENCE)
-      else
+      check_files << FormatResult.new(filename, FormatResult::Code::INVALID_BYTE_SEQUENCE)
+      unless check
         STDERR.print "Error: ".colorize.toggle(@color).red.bold
         STDERR.print "file '#{Crystal.relative_filename(filename)}' is not a valid Crystal source file: ".colorize.toggle(@color).bold
         STDERR.puts ex.message
       end
     rescue ex : Crystal::SyntaxException
-      if check_files
-        check_files << FormatResult.new(filename, FormatResult::Code::SYNTAX)
-      else
+      check_files << FormatResult.new(filename, FormatResult::Code::SYNTAX)
+      unless check
         STDERR << "Syntax Error:".colorize(:yellow).toggle(@color) << ' ' << ex.message << " at " << filename << ':' << ex.line_number << ':' << ex.column_number << '\n'
       end
     rescue ex
-      couldnt_format "'#{filename}'", ex
+      check_files << FormatResult.new(filename, FormatResult::Code::BUG)
+      unless check
+        couldnt_format "'#{filename}'", show_backtrace ? ex : nil
+      end
     end
   end
 
   private def couldnt_format(file, ex)
-    ex.inspect_with_backtrace STDERR
-    Crystal.error <<-MSG, @color, exit_code: 1
-    couldn't format #{file}. You've found a bug in the Crystal formatter. Please open an issue, including source code that will allow us to reproduce the bug: https://github.com/crystal-lang/crystal/issues
-    MSG
+    if ex
+      ex.inspect_with_backtrace STDERR
+      STDERR.puts
+      error "couldn't format #{file}, please report a bug including the contents of it: https://github.com/crystal-lang/crystal/issues", exit_code: nil
+    else
+      error "there's a bug formatting #{file}, to show more information, please run:\n\n  $ crystal tool format --show-backtrace #{file}\n", exit_code: nil
+    end
+
+    STDERR.flush
   end
 end
