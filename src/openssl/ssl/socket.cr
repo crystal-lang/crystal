26246dab72a6e7f6551a4f6245c307c0c854cc6d
Bar Hofesh <bar.hofesh@gmail.com>
2019-01-09 17:59:34 +0200

    updated docs comment

--- src/openssl/ssl/socket.cr
+++ src/openssl/ssl/socket.cr
@@ -182,7 +182,7 @@ abstract class OpenSSL::SSL::Socket < IO
     raise IO::Error.new("Can't rewind OpenSSL::SSL::Socket::Client")
   end
 
-  # This method allows to fetch the SSL SNI string if present
+  # Returns the hostname provided through Server Name Indication (SNI)
   def hostname : String?
     if host_name = LibSSL.ssl_get_servername(@ssl, LibSSL::TLSExt::NAMETYPE_host_name)
       String.new(host_name)
