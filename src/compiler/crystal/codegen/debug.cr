30f7c7085982d6e64d55fae7e6585b3d6770ef7f
Brian J. Cardiff <bcardiff@gmail.com>
2019-01-24 13:10:25 -0300

    Workaround buggy offset debug info values (#7335)

--- src/compiler/crystal/codegen/debug.cr
+++ src/compiler/crystal/codegen/debug.cr
@@ -100,6 +100,10 @@ module Crystal
         if (ivar_type = ivar.type?) && (ivar_debug_type = get_debug_type(ivar_type))
           offset = @program.target_machine.data_layout.offset_of_element(struct_type, idx &+ (type.struct? ? 0 : 1))
           size = @program.target_machine.data_layout.size_in_bits(llvm_embedded_type(ivar_type))
+
+          # FIXME structs like LibC::PthreadMutexT generate huge offset values
+          next if offset > UInt64::MAX / 8u64
+
           member = di_builder.create_member_type(nil, name[1..-1], nil, 1, size, size, 8u64 * offset, LLVM::DIFlags::Zero, ivar_debug_type)
           element_types << member
         end
