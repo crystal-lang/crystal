b1abae7b7386f58761ac83e3af70620c15e0cb74
Brian J. Cardiff <bcardiff@gmail.com>
2019-02-04 15:47:34 -0300

    Fix SSL_shutdown (#7372)
    * Add manual specs to perform https requests
    * Assume connection is closed when ssl_shutdown reports syscall error
    Based on d5fe700ee0dfa6856364f31650fb2331dcbfdaf2
    * Cleanup unused code

---
+++ spec/manual/https_client_spec.cr
@@ -0,0 +1,15 @@
+require "spec"
+require "openssl"
+require "http"
+
+describe "https requests" do
+  it "can fetch from google.com" do
+    HTTP::Client.get("https://google.com")
+  end
+
+  it "can close request before consuming body" do
+    HTTP::Client.get("https://crystal-lang.org") do
+      break
+    end
+  end
+end
