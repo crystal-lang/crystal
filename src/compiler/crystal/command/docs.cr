2d3cda12cc22ae85142739b2702b8217052ee5e3
Johannes Müller <straightshoota@gmail.com>
2019-02-15 23:47:02 +0100

    Removes overriding local var

--- src/compiler/crystal/command/docs.cr
+++ src/compiler/crystal/command/docs.cr
@@ -84,7 +84,6 @@ class Crystal::Command
 
     included_dirs << File.expand_path("./src")
 
-    compiler = Compiler.new
     compiler.flags << "docs"
     compiler.wants_doc = true
     result = compiler.top_level_semantic sources
