a9447e8632c3af75de955869cb6d8e025a7e33c5
Brian J. Cardiff <bcardiff@gmail.com>
2019-03-01 16:01:33 -0300

    Playground: Compile programs using without_openssl flag

--- src/compiler/crystal/tools/playground/server.cr
+++ src/compiler/crystal/tools/playground/server.cr
@@ -55,6 +55,7 @@ module Crystal::Playground
       output_filename = tempfile "play-#{@session_key}-#{tag}"
       compiler = Compiler.new
       compiler.color = false
+      compiler.flags << "without_openssl"
       begin
         @logger.info "Instrumented code compilation started (session=#{@session_key}, tag=#{tag})."
         result = compiler.compile sources, output_filename
