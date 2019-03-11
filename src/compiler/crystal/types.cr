126e65307c0a1b3867a9a820d260058452d61d3f
Julien Reichardt <mi@jrei.ch>
2019-03-11 16:08:59 -0500

    Update src/compiler/crystal/types.cr
    Co-Authored-By: wontruefree <javajax@gmail.com>

--- src/compiler/crystal/types.cr
+++ src/compiler/crystal/types.cr
@@ -2804,7 +2804,7 @@ module Crystal
       end
     end
 
-    def to_s_with_options(io : IO, skip_union_parens : Bool = false, generic_args : Bool = true, codegen = false) : Nil
+    def to_s_with_options(io : IO, skip_union_parens : Bool = false, generic_args : Bool = true, codegen : Bool = false) : Nil
       instance_type.to_s(io)
       io << ".class"
     end
