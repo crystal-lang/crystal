d326f4530b33a1928f51d9cfbb55cee25f38fd7f
Julien Reichardt <mi@jrei.ch>
2019-03-11 16:07:44 -0500

    Update src/compiler/crystal/types.cr
    Co-Authored-By: wontruefree <javajax@gmail.com>

--- src/compiler/crystal/types.cr
+++ src/compiler/crystal/types.cr
@@ -2224,7 +2224,7 @@ module Crystal
       super
     end
 
-    def to_s_with_options(io : IO, skip_union_parens : Bool = false, generic_args : Bool = true, codegen = false) : Nil
+    def to_s_with_options(io : IO, skip_union_parens : Bool = false, generic_args : Bool = true, codegen : Bool = false) : Nil
       io << "Proc("
       arg_types.each do |type|
         type = type.devirtualize unless codegen
