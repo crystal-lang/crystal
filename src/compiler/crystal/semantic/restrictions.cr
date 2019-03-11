47995227a89b8534a1945f9114dd27c2b744c410
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2019-03-11 03:34:56 +0100

    Fix GenericClassType vs GenericClassInstanceType restrictions

--- src/compiler/crystal/semantic/restrictions.cr
+++ src/compiler/crystal/semantic/restrictions.cr
@@ -281,10 +281,10 @@ module Crystal
 
   class Generic
     def restriction_of?(other : Path, owner)
-      other_type = owner.lookup_type?(self)
-      if other_type
-        self_type = owner.lookup_path(other)
-        if self_type
+      self_type = owner.lookup_type?(self)
+      if self_type
+        other_type = owner.lookup_path(other)
+        if other_type
           return self_type.restriction_of?(other_type, owner)
         end
       end
@@ -300,6 +300,44 @@ module Crystal
         return false unless type_var.restriction_of?(other_type_var, owner)
       end
 
+      true
+    end
+  end
+
+  class GenericClassType
+    def restriction_of?(other : GenericClassInstanceType, owner)
+      # ```
+      # def foo(param : Array)
+      # end
+      #
+      # def foo(param : Array(Int32))
+      # end
+      # ```
+      #
+      # Here, self is `Array`, other is `Array(Int32)`
+
+      return unless self == other.generic_type
+
+      # `SomeGeneric` is never a restriction of `SomeGeneric(X)`
+      false
+    end
+  end
+
+  class GenericClassInstanceType
+    def restriction_of?(other : GenericClassType, owner)
+      # ```
+      # def foo(param : Array(Int32))
+      # end
+      #
+      # def foo(param : Array)
+      # end
+      # ```
+      #
+      # Here, self is `Array(Int32)`, other is `Array`
+
+      return unless self.generic_type == other
+
+      # `SomeGeneric(X)` is always a restriction of `SomeGeneric`
       true
     end
   end
