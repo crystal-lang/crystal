e0a1afd5f08b296d7dc8e2b8e51014dfb9e800f1
Julien Reichardt <mi@jrei.ch>
2019-03-11 16:06:11 -0500

    Update src/compiler/crystal/types.cr
    Co-Authored-By: wontruefree <javajax@gmail.com>

--- src/compiler/crystal/types.cr
+++ src/compiler/crystal/types.cr
@@ -1689,7 +1689,7 @@ module Crystal
       super(program)
     end
 
-    def to_s_with_options(io : IO, skip_union_parens : Bool = false, generic_args : Bool = true, codegen = false) : Nil
+    def to_s_with_options(io : IO, skip_union_parens : Bool = false, generic_args : Bool = true, codegen : Bool = false) : Nil
       io << '*' << @splatted_type
     end
   end
