8f6379ab62ee07f0c73ef591f691f4aab89b150f
maiha <maiha@wota.jp>
2019-03-21 04:14:12 +0900

    examples: fix HTTP::Server - Fix expecting token ')', not '127.0'

--- src/http/server.cr
+++ src/http/server.cr
@@ -296,9 +296,9 @@ class HTTP::Server
   #
   # ```
   # server = HTTP::Server.new { }
-  # server.bind("tcp://localhost:80")  # => Socket::IPAddress.new("127.0.0.1", 8080)
-  # server.bind("unix:///tmp/server.sock")  # => Socket::UNIXAddress.new("/tmp/server.sock")
-  # server.bind("tls://127.0.0.1:443?key=private.key&cert=certificate.cert&ca=ca.crt)  # => Socket::IPAddress.new("127.0.0.1", 443)
+  # server.bind("tcp://localhost:80")                                                  # => Socket::IPAddress.new("127.0.0.1", 8080)
+  # server.bind("unix:///tmp/server.sock")                                             # => Socket::UNIXAddress.new("/tmp/server.sock")
+  # server.bind("tls://127.0.0.1:443?key=private.key&cert=certificate.cert&ca=ca.crt") # => Socket::IPAddress.new("127.0.0.1", 443)
   # ```
   def bind(uri : String) : Socket::Address
     bind(URI.parse(uri))
