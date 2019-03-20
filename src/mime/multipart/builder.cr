78a0a0a3cd25cd4db3c45710ccf369914bdc2a61
maiha <maiha@wota.jp>
2019-03-21 05:15:20 +0900

    examples: fix MIME - Fix typo

--- src/mime/multipart/builder.cr
+++ src/mime/multipart/builder.cr
@@ -27,7 +27,7 @@ module MIME::Multipart
     # ```
     # io = IO::Memory.new # This is a stub. Actually, any IO can be used.
     # builder = MIME::Multipart::Builder.new(io, "a4VF")
-    # builder.content_type("mixed") # => "multipart/mixed; boundary=\"a4VF\""
+    # builder.content_type("mixed") # => "multipart/mixed; boundary=a4VF"
     # ```
     def content_type(subtype = "mixed")
       MIME::MediaType.new("multipart/#{subtype}", {"boundary" => @boundary}).to_s
