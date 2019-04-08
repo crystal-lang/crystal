fdec4df1b3d4cb0eace38c35643db70fbf7b3c62
Brian J. Cardiff <bcardiff@gmail.com>
2019-04-08 20:48:39 -0300

    Add Deprecated annotation to prelude and docs

---
+++ src/annotations.cr
@@ -0,0 +1,11 @@
+# This annotations marks methods, classes, constants, and macros as deprecated.
+#
+# It receives an optional `StringLiteral` as single argument containing a deprecation notice.
+#
+# ```
+# @[Deprecated("Use `#bar` instead")]
+# def foo
+# end
+# ```
+annotation Deprecated
+end
--- src/docs_main.cr
+++ src/docs_main.cr
@@ -2,6 +2,7 @@
 # It, for example, doesn't include API for the compiler, but does include
 # the fictitious API for the Crystal::Macros module.
 
+require "./annotations"
 require "./big"
 require "./compiler/crystal/macros"
 require "./crypto/**"
