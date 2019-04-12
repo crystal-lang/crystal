35d4ae30fdc8930c1a3783dace0f9cdd38fcfb23
Johannes Müller <straightshoota@gmail.com>
2019-04-12 16:31:47 +0200

    Update partial_comparable.cr

--- src/partial_comparable.cr
+++ src/partial_comparable.cr
@@ -7,8 +7,7 @@
 #
 # `PartialComparable` uses `<=>` to implement the conventional
 # comparison operators (`<`, `<=`, `==`, `>=`, and `>`).
-#
-# DEPRECATED: This module is deprecated as of Crystal 0.28.0. Its behaviour has been fully integrated into `Comparable`.
+@[Deprecated("This module is deprecated as of Crystal 0.28.0. Its behaviour has been fully integrated into `Comparable`.")]
 module PartialComparable(T)
   # Compares this object to *other* based on the receiver's `<=>` method,
   # returning `true` if it returns a negative number.
