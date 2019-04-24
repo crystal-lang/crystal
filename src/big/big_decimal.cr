d1beeeee32b1118346a9478f95234979360899f2
Johannes Müller <johannes.mueller@smj-fulda.org>
2019-04-24 22:14:09 +0200

    Apply @straight-shoota's suggestion
    Co-Authored-By: r00ster91 <r00ster91@protonmail.com>

--- src/big/big_decimal.cr
+++ src/big/big_decimal.cr
@@ -10,7 +10,7 @@ end
 #
 # It is internally represented by a pair of `BigInt` and `UInt64`: value and scale.
 # Value contains the actual value, and scale tells the decimal point place.
-# E.g. when value is 1234 and scale 2, the result is 12.34.
+# E.g. when value is `1234` and scale `2`, the result is `12.34`.
 #
 # The general idea and some of the arithmetic algorithms were adapted from
 # the MIT/APACHE-licensed [bigdecimal-rs](https://github.com/akubera/bigdecimal-rs).
