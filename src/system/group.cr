e05e20e6c44239e2146325b431ff9fc1ddb33704
William Woodruff <william@yossarian.net>
2019-04-28 21:58:30 -0400

    src: Document System::User and System::Group

--- src/system/group.cr
+++ src/system/group.cr
@@ -1,6 +1,7 @@
 require "crystal/system/group"
 
 class System::Group
+  # Raised on group lookup failure.
   class NotFound < Exception
   end
 
@@ -9,23 +10,47 @@ class System::Group
   private def initialize(@sys_group)
   end
 
+  # Returns the group associated with the given name, if it exists.
+  #
+  # Raises `Errno` if a system error occurs.
   def self.from_name?(name)
     sys_group = Crystal::System::Group.from_name?(name)
     new(sys_group) if sys_group
   end
 
+  # Returns the group associated with the given name.
+  #
+  # Raises `NotFound` if no such group exists.
+  # See `from_name?`.
   def self.from_name(name)
     self.from_name?(name) || raise NotFound.new("no such group: #{name}")
   end
 
+  # Returns the group associated with the given ID, if it exists.
+  #
+  # Raises `Errno` if a system error occurs.
   def self.from_id?(id)
     sys_group = Crystal::System::Group.from_id?(id)
     new(sys_group) if sys_group
   end
 
+  # Returns the group associated with the given ID.
+  #
+  # Raises `NotFound` if no such group exists.
+  # See `from_id?`.
   def self.from_id(id)
     self.from_id?(id) || raise NotFound.new("no such group: #{id}")
   end
 
-  delegate name, password, id, members, to: @sys_group
+  # Returns the group's name.
+  delegate name, to: @sys_group
+
+  # Returns the group's password.
+  delegate password, to: @sys_group
+
+  # Returns the group's ID.
+  delegate id, to: @sys_group
+
+  # Returns an array of the group's members.
+  delegate members, to: @sys_group
 end
--- src/system/user.cr
+++ src/system/user.cr
@@ -1,6 +1,7 @@
 require "crystal/system/user"
 
 class System::User
+  # Raised on user lookup failure.
   class NotFound < Exception
   end
 
@@ -9,23 +10,53 @@ class System::User
   private def initialize(@sys_user)
   end
 
+  # Returns the user associated with the given name, if it exists.
+  #
+  # Raises `Errno` if a system error occurs.
   def self.from_name?(name)
     sys_user = Crystal::System::User.from_name?(name)
     new(sys_user) if sys_user
   end
 
+  # Returns the user associated with the given name.
+  #
+  # Raises `NotFound` if no such user exists.
+  # See `from_name?`.
   def self.from_name(name)
     self.from_name?(name) || raise NotFound.new("no such user: #{name}")
   end
 
+  # Returns the user associated with the given ID, if it exists.
+  #
+  # Raises `Errno` if a system error occurs.
   def self.from_id?(id)
     sys_user = Crystal::System::User.from_id?(id)
     new(sys_user) if sys_user
   end
 
+  # Returns the user associated with the given ID.
+  #
+  # Raises `NotFound` if no such user exists.
+  # See `from_name?`.
   def self.from_id(id)
     self.from_id?(id) || raise NotFound.new("no such user: #{id}")
   end
 
-  delegate name, password, user_id, group_id, directory, shell, to: @sys_user
+  # Returns the user's username.
+  delegate name, to: @sys_user
+
+  # Returns the user's password.
+  delegate password, to: @sys_user
+
+  # Returns the user's ID.
+  delegate user_id, to: @sys_user
+
+  # Returns the user's group ID.
+  delegate group_id, to: @sys_user
+
+  # Returns the user's home directory.
+  delegate directory, to: @sys_user
+
+  # Returns the user's shell.
+  delegate shell, to: @sys_user
 end
