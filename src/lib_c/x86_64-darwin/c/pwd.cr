1b63e44d39ceed4d0badcdf8a204eb55e96d83d6
William Woodruff <william@yossarian.net>
2019-04-30 22:06:16 -0400

    src: Update passwd structure for Darwin

--- src/lib_c/x86_64-darwin/c/pwd.cr
+++ src/lib_c/x86_64-darwin/c/pwd.cr
@@ -4,9 +4,13 @@ lib LibC
     pw_passwd : Char*
     pw_uid : UidT
     pw_gid : GidT
+    pw_change : TimeT
+    pw_class : Char*
     pw_gecos : Char*
     pw_dir : Char*
     pw_shell : Char*
+    pw_expire : TimeT
+    pw_fields : Int
   end
 
   fun getpwnam_r(login : Char*, pwstore : Passwd*, buf : Char*, bufsize : SizeT, result : Passwd**) : Int
