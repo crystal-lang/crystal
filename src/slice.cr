d8e3d035b4f8b9d6c4ef0e38841ed74d28166fb7
Sijawusz Pur Rahnama <sija@sija.pl>
2019-05-02 21:14:04 +0200

    Implement `Slice#[]?` (#7701)

--- src/slice.cr
+++ src/slice.cr
@@ -197,6 +197,25 @@ struct Slice(T)
   # Returns a new slice that starts at *start* elements from this slice's start,
   # and of *count* size.
   #
+  # Returns `nil` if the new slice falls outside this slice.
+  #
+  # ```
+  # slice = Slice.new(5) { |i| i + 10 }
+  # slice # => Slice[10, 11, 12, 13, 14]
+  #
+  # slice2 = slice[1, 3]
+  # slice2 # => Slice[11, 12, 13]
+  # ```
+  def []?(start : Int, count : Int)
+    return unless 0 <= start <= @size
+    return unless 0 <= count <= @size - start
+
+    Slice.new(@pointer + start, count, read_only: @read_only)
+  end
+
+  # Returns a new slice that starts at *start* elements from this slice's start,
+  # and of *count* size.
+  #
   # Raises `IndexError` if the new slice falls outside this slice.
   #
   # ```
@@ -206,24 +225,37 @@ struct Slice(T)
   # slice2 = slice[1, 3]
   # slice2 # => Slice[11, 12, 13]
   # ```
-  def [](start, count)
-    unless 0 <= start <= @size
-      raise IndexError.new
-    end
-
-    unless 0 <= count <= @size - start
-      raise IndexError.new
-    end
-
-    Slice.new(@pointer + start, count, read_only: @read_only)
+  def [](start : Int, count : Int)
+    self[start, count]? || raise IndexError.new
   end
 
   # Returns a new slice with the elements in the given range.
   #
-  #
-  # Negative indices count backward from the end of the slice (-1 is the last
+  # Negative indices count backward from the end of the slice (`-1` is the last
   # element). Additionally, an empty slice is returned when the starting index
   # for an element range is at the end of the slice.
+  #
+  # Returns `nil` if the new slice falls outside this slice.
+  #
+  # ```
+  # slice = Slice.new(5) { |i| i + 10 }
+  # slice # => Slice[10, 11, 12, 13, 14]
+  #
+  # slice2 = slice[1..3]
+  # slice2 # => Slice[11, 12, 13]
+  # ```
+  def []?(range : Range)
+    start, count = Indexable.range_to_index_and_count(range, size)
+    self[start, count]?
+  end
+
+  # Returns a new slice with the elements in the given range.
+  #
+  # Negative indices count backward from the end of the slice (`-1` is the last
+  # element). Additionally, an empty slice is returned when the starting index
+  # for an element range is at the end of the slice.
+  #
+  # Raises `IndexError` if the new slice falls outside this slice.
   #
   # ```
   # slice = Slice.new(5) { |i| i + 10 }
