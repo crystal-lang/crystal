775c5c3d2be959981ef444d3d203bf6586a12a05
Ary Borenszweig <asterite@gmail.com>
2019-05-22 10:02:13 -0300

    Fix record macro with inheritance

--- spec/std/record_spec.cr
+++ spec/std/record_spec.cr
@@ -14,6 +14,11 @@ private module RecordSpec
     y = [2, 3]
 end
 
+private abstract struct Base
+end
+
+private record Sub < Base, x : Int32
+
 describe "record" do
   it "defines record with type declarations" do
     ary = [2, 3]
@@ -59,5 +64,15 @@ describe "record" do
     cloned.x.should eq(0)
     cloned.y.should eq(rec.y)
     cloned.y.should_not be(rec.y)
+  end
+
+  it "can clone record with parent type" do
+    rec = Sub.new 1
+    rec.clone.x.should eq(1)
+  end
+
+  it "can copy_with record with parent type" do
+    rec = Sub.new 1
+    rec.copy_with(x: 2).x.should eq(2)
   end
 end
