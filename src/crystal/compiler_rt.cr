1eab14075159846053e0a30fbec7b3fe6a842b7b
Jack Thorne <jack@kilmer.co>
2019-05-25 19:18:12 -0500

    testing

--- src/crystal/compiler_rt.cr
+++ src/crystal/compiler_rt.cr
@@ -12,19 +12,25 @@
 # fun __ashlhi3(a : Int16, b : Int32) : Int16
 # fun __ashlsi3(a : Int32, b : Int32) : Int32
 # fun __ashldi3(a : Int64, b : Int32) : Int64
-# fun __ashlti3(a : Int128, b : Int32) : Int128
+fun __ashlti3(a : Int128, b : Int32) : Int128
+  raise "__ashlti3"
+end
 
 # Functions for arithmetically shifting bits right eg. `a >> b`
 # fun __ashrhi3(a : Int16, b : Int32) : Int16
 # fun __ashrsi3(a : Int32, b : Int32) : Int32
 # fun __ashrdi3(a : Int64, b : Int32) : Int64
-# fun __ashrti3(a : Int128, b : Int32) : Int128
+fun __ashrti3(a : Int128, b : Int32) : Int128
+  raise "__ashrti3"
+end
 
 # Function for logically shifting left (signed shift)
 # fun __lshrhi3(a : Int16, b : Int32) : Int16
 # fun __lshrsi3(a : Int32, b : Int32) : Int32
 # fun __lshrdi3(a : Int64, b : Int32) : Int64
-# fun __lshrti3(a : Int128, b : Int32) : Int128
+fun __lshrti3(a : Int128, b : Int32) : Int128
+  raise "__lshrti3"
+end
 
 # Functions for returning the product eg. `a * b`
 # fun __mulqi3(a : Int8, b : Int8) : Int8
@@ -33,39 +39,6 @@
 # fun __muldi3(a : Int64, b : Int64) : Int64
 fun __multi3(a : Int128, b : Int128) : Int128
   raise "__multi3"
-  n = 128
-  min = Int128::MIN
-  max = Int128::MAX
-  result = a &* b
-  if a == min
-    if b != 0 && b != 1
-      raise
-    end
-    return result
-  end
-  if b == min
-    if a != 0 && a != 1
-      raise
-    end
-    return result
-  end
-  sa = a >> (n &- 1)
-  abs_a = (a ^ sa) &- sa
-  sb = b >> (n &- 1)
-  abs_b = (b ^ sb) &- sb
-  if abs_a < 2 || abs_b < 2
-    return result
-  end
-  if sa == sb
-    if abs_a > max / abs_b
-      raise
-    end
-  else
-    if abs_a > min / (0_i128 &- abs_b)
-      raise
-    end
-  end
-  return result
 end
 
 # Functions for returning the product with overflow eg. `a * b`
@@ -152,28 +125,36 @@ end
 # fun __divhi3(a : Int16, b : Int16) : Int16
 # fun __divsi3(a : Int32, b : Int32) : Int32
 # fun __divdi3(a : Int64, b : Int64) : Int64
-# fun __divti3(a : Int128, b : Int128) : Int128
+fun __divti3(a : Int128, b : Int128) : Int128
+  raise "__divti3"
+end
 
 # Function returning quotient for unsigned division eg. `a / b`
 # fun __udivqi3(a : Int8, b : Int8) : Int8
 # fun __udivhi3(a : Int16, b : Int16) : Int16
 # fun __udivsi3(a : Int32, b : Int32) : Int32
 # fun __udivdi3(a : Int64, b : Int64) : Int64
-# fun __udivti3(a : Int128, b : Int128) : Int128
+fun __udivti3(a : Int128, b : Int128) : Int128
+  raise "__udivti3"
+end
 
 # Function return the remainder of the signed division eg. `a % b`
 # fun __modqi3(a : Int8, b : Int8) : Int8
 # fun __modhi3(a : Int16, b : Int16) : Int16
 # fun __modsi3(a : Int32, b : Int32) : Int32
 # fun __moddi3(a : Int64, b : Int64) : Int64
-# fun __modti3(a : Int128, b : Int128) : Int128
+fun __modti3(a : Int128, b : Int128) : Int128
+  raise "__modti3"
+end
 
 # Function return the remainder of the unsigned division eg. `a % b`
 # fun __umodqi3(a : Int8, b : Int8) : Int8
 # fun __umodhi3(a : Int16, b : Int16) : Int16
 # fun __umodsi3(a : Int32, b : Int32) : Int32
 # fun __umoddi3(a : Int64, b : Int64) : Int64
-# fun __umodti3(a : Int128, b : Int128) : Int128
+fun __umodti3(a : Int128, b : Int128) : Int128
+  raise "__modti3"
+end
 
 
 
