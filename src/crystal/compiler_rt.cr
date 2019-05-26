d94a035aeed4f3d51af6b44f3991df13dc53f8ce
Jack Thorne <jack@kilmer.co>
2019-05-25 19:06:31 -0500

    unite 64 info and 128 info

--- src/crystal/compiler_rt.cr
+++ src/crystal/compiler_rt.cr
@@ -31,12 +31,48 @@
 # fun __mulhi3(a : Int16, b : Int16) : Int16
 # fun __mulsi3(a : Int32, b : Int32) : Int32
 # fun __muldi3(a : Int64, b : Int64) : Int64
-# fun __multi3(a : Int128, b : Int128) : Int128
+fun __multi3(a : Int128, b : Int128) : Int128
+  raise "__multi3"
+  n = 128
+  min = Int128::MIN
+  max = Int128::MAX
+  result = a &* b
+  if a == min
+    if b != 0 && b != 1
+      raise
+    end
+    return result
+  end
+  if b == min
+    if a != 0 && a != 1
+      raise
+    end
+    return result
+  end
+  sa = a >> (n &- 1)
+  abs_a = (a ^ sa) &- sa
+  sb = b >> (n &- 1)
+  abs_b = (b ^ sb) &- sb
+  if abs_a < 2 || abs_b < 2
+    return result
+  end
+  if sa == sb
+    if abs_a > max / abs_b
+      raise
+    end
+  else
+    if abs_a > min / (0_i128 &- abs_b)
+      raise
+    end
+  end
+  return result
+end
 
 # Functions for returning the product with overflow eg. `a * b`
 # NOTE: This is not in the GCC spec
 # fun __mulosi4(a : Int32, b : Int32, overflow : Int32*) : Int32
 fun __mulodi4(a : Int64, b : Int64, overflow : Int32*) : Int64
+  raise "__mulodi4"
   n = 64
   min = Int64::MIN
   max = Int64::MAX
@@ -73,44 +109,43 @@ fun __mulodi4(a : Int64, b : Int64, overflow : Int32*) : Int64
   return result
 end
 
-{% unless flag?(:bits64) %}
-  fun __muloti4(a : Int128, b : Int128, overflow : Int32*) : Int128
-    n = 128
-    min = Int128::MIN
-    max = Int128::MAX
-    overflow.value = 0
-    result = a &* b
-    if a == min
-      if b != 0 && b != 1
-        overflow.value = 1
-      end
-      return result
-    end
-    if b == min
-      if a != 0 && a != 1
-        overflow.value = 1
-      end
-      return result
-    end
-    sa = a >> (n &- 1)
-    abs_a = (a ^ sa) &- sa
-    sb = b >> (n &- 1)
-    abs_b = (b ^ sb) &- sb
-    if abs_a < 2 || abs_b < 2
-      return result
-    end
-    if sa == sb
-      if abs_a > max / abs_b
-        overflow.value = 1
-      end
-    else
-      if abs_a > min / (0_i128 &- abs_b)
-        overflow.value = 1
-      end
+fun __muloti4(a : Int128, b : Int128, overflow : Int32*) : Int128
+  raise "__muloti4"
+  n = 128
+  min = Int128::MIN
+  max = Int128::MAX
+  overflow.value = 0
+  result = a &* b
+  if a == min
+    if b != 0 && b != 1
+      overflow.value = 1
     end
     return result
   end
-{% end %}
+  if b == min
+    if a != 0 && a != 1
+      overflow.value = 1
+    end
+    return result
+  end
+  sa = a >> (n &- 1)
+  abs_a = (a ^ sa) &- sa
+  sb = b >> (n &- 1)
+  abs_b = (b ^ sb) &- sb
+  if abs_a < 2 || abs_b < 2
+    return result
+  end
+  if sa == sb
+    if abs_a > max / abs_b
+      overflow.value = 1
+    end
+  else
+    if abs_a > min / (0_i128 &- abs_b)
+      overflow.value = 1
+    end
+  end
+  return result
+end
 
 # Function returning quotient for signed division eg `a / b`
 # fun __divqi3(a : Int8, b : Int8) : Int8
