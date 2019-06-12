69214a300fbee430691966173ee4e5c38d4df2f2
Jack Thorne <jack@kilmer.co>
2019-06-12 15:55:50 -0500

    update umuldi3

--- src/crystal/compiler_rt.cr
+++ src/crystal/compiler_rt.cr
@@ -3,6 +3,11 @@
 # https://gcc.gnu.org/onlinedocs/gccint/Libgcc.html#Libgcc
 {% skip_file unless flag?(:compiler_rt) %}
 
+struct Int128Info
+  property low : UInt64 = 0_u64, high : Int64 = 0_i64
+  def initialize; end
+end
+
 # Integer Runtime Routines
 
 # Arithmetic Routines
@@ -36,8 +41,10 @@
 # fun __mulqi3(a : Int8, b : Int8) : Int8
 # fun __mulhi3(a : Int16, b : Int16) : Int16
 # fun __mulsi3(a : Int32, b : Int32) : Int32
-# fun __muldi3(a : Int64, b : Int64) : Int64
-def mulddi3(a : UInt64, b : UInt64) : Int128
+fun __muldi3(a : Int64, b : Int64) : Int64
+end
+
+def __umuldi3(a : UInt64, b : UInt64) : UInt128
   r = Int128Info.new
   bits_in_dword_2 = (sizeof(Int64) * 8) / 2
   lower_mask = ~0_u64 >> bits_in_dword_2
@@ -46,23 +53,24 @@ def mulddi3(a : UInt64, b : UInt64) : Int128
   r.low &= lower_mask
   t += (a >> bits_in_dword_2) * (b & lower_mask)
   r.low += (t & lower_mask) << bits_in_dword_2
-  r.high = (t >> bits_in_dword_2).unsafe_as(Int64) ## TODO validate cast to Int64
+  r.high = t >> bits_in_dword_2
   t = r.low >> bits_in_dword_2
   r.low &= lower_mask
   t += (b >> bits_in_dword_2) * (a & lower_mask)
   r.low += (t & lower_mask) << bits_in_dword_2
   r.high += t >> bits_in_dword_2
   r.high += (a >> bits_in_dword_2) * (b >> bits_in_dword_2)
-  r.unsafe_as(Int128)
+  r.unsafe_as(UInt128)
 end
 
-def multi3(a : Int128, b : Int128) : Int128
+def __multi3(a : Int128, b : Int128) : Int128
   x = a.unsafe_as(Int128Info)
   y = b.unsafe_as(Int128Info)
 
-  r = (x.low * y.low).unsafe_as(Int128Info)
+  r = umuldi3(x.low, y.low).unsafe_as(Int128Info)
   r.high += (x.high * y.low + x.low * y.high).unsafe_as(Int64)
 
+  # p x, y, r.unsafe_as(Int128Info)
   r.unsafe_as(Int128)
 end
 
