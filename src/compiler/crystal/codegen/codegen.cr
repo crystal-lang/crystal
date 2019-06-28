e00e7c383077f26529ab7ff4d63b110f1cdfcba6
Brian J. Cardiff <bcardiff@gmail.com>
2019-06-28 17:37:07 -0300

    Fix unions abstraction leak in instance_var_ptr

--- src/compiler/crystal/codegen/codegen.cr
+++ src/compiler/crystal/codegen/codegen.cr
@@ -2068,9 +2068,9 @@ module Crystal
 
       if type.is_a?(VirtualType)
         if type.struct?
-          if type.remove_indirection.is_a?(UnionType)
+          if (_type = type.remove_indirection).is_a?(UnionType)
             # For a struct we need to cast the second part of the union to the base type
-            value_ptr = gep(pointer, 0, 1)
+            _, value_ptr = union_type_and_value_pointer(pointer, _type)
             pointer = bit_cast value_ptr, llvm_type(type.base_type).pointer
           else
             # Nothing, there's only one subclass so it's the struct already
