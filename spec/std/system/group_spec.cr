b01143acca949ec8b3fb0cc8c881edba26ce1b91
William Woodruff <william@yossarian.net>
2019-07-21 19:49:09 -0400

    spec: Update tests

--- spec/std/system/group_spec.cr
+++ spec/std/system/group_spec.cr
@@ -5,24 +5,9 @@ GROUP_NAME = {{ `id -gn`.stringify.chomp }}
 GROUP_ID   = {{ `id -g`.stringify.chomp }}
 
 describe System::Group do
-  describe "from_name?" do
+  describe "find" do
     it "returns a group by name" do
-      group = System::Group.from_name?(GROUP_NAME).not_nil!
-
-      group.should be_a(System::Group)
-      group.name.should eq(GROUP_NAME)
-      group.id.should eq(GROUP_ID)
-    end
-
-    it "returns nil on nonexistent group" do
-      group = System::Group.from_name?("this_group_does_not_exist")
-      group.should eq(nil)
-    end
-  end
-
-  describe "from_name" do
-    it "returns a group by name" do
-      group = System::Group.from_name(GROUP_NAME)
+      group = System::Group.find(name: GROUP_NAME)
 
       group.should be_a(System::Group)
       group.name.should eq(GROUP_NAME)
@@ -31,63 +16,74 @@ describe System::Group do
 
     it "raises on nonexistent group" do
       expect_raises System::Group::NotFoundError, "No such group" do
-        System::Group.from_name("this_group_does_not_exist")
+        System::Group.find(name: "this_group_does_not_exist")
       end
     end
-  end
 
-  describe "from_id?" do
     it "returns a group by id" do
-      group = System::Group.from_id?(GROUP_ID).not_nil!
+      group = System::Group.find(id: GROUP_ID)
 
       group.should be_a(System::Group)
       group.id.should eq(GROUP_ID)
       group.name.should eq(GROUP_NAME)
+    end
+
+    it "raises on nonexistent group name" do
+      expect_raises System::Group::NotFoundError, "No such group" do
+        System::Group.find(id: "1234567")
+      end
+    end
+  end
+
+  describe "find?" do
+    it "returns a group by name" do
+      group = System::Group.find?(name: GROUP_NAME).not_nil!
+
+      group.should be_a(System::Group)
+      group.name.should eq(GROUP_NAME)
+      group.id.should eq(GROUP_ID)
     end
 
     it "returns nil on nonexistent group" do
-      group = System::Group.from_id?("1234567")
+      group = System::Group.find?(name: "this_group_does_not_exist")
       group.should eq(nil)
     end
-  end
 
-  describe "from_id" do
     it "returns a group by id" do
-      group = System::Group.from_id(GROUP_ID)
+      group = System::Group.find?(id: GROUP_ID).not_nil!
 
       group.should be_a(System::Group)
       group.id.should eq(GROUP_ID)
       group.name.should eq(GROUP_NAME)
     end
 
-    it "raises on nonexistent group" do
-      expect_raises System::Group::NotFoundError, "No such group" do
-        System::Group.from_id("1234567")
-      end
+    it "returns nil on nonexistent group id" do
+      group = System::Group.find?(id: "1234567")
+      group.should eq(nil)
     end
   end
 
   describe "name" do
     it "is the same as the source name" do
-      System::Group.from_name(GROUP_NAME).name.should eq(GROUP_NAME)
+      System::Group.find(name: GROUP_NAME).name.should eq(GROUP_NAME)
     end
   end
 
   describe "id" do
     it "is the same as the source ID" do
-      System::Group.from_id(GROUP_ID).id.should eq(GROUP_ID)
+      System::Group.find(id: GROUP_ID).id.should eq(GROUP_ID)
     end
   end
 
   describe "members" do
     it "calls without raising" do
-      System::Group.from_name(GROUP_NAME).members
+      System::Group.find(name: GROUP_NAME).members
     end
   end
 
   describe "to_s" do
     it "returns a string representation" do
-      System::Group.from_name(GROUP_NAME).to_s.should eq("#{GROUP_NAME} (#{GROUP_ID})")
+      System::Group.find(name: GROUP_NAME).to_s.should eq("#{GROUP_NAME} (#{GROUP_ID})")
     end
   end
 end
--- spec/std/system/user_spec.cr
+++ spec/std/system/user_spec.cr
@@ -5,24 +5,9 @@ USER_NAME = {{ `id -un`.stringify.chomp }}
 USER_ID   = {{ `id -u`.stringify.chomp }}
 
 describe System::User do
-  describe "from_username?" do
+  describe "find" do
     it "returns a user by name" do
-      user = System::User.from_username?(USER_NAME).not_nil!
-
-      user.should be_a(System::User)
-      user.username.should eq(USER_NAME)
-      user.user_id.should eq(USER_ID)
-    end
-
-    it "returns nil on nonexistent user" do
-      user = System::User.from_username?("this_user_does_not_exist")
-      user.should eq(nil)
-    end
-  end
-
-  describe "from_username" do
-    it "returns a user by name" do
-      user = System::User.from_username(USER_NAME)
+      user = System::User.find(name: USER_NAME)
 
       user.should be_a(System::User)
       user.username.should eq(USER_NAME)
@@ -31,75 +16,86 @@ describe System::User do
 
     it "raises on a nonexistent user" do
       expect_raises System::User::NotFoundError, "No such user" do
-        System::User.from_username("this_user_does_not_exist")
+        System::User.find(name: "this_user_does_not_exist")
       end
     end
-  end
 
-  describe "from_id?" do
     it "returns a user by id" do
-      user = System::User.from_id?(USER_ID).not_nil!
+      user = System::User.find(id: USER_ID)
 
       user.should be_a(System::User)
       user.user_id.should eq(USER_ID)
       user.username.should eq(USER_NAME)
+    end
+
+    it "raises on nonexistent user id" do
+      expect_raises System::User::NotFoundError, "No such user" do
+        System::User.find(id: "1234567")
+      end
+    end
+  end
+
+  describe "find?" do
+    it "returns a user by name" do
+      user = System::User.find?(name: USER_NAME).not_nil!
+
+      user.should be_a(System::User)
+      user.username.should eq(USER_NAME)
+      user.user_id.should eq(USER_ID)
     end
 
     it "returns nil on nonexistent user" do
-      user = System::User.from_id?("1234567")
+      user = System::User.find?(name: "this_user_does_not_exist")
       user.should eq(nil)
     end
-  end
 
-  describe "from_id" do
     it "returns a user by id" do
-      user = System::User.from_id(USER_ID)
+      user = System::User.find?(id: USER_ID).not_nil!
 
       user.should be_a(System::User)
       user.user_id.should eq(USER_ID)
       user.username.should eq(USER_NAME)
     end
 
-    it "raises on nonexistent user" do
-      expect_raises System::User::NotFoundError, "No such user" do
-        System::User.from_id("1234567")
-      end
+    it "returns nil on nonexistent user id" do
+      user = System::User.find?(id: "1234567")
+      user.should eq(nil)
     end
   end
 
   describe "username" do
     it "is the same as the source name" do
-      System::User.from_username(USER_NAME).username.should eq(USER_NAME)
+      System::User.find(name: USER_NAME).username.should eq(USER_NAME)
     end
   end
 
   describe "user_id" do
     it "is the same as the source ID" do
-      System::User.from_id(USER_ID).user_id.should eq(USER_ID)
+      System::User.find(id: USER_ID).user_id.should eq(USER_ID)
     end
   end
 
   describe "group_id" do
     it "calls without raising" do
-      System::User.from_username(USER_NAME).group_id
+      System::User.find(name: USER_NAME).group_id
     end
   end
 
   describe "directory" do
     it "calls without raising" do
-      System::User.from_username(USER_NAME).directory
+      System::User.find(name: USER_NAME).directory
     end
   end
 
   describe "shell" do
     it "calls without raising" do
-      System::User.from_username(USER_NAME).shell
+      System::User.find(name: USER_NAME).shell
     end
   end
 
   describe "to_s" do
     it "returns a string representation" do
-      System::User.from_username(USER_NAME).to_s.should eq("#{USER_NAME} (#{USER_ID})")
+      System::User.find(name: USER_NAME).to_s.should eq("#{USER_NAME} (#{USER_ID})")
     end
   end
 end
