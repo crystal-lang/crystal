595a63cc81440c8ab37c2f805378eb440abbb666
Johannes Müller <johannes.mueller@smj-fulda.org>
2019-08-02 13:19:00 +0200

    Update src/http/client.cr
    Co-Authored-By: Julien Portalier <julien@portalier.com>

--- src/http/client.cr
+++ src/http/client.cr
@@ -762,7 +762,7 @@ class HTTP::Client
         begin
           socket = OpenSSL::SSL::Socket::Client.new(tcp_socket, context: tls, sync_close: true, hostname: @host)
         rescue exc
-          # TODO: This fixes #7843 but needs a spec
+         # don't leak the TCP socket when the SSL connection failed
           tcp_socket.close
           raise exc
         end
