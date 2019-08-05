f9f57b53d38c1721584998d2b04850e97f13c0d5
Desone Burns II <dburnsii@live.com>
2019-08-05 09:46:27 -0700

    Removed standard library paths from `lib_flags_posix`

--- src/compiler/crystal/codegen/link.cr
+++ src/compiler/crystal/codegen/link.cr
@@ -105,7 +105,7 @@ module Crystal
     end
 
     private def lib_flags_posix
-      library_path = ["/usr/lib", "/usr/local/lib"]
+      library_path = [] of String
       has_pkg_config = nil
 
       String.build do |flags|
