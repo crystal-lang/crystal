8dc5c8f467c04494299d27cb3dae691ca71535ed
Juan Wajnerman <jwajnerman@manas.com.ar>
2019-08-13 18:04:51 -0300

    LibEvent MT support
    Move event_loop ivars to Thread
    Use libevent pthread support

--- src/crystal/event_loop.cr
+++ src/crystal/event_loop.cr
@@ -1,28 +1,54 @@
 require "./event"
 
-module Crystal::EventLoop
-  @@eb = Crystal::Event::Base.new
-  @@dns_base : Crystal::Event::DnsBase?
+class Thread
+  @eb : Crystal::Event::Base?
+  @dns_base : Crystal::Event::DnsBase?
+  @loop_fiber : Fiber?
 
+  # :nodoc:
+  def eb : Crystal::Event::Base
+    @eb ||= Crystal::Event::Base.new
+  end
+
+  # :nodoc:
+  def dns_base : Crystal::Event::DnsBase
+    @dns_base ||= self.eb.new_dns_base
+  end
+
+  # :nodoc:
+  def loop_fiber : Fiber
+    @loop_fiber ||= Fiber.new do
+      loop do
+        self.eb.run_once
+        Crystal::Scheduler.reschedule
+      end
+    end
+  end
+end
+
+module Crystal::EventLoop
   def self.after_fork
-    @@eb.reinit
+    raise "Not Supported"
   end
 
   def self.resume
     loop_fiber.resume
   end
 
+  private def self.eb
+    Thread.current.eb
+  end
+
+  private def self.dns_base
+    Thread.current.dns_base
+  end
+
   private def self.loop_fiber
-    @@loop_fiber ||= Fiber.new do
-      loop do
-        @@eb.run_once
-        Crystal::Scheduler.reschedule
-      end
-    end
+    Thread.current.loop_fiber
   end
 
   def self.create_resume_event(fiber)
-    @@eb.new_event(-1, LibEvent2::EventFlags::None, fiber) do |s, flags, data|
+    eb.new_event(-1, LibEvent2::EventFlags::None, fiber) do |s, flags, data|
       Crystal::Scheduler.enqueue data.as(Fiber)
     end
   end
@@ -31,7 +57,7 @@ module Crystal::EventLoop
     flags = LibEvent2::EventFlags::Write
     flags |= LibEvent2::EventFlags::Persist | LibEvent2::EventFlags::ET if edge_triggered
 
-    @@eb.new_event(io.fd, flags, io) do |s, flags, data|
+    eb.new_event(io.fd, flags, io) do |s, flags, data|
       io_ref = data.as(typeof(io))
       if flags.includes?(LibEvent2::EventFlags::Write)
         io_ref.resume_write
@@ -45,7 +71,7 @@ module Crystal::EventLoop
     flags = LibEvent2::EventFlags::Read
     flags |= LibEvent2::EventFlags::Persist | LibEvent2::EventFlags::ET if edge_triggered
 
-    @@eb.new_event(io.fd, flags, io) do |s, flags, data|
+    eb.new_event(io.fd, flags, io) do |s, flags, data|
       io_ref = data.as(typeof(io))
       if flags.includes?(LibEvent2::EventFlags::Read)
         io_ref.resume_read
@@ -53,10 +79,6 @@ module Crystal::EventLoop
         io_ref.resume_read(timed_out: true)
       end
     end
-  end
-
-  private def self.dns_base
-    @@dns_base ||= @@eb.new_dns_base
   end
 
   def self.create_dns_request(nodename, servname, hints, data, &callback : LibEvent2::DnsGetAddrinfoCallback)
