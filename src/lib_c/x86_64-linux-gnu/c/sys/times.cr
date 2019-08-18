fd0420fc1366763507a92479de9171b610598c5d
Jamie Gaskins <jgaskins@gmail.com>
2019-08-17 23:16:46 -0400

    Add RUsage for Linux on x86_64

--- src/lib_c/x86_64-linux-gnu/c/sys/times.cr
+++ src/lib_c/x86_64-linux-gnu/c/sys/times.cr
@@ -8,5 +8,28 @@ lib LibC
     tms_cstime : ClockT
   end
 
+  struct RUsage
+    ru_utime : Timeval
+    ru_stime : Timeval
+    ru_maxrss : Int32
+    ru_ix_rss : Int32
+    ru_idrss : Int32
+    ru_isrss : Int32
+    ru_minflt : Int32
+    ru_majflt : Int32
+    ru_nswap : Int32
+    ru_inblock : Int32
+    ru_oublock : Int32
+    ru_msgsnd : Int32
+    ru_msgrcv : Int32
+    ru_nsignals : Int32
+    ru_nvcsw : Int32
+    ru_nivcsw : Int32
+  end
+
+  RUSAGE_SELF     =  0
+  RUSAGE_CHILDREN = -1
+
   fun times(buffer : Tms*) : ClockT
+  fun getrusage(who : Int, usage : RUsage*) : Int16
 end
