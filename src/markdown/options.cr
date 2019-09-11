6e40d02fb2d51852ea6b136432fdb17dbaa87a89
Ary Borenszweig <asterite@gmail.com>
2019-09-11 16:38:08 -0300

    Markdown: remove `Options#gfm` because it's not supported yet
    No point in keeping a variable that, for now, has no use.

--- src/markdown/options.cr
+++ src/markdown/options.cr
@@ -1,9 +1,8 @@
 module Markdown
   struct Options
-    property gfm, toc, smart, source_pos, safe, prettyprint
+    property toc, smart, source_pos, safe, prettyprint
 
     def initialize(
-      @gfm = false,
       @toc = false,
       @smart = false,
       @source_pos = false,
