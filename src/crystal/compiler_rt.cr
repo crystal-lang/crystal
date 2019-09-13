0da755c1b6b825b37acf4b6654087c7e4f057986
Jack Thorne <jack@kilmer.co>
2019-09-13 13:53:30 -0500

    move some of tmp to compiler_rt

--- src/crystal/compiler_rt.cr
+++ src/crystal/compiler_rt.cr
@@ -8,6 +8,11 @@ struct Int128Info
   def initialize; end
 end
 
+struct UInt128Info
+  property low : UInt64 = 0_u64, high : UInt64 = 0_i64
+  def initialize; end
+end
+
 # Integer Runtime Routines
 
 # Arithmetic Routines
@@ -44,7 +49,7 @@ end
 # fun __muldi3(a : Int64, b : Int64) : Int64
 
 def __umuldi3(a : UInt64, b : UInt64) : UInt128
-  r = Int128Info.new
+  r = UInt128Info.new
   bits_in_dword_2 = (sizeof(Int64) * 8) / 2
   lower_mask = ~0_u64 >> bits_in_dword_2
   r.low = (a & lower_mask) * (b & lower_mask)
@@ -66,11 +71,9 @@ def __multi3(a : Int128, b : Int128) : Int128
   x = a.unsafe_as(Int128Info)
   y = b.unsafe_as(Int128Info)
 
-  ## TODO: deal with signed integer
   r = umuldi3(x.low, y.low).unsafe_as(Int128Info)
   r.high += (x.high * y.low + x.low * y.high).unsafe_as(Int64)
 
-  # p x, y, r.unsafe_as(Int128Info)
   r.unsafe_as(Int128)
 end
 
@@ -157,7 +160,13 @@ end
 # fun __divsi3(a : Int32, b : Int32) : Int32
 # fun __divdi3(a : Int64, b : Int64) : Int64
 fun __divti3(a : Int128, b : Int128) : Int128
-  raise "__divti3"
+  bits_in_tword_m1 = sizeof(Int128) * sizeof(Char) - 1
+  s_a = a >> bits_in_tword_m1                           # s_a = a < 0 ? -1 : 0
+  s_b = b >> bits_in_tword_m1                           # s_b = b < 0 ? -1 : 0
+  a = (a ^ s_a) - s_a                                   # negate if s_a == -1
+  b = (b ^ s_b) - s_b                                   # negate if s_b == -1
+  s_a ^= s_b                                            # sign of quotient
+  return __udivmodti4(a, b, (0_i128 ^ s_a)) - s_a         # negate if s_a == -1
 end
 
 # Function returning quotient for unsigned division eg. `a / b`
@@ -187,7 +196,127 @@ fun __umodti3(a : Int128, b : Int128) : Int128
   raise "__modti3"
 end
 
+fun __udivmodti4(a : UInt128, b : UInt128, rem : UInt128*)
+  n_udword_bits = sizeof(Int64) * sizeof(Char);
+  n_utword_bits = sizeof(Int128) * sizeof(Char);
+  n = a.unsafe_as(UInt128Info)
+  d = b.unsafe_as(UInt128Info)
+  q = UInt128Info.new
+  r = UInt128Info.new
+  sr = 0_u32
 
+  if (n.high == 0)
+    if (d.high == 0)
+      if rem
+        rem.value = (n.low % d.low).to_u128
+      end
+      return n.low / d.low
+    end
+    rem.value = n.low.to_u128 if rem
+    return 0
+    if (d.low == 0)
+      if (d.high == 0)
+        if rem
+          rem.value = n.high % d.low
+        end
+        return n.high / d.low
+      end
+      if (n.low == 0)
+        if rem
+          r.high = n.high % d.high
+          r.low = 0
+          rem.value = r.unsafe_as(UInt128)
+        end
+        return n.high / d.high
+      end
+      if ((d.high & (d.high - 1)) == 0) # if d is a power of 2
+        if rem
+          r.low = n.low
+          r.high = n.high & (d.high - 1)
+          rem.value = r.unsafe_as(UInt128)
+        end
+        return n.high >> d.s.high.trailing_zeros_count
+      end
+      sr = d.high.trailing_zeros_count - n.high.trailing_zeros_count
+      if (sr > n_udword_bits - 2)
+        if rem
+          rem.value = n.unsafe_as(UInt128)
+        end
+        return 0
+      end
+      sr = sr + 1
+      q.low = 0
+      q.high = n.low << (n_udword_bits - sr)
+      r.high = n.high >> sr
+      r.low = (n.high << (n_udword_bits - sr)) | (n.low >> sr)
+    end
+  else
+    if (d.high == 0)
+      if ((d.low & (d.low - 1)) == 0)
+        if rem
+          rem.value = (n.low & (d.low - 1)).to_u128
+        end
+        return n.unsafe_as(UInt128) if d.low == 1
+
+        sr = d.low.trailing_zeros_count
+        q.high = n.high >> sr
+        q.low = (n.high << (n_udword_bits - sr)) | (n.low >> sr)
+        return q.unsafe_as(UInt128)
+      end
+      sr = 1 + n_udword_bits + d.low.trailing_zeros_count - n.high.trailing_zeros_count
+      if (sr == n_udword_bits)
+        q.low = 0
+        q.high = n.low
+        r.high = 0
+        r.low = n.high
+      elsif (sr < n_udword_bits)
+        q.low = 0
+        q.high = n.low << (n_udword_bits - sr)
+        r.high = n.high >> sr
+        r.low = (n.high << (n_udword_bits - sr)) | (n.low >> sr)
+      else
+        q.low = n.low << (n_utword_bits - sr)
+        q.high = (n.high << (n_utword_bits - sr)) | (n.low >> (sr - n_udword_bits))
+        r.high = 0
+        r.low = n.high >> (sr - n_udword_bits)
+      end
+    else
+      sr = d.high.trailing_zeros_count - n.high.trailing_zeros_count
+      if (sr > n_udword_bits - 1)
+        rem.value = n.unsafe_as(UInt128) if rem
+        return 0
+      end
+      sr = sr + 1
+      q.low = 0
+      if (sr == n_udword_bits)
+        q.high = n.low
+        r.high = 0
+        r.low = n.high
+      else
+        r.high = n.high >> sr
+        r.low = (n.high << (n_udword_bits - sr)) | (n.low >> sr)
+        q.high = n.low << (n_udword_bits - sr)
+      end
+    end
+  end
+
+  carry = 0_u32
+  (sr..0).each do
+    r.high = (r.high << 1) | (r.low  >> (n_udword_bits - 1))
+    r.low  = (r.low  << 1) | (q.high >> (n_udword_bits - 1))
+    q.high = (q.high << 1) | (q.low  >> (n_udword_bits - 1))
+    q.low  = (q.low  << 1) | carry
+    s = (d.unsafe_as(UInt128) - r.unsafe_as(UInt128) - 1) >> (n_utword_bits - 1)
+    carry = s & 1
+    r = (r.unsafe_as(UInt128) - (d.unsafe_as(UInt128) & s)).unsafe_as(UInt128Info)
+  end
+
+  q = ((q.unsafe_as(UInt128) << 1) | carry).unsafe_as(UInt128Info)
+  if rem
+    rem.value = r.unsafe_as(UInt128)
+  end
+  return q.unsafe_as(UInt128)
+end
 
 
 
--- tmp.cr
+++ tmp.cr
@@ -105,10 +105,9 @@ o = 0
 muloti4(x, y, pointerof(o));
 puts "#{x} * #{y} raises #{o == 0 ? false : true}"
 
-CHAR_BIT = 1
 def udivmodti4(a : UInt128, b : UInt128, rem : UInt128*)
-  n_udword_bits = sizeof(Int64) * CHAR_BIT;
-  n_utword_bits = sizeof(Int128) * CHAR_BIT;
+  n_udword_bits = sizeof(Int64) * sizeof(Char);
+  n_utword_bits = sizeof(Int128) * sizeof(Char);
   n = a.unsafe_as(UInt128Info)
   d = b.unsafe_as(UInt128Info)
   q = UInt128Info.new
@@ -241,7 +240,7 @@ results = udivmodti4(x, y, pointerof(rem))
 puts "#{x} / #{y} = #{results} & #{rem}"
 
 def divti3(a : Int128, b : Int128) : Int128
-  bits_in_tword_m1 = sizeof(Int128) * CHAR_BIT - 1
+  bits_in_tword_m1 = sizeof(Int128) * sizeof(Char) - 1
   s_a = a >> bits_in_tword_m1                           # s_a = a < 0 ? -1 : 0
   s_b = b >> bits_in_tword_m1                           # s_b = b < 0 ? -1 : 0
   a = (a ^ s_a) - s_a                                   # negate if s_a == -1
