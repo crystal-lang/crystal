493f8a2bf2a8042500612a51058cbb59107e28fd
Jack Thorne <jack@kilmer.co>
2019-09-27 13:33:10 -0500

    remove unused initializers

--- src/crystal/compiler_rt/i128_info.cr
+++ src/crystal/compiler_rt/i128_info.cr
@@ -1,7 +1,5 @@
 module CompilerRT
   struct I128Info
     property low : UInt64 = 0_u64, high : Int64 = 0_i64
-
-    def initialize; end
   end
 end
--- src/crystal/compiler_rt/u128_info.cr
+++ src/crystal/compiler_rt/u128_info.cr
@@ -1,7 +1,5 @@
 module CompilerRT
   struct U128Info
     property low : UInt64 = 0_u64, high : UInt64 = 0_i64
-
-    def initialize; end
   end
 end
