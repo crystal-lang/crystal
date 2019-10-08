869c553887732f0e04c726ed5b7f9de6d9e09415
Julien Reichardt <mi@jrei.ch>
2019-10-08 16:12:35 +0200

    Change Process::Status class to struct

--- src/process/status.cr
+++ src/process/status.cr
@@ -1,5 +1,5 @@
 # The status of a terminated process.
-class Process::Status
+struct Process::Status
   # Platform-specific exit status code, which usually contains either the exit code or a termination signal.
   # The other `Process::Status` methods extract the values from `exit_status`.
   getter exit_status : Int32
