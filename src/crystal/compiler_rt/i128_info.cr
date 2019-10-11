bd6a9d934d9c509b7fa892576dd87583a19542fa
Jack Thorne <jack@kilmer.co>
2019-10-11 16:58:48 -0500

    use extern for strructs

--- src/crystal/compiler_rt/i128_info.cr
+++ src/crystal/compiler_rt/i128_info.cr
@@ -1,3 +1,4 @@
+@[Extern]
 module CompilerRT
   struct I128Info
     property low : UInt64 = 0_u64, high : Int64 = 0_i64
--- src/crystal/compiler_rt/u128_info.cr
+++ src/crystal/compiler_rt/u128_info.cr
@@ -1,3 +1,4 @@
+@[Extern]
 module CompilerRT
   struct U128Info
     property low : UInt64 = 0_u64, high : UInt64 = 0_i64
