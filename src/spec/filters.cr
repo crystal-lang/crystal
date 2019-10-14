7633bd33bc2718bef9429073d0676db440b2464c
Ary Borenszweig <asterite@gmail.com>
2019-10-14 14:40:04 -0300

    Spec: use nodoc instead of protected

--- src/spec/filters.cr
+++ src/spec/filters.cr
@@ -4,55 +4,67 @@ require "./context"
 
 module Spec
   module Item
-    protected def matches_pattern?(pattern : Regex) : Bool
+    # :nodoc:
+    def matches_pattern?(pattern : Regex) : Bool
       !!(@description =~ pattern)
     end
 
-    protected def matches_line?(line : Int32) : Bool
+    # :nodoc:
+    def matches_line?(line : Int32) : Bool
       @line == line || @line <= line <= @end_line
     end
 
-    protected def matches_locations?(locations : Hash(String, Array(Int32))) : Bool
+    # :nodoc:
+    def matches_locations?(locations : Hash(String, Array(Int32))) : Bool
       lines = locations[file]?
       !!(lines && lines.any? { |line| matches_line?(line) })
     end
   end
 
   class RootContext
-    protected def filter_by_pattern(pattern : Regex)
+    # :nodoc:
+    def filter_by_pattern(pattern : Regex)
       children.select!(&.filter_by_pattern(pattern))
     end
 
-    protected def filter_by_line(line : Int32)
+    # :nodoc:
+    def filter_by_line(line : Int32)
       children.select!(&.filter_by_line(line))
     end
 
-    protected def filter_by_locations(locations : Hash(String, Array(Int32)))
+    # :nodoc:
+    def filter_by_locations(locations : Hash(String, Array(Int32)))
       children.select!(&.filter_by_locations(locations))
     end
 
-    protected def filter_by_focus
+    # :nodoc:
+    def filter_by_focus
       children.select!(&.filter_by_focus)
     end
 
-    protected def filter_by_split(split_filter : SplitFilter)
+    # :nodoc:
+    def filter_by_split(split_filter : SplitFilter)
       children.select!(&.filter_by_split(split_filter))
     end
   end
 
   class ExampleGroup
+    # :nodoc:
+    #
     # Filters a context and its children by pattern.
     # Returns `true` if the context matches the pattern, `false` otherwise.
-    protected def filter_by_pattern(pattern : Regex) : Bool
+    def filter_by_pattern(pattern : Regex) : Bool
       return true if matches_pattern?(pattern)
 
       children.select!(&.filter_by_pattern(pattern))
       !children.empty?
     end
 
+    # :nodoc:
+    #
     # Filters a context and its children by line.
     # Returns `true` if the context matches the line, `false` otherwise.
-    protected def filter_by_line(line : Int32) : Bool
+    def filter_by_line(line : Int32) : Bool
       # If any children matches then we match too, but we filter children
       if children.any? &.matches_line?(line)
         children.select!(&.filter_by_line(line))
@@ -69,9 +81,11 @@ module Spec
       false
     end
 
+    # :nodoc:
+    #
     # Filters a context and its children by the given locations.
     # Returns `true` if the context matches the locations, `false` otherwise.
-    protected def filter_by_locations(locations : Hash(String, Array(Int32))) : Bool
+    def filter_by_locations(locations : Hash(String, Array(Int32))) : Bool
       # If any children matches then we match too, but we filter children
       if children.any? &.matches_locations?(locations)
         children.select!(&.filter_by_locations(locations))
@@ -88,53 +102,67 @@ module Spec
       false
     end
 
+    # :nodoc:
+    #
     # Filters a context and its children that are marked as focus.
     # Returns `true` if the context or any of its children have focus,
     # `false` otherwise.
-    protected def filter_by_focus : Bool
+    def filter_by_focus : Bool
       return true if focus?
 
       children.select!(&.filter_by_focus)
       !children.empty?
     end
 
+    # :nodoc:
+    #
     # Filters a context and its children by the given split filter
     # Returns `true` if the context matches the filter, `false` otherwise.
-    protected def filter_by_split(split_filter : SplitFilter) : Bool
+    def filter_by_split(split_filter : SplitFilter) : Bool
       children.select!(&.filter_by_split(split_filter))
       !children.empty?
     end
   end
 
   class Example
+    # :nodoc:
+    #
     # Returns `true` if the example matches the pattern,
     # `false` otherwise.
-    protected def filter_by_pattern(pattern : Regex) : Bool
+    def filter_by_pattern(pattern : Regex) : Bool
       matches_pattern?(pattern)
     end
 
+    # :nodoc:
+    #
     # Returns `true` if the example is contained in the given line,
     # `false` otherwise.
-    protected def filter_by_line(line : Int32) : Bool
+    def filter_by_line(line : Int32) : Bool
       matches_line?(line)
     end
 
+    # :nodoc:
+    #
     # Returns `true` if the example is contained in the any of the given locations,
     # `false` otherwise.
-    protected def filter_by_locations(locations : Hash(String, Array(Int32))) : Bool
+    def filter_by_locations(locations : Hash(String, Array(Int32))) : Bool
       matches_locations?(locations)
     end
 
+    # :nodoc:
+    #
     # Returns `true` if this example is marked as focus, `false` otherwise
-    protected def filter_by_focus : Bool
+    def filter_by_focus : Bool
       @focus
     end
 
     @@example_counter = -1
 
+    # :nodoc:
+    #
     # Returns `true` if the example is matches the given split filter,
     # `false` otherwise.`
-    protected def filter_by_split(split_filter : SplitFilter) : Bool
+    def filter_by_split(split_filter : SplitFilter) : Bool
       @@example_counter += 1
       @@example_counter % split_filter.quotient == split_filter.remainder
     end
