498997889b14f0f63a7417e94725bb0c5c7f0cd0
Jack Thorne <jack@kilmer.co>
2019-10-25 15:59:47 -0500

    add macros for i128 info structs

--- src/crystal/compiler_rt/i128_info.cr
+++ src/crystal/compiler_rt/i128_info.cr
@@ -6,6 +6,13 @@ end
 
 @[Extern(union: true)]
 struct Int128RT
+  macro [](high, low)
+    %i_info = uninitialized Int128RT
+    %i_info.info.high = {{high}}.unsafe_as(Int64)
+    %i_info.info.low = {{low}}.unsafe_as(UInt64)
+    %i_info
+  end
+
   property all : Int128 = 0_i128
   property info : Int128Info = Int128Info.new
 end
--- src/crystal/compiler_rt/u128_info.cr
+++ src/crystal/compiler_rt/u128_info.cr
@@ -6,6 +6,13 @@ end
 
 @[Extern(union: true)]
 struct UInt128RT
+  macro [](high, low)
+    %i_info = uninitialized UInt128RT
+    %i_info.info.high = {{high}}.unsafe_as(UInt64)
+    %i_info.info.low = {{low}}.unsafe_as(UInt64)
+    %i_info
+  end
+
   property all : UInt128 = 0_u128
   property info : UInt128Info = UInt128Info.new
 end
