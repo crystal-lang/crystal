7a08b32683d6941043fab26e5f494002321cdee7
Jack Thorne <jack@kilmer.co>
2019-10-28 22:06:58 -0500

    update divti3 and specs

--- src/crystal/compiler_rt/divti3.cr
+++ src/crystal/compiler_rt/divti3.cr
@@ -3,12 +3,12 @@ require "./udivmodti4"
 # Function returning quotient for signed division eg `a / b`
 
 fun __divti3(a : Int128, b : Int128) : Int128
-  bits_in_tword_m1 = sizeof(Int128) &* sizeof(Char) &- 1
+  bits_in_tword_m1 = (sizeof(Int128) &* sizeof(Char)) &- 1
   s_a = a >> bits_in_tword_m1
   s_b = b >> bits_in_tword_m1
   a = (a ^ s_a) &- s_a
   b = (b ^ s_b) &- s_b
   s_a ^= s_b
-  r = (0_i128 ^ s_a).unsafe_as(UInt128)
-  (__udivmodti4(a.unsafe_as(UInt128), b.unsafe_as(UInt128), pointerof(r)) &- s_a).unsafe_as(Int128)
+  r = 0_u128
+  ((__udivmodti4(a.unsafe_as(UInt128), b.unsafe_as(UInt128), pointerof(r)) ^ s_a) &- s_a).unsafe_as(Int128)
 end
