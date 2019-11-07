c3a6734e62a7210860b70a3a870deb86c9ac4f30
Ary Borenszweig <asterite@gmail.com>
2019-11-07 17:19:53 -0300

    Fix warning on using Channel.select

--- spec/std/channel_spec.cr
+++ spec/std/channel_spec.cr
@@ -362,7 +362,7 @@ describe "unbuffered" do
 
   it "works with select else" do
     ch1 = Channel(Int32).new
-    Channel.select({ch1.receive_select_action}, true).should eq({1, Channel::NotReady.new})
+    Channel.non_blocking_select({ch1.receive_select_action}).should eq({1, Channel::NotReady.new})
   end
 
   it "can send and receive nil" do
