2f796bc8a9cb088793449aa5dab300923619657e
Jan Zajic <jan.zajic@gmail.com>
2019-11-16 23:19:46 +0100

    add doc to hash select method - splat variants
    Co-Authored-By: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/hash.cr
+++ src/hash.cr
@@ -1439,7 +1439,7 @@ class Hash(K, V)
   # h1 = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}.select!({"a", "c"})
   # h2 = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}.select!("a", "c")
   # h3 = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}.select!(["a", "c"])
-  # h1 == h2 == h3
+  # h1 == h2 == h3 # => true
   # h1 # => {"a" => 1, "c" => 3}
   # ```
   def select!(keys : Array | Tuple)
