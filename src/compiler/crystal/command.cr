f87c135ffd2b7aa75552de5a11cf9d098e032c27
Roger Pack <rogerpack2005@gmail.com>
2019-11-28 03:36:09 -0700

    mention that there are many other options, not shown in the default help (#3628)
    * mention that there are many other options, not shown in the default help
    
    * incorporate suggestions, cleanup

--- src/compiler/crystal/command.cr
+++ src/compiler/crystal/command.cr
@@ -27,6 +27,9 @@ class Crystal::Command
         tool                     run a tool
         help, --help, -h         show this help
         version, --version, -v   show version
+
+    Run a command followed by --help to see command specific information, ex:
+        crystal <command> --help
     USAGE
 
   COMMANDS_USAGE = <<-USAGE
