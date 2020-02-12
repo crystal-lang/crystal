f2c339b96df7339290924814d9ddd9b55f9f4182
Juan Wajnerman <waj@manas.tech>
2020-02-12 15:28:03 -0300

    Use `print_error` instead of `STDERR` for top level error reporting. Ignore IO errors when flushing stdio streams.

--- src/crystal/system/print_error.cr
+++ src/crystal/system/print_error.cr
@@ -11,4 +11,19 @@ module Crystal::System
       LibC._write 2, buffer, len
     {% end %}
   end
+
+  def self.print_exception(message, ex)
+    print_error "%s: %s (%s)\n", message, ex.message || "(no message)", ex.class.name
+    begin
+      if bt = ex.backtrace?
+        bt.each do |frame|
+          print_error "  from %s\n", frame
+        end
+      else
+        print_error "  (no backtrace)\n"
+      end
+    rescue ex
+      print_error "Error while trying to dump the backtrace: %s (%s)\n", ex.message || "(no message)", ex.class.name
+    end
+  end
 end
