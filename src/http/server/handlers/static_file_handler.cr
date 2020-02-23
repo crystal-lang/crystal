cabf8ce4b62d2be93fe94bac847d8c3c08c67dbc
Sijawusz Pur Rahnama <sija@sija.pl>
2020-02-24 00:44:54 +0100

    Expand home within the path in couple of places moar

--- src/http/server/handlers/static_file_handler.cr
+++ src/http/server/handlers/static_file_handler.cr
@@ -19,7 +19,7 @@ class HTTP::StaticFileHandler
   # If *directory_listing* is `false`, directory listing is disabled. This means that
   # paths matching directories are ignored and next handler is called.
   def initialize(public_dir : String, fallthrough = true, directory_listing = true)
-    @public_dir = Path.new(public_dir).expand
+    @public_dir = Path.new(public_dir).expand(home: true)
     @fallthrough = !!fallthrough
     @directory_listing = !!directory_listing
   end
