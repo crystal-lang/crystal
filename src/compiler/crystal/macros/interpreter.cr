35780f760a05e435be481fcceef39da76e819929
Ary Borenszweig <asterite@gmail.com>
2020-02-23 10:39:26 -0300

    Execute while and until inside macros

--- src/compiler/crystal/macros/interpreter.cr
+++ src/compiler/crystal/macros/interpreter.cr
@@ -597,6 +597,24 @@ module Crystal
       end
     end
 
+    def visit(node : While)
+      while true
+        node.cond.accept self
+        break if !@last.truthy?
+        node.body.accept self
+      end
+      false
+    end
+
+    def visit(node : Until)
+      while true
+        node.cond.accept self
+        break if @last.truthy?
+        node.body.accept self
+      end
+      false
+    end
+
     def visit(node : OpAssign)
       @program.normalize(node).accept self
       false
