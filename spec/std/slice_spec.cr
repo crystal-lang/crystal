8dbb230b73a70246278777d354312dcedfb963f6
Stephen von Takach <steve@acaprojects.com>
2020-02-25 23:33:23 +1100

    fix slice spec formatting

--- spec/std/slice_spec.cr
+++ spec/std/slice_spec.cr
@@ -143,7 +143,7 @@ describe "Slice" do
 
     expect_raises(IndexError) { slice.copy_to(pointer, 5) }
   end
-  
+
   it "does convert to_bytes" do
     slice = Slice.new(3) { |i| i + 1 }
     slice.to_bytes.should eq(Bytes[1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0])
