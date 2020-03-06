7593670d79f95c3a52be50940603749a9795044f
Brian J. Cardiff <bcardiff@manas.tech>
2020-03-06 09:43:33 -0300

    Make Reflect an Enumerable private definition (#8884)

--- src/enumerable.cr
+++ src/enumerable.cr
@@ -1732,4 +1732,18 @@ module Enumerable(T)
       end
     {% end %}
   end
+
+  # :nodoc:
+  private struct Reflect(X)
+    # For now it's just a way to implement `Enumerable#sum` in a way that the
+    # initial value given to it has the type of the first type in the union,
+    # if the type is a union.
+    def self.first
+      {% if X.union? %}
+        {{X.union_types.first}}
+      {% else %}
+        X
+      {% end %}
+    end
+  end
 end
