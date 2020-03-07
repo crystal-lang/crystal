7896a191613affbcf3b469e64f3a70ca892aa66d
nia <nia@netbsd.org>
2020-03-07 21:33:07 +0000

    netbsd: More accurate pthreads

--- src/lib_c/x86_64-netbsd/c/sys/types.cr
+++ src/lib_c/x86_64-netbsd/c/sys/types.cr
@@ -2,6 +2,7 @@ require "../stddef"
 require "../stdint"
 
 lib LibC
+
   alias BlkcntT = LongLong
   alias BlksizeT = Int
   alias ClockT = LongLong
@@ -14,13 +15,52 @@ lib LibC
   alias NlinkT = UInt
   alias OffT = LongLong
   alias PidT = Int
-  type PthreadAttrT = Void*
-  type PthreadCondT = Void*
-  type PthreadCondattrT = Void*
-  type PthreadKeyT = Int
-  type PthreadMutexT = Void*
-  type PthreadMutexattrT = Void*
+
   type PthreadT = Void*
+
+  struct PthreadAttrT
+    pta_magic : UInt
+    pta_flags : Int
+    pta_private : Void*
+  end
+
+  struct PthreadQueueT
+    ptqh_first : Void*
+    ptqh_last : Void*
+  end
+
+  struct PthreadCondT
+    ptc_magic : UInt
+    ptc_lock : UInt8
+    ptc_waiters : PthreadQueueT
+    ptc_mutex : Void*
+    ptc_private : Void*
+  end
+
+  struct PthreadCondattrT
+    ptca_magic : UInt
+    ptca_private : Void*
+  end
+
+  type PthreadKeyT = Int
+
+  struct PthreadMutexT
+    ptm_magic : UInt
+    ptm_errorcheck : UInt8
+    ptm_pad1 : UInt8[3]
+    ptm_ceiling: UInt8
+    ptm_pad2 : UInt8[2]
+    ptm_owner: PthreadT
+    ptm_waiters : PthreadT*
+    ptm_recursed : Uint
+    ptm_spare2 : Void*
+  end
+
+  struct PthreadMutexattrT
+    ptma_magic : UInt
+    ptma_private : Void*
+  end
+
   alias SSizeT = Long
   alias SusecondsT = Long
   alias TimeT = LongLong
