e3bc9bb0cab60ed9d971b09f6a6c18abcd1b210c
RX14 <steph@rx14.co.uk>
2020-03-07 14:46:31 +0000

    Build only X86 target

--- src/llvm/lib_llvm.cr
+++ src/llvm/lib_llvm.cr
@@ -13,7 +13,8 @@ end
   {% end %}
   lib LibLLVM
     VERSION = {{`#{LibLLVM::LLVM_CONFIG} --version`.chomp.stringify}}
-    BUILT_TARGETS = {{ `#{LibLLVM::LLVM_CONFIG} --targets-built`.strip.downcase.split(' ').map(&.id.symbolize) }}
+    # BUILT_TARGETS = {{ `#{LibLLVM::LLVM_CONFIG} --targets-built`.strip.downcase.split(' ').map(&.id.symbolize) }}
+    BUILT_TARGETS = ["X86"]
   end
 {% end %}
 
