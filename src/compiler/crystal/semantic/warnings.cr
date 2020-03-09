2d91f14d0017e78a5609b78fec456921384dbece
Brian J. Cardiff <bcardiff@manas.tech>
2020-03-09 09:52:02 -0300

    Show warnings on docs command (#8880)
    * Add compiler warning options to docs command
    
    * Add Program#report_warning_at for formatted messages at location
    
    * Show warnings at specific location for nodoc and ditto

--- src/compiler/crystal/semantic/warnings.cr
+++ src/compiler/crystal/semantic/warnings.cr
@@ -6,5 +6,20 @@ module Crystal
 
       self.warning_failures << node.warning(message)
     end
+
+    def report_warning_at(location : Location?, message : String)
+      return unless self.warnings.all?
+      return if self.ignore_warning_due_to_location?(location)
+
+      if location
+        message = String.build do |io|
+          exception = SyntaxException.new message, location.line_number, location.column_number, location.filename
+          exception.warning = true
+          exception.append_to_s(nil, io)
+        end
+      end
+
+      self.warning_failures << message
+    end
   end
 end
