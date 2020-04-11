08e9449f7837c01ed9bb14f0b8c1baef40a61b16
Johannes Müller <straightshoota@gmail.com>
2020-04-11 17:35:19 +0200

    Use git_run helper instead of CI setup for git config

--- spec/compiler/crystal/tools/doc/project_info_spec.cr
+++ spec/compiler/crystal/tools/doc/project_info_spec.cr
@@ -3,6 +3,10 @@ require "../../../../support/tempfile"
 
 private alias ProjectInfo = Crystal::Doc::ProjectInfo
 
+private def run_git(command)
+  `git  -c user.email="" -c user.name="spec" #{command}`
+end
+
 describe Crystal::Doc::ProjectInfo do
   around_each do |example|
     with_tempfile("docs-project") do |tempdir|
@@ -31,20 +35,20 @@ describe Crystal::Doc::ProjectInfo do
       end
 
       it "git tagged version" do
-        `git init`
-        `git add shard.yml`
-        `git commit -m 'Initial commit' --no-gpg-sign`
-        `git tag v3.0`
+        run_git "init"
+        run_git "add shard.yml"
+        run_git "commit -m 'Initial commit' --no-gpg-sign"
+        run_git "tag v3.0"
 
         ProjectInfo.new_with_defaults(nil, nil) { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("foo", "3.0")
         ProjectInfo.new_with_defaults("bar", "2.0") { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("bar", "2.0")
       end
 
       it "git tagged version dirty" do
-        `git init`
-        `git add shard.yml`
-        `git commit -m 'Initial commit' --no-gpg-sign`
-        `git tag v3.0`
+        run_git "init"
+        run_git "add shard.yml"
+        run_git "commit -m 'Initial commit' --no-gpg-sign"
+        run_git "tag v3.0"
         File.write("foo.txt", "bar")
 
         ProjectInfo.new_with_defaults(nil, nil) { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("foo", "3.0-dev")
@@ -53,9 +57,9 @@ describe Crystal::Doc::ProjectInfo do
       end
 
       it "git non-tagged commit" do
-        `git init`
-        `git add shard.yml`
-        `git commit -m 'Initial commit' --no-gpg-sign`
+        run_git "init"
+        run_git "add shard.yml"
+        run_git "commit -m 'Initial commit' --no-gpg-sign"
 
         ProjectInfo.new_with_defaults(nil, nil) { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("foo", "master")
         ProjectInfo.new_with_defaults(nil, "1.1") { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("foo", "1.1")
@@ -63,9 +67,9 @@ describe Crystal::Doc::ProjectInfo do
       end
 
       it "git non-tagged commit dirty" do
-        `git init`
-        `git add shard.yml`
-        `git commit -m 'Initial commit' --no-gpg-sign`
+        run_git "init"
+        run_git "add shard.yml"
+        run_git "commit -m 'Initial commit' --no-gpg-sign"
         File.write("foo.txt", "bar")
 
         ProjectInfo.new_with_defaults(nil, nil) { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("foo", "master-dev")
@@ -76,10 +80,10 @@ describe Crystal::Doc::ProjectInfo do
 
     it "no shard.yml, but git tagged version" do
       File.write("foo.txt", "bar")
-      `git init`
-      `git add foo.txt`
-      `git commit -m 'Remove shard.yml' --no-gpg-sign`
-      `git tag v4.0`
+      run_git "init"
+      run_git "add foo.txt"
+      run_git "commit -m 'Remove shard.yml' --no-gpg-sign"
+      run_git "tag v4.0"
 
       ProjectInfo.new_with_defaults(nil, nil) { |name, version| "missing:#{name}:#{version}" }.should eq "missing::4.0"
       ProjectInfo.new_with_defaults("foo", nil) { |name, version| "missing:#{name}:#{version}" }.should eq ProjectInfo.new("foo", "4.0")
@@ -92,23 +96,23 @@ describe Crystal::Doc::ProjectInfo do
     ProjectInfo.find_git_version.should be_nil
 
     # Empty git directory
-    `git init`
+    run_git "init"
     ProjectInfo.find_git_version.should be_nil
 
     # Non-tagged commit
     File.write("file.txt", "foo")
-    `git add file.txt`
-    `git commit -m 'Initial commit' --no-gpg-sign`
+    run_git "add file.txt"
+    run_git "commit -m 'Initial commit' --no-gpg-sign"
     ProjectInfo.find_git_version.should eq "master"
 
     # Non-tagged commit, dirty workdir
     File.write("file.txt", "bar")
     ProjectInfo.find_git_version.should eq "master-dev"
 
-    `git checkout -- .`
+    run_git "checkout -- ."
 
     # Tagged commit
-    `git tag v0.1.0`
+    run_git "tag v0.1.0"
     ProjectInfo.find_git_version.should eq "0.1.0"
 
     # Tagged commit, dirty workdir
@@ -116,18 +120,18 @@ describe Crystal::Doc::ProjectInfo do
     ProjectInfo.find_git_version.should eq "0.1.0-dev"
 
     # Tagged commit, dirty index
-    `git add file.txt`
+    run_git "add file.txt"
     ProjectInfo.find_git_version.should eq "0.1.0-dev"
 
-    `git reset --hard v0.1.0`
+    run_git "reset --hard v0.1.0"
     ProjectInfo.find_git_version.should eq "0.1.0"
 
     # Multiple tags
-    `git tag v0.2.0`
+    run_git "tag v0.2.0"
     ProjectInfo.find_git_version.should eq "master"
 
     # Other branch
-    `git checkout -b foo`
+    run_git "checkout -b foo"
     ProjectInfo.find_git_version.should eq "foo"
   end
 
