f328f976fa5cd0f33c52ace0570685e304cc5fee
Johannes Müller <straightshoota@gmail.com>
2020-04-15 13:50:48 +0200

    Remove README link in API docs
    Also removes repository-links section which had only this single item.
    It can be added again in case of future expansion.

--- src/compiler/crystal/tools/doc/html/js/_search.js
+++ src/compiler/crystal/tools/doc/html/js/_search.js
@@ -353,11 +353,9 @@ CrystalDoc.displaySearchResults = function(results, query) {
 CrystalDoc.toggleResultsList = function(visible) {
   if (visible) {
     document.querySelector(".types-list").classList.add("hidden");
-    document.querySelector(".repository-links").classList.add("hidden");
     document.querySelector(".search-results").classList.remove("hidden");
   } else {
     document.querySelector(".types-list").classList.remove("hidden");
-    document.querySelector(".repository-links").classList.remove("hidden");
     document.querySelector(".search-results").classList.add("hidden");
   }
 };
