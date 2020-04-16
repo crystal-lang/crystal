6760e8f4e645b7be48adc5902e1fb32d02c6a433
Brian J. Cardiff <bcardiff@gmail.com>
2020-04-16 15:11:32 -0300

    Drop top-level parallel macro

--- spec/std/concurrent_spec.cr
+++ spec/std/concurrent_spec.cr
@@ -9,43 +9,7 @@ private def method_named(expected_named, chan)
   chan.close
 end
 
-class SomeParallelJobException < Exception
-end
-
-private def raising_job : String
-  raise SomeParallelJobException.new("boom")
-  "result"
-end
-
 describe "concurrent" do
-  describe "parallel" do
-    pending_win32 "does four things concurrently" do
-      a, b, c, d = parallel(1 + 2, "hello".size, [1, 2, 3, 4].size, nil)
-      a.should eq(3)
-      b.should eq(5)
-      c.should eq(4)
-      d.should be_nil
-    end
-
-    pending_win32 "re-raises errors from Fibers as ConcurrentExecutionException" do
-      exception = expect_raises(ConcurrentExecutionException) do
-        a, b = parallel(raising_job, raising_job)
-      end
-
-      exception.cause.should be_a(SomeParallelJobException)
-    end
-
-    # FIXME: Compiler bug with typeof inside an unused block https://github.com/crystal-lang/crystal/issues/8669
-    {% unless flag?(:win32) %}
-      pending_win32 "is strict about the return value type" do
-        a, b = parallel(1 + 2, "hello")
-
-        typeof(a).should eq(Int32)
-        typeof(b).should eq(String)
-      end
-    {% end %}
-  end
-
   describe "spawn" do
     it "uses spawn macro" do
       chan = Channel(Int32).new
