f2d0188bac0266f9e4fd4ada4fd048643a6c9100
max-codeware <max.codeware@gmail.com>
2020-04-29 15:55:03 +0200

    Added one more spec

--- spec/compiler/parser/parser_spec.cr
+++ spec/compiler/parser/parser_spec.cr
@@ -1854,6 +1854,7 @@ module Crystal
       it_parses "def foo(x,y);end;foo = 10;puts(foo +a, b)", [Def.new("foo", ["x".arg, "y".arg] of Arg), Assign.new("foo".var, 10.int32), Call.new(nil, "puts", [Call.new("foo".var, "+", "b".var), "b".var] of ASTNode)]
       it_parses "def foo(x,y);end;foo = 10; puts foo a, b", [Def.new("foo", ["x".arg, "y".arg] of Arg), Assign.new("foo".var, 10.int32), Call.new(nil, "puts", Call.new(nil, "foo", ["a".var, "b".var] of ASTNode))]
       it_parses "def foo(x,y);end;foo = 10; puts(foo a, b)", [Def.new("foo", ["x".arg, "y".arg] of Arg), Assign.new("foo".var, 10.int32), Call.new(nil, "puts", Call.new(nil, "foo", ["a".var, "b".var] of ASTNode))]
+      it_parses "def foo(x,y);end; puts foo -a, b", [Def.new("foo", ["x".arg, "y".arg] of Arg), Call.new(nil, "puts", Call.new(nil, "foo", ["a".var, "b".var] of ASTNode))]
 
       it "gets corrects of ~" do
         node = Parser.parse("\n  ~1")
