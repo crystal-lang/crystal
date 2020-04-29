2316e70eb1de241c504a3ad243332581649948ef
max-codeware <max.codeware@gmail.com>
2020-04-29 21:54:15 +0200

    commented #inspect

--- src/compiler/crystal/syntax/to_s.cr
+++ src/compiler/crystal/syntax/to_s.cr
@@ -3,9 +3,9 @@ require "./visitor"
 
 module Crystal
   class ASTNode
-    def inspect(io : IO) : Nil
-      to_s(io)
-    end
+    # def inspect(io : IO) : Nil
+    #   to_s(io)
+    # end
 
     def to_s(io : IO, macro_expansion_pragmas = nil, emit_doc = false) : Nil
       visitor = ToSVisitor.new(io, macro_expansion_pragmas: macro_expansion_pragmas, emit_doc: emit_doc)
