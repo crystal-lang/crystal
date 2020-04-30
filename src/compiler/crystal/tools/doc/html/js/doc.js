64d7d9ef2c371d795fa08b86a9cb98b0e301e1ab
Brian J. Cardiff <bcardiff@gmail.com>
2020-04-30 11:35:42 -0300

    Avoid double escaping arguments
    arg_to_html already do the escaping
    arg_to_s uses arg_to_html
    id uses to_s
    html_id uses id, no need to HTML.escape it

--- src/compiler/crystal/tools/doc/html/js/doc.js
+++ src/compiler/crystal/tools/doc/html/js/doc.js
@@ -186,14 +186,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var scrollToEntryFromLocationHash = function() {
     var hash = window.location.hash;
     if (hash) {
-      var targetAnchor = unescape(hash.substr(1));
-      var targetEl = document.querySelectorAll('.entry-detail[id="' + targetAnchor.replace(/"/g, '&quot;') + '"]')
-
-      if (targetEl && targetEl.length > 0) {
-        targetEl[0].offsetParent.scrollTop = targetEl[0].offsetTop;
+      var targetAnchor = decodeURI(hash.substr(1));
+      var targetEl = document.getElementById(targetAnchor)
+      if (targetEl) {
+        targetEl.offsetParent.scrollTop = targetEl.offsetTop;
       }
     }
   };
-  window.addEventListener("hashchange", scrollToEntryFromLocationHash, false);
   scrollToEntryFromLocationHash();
 });
