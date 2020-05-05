ebdb3fee876f263dbce857d330e29690f4b604a0
Ewan Slater <crush-157@users.noreply.github.com>
2020-05-05 20:53:24 +0100

    Update src/oauth2/auth_scheme.cr
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/oauth2/auth_scheme.cr
+++ src/oauth2/auth_scheme.cr
@@ -2,10 +2,10 @@
 #
 # According to https://tools.ietf.org/html/rfc6749#section-2.3.1:
 #
-# "Including the client credentials in the request-body using the
-#  two parameters is NOT RECOMMENDED and SHOULD be limited to
-#  clients unable to directly utilize the HTTP Basic authentication
-#   scheme (or other password-based HTTP authentication schemes)."
+# > "Including the client credentials in the request-body using the
+# > two parameters is NOT RECOMMENDED and SHOULD be limited to
+# > clients unable to directly utilize the HTTP Basic authentication
+# > scheme (or other password-based HTTP authentication schemes)."
 #
 # Therefore, HTTP Basic is preferred, and Request Body should only
 # be used if the server does not support HTTP Basic.
