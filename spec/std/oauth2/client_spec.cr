25b31ad94b81401dc4e40e12a7d654c6c381122c
Ewan Slater <ewan.slater@gmail.com>
2020-05-05 21:40:40 +0100

    replaced 'expected' with inlined values, as per Sija's suggestion

--- spec/std/oauth2/client_spec.cr
+++ spec/std/oauth2/client_spec.cr
@@ -48,7 +48,6 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("redirect_uri=&grant_type=authorization_code&code=SDFhw39fwfg23flSfpawbef")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
@@ -67,14 +66,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("grant_type=password&username=user123&password=monkey&scope=read_posts")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http"
 
           token = client.get_access_token_using_resource_owner_credentials(username: "user123", password: "monkey", scope: "read_posts")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("grant_type=password&username=user123&password=monkey&scope=read_posts")
           token.access_token.should eq "access_token"
         end
       end
@@ -86,14 +84,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("grant_type=client_credentials&scope=read_posts")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http"
 
           token = client.get_access_token_using_client_credentials(scope: "read_posts")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("grant_type=client_credentials&scope=read_posts")
           token.access_token.should eq "access_token"
         end
       end
@@ -105,14 +102,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("grant_type=refresh_token&refresh_token=some_refresh_token&scope=read_posts")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http"
 
           token = client.get_access_token_using_refresh_token(scope: "read_posts", refresh_token: "some_refresh_token")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("grant_type=refresh_token&refresh_token=some_refresh_token&scope=read_posts")
           token.access_token.should eq "access_token"
         end
       end
@@ -125,14 +121,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("client_id=client_id&client_secret=client_secret&redirect_uri=&grant_type=authorization_code&code=SDFhw39fwfg23flSfpawbef")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http", auth_scheme: OAuth2::AuthScheme::RequestBody
 
           token = client.get_access_token_using_authorization_code(authorization_code: "SDFhw39fwfg23flSfpawbef")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("client_id=client_id&client_secret=client_secret&redirect_uri=&grant_type=authorization_code&code=SDFhw39fwfg23flSfpawbef")
           token.access_token.should eq "access_token"
         end
       end
@@ -144,14 +139,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("client_id=client_id&client_secret=client_secret&grant_type=password&username=user123&password=monkey&scope=read_posts")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http", auth_scheme: OAuth2::AuthScheme::RequestBody
 
           token = client.get_access_token_using_resource_owner_credentials(username: "user123", password: "monkey", scope: "read_posts")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("client_id=client_id&client_secret=client_secret&grant_type=password&username=user123&password=monkey&scope=read_posts")
           token.access_token.should eq "access_token"
         end
       end
@@ -163,14 +157,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("client_id=client_id&client_secret=client_secret&grant_type=client_credentials&scope=read_posts")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http", auth_scheme: OAuth2::AuthScheme::RequestBody
 
           token = client.get_access_token_using_client_credentials(scope: "read_posts")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("client_id=client_id&client_secret=client_secret&grant_type=client_credentials&scope=read_posts")
           token.access_token.should eq "access_token"
         end
       end
@@ -182,14 +175,13 @@ describe OAuth2::Client do
           context.response.print response.to_json
         end
 
-        expected = %("client_id=client_id&client_secret=client_secret&grant_type=refresh_token&refresh_token=some_refresh_token&scope=read_posts")
         address = server.bind_unused_port "::1"
 
         run_server(server) do
           client = OAuth2::Client.new "[::1]", "client_id", "client_secret", port: address.port, scheme: "http", auth_scheme: OAuth2::AuthScheme::RequestBody
 
           token = client.get_access_token_using_refresh_token(scope: "read_posts", refresh_token: "some_refresh_token")
-          token.extra.not_nil!["body"].should eq expected
+          token.extra.not_nil!["body"].should eq %("client_id=client_id&client_secret=client_secret&grant_type=refresh_token&refresh_token=some_refresh_token&scope=read_posts")
           token.access_token.should eq "access_token"
         end
       end
