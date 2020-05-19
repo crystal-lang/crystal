a5515df581e9e6980eed9a3677b631a7d21f7303
Stephanie Wilde-Hobbs <steph@rx14.co.uk>
2020-05-19 12:14:43 +0100

    Fix args_test.cr being required as a spec

--- spec/compiler/compiler_spec.cr
+++ spec/compiler/compiler_spec.cr
@@ -30,7 +30,7 @@ describe "Compiler" do
 
   it "treats all arguments post-filename as program arguments" do
     with_tempfile "args_test" do |path|
-      `bin/crystal '#{compiler_datapath}/args_test.cr' -Dother_flag -- bar '#{path}'`
+      `bin/crystal '#{compiler_datapath}/args_test' -Dother_flag -- bar '#{path}'`
 
       File.read(path).should eq(<<-FILE)
         ["-Dother_flag", "--", "bar"]
