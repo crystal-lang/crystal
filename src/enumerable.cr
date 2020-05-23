c08ddfece45e3f477759db8b092681bda587f3d2
Michael Neumann <mneumann@ntecs.de>
2020-05-23 10:52:54 +0200

    Add back optimization of Enumerable#{zip,zip?} when self is Indexable

--- src/enumerable.cr
+++ src/enumerable.cr
@@ -1634,7 +1634,8 @@ module Enumerable(T)
   # a.zip(b, c) # => [{1, 4, 8}, {2, 5, 7}, {3, 6, 6}]
   # ```
   def zip(*others : Indexable | Iterable | Iterator)
-    pairs = Array(typeof(zip(*others) { |e| break e }.not_nil!)).new
+    size = self.is_a?(Indexable) ? self.size : 0
+    pairs = Array(typeof(zip(*others) { |e| break e }.not_nil!)).new(size)
     zip(*others) { |e| pairs << e }
     pairs
   end
@@ -1704,7 +1705,8 @@ module Enumerable(T)
   # a.zip?(b, c) # => [{1, 4, 8}, {2, 5, 7}, {3, nil, nil}]
   # ```
   def zip?(*others : Indexable | Iterable | Iterator)
-    pairs = Array(typeof(zip?(*others) { |e| break e }.not_nil!)).new
+    size = self.is_a?(Indexable) ? self.size : 0
+    pairs = Array(typeof(zip?(*others) { |e| break e }.not_nil!)).new(size)
     zip?(*others) { |e| pairs << e }
     pairs
   end
