cb61d109ab25bb1fe90ee8b831b0ee84141a14f4
Brian J. Cardiff <bcardiff@manas.tech>
2020-05-28 14:14:47 -0300

    Revert "Change IO#write, IO#skip, IO.copy to return Int64 (#9363)"
    This reverts commit 6380fa94f9798f440fe92213d7fb6870696063d9.

--- src/compress/deflate/writer.cr
+++ src/compress/deflate/writer.cr
@@ -43,16 +43,16 @@ class Compress::Deflate::Writer < IO
   end
 
   # See `IO#write`.
-  def write(slice : Bytes) : Int64
+  def write(slice : Bytes) : UInt64
     check_open
 
-    return 0i64 if slice.empty?
+    return 0u64 if slice.empty?
 
     @stream.avail_in = slice.size
     @stream.next_in = slice
     consume_output LibZ::Flush::NO_FLUSH
 
-    slice.size.to_i64
+    slice.size.to_u64
   end
 
   # See `IO#flush`.
