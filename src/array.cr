b0cc6258b9b71900ec8a26bd8a34d36e25d56f7d
Ary Borenszweig <asterite@gmail.com>
2020-06-01 14:01:50 -0300

    Array: fix delete_at bug with negative start index

--- src/array.cr
+++ src/array.cr
@@ -751,6 +751,8 @@ class Array(T)
   # a.delete_at(99, 1) # raises IndexError
   # ```
   def delete_at(index : Int, count : Int)
+    index += size if index < 0
+
     val = self[index, count]
     count = index + count <= size ? count : size - index
     (@buffer + index).move_from(@buffer + index + count, size - index - count)
