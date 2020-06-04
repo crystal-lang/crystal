759f6a35cafd3aaad5cc290ee085918ebf400058
Jonne Haß <me@jhass.eu>
2020-06-04 14:27:40 +0200

    Fix VaList and disable va_arg for AArch64
    va_arg is broken on most platforms, including AArch64

--- spec/support/tempfile.cr
+++ spec/support/tempfile.cr
@@ -43,6 +43,16 @@ def with_temp_executable(name, file = __FILE__)
   end
 end
 
+def with_temp_c_object_file(c_code, file = __FILE__)
+  with_tempfile("temp_c.c", "temp_c.o", file: file) do |c_filename, o_filename|
+    File.write(c_filename, c_code)
+
+    `#{ENV["CC"]? || "cc"} #{Process.quote(c_filename)} -c -o #{Process.quote(o_filename)}`.should be_truthy
+
+    yield o_filename
+  end
+end
+
 if SPEC_TEMPFILE_CLEANUP
   at_exit do
     FileUtils.rm_r(SPEC_TEMPFILE_PATH) if Dir.exists?(SPEC_TEMPFILE_PATH)
