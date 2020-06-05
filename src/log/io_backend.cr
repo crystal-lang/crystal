23bcb0096bdbabf589685538724daea5b06ba2e3
Brian J. Cardiff <bcardiff@gmail.com>
2020-06-05 19:17:50 -0300

    Make IOBackend formatter a named argument

--- src/log/io_backend.cr
+++ src/log/io_backend.cr
@@ -3,8 +3,9 @@ class Log::IOBackend < Log::Backend
   property io : IO
   property formatter : Formatter
 
-  def initialize(@io = STDOUT, @formatter : Formatter = ShortFormat)
+  def initialize(@io = STDOUT, *, formatter : Formatter = ShortFormat)
     @mutex = Mutex.new(:unchecked)
+    @formatter = formatter
   end
 
   def write(entry : Entry)
