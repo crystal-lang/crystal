6bef14045a66158e187ffa88ad43e96a9dcc1cf1
Jonne Haß <me@jhass.eu>
2020-06-06 14:55:04 +0200

    Make specs pass in non-IPv6 environments
    It's 2020 and sadly IPv6 is not yet truly ubiquitous. While supported by all
    relevant operating systems, enabled, even just locally, in all relevant execution
    environments, not so much. Prominently Docker does not enable it in its default
    configuration.

--- spec/std/socket/spec_helper.cr
+++ spec/std/socket/spec_helper.cr
@@ -1,9 +1,24 @@
 require "spec"
 require "socket"
 
-def unused_local_port
-  TCPServer.open("::", 0) do |server|
-    server.local_address.port
+module SocketSpecHelper
+  @supports_ipv6 : Bool? = nil
+
+  def self.supports_ipv6?
+    return @@supports_ipv6 unless @@supports_ipv6.nil?
+
+    TCPServer.open("::1", 0) { return @@supports_ipv6 = true }
+    @@supports_ipv6 = false
+  rescue Socket::BindError
+    @@supports_ipv6 = false
+  end
+end
+
+def pending_ipv6(description = "assert", file = __FILE__, line = __LINE__, end_line = __END_LINE__, &block)
+  if SocketSpecHelper.supports_ipv6?
+    it(description, file: file, line: line, end_line: end_line, &block)
+  else
+    pending(description, file: file, line: line, end_line: end_line)
   end
 end
 
@@ -12,7 +27,17 @@ def each_ip_family(&block : Socket::Family, String, String ->)
     block.call Socket::Family::INET, "127.0.0.1", "0.0.0.0"
   end
 
-  describe "using IPv6" do
-    block.call Socket::Family::INET6, "::1", "::"
+  if SocketSpecHelper.supports_ipv6?
+    describe "using IPv6" do
+      block.call Socket::Family::INET6, "::1", "::"
+    end
+  else
+    pending "using IPv6"
+  end
+end
+
+def unused_local_port
+  TCPServer.open("::", 0) do |server|
+    server.local_address.port
   end
 end
