4a88509bc903457ed2e49ec7915b5bcc15a756e4
George Dietrich <george@dietrich.app>
2020-06-16 19:45:34 -0400

    Ensure exceptions are logged within the same context as handler

--- src/http/server/request_processor.cr
+++ src/http/server/request_processor.cr
@@ -46,10 +46,8 @@ class HTTP::Server::RequestProcessor
         response.headers["Connection"] = "keep-alive" if request.keep_alive?
         context = Context.new(request, response)
 
-        begin
-          Log.with_context do
-            @handler.call(context)
-          end
+        Log.with_context do
+          @handler.call(context)
         rescue ex : ClientError
           Log.debug(exception: ex.cause) { ex.message }
         rescue ex
