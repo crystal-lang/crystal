a58b2d55605fae9a0f87eb23c62ad79b40fee657
Brian J. Cardiff <bcardiff@gmail.com>
2020-06-17 19:42:35 -0300

    Remove macro

--- spec/std/digest/algorithm.cr
+++ spec/std/digest/algorithm.cr
@@ -1,47 +1,47 @@
-macro acts_as_digest_algorithm(expr)
+def acts_as_digest_algorithm(type : T.class) forall T
   it "#hexdigest can update within a loop from explicit expr (#9483)" do
-    %i = 0
-    {{expr}}.hexdigest do |%digest|
-      while %i < 3
-        %digest.update("")
-        %i += 1
+    i = 0
+    type.hexdigest do |digest|
+      while i < 3
+        digest.update("")
+        i += 1
       end
     end
   end
 
   it "#hexdigest can update within a loop by indirect expr (#9483)" do
-    %algorithm = {} of String => Digest::Algorithm.class
-    %algorithm["me"] = {{expr}}
-    %i = 0
-    %algorithm["me"].hexdigest do |%digest|
-      while %i < 3
-        %digest.update("")
-        %i += 1
+    algorithm = {} of String => Digest::Algorithm.class
+    algorithm["me"] = type
+    i = 0
+    algorithm["me"].hexdigest do |digest|
+      while i < 3
+        digest.update("")
+        i += 1
       end
     end
   end
 
   it "context are independent" do
-    %algorithm = {{expr}}
-    %res = %algorithm.hexdigest do |%digest|
-      %digest.update("a")
-      %digest.update("b")
+    algorithm = type
+    res = algorithm.hexdigest do |digest|
+      digest.update("a")
+      digest.update("b")
     end
 
-    %inner_res = nil
+    inner_res = nil
 
-    %outer_res = %algorithm.hexdigest do |%outer|
-      %outer.update("a")
+    outer_res = algorithm.hexdigest do |outer|
+      outer.update("a")
 
-      %inner_res = %algorithm.hexdigest do |%inner|
-        %inner.update("a")
-        %inner.update("b")
+      inner_res = algorithm.hexdigest do |inner|
+        inner.update("a")
+        inner.update("b")
       end
 
-      %outer.update("b")
+      outer.update("b")
     end
 
-    %outer_res.should eq(%res)
-    %inner_res.should eq(%res)
+    outer_res.should eq(res)
+    inner_res.should eq(res)
   end
 end
