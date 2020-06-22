5f5001f29dbcdae3c8678da4dced112850a5d3b6
Oleh Prypin <oleh@pryp.in>
2020-06-22 23:53:58 +0200

    C ABI: Add a spec for accepting a large struct - failing on win32, aarch64

--- spec/compiler/codegen/c_abi/c_abi_spec.cr
+++ spec/compiler/codegen/c_abi/c_abi_spec.cr
@@ -199,4 +199,45 @@ describe "Code gen: C ABI" do
         (str.x + str.y + str.z).to_i32
       ), &.to_i.should eq(6))
   end
+
+  {% if flag?(:win32) || flag?(:aarch64) %}
+    pending "accepts large struct in a callback (for real) (#9533)"
+  {% else %}
+    it "accepts large struct in a callback (for real)" do
+      test_c(
+        %(
+          struct s {
+              long long x, y, z;
+          };
+
+          void ccaller(void (*func)(struct s)) {
+              struct s v = {1, 2, 3};
+              func(v);
+          }
+        ),
+        %(
+          lib LibFoo
+            struct S
+              x : Int64
+              y : Int64
+              z : Int64
+            end
+
+            fun ccaller(func : (S) ->)
+          end
+
+          module Global
+            class_property x = 0i64
+          end
+
+          fun callback(v : LibFoo::S)
+            Global.x = v.x &+ v.y &+ v.z
+          end
+
+          LibFoo.ccaller(->callback)
+
+          Global.x
+        ), &.to_string.should eq("6"))
+    end
+  {% end %}
 end
