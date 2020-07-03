780f7d35c23ed876e6c631a9693cde9902c0620a
Stephen von Takach <steve@acaprojects.com>
2020-07-03 11:38:49 +1000

    add support for selective JSON and YAML serialization

--- spec/std/yaml/serializable_spec.cr
+++ spec/std/yaml/serializable_spec.cr
@@ -67,6 +67,21 @@ class YAMLAttrPersonEmittingNull
   property age : Int32?
 end
 
+struct YAMLAttrPersonWithSelectiveSerialization
+  include YAML::Serializable
+
+  property name : String
+
+  @[YAML::Field(ignore_serialize: true)]
+  property password : String
+
+  @[YAML::Field(ignore_deserialize: true)]
+  property generated : String = "generated-internally"
+
+  def initialize(@name : String, @password : String)
+  end
+end
+
 @[YAML::Serializable::Options(emit_nulls: true)]
 class YAMLAttrPersonEmittingNullsByOptions
   include YAML::Serializable
@@ -402,6 +417,16 @@ describe "YAML::Serializable" do
         YAML
     end
     ex.location.should eq({3, 1})
+  end
+
+  it "works with selective serialization" do
+    person = YAMLAttrPersonWithSelectiveSerialization.new("Vasya", "P@ssw0rd")
+    person.to_json.should eq "{\"name\":\"Vasya\",\"generated\":\"generated-internally\"}"
+
+    person_json = "{\"name\":\"Vasya\",\"generated\":\"should not set\",\"password\":\"update\"}"
+    person = YAMLAttrPersonWithSelectiveSerialization.from_json(person_json)
+    person.generated.should eq "generated-internally"
+    person.password.should eq "update"
   end
 
   it "does to_yaml" do
--- src/yaml/serialization.cr
+++ src/yaml/serialization.cr
@@ -60,6 +60,8 @@ module YAML
   #
   # `YAML::Field` properties:
   # * **ignore**: if `true` skip this field in seriazation and deserialization (by default false)
+  # * **ignore_serialize**: if `true` skip this field in serialization (by default false)
+  # * **ignore_deserialize**: if `true` skip this field in deserialization (by default false)
   # * **key**: the value of the key in the yaml object (by default the name of the instance variable)
   # * **converter**: specify an alternate type for parsing and generation. The converter must define `from_yaml(YAML::ParseContext, YAML::Nodes::Node)` and `to_yaml(value, YAML::Nodes::Builder)` as class methods. Examples of converters are `Time::Format` and `Time::EpochConverter` for `Time`.
   # * **presence**: if `true`, a `@{{key}}_present` instance variable will be generated when the key was present (even if it has a `null` value), `false` by default
@@ -164,7 +166,7 @@ module YAML
         {% properties = {} of Nil => Nil %}
         {% for ivar in @type.instance_vars %}
           {% ann = ivar.annotation(::YAML::Field) %}
-          {% unless ann && ann[:ignore] %}
+          {% unless ann && (ann[:ignore] || ann[:ignore_deserialize]) %}
             {%
               properties[ivar.id] = {
                 type:        ivar.type,
@@ -270,7 +272,7 @@ module YAML
         {% properties = {} of Nil => Nil %}
         {% for ivar in @type.instance_vars %}
           {% ann = ivar.annotation(::YAML::Field) %}
-          {% unless ann && ann[:ignore] %}
+          {% unless ann && (ann[:ignore] || ann[:ignore_serialize]) %}
             {%
               properties[ivar.id] = {
                 type:      ivar.type,
