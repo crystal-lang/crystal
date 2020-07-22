4d81b9a97d570bb026a0e5cacfd6ed124acc1f75
Ary Borenszweig <asterite@gmail.com>
2020-07-22 14:32:16 -0300

    Remove wrong spec

--- spec/compiler/semantic/automatic_cast_spec.cr
+++ spec/compiler/semantic/automatic_cast_spec.cr
@@ -636,15 +636,6 @@ describe "Semantic: automatic cast" do
       "ambiguous call, implicit cast of 255 matches all of UInt64, Int64"
   end
 
-  it "casts integer variable to larger type for variable with type restriction (#9565)" do
-    assert_type(%(
-      a : Int64 = 0_i64
-      x = 1
-      a = x
-      a
-      )) { int64 }
-  end
-
   it "casts integer variable to float type (#9565)" do
     assert_type(%(
       def foo(x : Float64)
