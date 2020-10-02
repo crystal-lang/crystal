21ad5606077887833e6213d2e5fcb39e0188623f
Brian J. Cardiff <bcardiff@gmail.com>
2020-10-02 12:04:02 -0300

    Use dwarf if available on Exception::CallStack.print_frame
    Calling callee at the beginning of the program will load dwarf

--- src/exception/call_stack.cr
+++ src/exception/call_stack.cr
@@ -123,6 +123,19 @@ struct Exception::CallStack
   end
 
   private def self.print_frame(repeated_frame)
+    if @@dwarf_loaded &&
+       (name = decode_function_name(repeated_frame.ip.address))
+      file, line, column = Exception::CallStack.decode_line_number(repeated_frame.ip.address)
+      if file && file != "??"
+        if repeated_frame.count == 0
+          Crystal::System.print_error "[0x%lx] %s at %s:%ld:%i\n", repeated_frame.ip, name, file, line, column
+        else
+          Crystal::System.print_error "[0x%lx] %s at %s:%ld:%i (%ld times)\n", repeated_frame.ip, name, file, line, column, repeated_frame.count + 1
+        end
+        return
+      end
+    end
+
     frame = decode_frame(repeated_frame.ip)
     if frame
       offset, sname = frame
