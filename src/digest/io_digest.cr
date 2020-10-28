4adb7942fa2ffcf753bc453145ecc96beb7b4c16
Brian J. Cardiff <bcardiff@gmail.com>
2020-10-28 17:58:58 -0300

    Move OpenSSL::DigestIO to IO::Digest (in "digest" package)

---
+++ src/digest/io_digest.cr
@@ -0,0 +1,53 @@
+require "./base"
+require "openssl"
+
+# Wraps an IO by calculating a specified digest on read or write operations.
+#
+# ### Example
+#
+# ```
+# require "digest"
+#
+# underlying_io = IO::Memory.new("foo")
+# io = IO::Digest.new(underlying_io, "SHA256")
+# buffer = Bytes.new(256)
+# io.read(buffer)
+# io.final.hexstring # => "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+# ```
+class IO::Digest < IO
+  getter io : IO
+  getter digest_algorithm : ::Digest::Base
+  getter mode : DigestMode
+
+  delegate close, closed?, flush, peek, tty?, rewind, to: @io
+  delegate final, base64digest, to: @digest_algorithm
+
+  enum DigestMode
+    Read
+    Write
+  end
+
+  def initialize(@io : IO, @digest_algorithm : ::Digest::Base, @mode = DigestMode::Read)
+  end
+
+  def initialize(@io : IO, algorithm : String, @mode = DigestMode::Read)
+    @digest_algorithm = OpenSSL::Digest.new(algorithm)
+  end
+
+  def read(slice : Bytes)
+    read_bytes = io.read(slice)
+    if @mode.read?
+      digest_algorithm.update(slice[0, read_bytes])
+    end
+    read_bytes
+  end
+
+  def write(slice : Bytes) : Nil
+    return if slice.empty?
+
+    if @mode.write?
+      digest_algorithm.update(slice)
+    end
+    io.write(slice)
+  end
+end
