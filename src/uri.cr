3d6bb4ee8b8f675c85dcc357dfdd2fbe1711630f
George Dietrich <george@dietrich.app>
2020-11-28 10:48:37 -0500

    Update docs

--- src/uri.cr
+++ src/uri.cr
@@ -192,7 +192,7 @@ class URI
   # require "uri"
   #
   # uri = URI.parse "http://foo.com/posts?id=30&limit=5#time=1305298413"
-  # uri.full_path # => "/posts?id=30&limit=5"
+  # uri.full_path # => "/posts?id=30&limit=5#time=1305298413"
   # ```
   def full_path : String
     String.build do |str|
