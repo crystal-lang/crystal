b744e8621d9c08b474ea87e66256cda7bd748513
Johannes Müller <straightshoota@gmail.com>
2020-12-03 00:37:02 +0100

    Fix specs

--- spec/compiler/normalize/array_literal_spec.cr
+++ spec/compiler/normalize/array_literal_spec.cr
@@ -7,7 +7,7 @@ describe "Normalize: array literal" do
 
   it "normalizes non-empty with of" do
     assert_expand "[1, 2] of Int8", <<-CR
-      __temp_1 = ::Array(Int8).new(2, __temp_2 = uninitialized Int8)
+      __temp_1 = ::Array(Int8).unsafe_build(2)
       __temp_1.to_unsafe[0] = 1
       __temp_1.to_unsafe[1] = 2
       __temp_1
@@ -16,7 +16,7 @@ describe "Normalize: array literal" do
 
   it "normalizes non-empty without of" do
     assert_expand "[1, 2]", <<-CR
-      __temp_1 = ::Array(typeof(1, 2)).new(2, __temp_2 = uninitialized typeof(1, 2))
+      __temp_1 = ::Array(typeof(1, 2)).unsafe_build(2)
       __temp_1.to_unsafe[0] = 1
       __temp_1.to_unsafe[1] = 2
       __temp_1
