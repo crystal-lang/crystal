79132c4c229a974f042a53d9d77a314a2a5ee8db
Johannes Müller <straightshoota@gmail.com>
2021-01-06 03:05:36 +0100

    Fix Range::StepIterator

--- spec/support/iterate.cr
+++ spec/support/iterate.cr
@@ -1,11 +1,21 @@
 # spec helper for generic iteration methods which tests both yielding and
 # iterator overloads.
-macro it_iterates(description, expected, method)
+macro it_iterates(description, expected, method, *, infinite = false)
   describe {{ description }} do
     it "yielding" do
+      remaining = ({{ expected }}).size
       ary = [] of typeof(({{ expected }})[0])
       {{ method.id }} do |x|
+        if remaining == 0
+          if {{ infinite }}
+            break
+          else
+            fail "Reached iteration limit #{({{ expected }}).size} receiving value #{x.inspect}"
+          end
+        end
+
         ary << x
+        remaining -= 1
       end
 
       ary.should eq {{ expected }}
@@ -24,7 +34,9 @@ macro it_iterates(description, expected, method)
         break if v.is_a?(Iterator::Stop)
         ary << v
       end
-      iter.next.should be_a(Iterator::Stop)
+      unless {{ infinite }}
+        iter.next.should be_a(Iterator::Stop)
+      end
 
       ary.should eq {{ expected }}
       ary.zip({{ expected }}).each_with_index do |(a, b), i|
