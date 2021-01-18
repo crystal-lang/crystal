d83d3e7cf432434a04d71e1b4aaf90972061dd8d
David Keller <blobcodes@protonmail.com>
2021-01-18 14:56:17 +0100

    Add support for --mcpu native (#10264)
    * Add support for --mcpu native
    
    * Run crystal tool format
    
    * Add documentation for --mcpu default
    
    * Actually add doumentation for --mcpu native

--- src/llvm.cr
+++ src/llvm.cr
@@ -83,6 +83,10 @@ module LLVM
     end
   end
 
+  def self.host_cpu_name : String
+    String.new LibLLVM.get_host_cpu_name
+  end
+
   def self.normalize_triple(triple : String)
     normalized = LibLLVMExt.normalize_target_triple(triple)
     normalized = LLVM.string_and_dispose(normalized)
