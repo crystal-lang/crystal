cd23add8541817f35d46e2230c5dc66909c3d0be
Sijawusz Pur Rahnama <sija@sija.pl>
2021-01-18 14:52:12 +0100

    Add specs for edge case with both nil values

--- spec/std/hash_spec.cr
+++ spec/std/hash_spec.cr
@@ -113,6 +113,10 @@ describe "Hash" do
         h1.proper_subset_of?(h3).should be_false
         h1.proper_subset_of?(h4).should be_false
       end
+
+      it "handles edge case where both values are nil" do
+        {"a" => nil}.proper_subset_of?({"b" => nil, "c" => nil}).should be_false
+      end
     end
 
     describe "#subset_of?" do
@@ -122,6 +126,10 @@ describe "Hash" do
         h1.subset_of?(h1).should be_true
         h1.subset_of?(h3).should be_false
         h1.subset_of?(h4).should be_false
+      end
+
+      it "handles edge case where both values are nil" do
+        {"a" => nil}.subset_of?({"b" => nil}).should be_false
       end
     end
 
