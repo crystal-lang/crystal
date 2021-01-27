5a41eaaa9c6e97aea5b566e221f855856eda00d0
Ary Borenszweig <asterite@gmail.com>
2021-01-27 15:38:54 -0300

    Pass `random` to `shuffle!`

--- src/range.cr
+++ src/range.cr
@@ -422,7 +422,7 @@ struct Range(B, E)
       if possible == available
         result = Array(B).new(possible)
         each { |value| result << value }
-        result.shuffle!
+        result.shuffle!(random)
         return result
       end
 
