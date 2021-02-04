3eb578a4c7e836175fb43240606eb9636ee2a419
Max Fierke <max@maxfierke.com>
2021-02-03 21:49:44 -0600

    aarch64-darwin: make VaList a Char* as per Apple documentation
    https://developer.apple.com/documentation/xcode/writing_arm64_code_for_apple_platforms

--- src/lib_c/aarch64-darwin/c/stdarg.cr
+++ src/lib_c/aarch64-darwin/c/stdarg.cr
@@ -1,10 +1,3 @@
 lib LibC
-  struct VaListTag
-    gp_offset : UInt
-    fp_offset : UInt
-    overflow_arg_area : Void*
-    reg_save_area : Void*
-  end
-
-  type VaList = VaListTag[1]
+  alias VaList = Char*
 end
