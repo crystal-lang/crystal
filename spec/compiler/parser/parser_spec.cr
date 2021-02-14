b4dbebd56a417e3eea865ec1b9757f1a6281a159
Quinton Miller <nicetas.c@gmail.com>
2021-02-14 21:29:26 +0800

    Always attach location to when/in clauses (#10382)

--- spec/compiler/parser/parser_spec.cr
+++ spec/compiler/parser/parser_spec.cr
@@ -1170,7 +1170,8 @@ module Crystal
     assert_syntax_error "case 1\nin Int32; 2; when 2", "expected 'in', not 'when'"
     assert_syntax_error "case 1\nwhen Int32; 2; in 2", "expected 'when', not 'in'"
     assert_syntax_error "case 1\nin Int32; 2; else", "exhaustive case (case ... in) doesn't allow an 'else'"
-    assert_syntax_error "case 1\nin 1; 2", "expression of exhaustive case (case ... in) must be a constant (like `IO::Memory`), a generic (like `Array(Int32)`) a bool literal (true or false), a nil literal (nil) or a question method (like `.red?`)"
+    assert_syntax_error "case 1\nin 1; 2", "expression of exhaustive case (case ... in) must be a constant (like `IO::Memory`), a generic (like `Array(Int32)`), a bool literal (true or false), a nil literal (nil) or a question method (like `.red?`)"
+    assert_syntax_error "case 1\nin .nil?; 2", "expression of exhaustive case (case ... in) must be a constant (like `IO::Memory`), a generic (like `Array(Int32)`), a bool literal (true or false), a nil literal (nil) or a question method (like `.red?`)"
     assert_syntax_error "case 1\nin _;", "'when _' is not supported"
 
     it_parses "case 1; when 2 then /foo/; end", Case.new(1.int32, [When.new([2.int32] of ASTNode, RegexLiteral.new("foo".string))], else: nil, exhaustive: false)
