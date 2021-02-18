61af95ba8b678cd017c00c0f1b7dff80318d2eaa
Quinton Miller <nicetas.c@gmail.com>
2021-02-18 16:55:45 +0800

    Support literal expansion of LHS multi-assign splats

--- spec/compiler/normalize/multi_assign_spec.cr
+++ spec/compiler/normalize/multi_assign_spec.cr
@@ -2,26 +2,93 @@ require "../../spec_helper"
 
 describe "Normalize: multi assign" do
   it "normalizes n to n" do
-    assert_expand "a, b, c = 1, 2, 3", "__temp_1 = 1\n__temp_2 = 2\n__temp_3 = 3\na = __temp_1\nb = __temp_2\nc = __temp_3"
+    assert_expand "a, b, c = 1, 2, 3", <<-CR
+      __temp_1 = 1
+      __temp_2 = 2
+      __temp_3 = 3
+      a = __temp_1
+      b = __temp_2
+      c = __temp_3
+      CR
   end
 
   it "normalizes 1 to n" do
-    assert_expand_second "d = 1\na, b, c = d", "__temp_1 = d\na = __temp_1[0]\nb = __temp_1[1]\nc = __temp_1[2]"
+    assert_expand_second "d = 1; a, b, c = d", <<-CR
+      __temp_1 = d
+      a = __temp_1[0]
+      b = __temp_1[1]
+      c = __temp_1[2]
+      CR
   end
 
   it "normalizes n to n with []" do
-    assert_expand_third "a = 1; b = 2; a[0], b[1] = 2, 3", "__temp_1 = 2\n__temp_2 = 3\na[0] = __temp_1\nb[1] = __temp_2"
+    assert_expand_third "a = 1; b = 2; a[0], b[1] = 2, 3", <<-CR
+      __temp_1 = 2
+      __temp_2 = 3
+      a[0] = __temp_1
+      b[1] = __temp_2
+      CR
   end
 
   it "normalizes 1 to n with []" do
-    assert_expand_third "a = 1; b = 2; a[0], b[1] = 2", "__temp_1 = 2\na[0] = __temp_1[0]\nb[1] = __temp_1[1]"
+    assert_expand_third "a = 1; b = 2; a[0], b[1] = 2", <<-CR
+      __temp_1 = 2
+      a[0] = __temp_1[0]
+      b[1] = __temp_1[1]
+      CR
   end
 
   it "normalizes n to n with call" do
-    assert_expand_third "a = 1; b = 2; a.foo, b.bar = 2, 3", "__temp_1 = 2\n__temp_2 = 3\na.foo = __temp_1\nb.bar = __temp_2"
+    assert_expand_third "a = 1; b = 2; a.foo, b.bar = 2, 3", <<-CR
+      __temp_1 = 2
+      __temp_2 = 3
+      a.foo = __temp_1
+      b.bar = __temp_2
+      CR
   end
 
   it "normalizes 1 to n with call" do
-    assert_expand_third "a = 1; b = 2; a.foo, b.bar = 2", "__temp_1 = 2\na.foo = __temp_1[0]\nb.bar = __temp_1[1]"
+    assert_expand_third "a = 1; b = 2; a.foo, b.bar = 2", <<-CR
+      __temp_1 = 2
+      a.foo = __temp_1[0]
+      b.bar = __temp_1[1]
+      CR
+  end
+
+  it "normalizes m to n, with splat on left-hand side, splat is empty" do
+    assert_expand_third "a = 1; b = 2; *a[0], b.foo, c = 3, 4", <<-CR
+      __temp_1 = ::Tuple.new
+      __temp_2 = 3
+      __temp_3 = 4
+      a[0] = __temp_1
+      b.foo = __temp_2
+      c = __temp_3
+      CR
+  end
+
+  it "normalizes m to n, with splat on left-hand side, splat is non-empty" do
+    assert_expand_third "a = 1; b = 2; a[0], *b.foo, c = 3, 4, 5, 6, 7", <<-CR
+      __temp_1 = 3
+      __temp_2 = ::Tuple.new(4, 5, 6)
+      __temp_3 = 7
+      a[0] = __temp_1
+      b.foo = __temp_2
+      c = __temp_3
+      CR
+  end
+
+  it "normalizes 1 to n, with splat on left-hand side" do
+    assert_expand_third "c = 1; d = 2; a, b, *c.foo, d[0], e, f = 3", <<-CR
+      __temp_1 = 3
+      if __temp_1.size < 5
+        ::raise("BUG: multiple assignment count mismatch")
+      end
+      a = __temp_1[0]
+      b = __temp_1[1]
+      c.foo = __temp_1[2..-4]
+      d[0] = __temp_1[-3]
+      e = __temp_1[-2]
+      f = __temp_1[-1]
+      CR
   end
 end
--- src/compiler/crystal/semantic/literal_expander.cr
+++ src/compiler/crystal/semantic/literal_expander.cr
@@ -545,6 +545,8 @@ module Crystal
 
     # Transform a multi assign into many assigns.
     def expand(node : MultiAssign)
+      lhs_has_splat = node.targets.any?(Splat)
+
       # From:
       #
       #     a, b = [1, 2]
@@ -555,17 +557,56 @@ module Crystal
       #     temp = [1, 2]
       #     a = temp[0]
       #     b = temp[1]
+      #
+      # From:
+      #
+      #     a, *b, c, d = [1, 2]
+      #
+      # To:
+      #
+      #     temp = [1, 2]
+      #     raise ... if temp.size < 3
+      #     a = temp[0]
+      #     b = temp[1..-3]
+      #     c = temp[-2]
+      #     d = temp[-1]
       if node.values.size == 1
         value = node.values[0]
 
         temp_var = new_temp_var
 
-        assigns = Array(ASTNode).new(node.targets.size + 1)
+        # temp = ...
+        assigns = Array(ASTNode).new(node.targets.size + (lhs_has_splat ? 2 : 1))
         assigns << Assign.new(temp_var.clone, value).at(value)
+
+        # raise ... if temp.size < ...
+        if lhs_has_splat
+          size_call = Call.new(temp_var.clone, "size").at(value)
+          size_comp = Call.new(size_call, "<", NumberLiteral.new(node.targets.size - 1)).at(value)
+          raise_call = Call.new(nil, "raise", args: [StringLiteral.new("BUG: multiple assignment count mismatch")] of ASTNode, global: true).at(value)
+          assigns << If.new(size_comp, raise_call).at(value)
+        end
+
+        # ... = temp[...]
+        found_splat = false
         node.targets.each_with_index do |target, i|
-          call = Call.new(temp_var.clone, "[]", NumberLiteral.new(i)).at(value)
+          if target.is_a?(Splat)
+            raise "BUG: splat assignment already specified" if found_splat
+            found_splat = true
+            indexer = RangeLiteral.new(
+              NumberLiteral.new(i),
+              NumberLiteral.new(i - node.targets.size),
+              false,
+            ).at(value)
+          elsif found_splat
+            indexer = NumberLiteral.new(i - node.targets.size)
+          else
+            indexer = NumberLiteral.new(i)
+          end
+          call = Call.new(temp_var.clone, "[]", indexer).at(value)
           assigns << transform_multi_assign_target(target, call)
         end
+
         exps = Expressions.new(assigns)
 
         # From:
@@ -578,32 +619,56 @@ module Crystal
         #     temp2 = d
         #     a = temp1
         #     b = temp2
+        #
+        # From:
+        #
+        #     a, *b, c = d, e, f, g
+        #
+        # To:
+        #
+        #     temp1 = d
+        #     temp2 = ::Tuple.new(e, f)
+        #     temp3 = g
+        #     a = temp1
+        #     b = temp2
+        #     c = temp3
       else
-        raise "BUG: multiple assignment count mismatch" unless node.targets.size == node.values.size
-
-        temp_vars = node.values.map { new_temp_var }
-
-        assign_to_temps = [] of ASTNode
-        assign_from_temps = [] of ASTNode
-
-        temp_vars.each_with_index do |temp_var_2, i|
-          target = node.targets[i]
-          value = node.values[i]
-          if target.is_a?(Path)
-            assign_from_temps << Assign.new(target, value).at(node)
-          else
-            assign_to_temps << Assign.new(temp_var_2.clone, value).at(node)
-            assign_from_temps << transform_multi_assign_target(target, temp_var_2.clone)
-          end
+        if lhs_has_splat
+          raise "BUG: multiple assignment count mismatch" if node.targets.size - 1 > node.values.size
+        else
+          raise "BUG: multiple assignment count mismatch" if node.targets.size != node.values.size
         end
 
-        exps = Expressions.new(assign_to_temps + assign_from_temps)
+        temp_vars = node.targets.map { new_temp_var }
+
+        assign_to_temps = Array(ASTNode).new(node.targets.size * 2)
+        assign_from_temps = Array(ASTNode).new(node.targets.size)
+
+        found_splat = false
+        temp_vars.each_with_index do |temp_var_2, i|
+          target = node.targets[i]
+          if target.is_a?(Splat)
+            raise "BUG: splat assignment already specified" if found_splat
+            found_splat = true
+            value = Call.new(Path.global("Tuple").at(node), "new", node.values[i..i - node.targets.size])
+          else
+            value = node.values[found_splat ? i - node.targets.size : i]
+          end
+          assign_to_temps << Assign.new(temp_var_2.clone, value).at(node)
+          assign_from_temps << transform_multi_assign_target(target, temp_var_2.clone)
+        end
+
+        exps = Expressions.new(assign_to_temps.concat(assign_from_temps))
       end
       exps.location = node.location
       exps
     end
 
     def transform_multi_assign_target(target, value)
+      if target.is_a?(Splat)
+        target = target.exp
+      end
+
       if target.is_a?(Call)
         target.name = "#{target.name}="
         target.args << value
