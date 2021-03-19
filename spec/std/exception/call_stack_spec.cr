77e2f4902cc1177f1b091ae737cf31fc23ed5014
Johannes Müller <straightshoota@gmail.com>
2021-03-20 00:20:17 +0100

    Fix Process::INITIAL_PWD for non-existent path

--- spec/std/exception/call_stack_spec.cr
+++ spec/std/exception/call_stack_spec.cr
@@ -57,4 +57,20 @@ describe "Backtrace" do
     output.to_s.empty?.should be_true
     error.to_s.should contain("Invalid memory access")
   end
+
+  it "print exception with non-existing PWD" do
+    source_file = datapath("blank_test_file.txt")
+    compile_file(source_file) do |executable_file|
+      output, error = IO::Memory.new, IO::Memory.new
+      with_tempfile("non-existent") do |path|
+        Dir.mkdir path
+        Dir.cd(path) do
+          Dir.delete(path)
+          status = Process.run executable_file
+
+          status.success?.should be_true
+        end
+      end
+    end
+  end
 end
