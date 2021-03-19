7c52f0d72d5ad709558c32d933affe7126fb4aaa
Johannes Müller <straightshoota@gmail.com>
2021-03-20 00:37:33 +0100

    Disable spec on win32

--- spec/std/exception/call_stack_spec.cr
+++ spec/std/exception/call_stack_spec.cr
@@ -58,13 +58,14 @@ describe "Backtrace" do
     error.to_s.should contain("Invalid memory access")
   end
 
-  it "print exception with non-existing PWD" do
+  pending_win32 "print exception with non-existing PWD" do
     source_file = datapath("blank_test_file.txt")
     compile_file(source_file) do |executable_file|
       output, error = IO::Memory.new, IO::Memory.new
       with_tempfile("non-existent") do |path|
         Dir.mkdir path
         Dir.cd(path) do
+          # on win32 it seems not possible to remove the directory while we're cd'ed into it
           Dir.delete(path)
           status = Process.run executable_file
 
