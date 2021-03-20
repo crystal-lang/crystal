dceb7715a1dd85e69ca6aca5e2a2bbfc8e3c63f0
Johannes Müller <straightshoota@gmail.com>
2021-03-20 13:28:52 +0100

    Avoid `/` as CURRENT_DIR when INITIAL_PWD does not exist

--- src/exception/call_stack.cr
+++ src/exception/call_stack.cr
@@ -22,9 +22,12 @@ struct Exception::CallStack
   # Compute current directory at the beginning so filenames
   # are always shown relative to the *starting* working directory.
   CURRENT_DIR = begin
-    dir = Process::INITIAL_PWD || ""
-    dir += File::SEPARATOR unless dir.ends_with?(File::SEPARATOR)
-    dir
+    if dir = Process::INITIAL_PWD
+      dir += File::SEPARATOR unless dir.ends_with?(File::SEPARATOR)
+      dir
+    else
+      ""
+    end
   end
 
   @@skip = [] of String
