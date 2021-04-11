22957255bfe3aba549f2e5770671bae1edc5cccc
Oleh Prypin <oleh@pryp.in>
2021-04-11 16:35:00 +0200

    Add specs, most of which are currently failing

--- spec/compiler/normalize/def_spec.cr
+++ spec/compiler/normalize/def_spec.cr
@@ -231,5 +231,25 @@ module Crystal
       other_def = a_def.expand_default_arguments(Program.new, 0, ["abstract"])
       other_def.to_s.should eq("def foo:abstract(abstract __arg0)\n  options = {}\n  @abstract = __arg0\nend")
     end
+
+    describe "gives correct body location with" do
+      {"default arg"                  => "def testing(foo = 5)",
+       "default arg with restriction" => "def testing(foo : Int = 5)",
+       "splat arg"                    => "def testing(*foo : Array)",
+       "block instance var arg"       => "def testing(&@foo : ->)",
+      }.each do |(suf, definition)|
+        describe suf + "," do
+          {"with body" => "zzz = 7\n", "without body" => ""}.each do |(suf, body)|
+            it suf do
+              a_def = parse("#{definition}\n#{body}end").as(Def)
+              actual = a_def.expand_default_arguments(Program.new, 1)
+
+              actual.location.should eq Location.new("", line_number: 1, column_number: 1)
+              actual.body.location.should eq Location.new("", line_number: 2, column_number: 1)
+            end
+          end
+        end
+      end
+    end
   end
 end
