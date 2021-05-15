8a7f3b01be535cb6217916eaf7f6c62730ec4f8a
Holden Omans <holden.omans@gmail.com>
2021-05-15 10:08:39 -0400

    Add UUID to yaml parsing

--- src/yaml/from_yaml.cr
+++ src/yaml/from_yaml.cr
@@ -301,6 +301,20 @@ def Union.new(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
   node.raise "Couldn't parse #{self}"
 end
 
+def UUID.new(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
+  ctx.read_alias(node, String) do |obj|
+    return UUID.new(obj)
+  end
+
+  if node.is_a?(YAML::Nodes::Scalar)
+    value = node.value
+    ctx.record_anchor(node, value)
+    UUID.new(value)
+  else
+    node.raise "Expected String, not #{node.kind}"
+  end
+end
+
 def Time.new(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
   parse_scalar(ctx, node, Time)
 end
