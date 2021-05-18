01b5e3122048842739a528a899ed06b4aedb94cf
Holden Omans <holden.omans@gmail.com>
2021-05-18 09:17:50 -0400

    moved uuid yaml new method

--- src/uuid/yaml.cr
+++ src/uuid/yaml.cr
@@ -1,39 +1,39 @@
 require "yaml"
 require "uuid"
 
-# Creates UUID from YAML using `YAML::ParseContext`.
-#
-# NOTE: `require "uuid/yaml"` is required to opt-in to this feature.
-#
-# ```
-# require "yaml"
-# require "uuid"
-# require "uuid/yaml"
-#
-# class Example
-#   include YAML::Serializable
-#
-#   property id : UUID
-# end
-#
-# example = Example.from_yaml("uuid: 50a11da6-377b-4bdf-b9f0-076f9db61c93")
-# example.id # => UUID(50a11da6-377b-4bdf-b9f0-076f9db61c93)
-# ```
-def UUID.new(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
-  ctx.read_alias(node, String) do |obj|
-    return UUID.new(obj)
-  end
-
-  if node.is_a?(YAML::Nodes::Scalar)
-    value = node.value
-    ctx.record_anchor(node, value)
-    UUID.new(value)
-  else
-    node.raise "Expected String, not #{node.kind}"
-  end
-end
-
 struct UUID
+  # Creates UUID from YAML using `YAML::ParseContext`.
+  #
+  # NOTE: `require "uuid/yaml"` is required to opt-in to this feature.
+  #
+  # ```
+  # require "yaml"
+  # require "uuid"
+  # require "uuid/yaml"
+  #
+  # class Example
+  #   include YAML::Serializable
+  #
+  #   property id : UUID
+  # end
+  #
+  # example = Example.from_yaml("uuid: 50a11da6-377b-4bdf-b9f0-076f9db61c93")
+  # example.id # => UUID(50a11da6-377b-4bdf-b9f0-076f9db61c93)
+  # ```
+  def self.new(ctx : YAML::ParseContext, node : YAML::Nodes::Node)
+    ctx.read_alias(node, String) do |obj|
+      return UUID.new(obj)
+    end
+
+    if node.is_a?(YAML::Nodes::Scalar)
+      value = node.value
+      ctx.record_anchor(node, value)
+      UUID.new(value)
+    else
+      node.raise "Expected String, not #{node.kind}"
+    end
+  end
+
   # Returns UUID as YAML value.
   #
   # NOTE: `require "uuid/yaml"` is required to opt-in to this feature.
