ad97abca1b97e3dc254211a36a33e269f604c8ee
Ali Naqvi <syed.alinaqvi@gmail.com>
2021-05-19 02:35:08 +0800

    Updated API to be consistent with String API

--- src/string/grapheme.cr
+++ src/string/grapheme.cr
@@ -1,10 +1,32 @@
 require "./grapheme/grapheme"
 
 class String
-  # returns graphemes cluster iterator over Unicode extended grapheme clusters, specified in the Unicode Standard Annex #29. Grapheme clusters correspond to
+  # returns an array of all Unicode extended grapheme clusters, specified in the Unicode Standard Annex #29. Grapheme clusters correspond to
   # "user-perceived characters". These characters often consist of multiple code points (e.g. the "woman kissing woman" emoji consists of 8 code points:
   # woman + ZWJ + heavy black heart (2 code points) + ZWJ + kiss mark + ZWJ + woman) and the rules described in Annex #29 must be applied to group those
   # code points into clusters perceived by the user as one character.
+  # ```
+  # "🧙‍♂️💈".graphemes # => [String::Grapheme::Cluster(@codepoints=[129497, 8205, 9794, 65039], @positions={0, 13}), String::Grapheme::Cluster(@codepoints=[128136], @positions={13, 17})]
+  # ```
+  def graphemes
+    Grapheme::Graphemes.new(self).to_a
+  end
+
+  # Yields each Unicode extended grapheme cluster in the string to the block.
+  #
+  # ```
+  # "🧙‍♂️💈".each_grapheme do |cluster|
+  #   p! cluster.codepoints
+  #   p! cluster.to_s
+  # end
+  # ```
+  def each_grapheme : Nil
+    Grapheme::Graphemes.new(self).each do |cluster|
+      yield cluster
+    end
+  end
+
+  # returns graphemes cluster iterator over Unicode extended grapheme clusters.
   # ```
   # "🔮👍🏼!".each do |cluster|
   #   pp cluster.codepoints
@@ -13,7 +35,7 @@ class String
   #   pp cluster.bytes
   # end
   # ```
-  def graphemes
+  def each_grapheme
     Grapheme::Graphemes.new(self)
   end
 end
