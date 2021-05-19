309745aceaab38711249338b99ea05fa1e42fb23
Quinton Miller <nicetas.c@gmail.com>
2021-05-19 20:44:01 +0800

    fixup

--- spec/compiler/crystal/tools/doc/type_spec.cr
+++ spec/compiler/crystal/tools/doc/type_spec.cr
@@ -99,12 +99,13 @@ describe Doc::Type do
     end
   end
 
-  it "ASTNode has no superclass" do
+  it "ASTNode has no superclass or ancestors" do
     program = semantic(<<-CODE).program
       module Crystal
         module Macros
           class ASTNode
           end
+
           class Arg < ASTNode
           end
         end
@@ -115,27 +116,10 @@ describe Doc::Type do
     macros_module = program.types["Crystal"].types["Macros"]
     astnode = generator.type(macros_module.types["ASTNode"])
     astnode.superclass.should eq(nil)
-    # Sanity check: subclasses of ASTNode has the right superclass
-    generator.type(macros_module.types["Arg"]).superclass.should eq(astnode)
-  end
-
-  it "ASTNode has no ancestors" do
-    program = semantic(<<-CODE).program
-      module Crystal
-        module Macros
-          class ASTNode
-          end
-          class Arg < ASTNode
-          end
-        end
-      end
-      CODE
-
-    generator = Doc::Generator.new program, [""]
-    macros_module = program.types["Crystal"].types["Macros"]
-    astnode = generator.type(macros_module.types["ASTNode"])
     astnode.ancestors.empty?.should be_true
+
     # Sanity check: subclasses of ASTNode has the right ancestors
+    generator.type(macros_module.types["Arg"]).superclass.should eq(astnode)
     generator.type(macros_module.types["Arg"]).ancestors.should eq([astnode])
   end
 end
