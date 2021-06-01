60b46174a24d02f71a5984c176024f15682abc86
Johannes Müller <straightshoota@gmail.com>
2021-06-01 23:54:58 +0200

    Reorganize and enhance ws2def

--- src/lib_c/x86_64-windows-msvc/c/ws2def.cr
+++ src/lib_c/x86_64-windows-msvc/c/ws2def.cr
@@ -43,6 +43,11 @@ lib LibC
   SOCK_RDM       = 4
   SOCK_SEQPACKET = 5
 
+  struct Sockaddr
+    sa_family : UInt8
+    sa_data : Char[14]
+  end
+
   IPPROTO_IP     =   0
   IPPROTO_ICMP   =   1
   IPPROTO_IGMP   =   2
@@ -51,23 +56,25 @@ lib LibC
   IPPROTO_ICMPV6 =  58
   IPPROTO_RAW    = 255
 
-  AI_PASSIVE                =     0x0001
-  AI_CANONNAME              =     0x0002
-  AI_NUMERICHOST            =     0x0004
-  AI_ALL                    =     0x0100
-  AI_ADDRCONFIG             =     0x0400
-  AI_V4MAPPED               =     0x0800
-  AI_NON_AUTHORITATIVE      =    0x04000
-  AI_SECURE                 =    0x08000
-  AI_RETURN_PREFERRED_NAMES =   0x010000
-  AI_FQDN                   = 0x00020000
-  AI_FILESERVER             = 0x00040000
-  AI_NUMERICSERV            = 0x00000008
+  AI_PASSIVE     = 0x00000001 # Socket address will be used in bind() call
+  AI_CANONNAME   = 0x00000002 # Return canonical name in first ai_canonname
+  AI_NUMERICHOST = 0x00000004 # Nodename must be a numeric address string
+  AI_NUMERICSERV = 0x00000008 # Servicename must be a numeric port number
+  AI_DNS_ONLY    = 0x00000010 # Restrict queries to unicast DNS only (no LLMNR, netbios, etc.)
 
-  struct Sockaddr
-    sa_family : UInt8
-    sa_data : Char[14]
-  end
+  AI_ALL        = 0x00000100 # Query both IP6 and IP4 with AI_V4MAPPED
+  AI_ADDRCONFIG = 0x00000400 # Resolution only if global address configured
+  AI_V4MAPPED   = 0x00000800 # On v6 failure, query v4 and convert to V4MAPPED format
+
+  AI_NON_AUTHORITATIVE      = 0x00004000 # LUP_NON_AUTHORITATIVE
+  AI_SECURE                 = 0x00008000 # LUP_SECURE
+  AI_RETURN_PREFERRED_NAMES = 0x00010000 # LUP_RETURN_PREFERRED_NAMES
+
+  AI_FQDN                 = 0x00020000 # Return the FQDN in ai_canonname
+  AI_FILESERVER           = 0x00040000 # Resolving fileserver name resolution
+  AI_DISABLE_IDN_ENCODING = 0x00080000 # Disable Internationalized Domain Names handling
+  AI_EXTENDED             = 0x80000000 # Indicates this is extended ADDRINFOEX(2/..) struct
+  AI_RESOLUTION_HANDLE    = 0x40000000 # Request resolution handle
 
   struct Addrinfo
     ai_flags : Int
