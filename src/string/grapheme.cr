eab81808bfb535df5ce6f46a9e959392e587590f
Ali Naqvi <syed.alinaqvi@gmail.com>
2021-06-03 23:25:22 +0800

    Made misc style and comments changes

--- src/string/grapheme.cr
+++ src/string/grapheme.cr
@@ -1,10 +1,11 @@
 require "./grapheme/grapheme"
 
 class String
-  # returns an array of all Unicode extended grapheme clusters, specified in the Unicode Standard Annex #29. Grapheme clusters correspond to
+  # Returns an array of all Unicode extended grapheme clusters, specified in the Unicode Standard Annex #29. Grapheme clusters correspond to
   # "user-perceived characters". These characters often consist of multiple code points (e.g. the "woman kissing woman" emoji consists of 8 code points:
   # woman + ZWJ + heavy black heart (2 code points) + ZWJ + kiss mark + ZWJ + woman) and the rules described in Annex #29 must be applied to group those
   # code points into clusters perceived by the user as one character.
+  #
   # ```
   # "🧙‍♂️💈".graphemes # => [String::Grapheme::Cluster(@cluster="🧙‍♂️"), String::Grapheme::Cluster(@cluster='💈')]
   # ```
@@ -30,7 +31,8 @@ class String
     end
   end
 
-  # returns graphemes cluster iterator over Unicode extended grapheme clusters.
+  # Returns an `Iterator` over each grapheme cluster in the string.
+  #
   # ```
   # "🔮👍🏼!".each do |cluster|
   #   pp cluster
