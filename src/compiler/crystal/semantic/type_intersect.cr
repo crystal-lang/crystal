b9232f0565dc8cab8014636e5d960f99c8509094
Quinton Miller <nicetas.c@gmail.com>
2021-06-03 13:46:21 +0800

    Don't use Type.descendent? at all

--- src/compiler/crystal/semantic/type_intersect.cr
+++ src/compiler/crystal/semantic/type_intersect.cr
@@ -180,7 +180,7 @@ module Crystal
     private def self.common_descendent_type1(type1, type2 : GenericClassType)
       type1.parents.try &.each do |parent|
         if parent.module?
-          return type1 if descendent?(parent, type2)
+          return type1 if parent.implements?(type2)
         else
           restricted = common_descendent(parent, type2)
           return type1 if restricted
@@ -200,7 +200,7 @@ module Crystal
         return type1
       end
 
-      if type1.parents.try &.any? { |parent| descendent?(parent, type2) }
+      if type1.parents.try &.any? &.implements?(type2)
         return type1
       end
 
@@ -212,7 +212,7 @@ module Crystal
 
       type1.parents.try &.each do |parent|
         if parent.module?
-          return type1 if descendent?(parent, type2)
+          return type1 if parent.implements?(type2)
         else
           restricted = common_descendent(parent, type2)
           return type1 if restricted
@@ -241,102 +241,6 @@ module Crystal
 
     private def self.common_descendent_generic_instance1(type1, type2)
       common_descendent_type1(type1, type2)
-    end
-
-    # replacement of `type1.restriction_of?(type2, owner)`, `owner` is unused
-    # TODO: check whether `#implements?` works
-    def self.descendent?(type1, type2)
-      type1 == type2 || type1.parents.try &.any? { |parent| descendent?(parent, type2) }
-    end
-
-    def self.descendent?(type1 : TupleInstanceType, type2 : TupleInstanceType)
-      type1 == type2 || type1.implements?(type2)
-    end
-
-    def self.descendent?(type1 : NamedTupleInstanceType, type2 : NamedTupleInstanceType)
-      type1 == type2 || type1.implements?(type2)
-    end
-
-    def self.descendent?(type1 : GenericClassInstanceType, type2 : GenericClassType)
-      # ```
-      # def foo(param : Array(Int32))
-      # end
-      #
-      # def foo(param : Array)
-      # end
-      # ```
-      #
-      # Here, type1 is `Array(Int32)`, type2 is `Array`
-
-      # When the underlying generic type is the same:
-      # `SomeGeneric(X)` is always a restriction of `SomeGeneric`
-      type1.generic_type == type2
-    end
-
-    def self.descendent?(type1 : GenericInstanceType, type2 : GenericType)
-      type1.generic_type == type2 || type1.parents.try &.any? { |parent| descendent?(parent, type2) }
-    end
-
-    def self.descendent?(type1 : GenericInstanceType, type2 : GenericInstanceType)
-      if type1.generic_type == type2.generic_type
-        type1.type_vars.each do |name, type_var1|
-          type_var2 = type2.type_vars[name]
-          if type_var1.is_a?(Var) && type_var2.is_a?(Var)
-            return false unless type_var1.type.devirtualize == type_var2.type.devirtualize
-          else
-            return false unless type_var1 == type_var2
-          end
-        end
-
-        return true
-      end
-
-      type1 == type2 || type1.parents.try &.any? { |parent| descendent?(parent, type2) }
-    end
-
-    def self.descendent?(type1 : GenericClassType, type2 : GenericClassInstanceType)
-      # ```
-      # def foo(param : Array)
-      # end
-      #
-      # def foo(param : Array(Int32))
-      # end
-      # ```
-      #
-      # Here, type1 is `Array`, type2 is `Array(Int32)`
-
-      # Even when the underlying generic type is the same,
-      # `SomeGeneric` is never a restriction of `SomeGeneric(X)`
-      false
-    end
-
-    def self.descendent?(type1 : MetaclassType, type2 : VirtualMetaclassType)
-      descendent?(type1, type2.base_type.metaclass)
-    end
-
-    def self.descendent?(type1 : UnionType, type2 : Type)
-      type1 == type2 || type1.union_types.all? { |union_type| descendent?(union_type, type2) }
-    end
-
-    def self.descendent?(type1 : VirtualType, type2 : Type)
-      type2 = type2.base_type if type2.is_a?(VirtualType)
-      type1.base_type.implements?(type2) || type2.implements?(type1.base_type)
-    end
-
-    def self.descendent?(type1 : AliasType, type2 : Type)
-      type1 == type2 || descendent?(type1.remove_alias, type2)
-    end
-
-    def self.descendent?(type1 : Type, type2 : UnionType)
-      type2.union_types.any? { |union_type| descendent?(type1, union_type) }
-    end
-
-    def self.descendent?(type1 : Type, type2 : VirtualType)
-      type1.implements?(type2.base_type)
-    end
-
-    def self.descendent?(type1 : Type, type2 : AliasType)
-      type1 == type2 || descendent?(type1, type2.remove_alias)
     end
   end
 end
