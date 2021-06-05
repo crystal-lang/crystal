03dc3e5ab2ebd58185804eaf9bd57dcd3212518c
Carl Hörberg <carl@84codes.com>
2021-06-06 00:18:14 +0200

    raise RuntimError on errno

--- src/crystal/system/unix/file_descriptor_limit.cr
+++ src/crystal/system/unix/file_descriptor_limit.cr
@@ -2,7 +2,7 @@ module Crystal::System
   def self.file_descriptor_limit
     rlimit = uninitialized LibC::Rlimit
     if LibC.getrlimit(LibC::RLIMIT_NOFILE, pointerof(rlimit)) != 0
-      raise Error.from_errno("getrlimit")
+      raise RuntimeError.from_errno("Could not get rlimit")
     end
     {rlimit.rlim_cur, rlimit.rlim_max}
   end
@@ -12,7 +12,7 @@ module Crystal::System
     rlimit.rlim_cur = limit
     rlimit.rlim_max = limit
     if LibC.setrlimit(LibC::RLIMIT_NOFILE, pointerof(rlimit)) != 0
-      raise Error.from_errno("setrlimit")
+      raise RuntimeError.from_errno("Could not set rlimit")
     end
   end
 end
