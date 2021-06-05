afb1bfd65aef6b5e8f7725cfad9ed3349ec71ae2
Carl Hörberg <carl@84codes.com>
2021-06-06 00:08:37 +0200

    format change

--- src/crystal/system/unix/file_descriptor_limit.cr
+++ src/crystal/system/unix/file_descriptor_limit.cr
@@ -4,7 +4,7 @@ module Crystal::System
     if LibC.getrlimit(LibC::RLIMIT_NOFILE, pointerof(rlimit)) != 0
       raise Error.from_errno("getrlimit")
     end
-    { rlimit.rlim_cur, rlimit.rlim_max }
+    {rlimit.rlim_cur, rlimit.rlim_max}
   end
 
   def self.file_descriptor_limit=(limit) : Nil
