5dd7c6dc363432888f42ac7bf53ea0838bd816ed
Carl Hörberg <carl@84codes.com>
2021-06-05 23:59:45 +0200

    System.file_descriptor_limit

--- src/crystal/system.cr
+++ src/crystal/system.cr
@@ -10,6 +10,7 @@ end
 
 {% if flag?(:unix) %}
   require "./system/unix/hostname"
+  require "./system/unix/file_descriptor_limit"
 
   {% if flag?(:bsd) %}
     require "./system/unix/sysctl_cpucount"
---
+++ src/crystal/system/unix/file_descriptor_limit.cr
@@ -0,0 +1,18 @@
+module Crystal::System
+  def self.file_descriptor_limit
+    rlimit = uninitialized LibC::Rlimit
+    if LibC.getrlimit(LibC::RLIMIT_NOFILE, pointerof(rlimit)) != 0
+      raise Error.from_errno("getrlimit")
+    end
+    { rlimit.rlim_cur, rlimit.rlim_max }
+  end
+
+  def self.file_descriptor_limit=(limit) : Nil
+    rlimit = LibC::Rlimit.new
+    rlimit.rlim_cur = limit
+    rlimit.rlim_max = limit
+    if LibC.setrlimit(LibC::RLIMIT_NOFILE, pointerof(rlimit)) != 0
+      raise Error.from_errno("setrlimit")
+    end
+  end
+end
