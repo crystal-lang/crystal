3be92605c02bb4a3965173b22179f285f94bdcdc
Johannes Müller <straightshoota@gmail.com>
2021-06-08 13:43:08 +0200

    [CI] Add more logging

--- src/socket/addrinfo.cr
+++ src/socket/addrinfo.cr
@@ -76,8 +76,10 @@ class Socket
           Crystal::System.print_error "#{addrinfo.to_s}\n"
           unless addrinfo
             if error.is_a?(Socket::ConnectError)
+              Crystal::System.print_error "k1\n"
               raise Socket::ConnectError.from_os_error("Error connecting to '#{domain}:#{service}'", error.os_error)
             else
+              Crystal::System.print_error "k2\n"
               raise error if error
             end
           end
