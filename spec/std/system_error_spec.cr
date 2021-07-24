559844dfc86f68c0e5e92c4f2ec675ba3c567e6d
Linus Sellberg <sellberg@gmail.com>
2021-07-24 14:39:12 +0200

    SystemError spec: Make it pass on mac.

--- spec/std/system_error_spec.cr
+++ spec/std/system_error_spec.cr
@@ -3,9 +3,9 @@ require "spec"
 describe SystemError do
   describe ".from_os_error" do
     it "Can build an error from an errno" do
-      errno = Errno.new(11)
+      errno = Errno.new(2)
       error = ::IO::Error.from_os_error(message: nil, os_error: errno)
-      error.message.should eq("Resource temporarily unavailable")
+      error.message.should eq("No such file or directory")
     end
   end
 end
