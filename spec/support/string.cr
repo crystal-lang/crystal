10680c6188bb20c1ad7ccfb715419547229ffc2b
Quinton Miller <nicetas.c@gmail.com>
2021-07-25 18:49:48 +0800

    don't emit BOM from the beginning

--- spec/support/string.cr
+++ spec/support/string.cr
@@ -1,8 +1,8 @@
 def string_build_via_utf16(& : IO -> _)
   io = IO::Memory.new
-  io.set_encoding("utf16")
+  io.set_encoding("UTF-16LE")
   yield io
   byte_slice = io.to_slice
   utf16_slice = Slice.new(byte_slice.to_unsafe.unsafe_as(Pointer(UInt16)), byte_slice.size // sizeof(UInt16))
-  String.from_utf16(utf16_slice).lchop('\uFEFF') # remove BOM (byte order mark)
+  String.from_utf16(utf16_slice)
 end
