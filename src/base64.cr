cf77229a506b40e6cdc3b4dc65dfc88483e29764
BlobCodes <davidkeller@tuta.io>
2021-07-27 19:56:40 +0200

    Ran crystal tool format

--- src/base64.cr
+++ src/base64.cr
@@ -293,7 +293,7 @@ module Base64
     bytes_begin = bytes
 
     # Get the position of the last valid base64 character (rstrip '\n', '\r' and '=')
-    while size > 0 && (b = bytes[size-1]) && (b == NL || b == NR || b == PAD)
+    while size > 0 && (b = bytes[size - 1]) && (b == NL || b == NR || b == PAD)
       size -= 1
     end
 
@@ -356,7 +356,7 @@ module Base64
     bytes_begin = bytes
 
     # Get the position of the last valid base64 character (rstrip '\n', '\r' and '=')
-    while size > 0 && (b = bytes[size-1]) && (b == NL || b == NR || b == PAD)
+    while size > 0 && (b = bytes[size - 1]) && (b == NL || b == NR || b == PAD)
       size -= 1
     end
 
