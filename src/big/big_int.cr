3ca4acf8f7372a3df9a6e5d68ab6244c807f90c0
Quinton Miller <nicetas.c@gmail.com>
2021-08-12 05:14:55 +0800

    Fix `BigInt#to_s` emitting null bytes for certain values (#11063)

--- src/big/big_int.cr
+++ src/big/big_int.cr
@@ -429,7 +429,25 @@ struct BigInt < Int
       if precision <= count
         len = count + (negative ? 1 : 0)
         String.new(len + 1) do |buffer| # null terminator required by GMP
+          buffer[len - 1] = 0
           LibGMP.get_str(buffer, upcase ? -base : base, self)
+
+          # `sizeinbase` may be 1 greater than the exact value
+          if buffer[len - 1] == 0
+            if precision == count
+              # In this case the exact `count` is `precision - 1`, i.e. one zero
+              # should be inserted at the beginning of the number
+              # e.g. precision = 3, count = 3, exact count = 2
+              # "85\0\0" -> "085\0" for positive
+              # "-85\0\0" -> "-085\0" for negative
+              start = buffer + (negative ? 1 : 0)
+              start.move_to(start + 1, count - 1)
+              start.value = '0'.ord.to_u8
+            else
+              len -= 1
+            end
+          end
+
           base62_swapcase(Slice.new(buffer, len)) if base == 62
           {len, len}
         end
@@ -439,14 +457,28 @@ struct BigInt < Int
           # e.g. precision = 13, count = 8
           # "_____12345678\0" for positive
           # "_____-12345678\0" for negative
-          LibGMP.get_str(buffer + precision - count, upcase ? -base : base, self)
+          buffer[len - 1] = 0
+          start = buffer + precision - count
+          LibGMP.get_str(start, upcase ? -base : base, self)
+
+          # `sizeinbase` may be 1 greater than the exact value
+          if buffer[len - 1] == 0
+            # e.g. precision = 7, count = 3, exact count = 2
+            # "____85\0\0" -> "____885\0" for positive
+            # "____-85\0\0" -> "____-885\0" for negative
+            # `start` will be zero-filled later
+            count -= 1
+            start += 1 if negative
+            start.move_to(start + 1, count)
+          end
+
           base62_swapcase(Slice.new(buffer + len - count, count)) if base == 62
 
           if negative
             buffer.value = '-'.ord.to_u8
             buffer += 1
           end
-          Intrinsics.memset(buffer, '0'.ord.to_u8, precision - count, false)
+          Slice.new(buffer, precision - count).fill('0'.ord.to_u8)
 
           {len, len}
         end
@@ -470,6 +502,9 @@ struct BigInt < Int
       count = LibGMP.sizeinbase(self, base).to_i
       ptr = LibGMP.get_str(nil, upcase ? -base : base, self)
       negative = self < 0
+
+      # `sizeinbase` may be 1 greater than the exact value
+      count -= 1 if ptr[count + (negative ? 0 : -1)] == 0
 
       if precision <= count
         buffer = Slice.new(ptr, count + (negative ? 1 : 0))
