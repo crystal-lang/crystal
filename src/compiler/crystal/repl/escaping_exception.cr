0253c75f67ece6904abd246fc6f19e841777bec6
Ary Borenszweig <asterite@gmail.com>
2021-08-31 13:28:15 -0300

    Don't pass STDOUT to `inspect_with_backtrace`

--- src/compiler/crystal/repl/escaping_exception.cr
+++ src/compiler/crystal/repl/escaping_exception.cr
@@ -11,12 +11,17 @@ class Crystal::Repl::EscapingException < Exception
     type = @interpreter.context.type_from_id(type_id)
 
     decl = UninitializedVar.new(Var.new("ex"), TypeNode.new(@interpreter.context.program.exception.virtual_type))
-    call = Call.new(Var.new("ex"), "inspect_with_backtrace", Path.new("STDOUT"))
+    call = Call.new(Var.new("ex"), "inspect_with_backtrace")
     exps = Expressions.new([decl, call] of ASTNode)
 
     begin
-      Interpreter.interpret(context, exps) do |stack|
+      value = Interpreter.interpret(context, exps) do |stack|
         stack.as(UInt8**).value = @exception_pointer
+      end
+      if value.type == context.program.string
+        value.pointer.as(UInt8**).value.unsafe_as(String).to_s(io)
+      else
+        io.puts "Exception#inspect_with_backtrace didn't return a String :/"
       end
     rescue ex
       io.puts "Error while calling inspect_with_backtrace on exception: #{ex.message}"
