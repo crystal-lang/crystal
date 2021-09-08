cba84251072273f2acb5a3891f059244ede30b9d
Quinton Miller <nicetas.c@gmail.com>
2021-09-08 18:43:53 +0800

    Fix edge case for intersection between virtual metaclasses

--- src/compiler/crystal/semantic/type_intersect.cr
+++ src/compiler/crystal/semantic/type_intersect.cr
@@ -82,7 +82,7 @@ module Crystal
       return nil if type1.instance_type.module?
 
       restricted = common_descendent(type1.instance_type, type2.instance_type.base_type)
-      restricted.try(&.virtual_type.metaclass)
+      restricted.try(&.metaclass)
     end
 
     def self.common_descendent(type1 : VirtualMetaclassType, type2 : MetaclassType)
@@ -90,8 +90,8 @@ module Crystal
     end
 
     def self.common_descendent(type1 : VirtualMetaclassType, type2 : VirtualMetaclassType)
-      restricted = common_descendent(type1.instance_type, type2.instance_type.base_type)
-      restricted.try(&.virtual_type.metaclass)
+      restricted = common_descendent(type1.instance_type, type2.instance_type)
+      restricted.try(&.metaclass)
     end
 
     def self.common_descendent(type1 : GenericClassInstanceMetaclassType | GenericModuleInstanceMetaclassType, type2 : MetaclassType)
