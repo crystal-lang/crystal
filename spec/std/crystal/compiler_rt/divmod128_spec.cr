1aa17f700ff1ad252183cfb344342ce70d2293c8
BlobCodes <davidkeller@tuta.io>
2021-09-13 19:14:17 +0200

    specs from #11196

---
+++ spec/std/crystal/compiler_rt/divmod128_spec.cr
@@ -0,0 +1,89 @@
+require "spec"
+
+# TODO: Remove these helpers in PR part 2
+
+private def make_ti(a : Int128, b : Int128)
+  (a << 64) + b
+end
+
+private def make_tu(a : UInt128, b : UInt128)
+  (a << 64) + b
+end
+
+# Specs ported from compiler-rt
+
+private def test__divti3(a : Int128, b : Int128, expected : Int128, file = __FILE__, line = __LINE__)
+  it "passes compiler-rt builtins unit tests" do
+    actual = __divti3(a, b)
+    actual.should eq(expected), file: file, line: line
+  end
+end
+
+private def test__modti3(a : Int128, b : Int128, expected : Int128, file = __FILE__, line = __LINE__)
+  it "passes compiler-rt builtins unit tests" do
+    actual = __modti3(a, b)
+    actual.should eq(expected), file: file, line: line
+  end
+end
+
+private def test__udivti3(a : UInt128, b : UInt128, expected : UInt128, file = __FILE__, line = __LINE__)
+  it "passes compiler-rt builtins unit tests" do
+    actual = __udivti3(a, b)
+    actual.should eq(expected), file: file, line: line
+  end
+end
+
+private def test__umodti3(a : UInt128, b : UInt128, expected : UInt128, file = __FILE__, line = __LINE__)
+  it "passes compiler-rt builtins unit tests" do
+    actual = __umodti3(a, b)
+    actual.should eq(expected), file: file, line: line
+  end
+end
+
+describe "__divti3" do
+  test__divti3(0, 1, 0)
+  test__divti3(0, -1, 0)
+  test__divti3(2, 1, 2)
+  test__divti3(2, -1, -2)
+  test__divti3(-2, 1, -2)
+  test__divti3(-2, -1, 2)
+  test__divti3(make_ti(-9223372036854775808, 0x0), 1, make_ti(-9223372036854775808, 0x0))
+  test__divti3(make_ti(-9223372036854775808, 0x0), -1, make_ti(-9223372036854775808, 0x0))
+  test__divti3(make_ti(-9223372036854775808, 0x0), -2, make_ti(0x4000000000000000, 0x0))
+  test__divti3(make_ti(-9223372036854775808, 0x0), 2, make_ti(-0x4000000000000000, 0x0))
+end
+
+describe "__modti3" do
+  test__modti3(0, 1, 0)
+  test__modti3(0, -1, 0)
+
+  test__modti3(5, 3, 2)
+  test__modti3(5, -3, 2)
+  test__modti3(-5, 3, -2)
+  test__modti3(-5, -3, -2)
+
+  test__modti3(make_ti(-9223372036854775808, 0x0), 1, 0)
+  test__modti3(make_ti(-9223372036854775808, 0x0), -1, 0)
+  test__modti3(make_ti(-9223372036854775808, 0x0), 2, 0)
+  test__modti3(make_ti(-9223372036854775808, 0x0), -2, 0)
+  test__modti3(make_ti(-9223372036854775808, 0x0), 3, -2)
+  test__modti3(make_ti(-9223372036854775808, 0x0), -3, -2)
+end
+
+describe "__udivti3" do
+  test__udivti3(0, 1, 0)
+  test__udivti3(2, 1, 2)
+
+  test__udivti3(make_tu(0x0, 0x8000000000000000), 1, make_tu(0x0, 0x8000000000000000))
+  test__udivti3(make_tu(0x0, 0x8000000000000000), 2, make_tu(0x0, 0x4000000000000000))
+  test__udivti3(make_tu(0xffffffffffffffff, 0xffffffffffffffff), 2, make_tu(0x7fffffffffffffff, 0xffffffffffffffff))
+end
+
+describe "__umodti3" do
+  test__umodti3(0, 1, 0)
+  test__umodti3(2, 1, 0)
+
+  test__umodti3(make_tu(0x0, 0x8000000000000000), 1, 0)
+  test__umodti3(make_tu(0x0, 0x8000000000000000), 2, 0)
+  test__umodti3(make_tu(0xffffffffffffffff, 0xffffffffffffffff), 2, 1)
+end
