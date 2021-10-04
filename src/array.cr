2a16db48d7dd1b2fb6045f6705e340f2667ee718
Beta Ziliani <beta@manas.tech>
2021-10-04 19:13:39 -0300

    bringing back removed annotation by mistake

--- src/array.cr
+++ src/array.cr
@@ -1046,7 +1046,7 @@ class Array(T)
   end
 
   # Optimized version of `Enumerable#map`.
-  def map(& : T -> _)
+  def map(& : T -> U) forall U
     Array.new(size) { |i| yield @buffer[i] }
   end
 
