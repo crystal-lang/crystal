abb956e726ae4a72f5bc55ea8d9e536140ce78e8
Quinton Miller <nicetas.c@gmail.com>
2021-11-12 13:16:25 +0800

    float-to-int compiler-rt methods

--- src/crystal/compiler_rt.cr
+++ src/crystal/compiler_rt.cr
@@ -1,5 +1,6 @@
 {% skip_file if flag?(:skip_crystal_compiler_rt) %}
 
+require "./compiler_rt/fixint.cr"
 require "./compiler_rt/float.cr"
 require "./compiler_rt/mul.cr"
 require "./compiler_rt/divmod128.cr"
