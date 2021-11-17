fc27bc0e0a63df5a19770c9402ce48cd3abfc4a8
Guilherme Bernal <guilherme@cubos.io>
2021-11-17 10:11:44 +0000

    fix: use constant

--- src/crystal/system/unix/getrandom.cr
+++ src/crystal/system/unix/getrandom.cr
@@ -78,7 +78,7 @@ module Crystal::System::Random
   # portable).
   private def self.sys_getrandom(buf : Bytes)
     loop do
-      read_bytes = Syscall.getrandom(buf.to_unsafe, LibC::SizeT.new(buf.size), 1)
+      read_bytes = Syscall.getrandom(buf.to_unsafe, LibC::SizeT.new(buf.size), Syscall::GRND_NONBLOCK)
       if read_bytes < 0
         err = Errno.new(-read_bytes.to_i)
         if err == Errno::EINTR || err == Errno::EAGAIN
