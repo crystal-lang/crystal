90d89413fd6c975b9508cb11bb5eee96be202b6b
Quinton Miller <nicetas.c@gmail.com>
2021-11-29 22:08:40 +0800

    Refer to `T.class` as "metaclass" in error messages, not "class" (#11378)

--- spec/compiler/semantic/class_spec.cr
+++ spec/compiler/semantic/class_spec.cr
@@ -1115,6 +1115,18 @@ describe "Semantic: class" do
       "Moo is not a class, it's a module"
   end
 
+  it "errors if inherits from metaclass" do
+    assert_error <<-CR, "Foo.class is not a class, it's a metaclass"
+      class Foo
+      end
+
+      alias FooClass = Foo.class
+
+      class Bar < FooClass
+      end
+      CR
+  end
+
   it "can use short name for top-level type" do
     assert_type(%(
       class T
