crystal-lang/crystal

commit 34b017795e824ae9664ade7bdbef5711f9cd5960

fix: getrandom syscall was blocking and didn't had proper error checking (#11460)

src/crystal/system/unix/getrandom.cr
@@ -75,15 +75,15 @@ module Crystal::System::Random
       end
 
       read_bytes = sys_getrandom(buf[0, chunk_size])
-      raise RuntimeError.from_errno("getrandom") if read_bytes == -1
 
       buf += read_bytes
     end
   end
 
   # Low-level wrapper for the `getrandom(2)` syscall, returns the number of
-  # bytes read or `-1` if an error occurred (or the syscall isn't available)
-  # and sets `Errno.value`.
+  # bytes read or the errno as a negative number if an error occurred (or the
+  # syscall isn't available). The GRND_NONBLOCK=1 flag is passed as last argument,
+  # so that it returns -EAGAIN if the requested entropy was not available.
   #
   # We use the kernel syscall instead of the `getrandom` C function so any
   # binary compiled for Linux will always use getrandom if the kernel is 3.17+
@@ -91,9 +91,14 @@ module Crystal::System::Random
   # portable).
   private def self.sys_getrandom(buf : Bytes)
     loop do
-      read_bytes = Syscall.getrandom(buf.to_unsafe, LibC::SizeT.new(buf.size), 0)
-      if read_bytes < 0 && (Errno.value == Errno::EINTR || Errno.value == Errno::EAGAIN)
-        ::Fiber.yield
+      read_bytes = Syscall.getrandom(buf.to_unsafe, LibC::SizeT.new(buf.size), Syscall::GRND_NONBLOCK)
+      if read_bytes < 0
+        err = Errno.new(-read_bytes.to_i)
+        if err == Errno::EINTR || err == Errno::EAGAIN
+          ::Fiber.yield
+        else
+          raise RuntimeError.from_os_error("getrandom", err)
+        end
       else
         return read_bytes
       end

src/crystal/system/unix/syscall.cr
@@ -4,5 +4,7 @@ require "c/unistd"
 require "syscall"
 
 module Crystal::System::Syscall
+  GRND_NONBLOCK = 1u32
+
   ::Syscall.def_syscall getrandom, LibC::SSizeT, buf : UInt8*, buflen : LibC::SizeT, flags : UInt32
 end

commit c00a961627342545f56d0f2a0a1e249a8a0a9f5d

Fix getrandom for interpreter
The workaround for the interpreter was broken by https://github.com/crystal-lang/crystal/pull/11460

src/crystal/system/unix/getrandom.cr
@@ -9,6 +9,8 @@ require "./syscall"
   end
 
   module Crystal::System::Syscall
+    GRND_NONBLOCK = 1u32
+
     # TODO: Implement syscall for interpreter
     def self.getrandom(buf : UInt8*, buflen : LibC::SizeT, flags : UInt32) : LibC::SSizeT
       LibC.getrandom(buf, buflen, flags)