e86b675311304ba6a31c919c9f7c26adf62e3df2
Guilherme Bernal <guilherme@cubos.io>
2021-12-23 19:42:24 +0000

    feat: implement Crystal::System::Fiber with malloc/free

--- src/crystal/system/wasm/fiber.cr
+++ src/crystal/system/wasm/fiber.cr
@@ -2,10 +2,10 @@ require "c/sys/mman"
 
 module Crystal::System::Fiber
   def self.allocate_stack(stack_size) : Void*
-    raise NotImplementedError.new("Crystal::System::Fiber.allocate_stack")
+    LibC.malloc(stack_size)
   end
 
   def self.free_stack(stack : Void*, stack_size) : Nil
-    raise NotImplementedError.new("Crystal::System::Fiber.free_stack")
+    LibC.free(stack)
   end
 end
