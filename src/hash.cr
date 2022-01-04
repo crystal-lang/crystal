9831e64e4cf7be4d48c10ae4b41feee85126590b
Jack Thorne <jack@kilmer.co>
2022-01-04 01:04:38 -0600

    update docs

--- src/hash.cr
+++ src/hash.cr
@@ -1476,7 +1476,8 @@ class Hash(K, V)
   # Removes a list of keys out of hash.
   #
   # ```
-  # {"a" => 1, "b" => 2, "c" => 3, "d" => 4}.reject!(["a", "c"]) # => {"b" => 2, "d" => 4}
+  # h = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}.reject!(["a", "c"])
+  # h # => {"b" => 2, "d" => 4}
   # ```
   def reject!(keys : Array | Tuple) : self
     keys.each { |k| delete(k) }
