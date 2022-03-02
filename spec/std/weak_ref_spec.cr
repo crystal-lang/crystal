3c149689a378ffa5ee0ece1601c56f8c3bd1f8f2
Johannes Müller <straightshoota@gmail.com>
2022-03-02 01:10:47 +0100

    Revert "Improve error for incompatible generic arguments for `WeakRef`" (#11863)

--- spec/std/weak_ref_spec.cr
+++ spec/std/weak_ref_spec.cr
@@ -1,6 +1,5 @@
 require "spec"
 require "weak_ref"
-require "../spec_helper"
 require "../support/finalize"
 
 private class Foo
@@ -66,20 +65,5 @@ describe WeakRef do
 
     # Use `last` to stop the variable from being optimised away in release mode.
     last.to_s
-  end
-
-  it "errors if weak referenced object is a module" do
-    assert_error %(
-      require "prelude"
-      require "weak_ref"
-      module M; end
-      class A; include M; end
-      class B
-        @foo : WeakRef(M)
-        def initialize(thing); @foo = WeakRef.new(thing.as(M)); end
-      end
-      B.new(A.new)
-      ),
-      "Cannot create a WeakRef of a module"
   end
 end
