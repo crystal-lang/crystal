a76f3d6eb9f412b238a552d355e7c118e103131d
Dominic Muller <nicklink483@gmail.com>
2022-03-13 10:58:05 -0400

    Start lib resolution earlier to overlap with time spent calling llvm

--- src/compiler/crystal/compiler.cr
+++ src/compiler/crystal/compiler.cr
@@ -408,6 +408,17 @@ module Crystal
       target_triple = target_machine.triple
       reused = [] of String
 
+      # We check again because maybe this directory was created in between (maybe with a macro run)
+      if Dir.exists?(output_filename)
+        error "can't use `#{output_filename}` as output filename because it's a directory"
+      end
+
+      output_filename = File.expand_path(output_filename)
+      linker_command_channel = Channel(Tuple(String, Array(String)?)).new
+      spawn do
+        linker_command_channel.send(linker_command(program, object_names, output_filename, output_dir, expand: true))
+      end
+
       @progress_tracker.stage("Codegen (bc+obj)") do
         @progress_tracker.stage_progress_total = units.size
 
@@ -421,16 +432,10 @@ module Crystal
         end
       end
 
-      # We check again because maybe this directory was created in between (maybe with a macro run)
-      if Dir.exists?(output_filename)
-        error "can't use `#{output_filename}` as output filename because it's a directory"
-      end
-
-      output_filename = File.expand_path(output_filename)
 
       @progress_tracker.stage("Codegen (linking)") do
         Dir.cd(output_dir) do
-          linker_command = linker_command(program, object_names, output_filename, output_dir, expand: true)
+          linker_command = linker_command_channel.receive
 
           process_wrapper(*linker_command) do |command, args|
             Process.run(command, args, shell: true,
