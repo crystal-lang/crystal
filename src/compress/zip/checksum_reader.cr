8a077309a824ebe5be555ff4dbcdf844008d38c8
Didactic Drunk <1479616+didactic-drunk@users.noreply.github.com>
2022-03-23 11:41:34 -0700

    Compress::Zip Accept `Path` arguments

--- src/compress/zip/checksum_reader.cr
+++ src/compress/zip/checksum_reader.cr
@@ -5,14 +5,14 @@ module Compress::Zip
   private class ChecksumReader < IO
     getter crc32 = ::Digest::CRC32.initial
 
-    def initialize(@io : IO, @filename : String, verify @expected_crc32 : UInt32? = nil)
+    def initialize(@io : IO, @filename : Path | String, verify @expected_crc32 : UInt32? = nil)
     end
 
     def read(slice : Bytes) : Int32
       read_bytes = @io.read(slice)
       if read_bytes == 0
         if (expected_crc32 = @expected_crc32) && crc32 != expected_crc32
-          raise Compress::Zip::Error.new("Checksum failed for entry #{@filename} (expected #{expected_crc32}, got #{crc32}")
+          raise Compress::Zip::Error.new("Checksum failed for entry #{@filename.to_s} (expected #{expected_crc32}, got #{crc32}")
         end
       else
         @crc32 = ::Digest::CRC32.update(slice[0, read_bytes], @crc32)
