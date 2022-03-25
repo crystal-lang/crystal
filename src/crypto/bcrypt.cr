e14ac82fbe66bf5b15410c176c602abb8d746f2c
maiha <maiha@wota.jp>
2022-03-25 23:13:42 +0900

    Revert "Fix example of Crypto::Bcrypt"
    This reverts commit 71d23f37874dabc724a9e17a49f48a7c41f9739b.

--- src/crypto/bcrypt.cr
+++ src/crypto/bcrypt.cr
@@ -66,7 +66,7 @@ class Crypto::Bcrypt
   # ```
   # require "crypto/bcrypt"
   #
-  # password = Crypto::Bcrypt.new "secret", "AQIDBAUGBwgJCgsMDQ4PEA=="
+  # password = Crypto::Bcrypt.new "secret", "salt_of_16_chars"
   # password.digest
   # ```
   def self.new(password : String, salt : String, cost = DEFAULT_COST)
