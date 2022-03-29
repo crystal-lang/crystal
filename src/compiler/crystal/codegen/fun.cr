f3364c775997cf717553136a9880389fe7819a09
Guilherme Bernal <guilherme@cubos.io>
2022-03-29 11:58:58 -0300

    feat: export all top-level `fun` functions

--- src/compiler/crystal/codegen/fun.cr
+++ src/compiler/crystal/codegen/fun.cr
@@ -202,9 +202,15 @@ class Crystal::CodeGenVisitor
         end
       end
 
-      if target_def.is_a? External && (wasm_import_module = target_def.wasm_import_module)
-        context.fun.add_attribute("wasm-import-name", target_def.real_name)
-        context.fun.add_attribute("wasm-import-module", wasm_import_module)
+      if @program.has_flag?("wasm32")
+        if target_def.is_a? External && (wasm_import_module = target_def.wasm_import_module)
+          context.fun.add_attribute("wasm-import-name", target_def.real_name)
+          context.fun.add_attribute("wasm-import-module", wasm_import_module)
+        end
+
+        if is_exported_fun
+          context.fun.add_attribute("wasm-export-name", mangled_name)
+        end
       end
 
       context.fun
