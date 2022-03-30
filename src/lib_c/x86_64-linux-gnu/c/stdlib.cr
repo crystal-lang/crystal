2408e3c6e255022a99ffaecc588f7949214de357
Quinton Miller <nicetas.c@gmail.com>
2022-03-31 07:35:05 +0800

    `c/stdlib.cr`

--- src/lib_c/x86_64-linux-gnu/c/stdlib.cr
+++ src/lib_c/x86_64-linux-gnu/c/stdlib.cr
@@ -2,20 +2,12 @@ require "./stddef"
 require "./sys/wait"
 
 lib LibC
-  struct DivT
-    quot : Int
-    rem : Int
-  end
-
-  fun atof(nptr : Char*) : Double
-  fun div(numer : Int, denom : Int) : DivT
   fun exit(status : Int) : NoReturn
   fun free(ptr : Void*) : Void
   fun getenv(name : Char*) : Char*
   fun malloc(size : SizeT) : Void*
   fun mkstemp(template : Char*) : Int
   fun mkstemps(template : Char*, suffixlen : Int) : Int
-  fun putenv(string : Char*) : Int
   fun realloc(ptr : Void*, size : SizeT) : Void*
   fun realpath(name : Char*, resolved : Char*) : Char*
   fun setenv(name : Char*, value : Char*, replace : Int) : Int
