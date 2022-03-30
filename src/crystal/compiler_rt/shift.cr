43a486351c44ccfb371e9fd5811ca83873a4b777
Guilherme Bernal <guilherme@cubos.io>
2022-03-30 14:37:48 -0300

    fix: use unsafe_shl and unsafe_shr

--- src/crystal/compiler_rt/shift.cr
+++ src/crystal/compiler_rt/shift.cr
@@ -6,11 +6,11 @@
 fun __ashlti3(a : Int128, b : Int32) : Int128
   low, high = a.unsafe_as(Tuple(UInt64, Int64))
   if b >= 64
-    low, high = 0u64, (low << (b &- 64)).to_i64!
+    low, high = 0u64, low.unsafe_shl(b &- 64).to_i64!
   elsif b == 0
     return a
   else
-    low, high = low << b, (high << b) | (low >> (64 &- b))
+    low, high = low.unsafe_shl(b), high.unsafe_shl(b) | low.unsafe_shr(64 &- b)
   end
 
   {low, high}.unsafe_as(Int128)
@@ -24,11 +24,11 @@ end
 fun __ashrti3(a : Int128, b : Int32) : Int128
   low, high = a.unsafe_as(Tuple(UInt64, Int64))
   if b >= 64
-    low, high = high >> (b &- 64), high >> 63
+    low, high = high.unsafe_shr(b &- 64), high.unsafe_shr(63)
   elsif b == 0
     return a
   else
-    low, high = (high << (64 &- b)) | (low >> b), high >> b
+    low, high = high.unsafe_shl(64 &- b) | low.unsafe_shr(b), high.unsafe_shr(b)
   end
 
   {low, high}.unsafe_as(Int128)
@@ -42,11 +42,11 @@ end
 fun __lshrti3(a : Int128, b : Int32) : Int128
   low, high = a.unsafe_as(Tuple(UInt64, UInt64))
   if b >= 64
-    low, high = high >> (b &- 64), 0u64
+    low, high = high.unsafe_shr(b &- 64), 0u64
   elsif b == 0
     return a
   else
-    low, high = (high << (64 &- b)) | (low >> b), high >> b
+    low, high = high.unsafe_shl(64 &- b) | low.unsafe_shr(b), high.unsafe_shr(b)
   end
 
   {low, high}.unsafe_as(Int128)
