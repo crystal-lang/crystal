e0b8cf33ea81400af9c70e45c9fe0acf5277ab00
Guilherme Bernal <guilherme@cubos.io>
2022-04-03 15:11:23 -0300

    fix: adjust wasm event loop to match updated interface

--- src/crystal/system/wasi/event_loop.cr
+++ src/crystal/system/wasi/event_loop.cr
@@ -1,39 +1,40 @@
 # :nodoc:
-module Crystal::EventLoop
+class Crystal::EventLoop
+  def self.create
+    Crystal::EventLoop
+  end
+
   # Runs the event loop.
   def self.run_once
     raise NotImplementedError.new("Crystal::EventLoop.run_once")
   end
 
   # Create a new resume event for a fiber.
-  def self.create_resume_event(fiber : Fiber) : Crystal::Event
+  def self.create_resume_event(fiber : Fiber) : Crystal::WasiEvent
     raise NotImplementedError.new("Crystal::EventLoop.create_resume_event")
   end
 
   # Creates a timeout_event.
-  def self.create_timeout_event(fiber) : Crystal::Event
+  def self.create_timeout_event(fiber) : Crystal::WasiEvent
     raise NotImplementedError.new("Crystal::EventLoop.create_timeout_event")
   end
 
   # Creates a write event for a file descriptor.
-  def self.create_fd_write_event(io : IO::Evented, edge_triggered : Bool = false) : Crystal::Event
+  def self.create_fd_write_event(io : IO::Evented, edge_triggered : Bool = false) : Crystal::WasiEvent
     raise NotImplementedError.new("Crystal::EventLoop.create_fd_write_event")
   end
 
   # Creates a read event for a file descriptor.
-  def self.create_fd_read_event(io : IO::Evented, edge_triggered : Bool = false) : Crystal::Event
+  def self.create_fd_read_event(io : IO::Evented, edge_triggered : Bool = false) : Crystal::WasiEvent
     raise NotImplementedError.new("Crystal::EventLoop.create_fd_read_event")
   end
 end
 
-struct Crystal::Event
-  def add(timeout : LibC::Timeval? = nil)
+struct Crystal::WasiEvent < Crystal::Event
+  def add(timeout : Time::Span?) : Nil
   end
 
-  def add(timeout : Time::Span)
-  end
-
-  def free
+  def free : Nil
   end
 
   def delete
