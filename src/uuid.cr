89a627e2e492ad8ffa62607e0566fbc680863160
Jamie Gaskins <jgaskins@hey.com>
2022-04-16 15:02:00 -0400

    Add document string and return type to UUID.parse?

--- src/uuid.cr
+++ src/uuid.cr
@@ -75,9 +75,9 @@ struct UUID
     new(uuid.bytes, variant, version)
   end
 
-  # Creates new UUID by decoding `value` string from hyphenated (ie. `ba714f86-cac6-42c7-8956-bcf5105e1b81`),
-  # hexstring (ie. `89370a4ab66440c8add39e06f2bb6af6`) or URN (ie. `urn:uuid:3f9eaf9e-cdb0-45cc-8ecb-0e5b2bfb0c20`)
-  # format.
+  # Creates new UUID by decoding `value` string from hyphenated (ie `ba714f86-cac6-42c7-8956-bcf5105e1b81`),
+  # hexstring (ie `89370a4ab66440c8add39e06f2bb6af6`) or URN (ie `urn:uuid:3f9eaf9e-cdb0-45cc-8ecb-0e5b2bfb0c20`)
+  # format, raising an `ArgumentError` if the string fors not match any of these formats.
   def self.new(value : String, variant = nil, version = nil)
     bytes = uninitialized UInt8[16]
 
@@ -107,7 +107,10 @@ struct UUID
     new(bytes, variant, version)
   end
 
-  def self.parse?(value : String, variant = nil, version = nil)
+  # Creates new UUID by decoding `value` string from hyphenated (ie `ba714f86-cac6-42c7-8956-bcf5105e1b81`),
+  # hexstring (ie `89370a4ab66440c8add39e06f2bb6af6`) or URN (ie `urn:uuid:3f9eaf9e-cdb0-45cc-8ecb-0e5b2bfb0c20`)
+  # format, returning `nil` if the string does not match any of these formats.
+  def self.parse?(value : String, variant = nil, version = nil) : UUID?
     bytes = uninitialized UInt8[16]
 
     case value.size
