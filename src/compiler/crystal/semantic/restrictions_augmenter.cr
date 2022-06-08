620a0db93424c41d1a634129485aecd26ac5553b
Ary Borenszweig <ary@noredink.com>
2022-06-08 10:17:01 -0300

    Experimental: restriction augmenter

---
+++ src/compiler/crystal/semantic/restrictions_augmenter.cr
@@ -0,0 +1,121 @@
+module Crystal
+  class RestrictionsAugmenter < Visitor
+    @args_hash : Hash(String, Arg)?
+    @current_type : Type
+    @def : Def?
+
+    # If an assignment happens conditionally we can't add a type
+    # restriction because... we can't be sure the assignment will happen!
+    @conditional_nest = 0
+
+    def initialize(@program : Program, @new_expansions : Array({original: Def, expanded: Def}))
+      @current_type = @program
+    end
+
+    def visit(node : ExpandableNode)
+      if expanded = node.expanded
+        expanded.accept self
+      end
+      false
+    end
+
+    def visit(node : ClassDef)
+      old_type = @current_type
+      @current_type = node.resolved_type
+      node.body.accept self
+      @current_type = old_type
+      false
+    end
+
+    def visit(node : Def)
+      @def = node
+      @args_hash = args_hash = {} of String => Arg
+      node.args.each do |arg|
+        next if arg.restriction
+        args_hash[arg.name] = arg
+      end
+      node.body.accept self
+      @args_hash = nil
+      @def = nil
+      false
+    end
+
+    def visit(node : If | Unless)
+      node.cond.accept self
+      @conditional_nest += 1
+      node.then.accept self
+      node.else.try &.accept self
+      @conditional_nest -= 1
+      false
+    end
+
+    def visit(node : While | Until)
+      node.cond.accept self
+      @conditional_nest += 1
+      node.body.accept self
+      @conditional_nest -= 1
+      false
+    end
+
+    def visit(node : Assign)
+      args_hash = @args_hash
+      return false unless args_hash
+
+      current_def = @def
+      return false unless current_def
+
+      target = node.target
+      value = node.value
+      current_type = @current_type
+
+      case target
+      when Var
+        args_hash.delete(target.name)
+      when InstanceVar
+        if @conditional_nest == 0
+          process_assign_instance_var(target, value, current_type, current_def, args_hash)
+        end
+      when ClassVar
+        # TODO: apply same logic but for assignments to class vars
+      end
+
+      false
+    end
+
+    def visit(node : ASTNode)
+      true
+    end
+
+    private def process_assign_instance_var(target, value, current_type, current_def, args_hash)
+      return unless current_type.is_a?(InstanceVarContainer)
+      return unless value.is_a?(Var)
+
+      arg = args_hash[value.name]?
+      return unless arg
+
+      ivar = current_type.instance_vars[target.name]?
+      return unless ivar
+
+      restriction = TypeToRestriction.convert(ivar.type)
+      return unless restriction
+
+      arg.restriction = restriction
+
+      # If this is an initialize, we can also add a type restriction to the
+      # auto-generated "new" so that it shows up in docs.
+      return unless current_def.name == "initialize"
+
+      # TODO: we should probably store the expansions as a Hash from now on
+      expansion = @new_expansions.find { |expansion| expansion[:original].same?(current_def) }
+      return unless expansion
+
+      expanded = expansion[:expanded]
+      expansion_arg = expanded.args.find do |expansion_arg|
+        expansion_arg.name == arg.name
+      end
+      return unless expansion_arg
+
+      expansion_arg.restriction = restriction.dup
+    end
+  end
+end
---
+++ src/compiler/crystal/semantic/type_to_restriction.cr
@@ -0,0 +1,124 @@
+require "../types"
+
+module Crystal
+  module TypeToRestriction
+    def self.convert(type : NilType)
+      Path.global("Nil")
+    end
+
+    def self.convert(type : BoolType)
+      Path.global("Bool")
+    end
+
+    def self.convert(type : CharType)
+      Path.global("Char")
+    end
+
+    def self.convert(type : SymbolType)
+      Path.global("Symbol")
+    end
+
+    def self.convert(type : IntegerType | FloatType)
+      case type.kind
+      in NumberKind::I8   then Path.global("Int8")
+      in NumberKind::I16  then Path.global("Int16")
+      in NumberKind::I32  then Path.global("Int32")
+      in NumberKind::I64  then Path.global("Int64")
+      in NumberKind::I128 then Path.global("Int128")
+      in NumberKind::U8   then Path.global("UInt8")
+      in NumberKind::U16  then Path.global("UInt16")
+      in NumberKind::U32  then Path.global("UInt32")
+      in NumberKind::U64  then Path.global("UInt64")
+      in NumberKind::U128 then Path.global("UInt128")
+      in NumberKind::F32  then Path.global("Float32")
+      in NumberKind::F64  then Path.global("Float64")
+      end
+    end
+
+    def self.convert(type : NonGenericClassType | NonGenericModuleType | EnumType)
+      return nil if type.private?
+
+      names = [] of String
+      append_namespace(type, names)
+      names << type.name
+      Path.global(names)
+    end
+
+    def self.convert(type : TupleInstanceType)
+      Generic.new(
+        Path.global("Tuple"),
+        type.tuple_types.map do |tuple_type|
+          convert(tuple_type) || Underscore.new
+        end
+      )
+    end
+
+    def self.convert(type : NamedTupleInstanceType)
+      Generic.new(
+        Path.global("NamedTuple"),
+        type_vars: [] of ASTNode,
+        named_args: type.entries.map do |entry|
+          NamedArgument.new(
+            entry.name,
+            convert(entry.type) || Underscore.new,
+          )
+        end
+      )
+    end
+
+    def self.convert(type : ProcInstanceType)
+      type_vars = Array(ASTNode).new(type.arg_types.size + 1)
+      type.arg_types.each do |arg_type|
+        type_vars.push(convert(arg_type) || Underscore.new)
+      end
+      type_vars.push(convert(type.return_type) || Underscore.new)
+
+      Generic.new(
+        Path.global("Proc"),
+        type_vars: type_vars,
+      )
+    end
+
+    def self.convert(type : GenericInstanceType)
+      return nil if type.private?
+
+      generic_type = type.generic_type
+      names = [] of String
+      append_namespace(generic_type, names)
+      names << generic_type.name
+      path = Path.global(names)
+      type_vars = type.type_vars.map do |name, type_var|
+        type_var_type = type_var.type?
+        if type_var_type
+          convert(type_var_type) || Underscore.new
+        else
+          Underscore.new
+        end
+      end
+      Generic.new(path, type_vars)
+    end
+
+    def self.convert(type : UnionType)
+      Union.new(
+        type.union_types.map do |union_type|
+          restriction = convert(union_type)
+          return unless restriction
+
+          restriction.as(ASTNode)
+        end
+      )
+    end
+
+    def self.convert(type : Type)
+      nil
+    end
+
+    private def self.append_namespace(type, names)
+      namespace = type.namespace
+      return if namespace.is_a?(Program)
+
+      append_namespace(namespace, names)
+      names << namespace.name
+    end
+  end
+end
