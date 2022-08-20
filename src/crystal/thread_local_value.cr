f0e7e7688f7e4e754323850b34204e509ec79e61
Ulrich Kramer <wonderix@googlemail.com>
2022-08-20 07:03:32 +0200

    Fix fomatting

--- src/crystal/thread_local_value.cr
+++ src/crystal/thread_local_value.cr
@@ -1,56 +1,56 @@
 # :nodoc:
 {% if flag?(:preview_mt) %}
-struct Crystal::ThreadLocalValue(T)
-  @values = Hash(Thread, T).new
-  @mutex = Crystal::SpinLock.new
+  struct Crystal::ThreadLocalValue(T)
+    @values = Hash(Thread, T).new
+    @mutex = Crystal::SpinLock.new
 
-  def get(&block : -> T)
-    th = Thread.current
-    @mutex.sync do
-      @values.fetch(th) do
-        @values[th] = yield
+    def get(&block : -> T)
+      th = Thread.current
+      @mutex.sync do
+        @values.fetch(th) do
+          @values[th] = yield
+        end
+      end
+    end
+
+    def get?
+      @mutex.sync do
+        @values[Thread.current]?
+      end
+    end
+
+    def set(value : T)
+      @mutex.sync do
+        @values[Thread.current] = value
+      end
+    end
+
+    def consume_each
+      @mutex.sync do
+        @values.each_value { |t| yield t }
+        @values.clear
       end
     end
   end
-
-  def get?
-    @mutex.sync do
-      @values[Thread.current]?
-    end
-  end
-
-  def set(value : T)
-    @mutex.sync do
-      @values[Thread.current] = value
-    end
-  end
-
-  def consume_each
-    @mutex.sync do
-      @values.each_value { |t| yield t }
-      @values.clear
-    end
-  end
-end
 {% else %}
-struct Crystal::ThreadLocalValue(T)
-  @value : T? = nil
+  struct Crystal::ThreadLocalValue(T)
+    @value : T? = nil
 
-  def get(&block : -> T)
-    @value ||= yield
-  end
+    def get(&block : -> T)
+      @value ||= yield
+    end
 
-  def get?
-    @value
-  end
+    def get?
+      @value
+    end
 
-  def set(value : T)
-    @value = value
-  end
+    def set(value : T)
+      @value = value
+    end
 
-  def consume_each
-    @value.try { |v| yield v }
-    @value = nil
+    def consume_each
+      @value.try { |v| yield v }
+      @value = nil
+    end
   end
-end
 {% end %}
