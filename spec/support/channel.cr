5eb3a207b6c5a31f0dca146ea1ab627008fd642a
Ary Borenszweig <ary@noredink.com>
2022-08-30 11:43:22 -0300

    Increase schedule_timeout timeout in interpreted mode

--- spec/support/channel.cr
+++ spec/support/channel.cr
@@ -1,6 +1,12 @@
 def schedule_timeout(c : Channel(Symbol))
   spawn do
-    sleep 1
+    {% if flag?(:interpreted) %}
+      # TODO: it's not clear why some interpreter specs
+      # take more than 1 second in some cases.
+      sleep 5
+    {% else %}
+      sleep 1
+    {% end %}
     c.send(:timeout)
   end
 end
