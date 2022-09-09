def3dc641883e912d1d8b59c84c242700040a0c4
Quinton Miller <nicetas.c@gmail.com>
2022-09-09 10:33:54 +0800

    fixup

--- spec/compiler/lexer/lexer_spec.cr
+++ spec/compiler/lexer/lexer_spec.cr
@@ -159,11 +159,7 @@ describe "Lexer" do
     :case, :when, :select, :then, :of, :abstract, :rescue, :ensure, :alias,
     :pointerof, :sizeof, :instance_sizeof, :offsetof, :as, :typeof, :for, :in,
     :with, :self, :super, :private, :protected, :asm, :uninitialized,
-    :annotation, :verbatim
-  it_lexes "is_a?", :IDENT, Keyword::IS_A_QUESTION
-  it_lexes "as?", :IDENT, Keyword::AS_QUESTION
-  it_lexes "nil?", :IDENT, Keyword::NIL_QUESTION
-  it_lexes "responds_to?", :IDENT, Keyword::RESPONDS_TO_QUESTION
+    :annotation, :verbatim, :is_a_question, :as_question, :nil_question, :responds_to_question
   it_lexes_idents ["ident", "something", "with_underscores", "with_1", "foo?", "bar!", "fooBar",
                    "❨╯°□°❩╯︵┻━┻"]
   it_lexes_idents ["def?", "if?", "else?", "elsif?", "end?", "true?", "false?", "class?", "while?",
