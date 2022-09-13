3b04cd8215018b09902946c9b7f35dcd1d878ab2
Ary Borenszweig <ary@noredink.com>
2022-09-13 14:29:36 -0300

    Fix spec

--- spec/compiler/semantic/automatic_cast_spec.cr
+++ spec/compiler/semantic/automatic_cast_spec.cr
@@ -208,7 +208,7 @@ describe "Semantic: automatic cast" do
 
       foo(:four)
       ),
-      "expected argument #1 to 'foo' to be Foo, not Symbol"
+      "expected argument #1 symbol to 'foo' to match a Foo enum member"
   end
 
   it "says ambiguous call for symbol" do
