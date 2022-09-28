1a8bb0f0c2e6ced288c119c1a2ccfb21b297d363
Johannes Müller <straightshoota@gmail.com>
2022-09-28 23:19:28 +0200

    Fix `HTTP::Client` implicit compression with retry

--- spec/std/http/client/client_spec.cr
+++ spec/std/http/client/client_spec.cr
@@ -270,6 +270,29 @@ module HTTP
       requests.should eq 1
     end
 
+    it "retry does not affect implicit compression (#11354)" do
+      server = HTTP::Server.new do |context|
+        context.response.headers["Content-Encoding"] = "gzip"
+        context.response.output.print "\u001F\x8B\b\u0000\u0000\u0000\u0000\u0000\u0004\u0003+\xCFH,I-K-\u0002\u0000\xB3C\u0011N\b\u0000\u0000\u0000"
+        context.response.output.close
+        io = context.response.@io.as(Socket)
+        io.linger = 0 # with linger 0 the socket will be RST on close
+        io.close
+      end
+      address = server.bind_unused_port "127.0.0.1"
+
+      run_server(server) do
+        client = HTTP::Client.new("127.0.0.1", address.port)
+        # First request establishes the server connection, but the server
+        # immediately closes it after sending the response.
+        client.get(path: "/")
+
+        # Second request tries to re-use the connection which fails (due to the
+        # server's hang up) and then it retries by establishing a new connection.
+        client.get(path: "/").body.should eq "whatever"
+      end
+    end
+
     it "doesn't read the body if request was HEAD" do
       resp_get = test_server("localhost", 0, 0) do |server|
         client = Client.new("localhost", server.local_address.port)
