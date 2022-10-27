708c68dcc367a8fd6da3ae27bf19459a0588a67e
Sijawusz Pur Rahnama <sija@sija.pl>
2022-10-27 23:47:05 +0200

    Make sure to call the `SDL.quit` on exit in raytracer sample

--- samples/sdl/raytracer.cr
+++ samples/sdl/raytracer.cr
@@ -222,19 +222,24 @@ SDL.hide_cursor
 surface = SDL.set_video_mode WIDTH, HEIGHT, 32, LibSDL::DOUBLEBUF | LibSDL::HWSURFACE | LibSDL::ASYNCBLIT
 
 first = true
-while true
-  SDL.poll_events do |event|
-    if event.type.in?(LibSDL::QUIT, LibSDL::KEYDOWN)
-      SDL.quit
-      exit
+
+begin
+  while true
+    SDL.poll_events do |event|
+      if event.type.in?(LibSDL::QUIT, LibSDL::KEYDOWN)
+        SDL.quit
+        exit
+      end
+    end
+
+    if first
+      start = SDL.ticks
+      render scene, surface
+      ms = SDL.ticks - start
+      puts "Rendered in #{ms} ms"
+      first = false
     end
   end
-
-  if first
-    start = SDL.ticks
-    render scene, surface
-    ms = SDL.ticks - start
-    puts "Rendered in #{ms} ms"
-    first = false
-  end
+ensure
+  SDL.quit
 end
