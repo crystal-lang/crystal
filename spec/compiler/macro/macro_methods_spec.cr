a4db8e7568e001508a8b5664bc201894c8544b11
Caspian Baska <email@caspian.computer>
2022-10-30 17:27:47 +0800

    Add `ArrayLiteral#-(other)` and `TupleLiteral#-(other)` (#12646)
    Co-authored-by: Vladislav Zarakovsky <vlad.zar@gmail.com>
    Co-authored-by: Beta Ziliani <beta@manas.tech>

--- spec/compiler/macro/macro_methods_spec.cr
+++ spec/compiler/macro/macro_methods_spec.cr
@@ -879,6 +879,20 @@ module Crystal
         end
       end
 
+      describe "#-" do
+        context "with TupleLiteral argument" do
+          it "removes the elements in RHS from LHS into an ArrayLiteral" do
+            assert_macro %({{ [1, 2, 3, 4] - {1, 3, 5} }}), %([2, 4])
+          end
+        end
+
+        context "with ArrayLiteral argument" do
+          it "removes the elements in RHS from LHS into an ArrayLiteral" do
+            assert_macro %({{ [1, 2, 3, 4] - [1, 3, 5] }}), %([2, 4])
+          end
+        end
+      end
+
       it "executes [] with range" do
         assert_macro %({{ [1, 2, 3, 4][1...-1] }}), %([2, 3])
       end
@@ -1392,6 +1406,20 @@ module Crystal
         context "with ArrayLiteral argument" do
           it "concatenates the literals into a TupleLiteral" do
             assert_macro %({{ {1, 2} + [3, 4, 5] }}), %({1, 2, 3, 4, 5})
+          end
+        end
+      end
+
+      describe "#-" do
+        context "with TupleLiteral argument" do
+          it "removes the elements in RHS from LHS into a TupleLiteral" do
+            assert_macro %({{ {1, 2, 3, 4} - {1, 3, 5} }}), %({2, 4})
+          end
+        end
+
+        context "with ArrayLiteral argument" do
+          it "removes the elements in RHS from LHS into a TupleLiteral" do
+            assert_macro %({{ {1, 2, 3, 4} - [1, 3, 5] }}), %({2, 4})
           end
         end
       end
