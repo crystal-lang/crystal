d1edd97f2fbde46c0e8b1f84a70b671a743d6979
Carl Hörberg <carl@84codes.com>
2022-10-31 20:48:17 +0100

    Fix Channel#try_send spec in MT

--- spec/std/channel_spec.cr
+++ spec/std/channel_spec.cr
@@ -902,11 +902,8 @@ describe "buffered" do
 
   it "can send non blocking to buffered channel" do
     ch = Channel(Int32).new(1)
-    spawn do
-      ch.receive.should eq 1
-    end
-    Fiber.yield # required to let the receiving fiber start
     ch.try_send(1).should be_true
+    ch.receive.should eq 1
     ch.try_send(2).should be_true
     ch.try_send(3).should be_false
   end
