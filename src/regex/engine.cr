f2ad0159f18658bbf46f085d435c3656fe80d966
Johannes Müller <straightshoota@gmail.com>
2022-11-26 01:53:45 +0100

    Initial PCRE2 implementation

--- src/regex/engine.cr
+++ src/regex/engine.cr
@@ -1,4 +1,11 @@
-require "./pcre"
+{% if flag?(:force_pcre2) || (!flag?(:force_pcre) && `hash pkg-config 2> /dev/null && pkg-config --silence-errors --modversion libpcre2-8 || printf %s false` != "false") %}
+  require "./pcre2"
 
-# :nodoc:
-alias Regex::Engine = PCRE
+  # :nodoc:
+  alias Regex::Engine = PCRE2
+{% else %}
+  require "./pcre"
+
+  # :nodoc:
+  alias Regex::Engine = PCRE
+{% end %}
