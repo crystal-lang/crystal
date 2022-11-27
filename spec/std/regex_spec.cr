cba37beb1b107a813b616aa0498dd5b4d59c5f9c
Johannes Müller <straightshoota@gmail.com>
2022-11-27 14:21:39 +0100

    Re-order and structure `Regex` specs

--- spec/std/regex_spec.cr
+++ spec/std/regex_spec.cr
@@ -1,106 +1,80 @@
 require "./spec_helper"
 
 describe "Regex" do
-  it "compare to other instances" do
-    Regex.new("foo").should eq(Regex.new("foo"))
-    Regex.new("foo").should_not eq(Regex.new("bar"))
+  describe ".new" do
+    it "doesn't crash when PCRE tries to free some memory (#771)" do
+      expect_raises(ArgumentError) { Regex.new("foo)") }
+    end
+
+    it "raises exception with invalid regex" do
+      expect_raises(ArgumentError) { Regex.new("+") }
+    end
   end
 
-  it "does =~" do
-    (/foo/ =~ "bar foo baz").should eq(4)
-    $~.group_size.should eq(0)
+  describe "#match" do
+    it "returns matchdata" do
+      md = "Crystal".match(/(?<bar>.)#{/(?<foo>.)/}/).not_nil!
+      md[0].should eq("Cr")
+      md["bar"].should eq("C")
+      md["foo"].should eq("r")
+    end
+
+    it "matches unicode char against [[:alnum:]] (#4704)" do
+      /[[:alnum:]]/x.match("à").should_not be_nil
+    end
   end
 
-  it "does inspect" do
-    /foo/.inspect.should eq("/foo/")
-    /foo/.inspect.should eq("/foo/")
-    /foo/imx.inspect.should eq("/foo/imx")
+  describe "#===" do
+    it "assigns captures" do
+      "foo" =~ /foo/
+      (/f(o+)(bar?)/ === "fooba").should be_true
+      $~.group_size.should eq(2)
+      $1.should eq("oo")
+      $2.should eq("ba")
+    end
   end
 
-  it "does to_s" do
-    /foo/.to_s.should eq("(?-imsx:foo)")
-    /foo/im.to_s.should eq("(?ims-x:foo)")
-    /foo/imx.to_s.should eq("(?imsx-:foo)")
+  describe "#=~" do
+    it "returns match index" do
+      (/foo/ =~ "bar foo baz").should eq(4)
+      $~.group_size.should eq(0)
+    end
 
-    "Crystal".match(/(?<bar>C)#{/(?<foo>R)/i}/).should be_truthy
-    "Crystal".match(/(?<bar>C)#{/(?<foo>R)/}/i).should be_falsey
+    it "matches ignore case" do
+      ("HeLlO" =~ /hello/).should be_nil
+      ("HeLlO" =~ /hello/i).should eq(0)
+    end
 
-    md = "Crystal".match(/(?<bar>.)#{/(?<foo>.)/}/).not_nil!
-    md[0].should eq("Cr")
-    md["bar"].should eq("C")
-    md["foo"].should eq("r")
-  end
+    it "matches lines beginnings on ^ in multiline mode" do
+      ("foo\nbar" =~ /^bar/).should be_nil
+      ("foo\nbar" =~ /^bar/m).should eq(4)
+    end
 
-  it "does inspect with slash" do
-    %r(/).inspect.should eq("/\\//")
-    %r(\/).inspect.should eq("/\\//")
-  end
+    it "matches multiline" do
+      ("foo\n<bar\n>baz" =~ /<bar.*?>/).should be_nil
+      ("foo\n<bar\n>baz" =~ /<bar.*?>/m).should eq(4)
+    end
 
-  it "does to_s with slash" do
-    %r(/).to_s.should eq("(?-imsx:\\/)")
-    %r(\/).to_s.should eq("(?-imsx:\\/)")
-  end
+    it "matches unicode char against [[:print:]] (#11262)" do
+      ("\n☃" =~ /[[:print:]]/).should eq(1)
+    end
 
-  it "doesn't crash when PCRE tries to free some memory (#771)" do
-    expect_raises(ArgumentError) { Regex.new("foo)") }
-  end
+    it "matches with =~ and captures" do
+      ("fooba" =~ /f(o+)(bar?)/).should eq(0)
+      $~.group_size.should eq(2)
+      $1.should eq("oo")
+      $2.should eq("ba")
+    end
 
-  it "checks if Char need to be escaped" do
-    Regex.needs_escape?('*').should be_true
-    Regex.needs_escape?('|').should be_true
-    Regex.needs_escape?('@').should be_false
-  end
+    it "matches with =~ and gets utf-8 codepoint index" do
+      index = "こんに" =~ /ん/
+      index.should eq(1)
+    end
 
-  it "checks if String need to be escaped" do
-    Regex.needs_escape?("10$").should be_true
-    Regex.needs_escape?("foo").should be_false
-  end
-
-  it "escapes" do
-    Regex.escape(" .\\+*?[^]$(){}=!<>|:-hello").should eq("\\ \\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-hello")
-  end
-
-  it "matches ignore case" do
-    ("HeLlO" =~ /hello/).should be_nil
-    ("HeLlO" =~ /hello/i).should eq(0)
-  end
-
-  it "matches lines beginnings on ^ in multiline mode" do
-    ("foo\nbar" =~ /^bar/).should be_nil
-    ("foo\nbar" =~ /^bar/m).should eq(4)
-  end
-
-  it "matches multiline" do
-    ("foo\n<bar\n>baz" =~ /<bar.*?>/).should be_nil
-    ("foo\n<bar\n>baz" =~ /<bar.*?>/m).should eq(4)
-  end
-
-  it "matches unicode char against [[:print:]] (#11262)" do
-    ("\n☃" =~ /[[:print:]]/).should eq(1)
-  end
-
-  it "matches unicode char against [[:alnum:]] (#4704)" do
-    /[[:alnum:]]/x.match("à").should_not be_nil
-  end
-
-  it "matches with =~ and captures" do
-    ("fooba" =~ /f(o+)(bar?)/).should eq(0)
-    $~.group_size.should eq(2)
-    $1.should eq("oo")
-    $2.should eq("ba")
-  end
-
-  it "matches with =~ and gets utf-8 codepoint index" do
-    index = "こんに" =~ /ん/
-    index.should eq(1)
-  end
-
-  it "matches with === and captures" do
-    "foo" =~ /foo/
-    (/f(o+)(bar?)/ === "fooba").should be_true
-    $~.group_size.should eq(2)
-    $1.should eq("oo")
-    $2.should eq("ba")
+    it "raises if outside match range with []" do
+      "foo" =~ /foo/
+      expect_raises(IndexError) { $1 }
+    end
   end
 
   describe "#matches?" do
@@ -122,7 +96,7 @@ describe "Regex" do
     end
   end
 
-  describe "name_table" do
+  describe "#name_table" do
     it "is a map of capture group number to name" do
       table = (/(?<date> (?<year>(\d\d)?\d\d) - (?<month>\d\d) - (?<day>\d\d) )/x).name_table
       table[1].should eq("date")
@@ -133,22 +107,85 @@ describe "Regex" do
     end
   end
 
-  describe "capture_count" do
-    it "returns the number of (named & non-named) capture groups" do
-      /(?:.)/x.capture_count.should eq(0)
-      /(?<foo>.+)/.capture_count.should eq(1)
-      /(.)?/x.capture_count.should eq(1)
-      /(.)|(.)/x.capture_count.should eq(2)
+  it "#capture_count" do
+    /(?:.)/x.capture_count.should eq(0)
+    /(?<foo>.+)/.capture_count.should eq(1)
+    /(.)?/x.capture_count.should eq(1)
+    /(.)|(.)/x.capture_count.should eq(2)
+  end
+
+  describe "#inspect" do
+    it "with options" do
+      /foo/.inspect.should eq("/foo/")
+      /foo/.inspect.should eq("/foo/")
+      /foo/imx.inspect.should eq("/foo/imx")
+    end
+
+    it "with slash" do
+      %r(/).inspect.should eq("/\\//")
+      %r(\/).inspect.should eq("/\\//")
     end
   end
 
-  it "raises exception with invalid regex" do
-    expect_raises(ArgumentError) { Regex.new("+") }
+  describe "#to_s" do
+    it "with options" do
+      /foo/.to_s.should eq("(?-imsx:foo)")
+      /foo/im.to_s.should eq("(?ims-x:foo)")
+      /foo/imx.to_s.should eq("(?imsx-:foo)")
+    end
+
+    it "escapes" do
+      "Crystal".match(/(?<bar>C)#{/(?<foo>R)/i}/).should be_truthy
+      "Crystal".match(/(?<bar>C)#{/(?<foo>R)/}/i).should be_falsey
+    end
+
+    it "with slash" do
+      %r(/).to_s.should eq("(?-imsx:\\/)")
+      %r(\/).to_s.should eq("(?-imsx:\\/)")
+    end
   end
 
-  it "raises if outside match range with []" do
-    "foo" =~ /foo/
-    expect_raises(IndexError) { $1 }
+  it "#==" do
+    regex = Regex.new("foo", Regex::Options::IGNORE_CASE)
+    (regex == Regex.new("foo", Regex::Options::IGNORE_CASE)).should be_true
+    (regex == Regex.new("foo")).should be_false
+    (regex == Regex.new("bar", Regex::Options::IGNORE_CASE)).should be_false
+    (regex == Regex.new("bar")).should be_false
+  end
+
+  it "#hash" do
+    hash = Regex.new("foo", Regex::Options::IGNORE_CASE).hash
+    hash.should eq(Regex.new("foo", Regex::Options::IGNORE_CASE).hash)
+    hash.should_not eq(Regex.new("foo").hash)
+    hash.should_not eq(Regex.new("bar", Regex::Options::IGNORE_CASE).hash)
+    hash.should_not eq(Regex.new("bar").hash)
+  end
+
+  it "#dup" do
+    regex = /foo/
+    regex.dup.should be(regex)
+  end
+
+  it "#clone" do
+    regex = /foo/
+    regex.clone.should be(regex)
+  end
+
+  describe ".needs_escape?" do
+    it "Char" do
+      Regex.needs_escape?('*').should be_true
+      Regex.needs_escape?('|').should be_true
+      Regex.needs_escape?('@').should be_false
+    end
+
+    it "String" do
+      Regex.needs_escape?("10$").should be_true
+      Regex.needs_escape?("foo").should be_false
+    end
+  end
+
+  it ".escape" do
+    Regex.escape(" .\\+*?[^]$(){}=!<>|:-hello").should eq("\\ \\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-hello")
   end
 
   describe ".union" do
@@ -187,31 +224,5 @@ describe "Regex" do
     it "combines Regex objects in the same way as Regex#+" do
       Regex.union(/skiing/i, /sledding/).should eq(/skiing/i + /sledding/)
     end
-  end
-
-  it "dups" do
-    regex = /foo/
-    regex.dup.should be(regex)
-  end
-
-  it "clones" do
-    regex = /foo/
-    regex.clone.should be(regex)
-  end
-
-  it "checks equality by ==" do
-    regex = Regex.new("foo", Regex::Options::IGNORE_CASE)
-    (regex == Regex.new("foo", Regex::Options::IGNORE_CASE)).should be_true
-    (regex == Regex.new("foo")).should be_false
-    (regex == Regex.new("bar", Regex::Options::IGNORE_CASE)).should be_false
-    (regex == Regex.new("bar")).should be_false
-  end
-
-  it "hashes" do
-    hash = Regex.new("foo", Regex::Options::IGNORE_CASE).hash
-    hash.should eq(Regex.new("foo", Regex::Options::IGNORE_CASE).hash)
-    hash.should_not eq(Regex.new("foo").hash)
-    hash.should_not eq(Regex.new("bar", Regex::Options::IGNORE_CASE).hash)
-    hash.should_not eq(Regex.new("bar").hash)
   end
 end
