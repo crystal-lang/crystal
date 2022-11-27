4571d1fda33b3c5f4d2a161ee1c39f9b9bc488f3
Johannes Müller <straightshoota@gmail.com>
2022-11-27 22:07:54 +0100

    Improve match specs

--- spec/std/regex_spec.cr
+++ spec/std/regex_spec.cr
@@ -35,84 +35,166 @@ describe "Regex" do
 
   describe "#match" do
     it "returns matchdata" do
-      md = "Crystal".match(/(?<bar>.)#{/(?<foo>.)/}/).not_nil!
-      md[0].should eq("Cr")
-      md["bar"].should eq("C")
-      md["foo"].should eq("r")
-    end
-
-    it "matches unicode char against [[:alnum:]] (#4704)" do
-      /[[:alnum:]]/x.match("à").should_not be_nil
-    end
-  end
-
-  describe "#===" do
-    it "basic" do
-      (/f(o+)(bar?)/ === "fooba").should be_true
-      (/f(o+)(bar?)/ === "pooba").should be_false
+      md = /(?<bar>.)(?<foo>.)/.match("Crystal").should_not be_nil
+      md[0].should eq "Cr"
+      md.captures.should eq [] of String
+      md.named_captures.should eq({"bar" => "C", "foo" => "r"})
     end
 
     it "assigns captures" do
-      (/f(o+)(bar?)/ === "fooba").should be_true
-      $~.group_size.should eq(2)
-      $1.should eq("oo")
-      $2.should eq("ba")
-    end
-  end
+      matchdata = /foo/.match("foo")
+      $~.should eq(matchdata)
 
-  describe "#=~" do
-    it "returns match index" do
-      (/foo/ =~ "bar foo baz").should eq(4)
-      $~.group_size.should eq(0)
+      /foo/.match("bar")
+      expect_raises(NilAssertionError) { $~ }
     end
 
-    it "ignore case" do
-      ("HeLlO" =~ /hello/).should be_nil
-      ("HeLlO" =~ /hello/i).should eq(0)
+    it "returns nil on non-match" do
+      /Crystal/.match("foo").should be_nil
     end
 
-    it "multiline anchor" do
-      ("foo\nbar" =~ /^bar/).should be_nil
-      ("foo\nbar" =~ /^bar/m).should eq(4)
+    describe "with pos" do
+      it "positive" do
+        /foo/.match("foo", 0).should_not be_nil
+        /foo/.match("foo", 1).should be_nil
+        /foo/.match(".foo", 1).should_not be_nil
+        /foo/.match("..foo", 1).should_not be_nil
+
+        /foo/.match("bar", 0).should be_nil
+        /foo/.match("bar", 1).should be_nil
+      end
+
+      it "char index" do
+        /foo/.match("öfoo", 1).should_not be_nil
+      end
+
+      pending "negative" do
+        /foo/.match("..foo", -3).should_not be_nil
+        /foo/.match("..foo", -2).should be_nil
+      end
     end
 
-    it "multiline span" do
-      ("foo\n<bar\n>baz" =~ /<bar.*?>/).should be_nil
-      ("foo\n<bar\n>baz" =~ /<bar.*?>/m).should eq(4)
-    end
-
-    it "matches unicode char against [[:print:]] (#11262)" do
-      ("\n☃" =~ /[[:print:]]/).should eq(1)
-    end
-
-    it "assigns captures" do
-      ("fooba" =~ /f(o+)(bar?)/).should eq(0)
-      $~.group_size.should eq(2)
-      $1.should eq("oo")
-      $2.should eq("ba")
-    end
-
-    it "utf-8 support" do
-      ("こんに" =~ /ん/).should eq(1)
-    end
-
-    it "raises if outside match range with []" do
-      "foo" =~ /foo/
-      expect_raises(IndexError) { $1 }
+    it "with options" do
+      /foo/.match(".foo", options: Regex::Options::ANCHORED).should be_nil
     end
   end
 
   describe "#match_at_byte_index" do
+    it "assigns captures" do
+      matchdata = /foo/.match_at_byte_index("..foo", 1)
+      $~.should eq(matchdata)
+
+      /foo/.match_at_byte_index("foo", 1)
+      expect_raises(NilAssertionError) { $~ }
+
+      /foo/.match("foo") # make sure $~ is assigned
+      $~.should_not be_nil
+
+      /foo/.match_at_byte_index("foo", 5)
+      expect_raises(NilAssertionError) { $~ }
+    end
+
+    it "positive index" do
+      md = /foo/.match_at_byte_index("foo", 0).should_not be_nil
+      md.begin.should eq 0
+      /foo/.match_at_byte_index("foo", 1).should be_nil
+      md = /foo/.match_at_byte_index(".foo", 1).should_not be_nil
+      md.begin.should eq 1
+      md = /foo/.match_at_byte_index("..foo", 1).should_not be_nil
+      md.begin.should eq 2
+      /foo/.match_at_byte_index("foo", 5).should be_nil
+
+      /foo/.match_at_byte_index("bar", 0).should be_nil
+      /foo/.match_at_byte_index("bar", 1).should be_nil
+    end
+
+    it "multibyte index" do
+      md = /foo/.match_at_byte_index("öfoo", 1).should_not be_nil
+      md.begin.should eq 1
+      md.byte_begin.should eq 2
+
+      md = /foo/.match_at_byte_index("öfoo", 2).should_not be_nil
+      md.begin.should eq 1
+      md.byte_begin.should eq 2
+    end
+
+    pending "negative" do
+      md = /foo/.match_at_byte_index("..foo", -3).should_not be_nil
+      md.begin.should eq 0
+      /foo/.match_at_byte_index("..foo", -2).should be_nil
+    end
+
+    it "with options" do
+      /foo/.match_at_byte_index("..foo", 1, options: Regex::Options::ANCHORED).should be_nil
+    end
   end
 
   describe "#matches?" do
-    it "matches but create no MatchData" do
-      /f(o+)(bar?)/.matches?("fooba").should be_true
-      /f(o+)(bar?)/.matches?("barfo").should be_false
+    it "basic" do
+      /foo/.matches?("foo").should be_true
+      expect_raises(NilAssertionError) { $~ }
+      /foo/.matches?("bar").should be_false
+      expect_raises(NilAssertionError) { $~ }
     end
 
-    it "can specify initial position of matching" do
-      /f(o+)(bar?)/.matches?("fooba", 1).should be_false
+    describe "options" do
+      it "ignore case" do
+        /hello/.matches?("HeLlO").should be_false
+        /hello/i.matches?("HeLlO").should be_true
+      end
+
+      describe "multiline" do
+        it "anchor" do
+          /^bar/.matches?("foo\nbar").should be_false
+          /^bar/m.matches?("foo\nbar").should be_true
+        end
+
+        it "span" do
+          /<bar.*?>/.matches?("foo\n<bar\n>baz").should be_false
+          /<bar.*?>/m.matches?("foo\n<bar\n>baz").should be_true
+        end
+      end
+
+      describe "extended" do
+        it "ignores white space" do
+          /foo   bar/.matches?("foobar").should be_false
+          /foo   bar/x.matches?("foobar").should be_true
+        end
+
+        it "ignores comments" do
+          /foo#comment\nbar/.matches?("foobar").should be_false
+          /foo#comment\nbar/x.matches?("foobar").should be_true
+        end
+      end
+
+      it "anchored" do
+        Regex.new("foo", Regex::Options::ANCHORED).matches?("foo").should be_true
+        Regex.new("foo", Regex::Options::ANCHORED).matches?(".foo").should be_false
+      end
+    end
+
+    describe "unicode" do
+      it "unicode support" do
+        /ん/.matches?("こんに").should be_true
+      end
+
+      it "matches unicode char against [[:alnum:]] (#4704)" do
+        /[[:alnum:]]/.matches?("à").should be_true
+      end
+
+      it "matches unicode char against [[:print:]] (#11262)" do
+        /[[:print:]]/.matches?("\n☃").should be_true
+      end
+
+      it "invalid codepoint" do
+        /foo/.matches?("f\x96o").should be_false
+        /f\x96o/.matches?("f\x96o").should be_false
+        /f.o/.matches?("f\x96o").should be_true
+      end
+    end
+
+    it "with options" do
+      /foo/.matches?(".foo", options: Regex::Options::ANCHORED).should be_false
     end
 
     it "matches a large single line string" do
@@ -125,6 +207,70 @@ describe "Regex" do
   end
 
   describe "#matches_at_byte_index?" do
+    it "positive index" do
+      /foo/.matches_at_byte_index?("foo", 0).should be_true
+      /foo/.matches_at_byte_index?("foo", 1).should be_false
+      /foo/.matches_at_byte_index?(".foo", 1).should be_true
+      /foo/.matches_at_byte_index?("..foo", 1).should be_true
+      /foo/.matches_at_byte_index?("foo", 5).should be_false
+
+      /foo/.matches_at_byte_index?("bar", 0).should be_false
+      /foo/.matches_at_byte_index?("bar", 1).should be_false
+    end
+
+    it "multibyte index" do
+      /foo/.matches_at_byte_index?("öfoo", 1).should be_true
+    end
+
+    pending "negative" do
+      /foo/.matches_at_byte_index?("..foo", -3).should be_true
+      /foo/.matches_at_byte_index?("..foo", -2).should be_false
+    end
+
+    it "with options" do
+      /foo/.matches_at_byte_index?("..foo", 1, options: Regex::Options::ANCHORED).should be_false
+    end
+  end
+
+  describe "#===" do
+    it "basic" do
+      (/f(o+)(bar?)/ === "fooba").should be_true
+      (/f(o+)(bar?)/ === "pooba").should be_false
+    end
+
+    it "assigns captures" do
+      /f(o+)(bar?)/ === "fooba"
+      $~.group_size.should eq(2)
+      $1.should eq("oo")
+      $2.should eq("ba")
+
+      /f(o+)(bar?)/ === "pooba"
+      expect_raises(NilAssertionError) { $~ }
+    end
+  end
+
+  describe "#=~" do
+    it "returns match index or nil" do
+      (/foo/ =~ "bar foo baz").should eq(4)
+      (/foo/ =~ "bar boo baz").should be_nil
+    end
+
+    it "assigns captures" do
+      "fooba" =~ /f(o+)(bar?)/
+      $~.group_size.should eq(2)
+      $1.should eq("oo")
+      $2.should eq("ba")
+
+      /foo/ =~ "bar boo baz"
+      expect_raises(NilAssertionError) { $~ }
+    end
+
+    it "accepts any type" do
+      (/foo/ =~ nil).should be_nil
+      (/foo/ =~ 1).should be_nil
+      (/foo/ =~ [1, 2]).should be_nil
+      (/foo/ =~ true).should be_nil
+    end
   end
 
   describe "#name_table" do
