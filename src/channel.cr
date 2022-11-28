4290b79dd957aec71b8112c2a0d1e2bb177c3e95
Carl Hörberg <carl.hoerberg@gmail.com>
2022-11-28 15:50:13 +0100

    better docs
    Co-authored-by: Johannes Müller <straightshoota@gmail.com>

--- src/channel.cr
+++ src/channel.cr
@@ -251,7 +251,7 @@ class Channel(T)
   # Otherwise it returns `false` without blocking, no value will be deliviered.
   #
   # Returns `true` if the value could be deliviered immediately, `false` otherwise.
-  # Raises `ClosedError` if the channel is closed or closes while waiting on a full channel.
+  # Raises `ClosedError` if the channel is closed.
   def try_send(value : T) : Bool
     @lock.lock
     case send_internal(value)
