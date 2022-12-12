3f4bf523229e5e04e7c9f526f29b95900084b379
Mike Robbins <mrobbins@alum.mit.edu>
2022-12-11 20:54:10 -0500

    Automatically cast Int to Float for [JSON,YAML]::Any#as_f

--- src/json/any.cr
+++ src/json/any.cr
@@ -186,28 +186,50 @@ struct JSON::Any
     as_i64 if @raw.is_a?(Int64)
   end
 
-  # Checks that the underlying value is `Float`, and returns its value as an `Float64`.
+  # Checks that the underlying value is `Float` (or `Int`), and returns its value as an `Float64`.
   # Raises otherwise.
   def as_f : Float64
-    @raw.as(Float64)
+    case raw = @raw
+    when Int
+      raw.to_f
+    else
+      raw.as(Float64)
+    end
   end
 
-  # Checks that the underlying value is `Float`, and returns its value as an `Float64`.
+  # Checks that the underlying value is `Float` (or `Int`), and returns its value as an `Float64`.
   # Returns `nil` otherwise.
   def as_f? : Float64?
-    @raw.as?(Float64)
+    case raw = @raw
+    when Int
+      raw.to_f
+    else
+      raw.as?(Float64)
+    end
   end
 
-  # Checks that the underlying value is `Float`, and returns its value as an `Float32`.
+  # Checks that the underlying value is `Float` (or `Int`), and returns its value as an `Float32`.
   # Raises otherwise.
   def as_f32 : Float32
-    @raw.as(Float).to_f32
+    case raw = @raw
+    when Int
+      raw.to_f32
+    else
+      raw.as(Float).to_f32
+    end
   end
 
-  # Checks that the underlying value is `Float`, and returns its value as an `Float32`.
+  # Checks that the underlying value is `Float` (or `Int`), and returns its value as an `Float32`.
   # Returns `nil` otherwise.
   def as_f32? : Float32?
-    as_f32 if @raw.is_a?(Float)
+    case raw = @raw
+    when Int
+      raw.to_f32
+    when Float
+      raw.to_f32
+    else
+      nil
+    end
   end
 
   # Checks that the underlying value is `String`, and returns its value.
