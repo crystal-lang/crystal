c05049de9db682aa37ac744056c8bf1511be843c
David Keller <davidkeller@tuta.io>
2022-12-15 18:55:16 +0100

    Remove oct/bin floating point literals (#12687)

--- spec/compiler/lexer/lexer_spec.cr
+++ spec/compiler/lexer/lexer_spec.cr
@@ -362,6 +362,9 @@ describe "Lexer" do
   assert_syntax_error "0o200_i8", "0o200 doesn't fit in an Int8"
   assert_syntax_error "0b10000000_i8", "0b10000000 doesn't fit in an Int8"
 
+  assert_syntax_error "0b11_f32", "binary float literal is not supported"
+  assert_syntax_error "0o73_f64", "octal float literal is not supported"
+
   # 2**31 - 1
   it_lexes_i32 [["0x7fffffff", "2147483647"], ["0o17777777777", "2147483647"], ["0b1111111111111111111111111111111", "2147483647"]]
   it_lexes_i32 [["0x7fffffff_i32", "2147483647"], ["0o17777777777_i32", "2147483647"], ["0b1111111111111111111111111111111_i32", "2147483647"]]
