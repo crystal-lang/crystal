a5373ade7157f940f21383e22a74656b6d264d89
Johannes Müller <straightshoota@gmail.com>
2022-12-16 12:07:39 +0100

    Implement `Regex` engine on PCRE2 (#12840)

--- src/regex/engine.cr
+++ src/regex/engine.cr
@@ -1,4 +1,11 @@
-require "./pcre"
+{% if flag?(:use_pcre2) || (!flag?(:use_pcre) && !flag?(:win32) && `hash pkg-config 2> /dev/null && pkg-config --silence-errors --modversion libpcre2-8 || printf %s false` != "false") %}
+  require "./pcre2"
 
-# :nodoc:
-alias Regex::Engine = PCRE
+  # :nodoc:
+  alias Regex::Engine = PCRE2
+{% else %}
+  require "./pcre"
+
+  # :nodoc:
+  alias Regex::Engine = PCRE
+{% end %}
