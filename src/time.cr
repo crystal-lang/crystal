7d5191f47c9290ad80e72fef80a72be24c9b093c
maiha <maiha@wota.jp>
2022-12-25 10:47:29 +0900

    Update src/time.cr

--- src/time.cr
+++ src/time.cr
@@ -84,7 +84,7 @@ require "crystal/system/time"
 #
 # ```
 # time = Time.local(2018, 3, 8, 22, 5, 13, location: Time::Location.load("Europe/Berlin"))
-# time.to_s     # => "2018-03-08 22:05:13 +01:00"
+# time          # => 2018-03-08 22:05:13 +01:00 Europe/Berlin
 # time.location # => #<Time::Location Europe/Berlin>
 # time.zone     # => #<Time::Location::Zone CET +01:00 (3600s) STD>
 # time.offset   # => 3600
