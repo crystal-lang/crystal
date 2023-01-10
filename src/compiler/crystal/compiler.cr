7e61b74f9c59ffdd6ce8db9c28863eac8e911824
Johannes Müller <straightshoota@gmail.com>
2023-01-10 21:00:52 +0100

    Use system fork in compiler

--- src/compiler/crystal/compiler.cr
+++ src/compiler/crystal/compiler.cr
@@ -453,7 +453,9 @@ module Crystal
         return all_reused
       end
 
-      {% if flag?(:preview_mt) %}
+      {% if !Crystal::System::Process.class.has_method?("fork") %}
+        raise "Cannot fork compiler. `Crystal::System::Process.fork` is not implemented on this system."
+      {% elsif flag?(:preview_mt) %}
         raise "Cannot fork compiler in multithread mode"
       {% else %}
         jobs_count = 0
@@ -477,7 +479,7 @@ module Crystal
               end
             end
 
-            codegen_process = Process.fork do
+            codegen_process = Crystal::System::Process.fork do
               pipe_w = pw
               slice.each do |unit|
                 unit.compile
