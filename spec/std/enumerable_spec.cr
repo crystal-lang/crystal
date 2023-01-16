7518bf20fd608dcf07d557f3664a1c1e7f53ac49
Johannes Müller <straightshoota@gmail.com>
2023-01-16 15:57:21 +0100

    Fix example group name

--- spec/std/enumerable_spec.cr
+++ spec/std/enumerable_spec.cr
@@ -89,7 +89,7 @@ describe "Enumerable" do
     end
   end
 
-  describe "compact" do
+  describe "#compact" do
     it "can be applied to an Array" do
       [nil, 1, nil, 2, nil, 3, nil].compact.should eq([1, 2, 3])
       [1, 2, 3].compact.should eq([1, 2, 3])
