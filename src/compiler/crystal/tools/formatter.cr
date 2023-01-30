100ac09d929416f7d0de0cedd19f48bb281aade7
Carl <meatballsofmeatballs@gmail.com>
2023-01-30 09:28:00 +0100

    Removed print statement

--- src/compiler/crystal/tools/formatter.cr
+++ src/compiler/crystal/tools/formatter.cr
@@ -462,7 +462,6 @@ module Crystal
       index = 0
 
       while index < str.size
-        p str.char_at(index)
         if escape && !valid_escape_sequences.includes?(str.char_at(index))
           str = str.delete_at(index - 1)
           escape = false
