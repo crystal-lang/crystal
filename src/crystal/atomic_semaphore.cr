f937208ff9ce4258d180ff5e7f34468bb129d241
Quinton Miller <nicetas.c@gmail.com>
2023-02-01 20:20:32 +0800

    format

--- src/crystal/atomic_semaphore.cr
+++ src/crystal/atomic_semaphore.cr
@@ -2,7 +2,7 @@
 class Crystal::AtomicSemaphore
   @m = Atomic(UInt32).new(0)
 
-  def wait : Nil
+  def wait(&) : Nil
     m = @m.get
     while m == 0 || !@m.compare_and_set(m, m &- 1).last
       yield
