713b42c2c4694f56d7f843b43960a71b342af9d8
Johannes Müller <straightshoota@gmail.com>
2023-02-02 15:35:19 +0100

    Replace multiple single-property JSON, YAML test classes by generic helper

--- spec/std/json/serializable_spec.cr
+++ spec/std/json/serializable_spec.cr
@@ -6,6 +6,12 @@ require "big/json"
 require "uuid"
 require "uuid/json"
 
+class JSONAttrValue(T)
+  include JSON::Serializable
+
+  property value : T
+end
+
 record JSONAttrPoint, x : Int32, y : Int32 do
   include JSON::Serializable
 end
@@ -82,30 +88,6 @@ class JSONAttrPersonEmittingNullsByOptions
   property value2 : Int32?
 end
 
-class JSONAttrWithBool
-  include JSON::Serializable
-
-  property value : Bool
-end
-
-class JSONAttrWithFloat
-  include JSON::Serializable
-
-  property value : Float64
-end
-
-class JSONAttrWithUUID
-  include JSON::Serializable
-
-  property value : UUID
-end
-
-class JSONAttrWithBigDecimal
-  include JSON::Serializable
-
-  property value : BigDecimal
-end
-
 class JSONAttrWithTime
   include JSON::Serializable
 
@@ -175,12 +157,6 @@ class JSONAttrWithTimeHash3
   property value : Hash(String, Time)
 end
 
-class JSONAttrWithPropertiesKey
-  include JSON::Serializable
-
-  property properties : Hash(String, String)
-end
-
 class JSONAttrWithSimpleMapping
   include JSON::Serializable
 
@@ -207,12 +183,6 @@ class JSONAttrWithProblematicKeys
 
   property key : Int32
   property pull : Int32
-end
-
-class JSONAttrWithSet
-  include JSON::Serializable
-
-  property set : Set(String)
 end
 
 class JSONAttrWithDefaults
@@ -275,12 +245,6 @@ class JSONAttrWithNilableRootEmitNull
 
   @[JSON::Field(root: "heroes", emit_null: true)]
   property result : Array(JSONAttrPerson)?
-end
-
-class JSONAttrWithNilableUnion
-  include JSON::Serializable
-
-  property value : Int32?
 end
 
 class JSONAttrWithPresence
@@ -627,18 +591,18 @@ describe "JSON mapping" do
   end
 
   it "doesn't raises on false value when not-nil" do
-    json = JSONAttrWithBool.from_json(%({"value": false}))
+    json = JSONAttrValue(Bool).from_json(%({"value": false}))
     json.value.should be_false
   end
 
   it "parses JSON integer into a float property (#8618)" do
-    json = JSONAttrWithFloat.from_json(%({"value": 123}))
+    json = JSONAttrValue(Float64).from_json(%({"value": 123}))
     json.value.should eq(123.0)
   end
 
   it "parses UUID" do
-    uuid = JSONAttrWithUUID.from_json(%({"value": "ba714f86-cac6-42c7-8956-bcf5105e1b81"}))
-    uuid.should be_a(JSONAttrWithUUID)
+    uuid = JSONAttrValue(UUID).from_json(%({"value": "ba714f86-cac6-42c7-8956-bcf5105e1b81"}))
+    uuid.should be_a(JSONAttrValue(UUID))
     uuid.value.should eq(UUID.new("ba714f86-cac6-42c7-8956-bcf5105e1b81"))
   end
 
@@ -672,11 +636,11 @@ describe "JSON mapping" do
     json.to_json.should eq(%({"value":null}))
   end
 
-  it "outputs JSON with properties key" do
+  it "outputs JSON with Hash" do
     input = {
-      properties: {"foo" => "bar"},
+      value: {"foo" => "bar"},
     }.to_json
-    json = JSONAttrWithPropertiesKey.from_json(input)
+    json = JSONAttrValue(Hash(String, String)).from_json(input)
     json.to_json.should eq(input)
   end
 
@@ -700,8 +664,8 @@ describe "JSON mapping" do
   end
 
   it "parses json array as set" do
-    json = JSONAttrWithSet.from_json(%({"set": ["a", "a", "b"]}))
-    json.set.should eq(Set(String){"a", "b"})
+    json = JSONAttrValue(Set(String)).from_json(%({"value": ["a", "a", "b"]}))
+    json.value.should eq(Set(String){"a", "b"})
   end
 
   it "allows small types of integer" do
@@ -896,15 +860,15 @@ describe "JSON mapping" do
   end
 
   it "parses nilable union" do
-    obj = JSONAttrWithNilableUnion.from_json(%({"value": 1}))
+    obj = JSONAttrValue(Int32?).from_json(%({"value": 1}))
     obj.value.should eq(1)
     obj.to_json.should eq(%({"value":1}))
 
-    obj = JSONAttrWithNilableUnion.from_json(%({"value": null}))
+    obj = JSONAttrValue(Int32?).from_json(%({"value": null}))
     obj.value.should be_nil
     obj.to_json.should eq(%({}))
 
-    obj = JSONAttrWithNilableUnion.from_json(%({}))
+    obj = JSONAttrValue(Int32?).from_json(%({}))
     obj.value.should be_nil
     obj.to_json.should eq(%({}))
   end
@@ -1005,22 +969,22 @@ describe "JSON mapping" do
 
   describe "BigDecimal" do
     it "parses json string with BigDecimal" do
-      json = JSONAttrWithBigDecimal.from_json(%({"value": "10.05"}))
+      json = JSONAttrValue(BigDecimal).from_json(%({"value": "10.05"}))
       json.value.should eq(BigDecimal.new("10.05"))
     end
 
     it "parses large json ints with BigDecimal" do
-      json = JSONAttrWithBigDecimal.from_json(%({"value": 9223372036854775808}))
+      json = JSONAttrValue(BigDecimal).from_json(%({"value": 9223372036854775808}))
       json.value.should eq(BigDecimal.new("9223372036854775808"))
     end
 
     it "parses json float with BigDecimal" do
-      json = JSONAttrWithBigDecimal.from_json(%({"value": 10.05}))
+      json = JSONAttrValue(BigDecimal).from_json(%({"value": 10.05}))
       json.value.should eq(BigDecimal.new("10.05"))
     end
 
     it "parses large precision json floats with BigDecimal" do
-      json = JSONAttrWithBigDecimal.from_json(%({"value": 0.00045808999999999997}))
+      json = JSONAttrValue(BigDecimal).from_json(%({"value": 0.00045808999999999997}))
       json.value.should eq(BigDecimal.new("0.00045808999999999997"))
     end
   end
