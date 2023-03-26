8b1ea300d0c54f48cb6071f04ef70b9e746a8107
Johannes Müller <straightshoota@gmail.com>
2023-03-26 15:37:33 +0200

    Suppress compiler output in `compile_file` spec helper

--- spec/std/spec_helper.cr
+++ spec/std/spec_helper.cr
@@ -78,11 +78,18 @@ end
 def compile_file(source_file, *, bin_name = "executable_file", flags = %w(), file = __FILE__, &)
   with_temp_executable(bin_name, file: file) do |executable_file|
     compiler = ENV["CRYSTAL_SPEC_COMPILER_BIN"]? || "bin/crystal"
-    Process.run(compiler, ["build"] + flags + ["-o", executable_file, source_file], env: {
+    args = ["build"] + flags + ["-o", executable_file, source_file]
+    output = IO::Memory.new
+    status = Process.run(compiler, args, env: {
       "CRYSTAL_PATH"         => Crystal::PATH,
       "CRYSTAL_LIBRARY_PATH" => Crystal::LIBRARY_PATH,
       "CRYSTAL_CACHE_DIR"    => Crystal::CACHE_DIR,
-    }, error: Process::Redirect::Inherit)
+    }, output: output, error: output)
+
+    unless status.success?
+      fail "Compiler command `#{compiler} #{args.join(" ")}` failed with status #{status}.#{"\n" if output}#{output}"
+    end
+
     File.exists?(executable_file).should be_true
 
     yield executable_file
