23f94362dc6b2aa76fa1f0d3e3b08a42cb819b27
Johannes Müller <straightshoota@gmail.com>
2023-03-28 15:54:39 +0200

    Revert raising on unsupported flag value for PCRE

--- src/regex/pcre.cr
+++ src/regex/pcre.cr
@@ -44,14 +44,8 @@ module Regex::PCRE
       end
     end
 
-    unless options.none?
-      {% if flag?(:pcre_allow_all_options) %}
-        # Unnamed values are explicitly used PCRE options, just pass them through:
-        flag |= options.value
-      {% else %}
-        raise ArgumentError.new("Unknown Regex::Option value: #{options}")
-      {% end %}
-    end
+    # Unnamed values are explicitly used PCRE options, just pass them through:
+    flag |= options.value
 
     flag
   end
