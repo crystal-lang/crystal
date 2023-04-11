59303c98b151f160a6df2dec95856a46d44f5149
Quinton Miller <nicetas.c@gmail.com>
2023-04-12 05:10:22 +0800

    remove fixme

--- src/compiler/crystal/codegen/debug.cr
+++ src/compiler/crystal/codegen/debug.cr
@@ -288,9 +288,6 @@ module Crystal
           offset = @program.target_machine.data_layout.offset_of_element(struct_type, idx &+ (type.struct? ? 0 : 1))
           size = @program.target_machine.data_layout.size_in_bits(llvm_embedded_type(ivar_type))
 
-          # FIXME structs like LibC::PthreadMutexT generate huge offset values
-          next if offset > UInt64::MAX // 8u64
-
           member = di_builder.create_member_type(nil, ivar.name, nil, 1, size, size, 8u64 * offset, LLVM::DIFlags::Zero, ivar_debug_type)
           element_types << member
         end
