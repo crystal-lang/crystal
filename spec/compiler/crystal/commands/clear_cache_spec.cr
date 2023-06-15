4023afb470352a6bc6e4bf6484be80a8969b2e44
Philip Ross <dev@baseballlover723.com>
2023-06-14 23:13:58 -0700

    use a seperate CacheDir for testing clear_cache

--- spec/compiler/crystal/commands/clear_cache_spec.cr
+++ spec/compiler/crystal/commands/clear_cache_spec.cr
@@ -1,7 +1,27 @@
 require "../../../spec_helper"
 
+class Crystal::CacheDir
+  class_setter instance
+
+  def initialize(@dir)
+    Dir.mkdir_p(dir)
+  end
+end
+
 describe Crystal::Command do
   describe "clear_cache" do
+    around_each do |example|
+      old_cache_dir = CacheDir.instance
+      temp_dir_name = File.tempname
+      begin
+        CacheDir.instance = CacheDir.new(temp_dir_name)
+        example.run
+      ensure
+        FileUtils.rm_rf(temp_dir_name)
+        CacheDir.instance = old_cache_dir
+      end
+    end
+
     it "clears any cached compiler files" do
       file_path = File.tempname(dir: CacheDir.instance.dir)
       Dir.mkdir_p(File.dirname(file_path))
