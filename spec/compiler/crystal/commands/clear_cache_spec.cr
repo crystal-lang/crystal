5f992db32a942cd0283f74f9be2ad48f2cec2dcd
Philip Ross <dev@baseballlover723.com>
2023-06-14 23:13:11 -0700

    use tempname and assert that the CacheDir doesn't exist

--- spec/compiler/crystal/commands/clear_cache_spec.cr
+++ spec/compiler/crystal/commands/clear_cache_spec.cr
@@ -3,7 +3,7 @@ require "../../../spec_helper"
 describe Crystal::Command do
   describe "clear_cache" do
     it "clears any cached compiler files" do
-      file_path = File.join(CacheDir.instance.dir, "test_cache_file")
+      file_path = File.tempname(dir: CacheDir.instance.dir)
       Dir.mkdir_p(File.dirname(file_path))
       File.touch(file_path)
       File.exists?(file_path).should be_true
@@ -11,6 +11,7 @@ describe Crystal::Command do
       Crystal::Command.run(["clear_cache"])
 
       File.exists?(file_path).should be_false
+      File.exists?(CacheDir.instance.dir).should be_false
     end
   end
 end
