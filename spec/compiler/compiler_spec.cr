a193b19acbac916096f699815ce0f5f95fe9a014
Robert Schulze <robert@dotless.de>
2023-07-13 17:56:29 +0200

    --static part is unnecessary for this test

--- spec/compiler/compiler_spec.cr
+++ spec/compiler/compiler_spec.cr
@@ -28,24 +28,22 @@ describe "Compiler" do
     end
   end
 
-  {% if !flag?(:windows) && !flag?(:darwin) %}
-    it "outputs correct cc command in preference to a filename when static cross compile" do
-      Dir.cd compiler_datapath do
-        with_temp_executable "compiler_spec_output" do |path|
-          FileUtils.mkdir_p("#{path}/bin")
-          expected_bin = "#{path}/bin/compiler_sample-linux-x86_64"
-          output = IO::Memory.new
-          Process.run(
-            ENV["CRYSTAL_SPEC_COMPILER_BIN"]? || "bin/crystal",
-            ["build"].concat(program_flags_options).concat(["--static", "--cross-compile", "--target=x86_64-unknown-linux-gnu", "-o", expected_bin, "compiler_sample"]),
-            output: output
-          )
-          File.exists?("#{expected_bin}.o").should be_true
-          output.to_s.should contain("cc #{expected_bin}.o -o #{expected_bin} ")
-        end
+  it "outputs correct cc command in preference to a filename when cross compile" do
+    Dir.cd compiler_datapath do
+      with_temp_executable "compiler_spec_output" do |path|
+        FileUtils.mkdir_p("#{path}/bin")
+        expected_bin = "#{path}/bin/compiler_sample-linux-x86_64"
+        output = IO::Memory.new
+        Process.run(
+          ENV["CRYSTAL_SPEC_COMPILER_BIN"]? || "bin/crystal",
+          ["build"].concat(program_flags_options).concat(["--cross-compile", "--target=x86_64-unknown-linux-gnu", "-o", expected_bin, "compiler_sample"]),
+          output: output
+        )
+        File.exists?("#{expected_bin}.o").should be_true
+        output.to_s.should contain("cc #{expected_bin}.o -o #{expected_bin} ")
       end
     end
-  {% end %}
+  end
 
   it "treats all arguments post-filename as program arguments" do
     with_tempfile "args_test" do |path|
