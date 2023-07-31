705147c3bdc609e40bf921084568fb30e80f4775
Philip Ross <dev@baseballlover723.com>
2023-07-31 16:54:42 -0400

    added a new example for testing list_tags with filtering by a tag

--- spec/std/spec/list_tags_spec.cr
+++ spec/std/spec/list_tags_spec.cr
@@ -34,6 +34,8 @@ describe Spec do
         pending "untagged #7" do
         end
       
+        pending "slow #5", tags: "slow"
+
         describe "describe specs", tags: "describe" do
           it "describe #1" do
           end
@@ -53,6 +55,7 @@ describe Spec do
 
         pending tags:
         untagged: 2
+            slow: 1
         OUT
     end
 
@@ -125,6 +128,8 @@ describe Spec do
         pending "untagged #7" do
         end
       
+        pending "slow #5", tags: "slow"
+
         describe "describe specs", tags: "describe" do
           pending "describe #1"
           pending "describe #2"
@@ -134,9 +139,64 @@ describe Spec do
         CRYSTAL
         pending tags:
         untagged: 7
-            slow: 4
+            slow: 5
         describe: 4
           flakey: 3
+        OUT
+    end
+
+    it "lists the count of slow tags (including pending seperately)", tags: %w[slow] do
+      compile_and_run_source(<<-CRYSTAL, flags: %w(--no-debug), runtime_args: %w(--list-tags --tag slow))[1].lines.should eq <<-OUT.lines
+        require "spec"
+
+        it "untagged #1" do
+        end
+        it "untagged #2" do
+        end
+        it "untagged #3" do
+        end
+      
+        it "slow #1", tags: "slow" do
+        end
+        it "slow #2", tags: "slow" do
+        end
+      
+        it "untagged #4" do
+        end
+      
+        it "flakey #1", tags: "flakey" do
+        end
+        it "flakey #2, slow #3", tags: ["flakey", "slow"] do
+        end
+      
+        it "untagged #5" do
+        end
+      
+        pending "untagged #6"
+      
+        pending "untagged #7" do
+        end
+
+        pending "slow #5", tags: "slow"
+      
+        describe "describe specs", tags: "describe" do
+          it "describe #1" do
+          end
+          it "describe #2" do
+          end
+          it "describe #3, slow #4", tags: "slow" do
+          end
+          it "describe #4, flakey #3", tags: "flakey" do
+          end
+        end
+        CRYSTAL
+        tags:
+            slow: 4
+          flakey: 1
+        describe: 1
+
+        pending tags:
+        slow: 1
         OUT
     end
   end
