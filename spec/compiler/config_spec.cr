0cb1b3f19b0dfe75b2e9443d2837efe563797818
Quinton Miller <nicetas.c@gmail.com>
2023-10-31 19:03:06 +0800

    use `HOST_TRIPLE`

--- spec/compiler/config_spec.cr
+++ spec/compiler/config_spec.cr
@@ -4,10 +4,8 @@ require "./spec_helper"
 describe Crystal::Config do
   it ".host_target" do
     {% begin %}
-      # TODO: CRYSTAL_SPEC_COMPILER_BIN must be quoted (#11456)
-      {% compiler = (env("CRYSTAL_SPEC_COMPILER_BIN") || "bin/crystal").id %}
-      # TODO: check against Crystal::HOST_TRIPLE instead
-      Crystal::Config.host_target.should eq Crystal::Codegen::Target.new({{ `#{compiler} --version`.lines[-1] }}.lchop("Default target: "))
+      {% host_triple = Crystal.constant("HOST_TRIPLE") || Crystal::DESCRIPTION.lines[-1].gsub(/^Default target: /, "") %}
+      Crystal::Config.host_target.should eq Crystal::Codegen::Target.new({{ host_triple }})
     {% end %}
   end
 
