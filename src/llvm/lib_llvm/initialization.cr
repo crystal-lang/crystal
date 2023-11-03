460891dc6a412f84c69bba9b6e57cb5ee45ad943
Quinton Miller <nicetas.c@gmail.com>
2023-11-04 00:11:36 +0800

    `llvm-c/Initialization.h`

---
+++ src/llvm/lib_llvm/initialization.cr
@@ -0,0 +1,18 @@
+require "./types"
+
+lib LibLLVM
+  {% if LibLLVM::IS_LT_170 %}
+    fun initialize_core = LLVMInitializeCore(r : PassRegistryRef)
+    fun initialize_transform_utils = LLVMInitializeTransformUtils(r : PassRegistryRef)
+    fun initialize_scalar_opts = LLVMInitializeScalarOpts(r : PassRegistryRef)
+    fun initialize_obj_c_arc_opts = LLVMInitializeObjCARCOpts(r : PassRegistryRef)
+    fun initialize_vectorization = LLVMInitializeVectorization(r : PassRegistryRef)
+    fun initialize_inst_combine = LLVMInitializeInstCombine(r : PassRegistryRef)
+    fun initialize_ipo = LLVMInitializeIPO(r : PassRegistryRef)
+    fun initialize_instrumentation = LLVMInitializeInstrumentation(r : PassRegistryRef)
+    fun initialize_analysis = LLVMInitializeAnalysis(r : PassRegistryRef)
+    fun initialize_ipa = LLVMInitializeIPA(r : PassRegistryRef)
+    fun initialize_code_gen = LLVMInitializeCodeGen(r : PassRegistryRef)
+    fun initialize_target = LLVMInitializeTarget(r : PassRegistryRef)
+  {% end %}
+end
