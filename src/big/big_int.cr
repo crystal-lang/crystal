60c033478af89e4ccab3d9f8cbacbad789a20e46
Quinton Miller <nicetas.c@gmail.com>
2023-11-24 00:40:29 +0800

    Optimize `BigInt#&`, `#|`, `#^` with `Int::Primitive` arguments (#14006)

--- src/big/big_int.cr
+++ src/big/big_int.cr
@@ -385,16 +385,34 @@ struct BigInt < Int
     BigInt.new { |mpz| LibGMP.com(mpz, self) }
   end
 
+  def &(other : BigInt) : BigInt
+    BigInt.new { |mpz| LibGMP.and(mpz, self, other) }
+  end
+
   def &(other : Int) : BigInt
-    BigInt.new { |mpz| LibGMP.and(mpz, self, other.to_big_i) }
+    ret = other.to_big_i
+    LibGMP.and(ret, ret, self)
+    ret
+  end
+
+  def |(other : BigInt) : BigInt
+    BigInt.new { |mpz| LibGMP.ior(mpz, self, other) }
   end
 
   def |(other : Int) : BigInt
-    BigInt.new { |mpz| LibGMP.ior(mpz, self, other.to_big_i) }
+    ret = other.to_big_i
+    LibGMP.ior(ret, ret, self)
+    ret
+  end
+
+  def ^(other : BigInt) : BigInt
+    BigInt.new { |mpz| LibGMP.xor(mpz, self, other) }
   end
 
   def ^(other : Int) : BigInt
-    BigInt.new { |mpz| LibGMP.xor(mpz, self, other.to_big_i) }
+    ret = other.to_big_i
+    LibGMP.xor(ret, ret, self)
+    ret
   end
 
   def >>(other : Int) : BigInt
