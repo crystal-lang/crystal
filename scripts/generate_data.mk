61adb0e30d015ba0b74efeb0781e96376e5ae8a1
Johannes Müller <straightshoota@gmail.com>
2023-11-23 21:24:49 +0100

    Ensure Makefile is portable

--- scripts/generate_data.mk
+++ scripts/generate_data.mk
@@ -4,33 +4,40 @@
 ## Run specific data generator
 ##   $ make -B -f scripts/generate_data.mk spec/std/string/graphemes_break_spec.cr
 
+ifeq ($(OS),Windows_NT)
+  BIN_CRYSTAL=bin\crystal
+else
+  BIN_CRYSTAL=bin/crystal
+endif
+
 all: ## Run all generators
 .PHONY: all
 
 all: spec/std/string/graphemes_break_spec.cr
 spec/std/string/graphemes_break_spec.cr: scripts/generate_grapheme_break_specs.cr
-	bin/crystal run $<
+	$(BIN_CRYSTAL) run $<
 
 all: src/string/grapheme/properties.cr
 src/string/grapheme/properties.cr: scripts/generate_grapheme_properties.cr
-	bin/crystal run $<
+	$(BIN_CRYSTAL) run $<
 
 all: src/openssl/ssl/defaults.cr
 src/openssl/ssl/defaults.cr: scripts/generate_ssl_server_defaults.cr
-	bin/crystal run $<
+	$(BIN_CRYSTAL) run $<
 
 all: src/unicode/data.cr
 src/unicode/data.cr: scripts/generate_unicode_data.cr
-	bin/crystal run $<
+	$(BIN_CRYSTAL) run $<
 
 all: src/crystal/system/win32/zone_names.cr
 src/crystal/system/win32/zone_names.cr: scripts/generate_windows_zone_names.cr
-	bin/crystal run $<
+	$(BIN_CRYSTAL) run $<
 
 all: src/html/entities.cr
 src/html/entities.cr: scripts/generate_html_entities.cr scripts/html_entities.ecr
-	bin/crystal run $<
+	$(BIN_CRYSTAL) run $<
 
+ifneq ($(OS),Windows_NT)
 .PHONY: help
 help: ## Show this help
 	@echo
@@ -47,3 +54,4 @@ help: ## Show this help
 	@printf '\033[34mrecipes:\033[0m\n'
 	@grep -hE '^##.*$$' $(MAKEFILE_LIST) |\
 		awk 'BEGIN {FS = "## "}; /^## [a-zA-Z_-]/ {printf "  \033[36m%s\033[0m\n", $$2}; /^##  / {printf "  %s\n", $$2}'
+endif
