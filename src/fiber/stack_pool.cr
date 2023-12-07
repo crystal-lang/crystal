ea8a3c1fe79f460e16831cf6791fb167a7a7a97c
Julien Portalier <julien@portalier.com>
2023-12-07 18:59:47 +0100

    MT: per scheduler stack pools (let's recycle)
    The stack pool was only used to create new stacks when MT was enabled.
    The stacks were then pushed to each scheduler's free stacks, and
    eventually dropped on reschedule (after context swap, so we're sure to
    only ever deallocated stacks that are no longer used). This led the
    stacks to never be reused with MT. Only created then deallocated.
    
    This patch changes the behavior to have a stack pool running on each
    scheduler, and to use it to create and collect the stacks, and reuse
    them when possible. It also drops the mutex since the stack pool can
    never be accessed in parallel (in fact it never was).
    
    Also starts a collecting fiber on each thread.
    
    It may only lead to better performance if there are different fibers,
    running on multiple threads that are spawning fibers. It won't have much
    (or any) impact if there is only one fiber spawning other fibers (e.g. a
    HTTP::Server) as the stack of fibers that run on another thread won't be
    reused (different stack pool).

--- src/fiber/stack_pool.cr
+++ src/fiber/stack_pool.cr
@@ -7,14 +7,13 @@ class Fiber
 
     def initialize
       @deque = Deque(Void*).new
-      @mutex = Thread::Mutex.new
     end
 
     # Removes and frees at most *count* stacks from the top of the pool,
     # returning memory to the operating system.
     def collect(count = lazy_size // 2) : Nil
       count.times do
-        if stack = @mutex.synchronize { @deque.shift? }
+        if stack = @deque.shift?
           Crystal::System::Fiber.free_stack(stack, STACK_SIZE)
         else
           return
@@ -22,21 +21,28 @@ class Fiber
       end
     end
 
+    def collect_loop(every = 5.seconds) : Nil
+      loop do
+        sleep every
+        collect
+      end
+    end
+
     # Removes a stack from the bottom of the pool, or allocates a new one.
     def checkout : {Void*, Void*}
-      stack = @mutex.synchronize { @deque.pop? } || Crystal::System::Fiber.allocate_stack(STACK_SIZE)
+      stack = @deque.pop? || Crystal::System::Fiber.allocate_stack(STACK_SIZE)
       {stack, stack + STACK_SIZE}
     end
 
     # Appends a stack to the bottom of the pool.
     def release(stack) : Nil
-      @mutex.synchronize { @deque.push(stack) }
+      @deque.push(stack)
     end
 
     # Returns the approximated size of the pool. It may be equal or slightly
     # bigger or smaller than the actual size.
     def lazy_size : Int32
-      @mutex.synchronize { @deque.size }
+      @deque.size
     end
   end
 end
