a3d36320146bb7fd11e08c500092f1de95b1e568
Johannes Müller <straightshoota@gmail.com>
2023-12-13 14:53:29 +0100

    Add workaround for Crystal < 1.1

--- src/tuple.cr
+++ src/tuple.cr
@@ -545,7 +545,11 @@ struct Tuple
   # {1, 2, 3, 4, 5}.to_a # => [1, 2, 3, 4, 5]
   # ```
   def to_a : Array(Union(*T))
-    to_a(&.itself)
+    {% if compare_versions(Crystal::VERSION, "1.1.0") < 0 %}
+      to_a(&.itself.as(Union(*T)))
+    {% else %}
+      to_a(&.itself)
+    {% end %}
   end
 
   # Returns an `Array` with the results of running *block* against each element of the tuple.
