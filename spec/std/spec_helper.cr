ff61dfbd0217379751c46c32f17f0b72697f69c2
Chao Yang <cyang05@outlook.com>
2023-12-18 17:49:10 -0600

    Print pwd

--- spec/std/spec_helper.cr
+++ spec/std/spec_helper.cr
@@ -113,6 +113,7 @@ def compile_and_run_file(source_file, flags = %w(), runtime_args = %w(), file = 
     status = 0
     {% if flag?(:interpreted) %}
       compiler = ENV["CRYSTAL_SPEC_COMPILER_BIN"]? || "bin/crystal"
+      puts ENV["PWD"]
       status = Process.run "#{compiler} i #{source_file}", args: runtime_args, output: output, error: error
     {% else %}
       status = Process.run executable_file, args: runtime_args, output: output, error: error
