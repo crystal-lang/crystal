fa55b108fc85b359c7c2a7f47bc342555cb8e0f7
Billy.Zheng <vil963@gmail.com>
2024-01-10 00:17:31 +0800

    Added an interpreter spec for check `crystal i` is running on linux.

--- spec/compiler/interpreter_spec.cr
+++ spec/compiler/interpreter_spec.cr
@@ -1,1 +1,11 @@
 require "./interpreter/*"
+
+describe "run interpreter" do
+  it "should running interpreter sucessful on linux" do
+    {% if flag?(:linux) %}
+      status_code = system("crystal i &>/dev/null <<< 'puts 1'")
+
+      status_code.should be_true
+    {% end %}
+  end
+end
