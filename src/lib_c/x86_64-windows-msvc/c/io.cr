ac6a61906b89be665ffda1d9f900e0d5561e7a28
Quinton Miller <nicetas.c@gmail.com>
2024-04-02 23:41:23 +0800

    fixup

--- src/lib_c/x86_64-windows-msvc/c/io.cr
+++ src/lib_c/x86_64-windows-msvc/c/io.cr
@@ -3,7 +3,6 @@ require "c/stdint"
 lib LibC
   fun _isatty(fd : Int) : Int
   fun _close(fd : Int) : Int
-  fun _waccess_s(path : WCHAR*, mode : Int) : ErrnoT
   fun _wexecvp(cmdname : WCHAR*, argv : WCHAR**) : IntPtrT
   fun _get_osfhandle(fd : Int) : IntPtrT
   fun _dup2(fd1 : Int, fd2 : Int) : Int
@@ -15,6 +14,7 @@ lib LibC
   fun _read(fd : Int, buffer : UInt8*, count : UInt) : Int
   fun _lseeki64(fd : Int, offset : Int64, origin : Int) : Int64
   fun _wopen(filename : WCHAR*, oflag : Int, ...) : Int
+  fun _waccess_s(path : WCHAR*, mode : Int) : ErrnoT
   fun _wchmod(filename : WCHAR*, pmode : Int) : Int
   fun _wunlink(filename : WCHAR*) : Int
   fun _wmktemp_s(template : WCHAR*, sizeInChars : SizeT) : ErrnoT
