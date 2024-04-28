b05efae02df215c0dcb40c72d3ad1a2e3d9666a4
Johannes Müller <straightshoota@gmail.com>
2024-04-28 21:38:55 +0200

    Add `src/SOURCE_DATE_EPOCH` to release tree

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -44,6 +44,11 @@ git switch $branch 2>/dev/null || git switch -c $branch;
 echo "${VERSION}" > src/VERSION
 git add src/VERSION
 
+# Write release date into src/SOURCE_DATE_EPOCH
+release_date=$(head -n1 $current_changelog | grep -o -P '(?<=\()[^)]+')
+echo "$(date --utc --date="${release_date}" +%s)" > src/SOURCE_DATE_EPOCH
+git add src/SOURCE_DATE_EPOCH
+
 if grep --silent -E "^## \[$VERSION\]" CHANGELOG.md; then
   echo "Replacing section in CHANGELOG"
 
