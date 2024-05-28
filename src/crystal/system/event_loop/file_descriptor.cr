7c2249043c38e45c34371f3b11b6dec5d6445b6a
Johannes Müller <straightshoota@gmail.com>
2024-05-28 23:19:06 +0200

    Improve doc comments for `EventLoop::FileDescriptor`

--- src/crystal/system/event_loop/file_descriptor.cr
+++ src/crystal/system/event_loop/file_descriptor.cr
@@ -1,13 +1,20 @@
 abstract class Crystal::EventLoop
   module FileDescriptor
-    # Reads at least one byte from the file descriptor into *slice* and continues
-    # fiber when the read is complete.
-    # Returns the number of bytes read.
+    # Reads at least one byte from the file descriptor into *slice*.
+    #
+    # Blocks the current fiber if no data is available for reading, continuing
+    # when available. Otherwise returns immediately.
+    #
+    # Returns the number of bytes read (up to `slice.size`).
+    # Returns 0 when EOF is reached.
     abstract def read(file_descriptor : Crystal::System::FileDescriptor, slice : Bytes) : Int32
 
-    # Writes at least one byte from *slice* to the file descriptor and continues
-    # fiber when the write is complete.
-    # Returns the number of bytes written.
+    # Writes at least one byte from *slice* to the file descriptor.
+    #
+    # Blocks the current fiber if the file descriptor isn't ready for writing,
+    # continuing when it is. Otherwise returns immediately.
+    #
+    # Returns the number of bytes written (up to `slice.size`).
     abstract def write(file_descriptor : Crystal::System::FileDescriptor, slice : Bytes) : Int32
 
     # Closes the file descriptor resource.
