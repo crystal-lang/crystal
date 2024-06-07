0277fd1329048d80fed760b5b9626c049ff2dc61
Julien Portalier <julien@portalier.com>
2024-06-07 12:19:13 +0200

    Specs to ensure CLOEXEC behavior

--- spec/std/socket/tcp_server_spec.cr
+++ spec/std/socket/tcp_server_spec.cr
@@ -136,4 +136,20 @@ describe TCPServer, tags: "network" do
       end
     end
   {% end %}
+
+  describe "accept" do
+    {% unless flag?(:win32) %}
+      it "sets close on exec flag" do
+        port = unused_local_port
+
+        TCPServer.open("::", port) do |server|
+          TCPSocket.open("::", port) do |client|
+            server.accept? do |sock|
+              sock.close_on_exec?.should be_true
+            end
+          end
+        end
+      end
+    {% end %}
+  end
 end
