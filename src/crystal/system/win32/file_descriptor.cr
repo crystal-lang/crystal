c6430879029eba74b955b94aca8ad1ff8a2a5cdb
Quinton Miller <nicetas.c@gmail.com>
2024-06-13 17:29:28 +0800

    Fix overflow in `File#read_at` for large offsets on Windows

--- src/crystal/system/win32/file_descriptor.cr
+++ src/crystal/system/win32/file_descriptor.cr
@@ -259,8 +259,8 @@ module Crystal::System::FileDescriptor
     handle = windows_handle(fd)
 
     overlapped = LibC::OVERLAPPED.new
-    overlapped.union.offset.offset = LibC::DWORD.new(offset)
-    overlapped.union.offset.offsetHigh = LibC::DWORD.new(offset >> 32)
+    overlapped.union.offset.offset = LibC::DWORD.new!(offset)
+    overlapped.union.offset.offsetHigh = LibC::DWORD.new!(offset >> 32)
     if LibC.ReadFile(handle, buffer, buffer.size, out bytes_read, pointerof(overlapped)) == 0
       error = WinError.value
       return 0_i64 if error == WinError::ERROR_HANDLE_EOF
