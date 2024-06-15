ca76f06cad89ff9433f9fa2120bf73261d10d822
George Dietrich <dev@dietrich.pub>
2024-06-15 10:25:23 -0400

    Apply method signature yield feature to rest of codebase

--- src/compiler/crystal/semantic/main_visitor.cr
+++ src/compiler/crystal/semantic/main_visitor.cr
@@ -2659,7 +2659,7 @@ module Crystal
       end
     end
 
-    private def visit_size_or_align_of(node)
+    private def visit_size_or_align_of(node, &)
       @in_type_args += 1
       node.exp.accept self
       @in_type_args -= 1
@@ -2685,7 +2685,7 @@ module Crystal
       false
     end
 
-    private def visit_instance_size_or_align_of(node)
+    private def visit_instance_size_or_align_of(node, &)
       @in_type_args += 1
       node.exp.accept self
       @in_type_args -= 1
