223621dc6843fafd7d251dfecc821f8aa351eee6
Quinton Miller <nicetas.c@gmail.com>
2024-06-19 10:18:40 +0800

    Make `ReferenceStorage(T)` non-atomic if `T` is non-atomic

---
+++ spec/primitives/pointer_spec.cr
@@ -0,0 +1,24 @@
+require "spec"
+require "../support/finalize"
+
+private class Inner
+  include FinalizeCounter
+
+  def initialize(@key : String)
+  end
+end
+
+private class Outer
+  @inner = Inner.new("reference-storage")
+end
+
+describe "Primitives: pointer" do
+  describe ".malloc" do
+    it "is non-atomic for ReferenceStorage(T) if T is non-atomic (#14692)" do
+      FinalizeState.reset
+      outer = Outer.unsafe_construct(Pointer(ReferenceStorage(Outer)).malloc(1))
+      GC.collect
+      FinalizeState.count("reference-storage").should eq(0)
+    end
+  end
+end
