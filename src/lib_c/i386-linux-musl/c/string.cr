185cefab340a604268f2d8760c3ae3c95fc26e35
Julien Portalier <julien@portalier.com>
2024-06-24 22:03:06 +0200

    Fix: prefer strerror_r over strerror (thread safety)
    Prefer the thread safe `strerror_r` C call over the thread unsafe
    `strerror`.

--- src/lib_c/i386-linux-musl/c/string.cr
+++ src/lib_c/i386-linux-musl/c/string.cr
@@ -5,5 +5,6 @@ lib LibC
   fun memcmp(x0 : Void*, x1 : Void*, x2 : SizeT) : Int
   fun strcmp(x0 : Char*, x1 : Char*) : Int
   fun strerror(x0 : Int) : Char*
+  fun strerror_r(Int, Char*, SizeT) : Int
   fun strlen(x0 : Char*) : SizeT
 end
