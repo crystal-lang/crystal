275f2ccff448ee82e9f4892c45b6ebad5dc47d69
Julien Portalier <julien@portalier.com>
2024-07-25 22:38:49 +0200

    Fix: Eventfd#write raises on success

--- src/crystal/system/unix/eventfd.cr
+++ src/crystal/system/unix/eventfd.cr
@@ -22,7 +22,7 @@ struct Crystal::System::EventFD
   def write(value : UInt64) : Nil
     buf = value.unsafe_as(StaticArray(UInt8, 8))
     bytes_written = LibC.write(@fd, buf.to_unsafe, buf.size)
-    raise RuntimeError.from_errno("eventfd_write") if bytes_written == 8
+    raise RuntimeError.from_errno("eventfd_write") unless bytes_written == 8
   end
 
   def close : Nil
