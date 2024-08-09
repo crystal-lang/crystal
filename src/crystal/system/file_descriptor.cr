dc71a5605a40d10b6a86f67f3b803d135109ba31
Johannes Müller <straightshoota@gmail.com>
2024-08-09 11:28:20 +0200

    Add internal docs

--- src/crystal/system/file_descriptor.cr
+++ src/crystal/system/file_descriptor.cr
@@ -14,6 +14,14 @@ module Crystal::System::FileDescriptor
   # cooked mode otherwise.
   # private def system_raw(enable : Bool, & : ->)
 
+  # Closes the internal file descriptor without notifying the event loop.
+  # This is directly used after the fork of a process to close the
+  # parent's Crystal::System::Signal.@@pipe reference before re initializing
+  # the event loop. In the case of a fork that will exec there is even
+  # no need to initialize the event loop at all.
+  # Also used in `IO::FileDescriptor#finalize`.
+  # def file_descriptor_close
+
   private def system_read(slice : Bytes) : Int32
     event_loop.read(self, slice)
   end
