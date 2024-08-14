03c6d25a5ec974da33fc52010b3754458343c823
George Dietrich <george@dietrich.app>
2024-08-14 10:37:56 -0400

    Remove redundant parentheses
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/string.cr
+++ src/string.cr
@@ -752,7 +752,7 @@ class String
   end
 
   private def to_f_impl(whitespace : Bool = true, strict : Bool = true, &)
-    return unless (first_char = self[0]?)
+    return unless first_char = self[0]?
     return unless whitespace || '0' <= first_char <= '9' || first_char.in?('-', '+', 'i', 'I', 'n', 'N')
 
     v, endptr = yield
