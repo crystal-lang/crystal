2dd16bded87e0f830e3699c2f18f87ebbf751ca6
Quinton Miller <nicetas.c@gmail.com>
2024-08-14 20:13:56 +0800

    fixup

--- src/llvm/orc/jit_dylib.cr
+++ src/llvm/orc/jit_dylib.cr
@@ -10,7 +10,7 @@ class LLVM::Orc::JITDylib
   end
 
   def link_symbols_from_current_process(global_prefix : Char) : Nil
-    LLVM.assert LibLLVM.orc_create_dynamic_library_search_generator_for_process(out dg, global_prefix.to_u8, nil, nil)
+    LLVM.assert LibLLVM.orc_create_dynamic_library_search_generator_for_process(out dg, global_prefix.ord.to_u8, nil, nil)
     LibLLVM.orc_jit_dylib_add_generator(self, dg)
   end
 end
