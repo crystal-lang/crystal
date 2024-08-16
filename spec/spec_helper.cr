7ee895fa8703aa7955b65bd9f12b8f6cf32835a9
Quinton Miller <nicetas.c@gmail.com>
2024-08-16 21:57:53 +0800

    Don't spawn subprocess if codegen spec uses flags but not the prelude (#14904)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -294,7 +294,7 @@ def run(code, filename : String? = nil, inject_primitives = true, debug = Crysta
   # in the current executable!), so instead we compile
   # the program and run it, printing the last
   # expression and using that to compare the result.
-  if code.includes?(%(require "prelude")) || flags
+  if code.includes?(%(require "prelude"))
     ast = Parser.parse(code).as(Expressions)
     last = ast.expressions.last
     assign = Assign.new(Var.new("__tempvar"), last)
@@ -315,7 +315,9 @@ def run(code, filename : String? = nil, inject_primitives = true, debug = Crysta
       return SpecRunOutput.new(output)
     end
   else
-    new_program.run(code, filename: filename, debug: debug)
+    program = new_program
+    program.flags.concat(flags) if flags
+    program.run(code, filename: filename, debug: debug)
   end
 end
 
@@ -324,10 +326,12 @@ def run(code, return_type : T.class, filename : String? = nil, inject_primitives
     code = %(require "primitives"\n#{code})
   end
 
-  if code.includes?(%(require "prelude")) || flags
+  if code.includes?(%(require "prelude"))
     fail "TODO: support the prelude in typed codegen specs", file: file
   else
-    new_program.run(code, return_type: T, filename: filename, debug: debug)
+    program = new_program
+    program.flags.concat(flags) if flags
+    program.run(code, return_type: T, filename: filename, debug: debug)
   end
 end
 
