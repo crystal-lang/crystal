ae253275597c32aae9e0cf59aea4e15dab57fdfe
Quinton Miller <nicetas.c@gmail.com>
2024-08-22 01:40:30 +0800

    Include `Crystal::System::Group` instead of extending it

--- src/crystal/system/group.cr
+++ src/crystal/system/group.cr
@@ -1,3 +1,13 @@
+module Crystal::System::Group
+  # def system_name : String
+
+  # def system_id : String
+
+  # def self.from_name?(groupname : String) : ::System::Group?
+
+  # def self.from_id?(groupid : String) : ::System::Group?
+end
+
 {% if flag?(:wasi) %}
   require "./wasi/group"
 {% elsif flag?(:unix) %}
