41ba97243593316dc7ef9c9946fa51b035bfa08d
Julien Portalier <julien@portalier.com>
2024-09-05 13:00:04 +0200

    Fix: don't involve evloop in System::Process.reopen_io (UNIX)
    This is called after fork before exec to reopen the stdio. We can
    leverage some abstractions (set blocking, unset cloexec) but musn't call
    to methods that involve the evloop and would mess with the parent
    evloop.

--- src/crystal/system/unix/process.cr
+++ src/crystal/system/unix/process.cr
@@ -342,15 +342,17 @@ struct Crystal::System::Process
 
   private def self.reopen_io(src_io : IO::FileDescriptor, dst_io : IO::FileDescriptor)
     if src_io.closed?
-      dst_io.close
-      return
+      dst_io.file_descriptor_close
+    else
+      src_io = to_real_fd(src_io)
+
+      # dst_io.reopen(src_io)
+      ret = LibC.dup2(src_io.fd, dst_io.fd)
+      raise IO::Error.from_errno("dup2") if ret == -1
+
+      dst_io.blocking = true
+      dst_io.close_on_exec = false
     end
-
-    src_io = to_real_fd(src_io)
-
-    dst_io.reopen(src_io)
-    dst_io.blocking = true
-    dst_io.close_on_exec = false
   end
 
   private def self.to_real_fd(fd : IO::FileDescriptor)
