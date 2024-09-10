95b64d959abc81cb9758e74c3ad9318879d327e7
Julien Portalier <julien@portalier.com>
2024-09-10 17:28:00 +0200

    Add PollDescriptor and Waiters objects
    Keeps waiting reader and writer events for an IO. The event themselves
    keep the information about the event and the associated fiber.

---
+++ src/crystal/system/unix/evented/waiters.cr
@@ -0,0 +1,57 @@
+# A FIFO queue of `Event` waiting on the same operation (either read or write)
+# for a fd. See `PollDescriptor`.
+#
+# Thread safe: mutations are protected with a lock, and race conditions are
+# handled through the ready atomic.
+struct Crystal::Evented::Waiters
+  @ready = Atomic(Bool).new(false)
+  @lock = SpinLock.new
+  @list = PointerLinkedList(Event).new
+
+  def add(event : Pointer(Event)) : Bool
+    {% if flag?(:preview_mt) %}
+      # we check for readyness to avoid a race condition with another thread
+      # running the evloop and trying to wakeup a waiting fiber while we try to
+      # add a waiting fiber
+      return false if ready?
+
+      @lock.sync do
+        return false if ready?
+        @list.push(event)
+      end
+    {% else %}
+      @list.push(event)
+    {% end %}
+
+    true
+  end
+
+  def delete(event) : Nil
+    @lock.sync { @list.delete(event) }
+  end
+
+  def consume_each(&) : Nil
+    @lock.sync do
+      @list.consume_each { |event| yield event }
+    end
+  end
+
+  def ready? : Bool
+    @ready.swap(false, :relaxed)
+  end
+
+  def ready! : Pointer(Event)?
+    @lock.sync do
+      {% if flag?(:preview_mt) %}
+        if event = @list.shift?
+          event
+        else
+          @ready.set(true, :relaxed)
+          nil
+        end
+      {% else %}
+        @list.shift?
+      {% end %}
+    end
+  end
+end
