03f229314bb7329f3783ff2ac294ecf311124dbe
Devonte <devnote.dev75@gmail.com>
2024-10-01 18:13:17 +0100

    fix(semantic_version): use correct method in doc
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/semantic_version.cr
+++ src/semantic_version.cr
@@ -53,7 +53,7 @@ struct SemanticVersion
   # ```
   # require "semantic_version"
   #
-  # semver = SemanticVersion.parse("2.61.4")
+  # semver = SemanticVersion.parse?("2.61.4")
   # semver # => #<SemanticVersion:0x55b3667c9e70 @major=2, @minor=61, @patch=4, ... >
   # ```
   #
