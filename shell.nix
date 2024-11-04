dd8d8fee1775463fd2bd90d706f4af9a7e5045c7
Johannes Müller <straightshoota@gmail.com>
2024-11-04 16:56:07 +0100

    Use `libiconvReal` on macOS

--- shell.nix
+++ shell.nix
@@ -74,7 +74,7 @@ let
 
   stdLibDeps = with pkgs; [
       boehmgc gmp libevent libiconv libxml2 libyaml openssl pcre2 zlib
-    ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
+    ] ++ lib.optionals stdenv.isDarwin [ libiconvReal ];
 
   tools = [ pkgs.hostname pkgs.git llvmPackages.bintools ] ++ pkgs.lib.optional (!llvmPackages.lldb.meta.broken) llvmPackages.lldb;
 in
