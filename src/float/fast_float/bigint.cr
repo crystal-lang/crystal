77a49874e597a7dd60678f92c14a98adf7d26920
Quinton Miller <nicetas.c@gmail.com>
2024-11-15 17:22:42 +0800

    don't use llvm intrinsic at all

--- src/float/fast_float/bigint.cr
+++ src/float/fast_float/bigint.cr
@@ -201,31 +201,13 @@ module Float::FastFloat
     uint64_hi64(x0, x1.unsafe_shl(32) | x2)
   end
 
-  {% unless flag?(:interpreted) %}
-    lib Intrinsics
-      {% if Limb == UInt64 %}
-        fun uadd_with_overflow_i64 = "llvm.uadd.with.overflow.i64"(UInt64, UInt64) : {UInt64, Bool}
-      {% else %}
-        fun uadd_with_overflow_i32 = "llvm.uadd.with.overflow.i32"(UInt32, UInt32) : {UInt32, Bool}
-      {% end %}
-    end
-  {% end %}
-
   # add two small integers, checking for overflow.
   # we want an efficient operation.
   # NOTE(crystal): returns also *overflow* by value
   def self.scalar_add(x : Limb, y : Limb) : {Limb, Bool}
-    {% if flag?(:interpreted) %}
-      z = x &+ y
-      overflow = z < x
-      {z, overflow}
-    {% else %}
-      {% if Limb == UInt64 %}
-        Intrinsics.uadd_with_overflow_i64(x, y)
-      {% else %}
-        Intrinsics.uadd_with_overflow_i32(x, y)
-      {% end %}
-    {% end %}
+    z = x &+ y
+    overflow = z < x
+    {z, overflow}
   end
 
   # multiply two small integers, getting both the high and low bits.
