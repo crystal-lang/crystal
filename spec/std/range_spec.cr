09de8a0c8ad06a3d0da2a4f74ee267f516edf222
Johannes Müller <straightshoota@gmail.com>
2024-12-02 16:21:39 +0100

    Fix convert small types to Int32

--- spec/std/range_spec.cr
+++ spec/std/range_spec.cr
@@ -670,47 +670,48 @@ describe "Range" do
 
   describe "#size" do
     describe "Int" do
-      describe "signed" do
-        it { (5..12).size.should eq(8) }
-        it { (5...12).size.should eq(7) }
-        it { (5..4).size.should eq(0) }
-        it { (0..0).size.should eq(1) }
-        it { (0...0).size.should eq(0) }
-        it { (1..1).size.should eq(1) }
-        it { (1...1).size.should eq(0) }
+      it { (5..12).size.should eq(8) }
+      it { (5...12).size.should eq(7) }
+      it { (5..4).size.should eq(0) }
+      it { (0..0).size.should eq(1) }
+      it { (0...0).size.should eq(0) }
+      it { (1..1).size.should eq(1) }
+      it { (1...1).size.should eq(0) }
 
-        it { (-12..-5).size.should eq(8) }
-        it { (-12...-5).size.should eq(7) }
-        it { (-4..-5).size.should eq(0) }
-        it { (-1..-1).size.should eq(1) }
-        it { (-1...-1).size.should eq(0) }
+      it { (-12..-5).size.should eq(8) }
+      it { (-12...-5).size.should eq(7) }
+      it { (-4..-5).size.should eq(0) }
+      it { (-1..-1).size.should eq(1) }
+      it { (-1...-1).size.should eq(0) }
 
-        it { (-3..3).size.should eq(7) }
-        it { (-3...3).size.should eq(6) }
-        it { (-3..0).size.should eq(4) }
-        it { (-3...0).size.should eq(3) }
-        it { (3..-3).size.should eq(0) }
-        it { (3...-3).size.should eq(0) }
-        it { (-128_i8..0_i8).size.should eq(129) }
-        it { (-128_i8...0_i8).size.should eq(128) }
+      it { (-3..3).size.should eq(7) }
+      it { (-3...3).size.should eq(6) }
+      it { (-3..0).size.should eq(4) }
+      it { (-3...0).size.should eq(3) }
+      it { (3..-3).size.should eq(0) }
+      it { (3...-3).size.should eq(0) }
+      it { (-128_i8..0_i8).size.should eq(129) }
+      it { (-128_i8...0_i8).size.should eq(128) }
 
-        it { (Int32::MAX..Int32::MAX).size.should eq(1) }
-        it { (Int32::MAX...Int32::MAX).size.should eq(0) }
-        it { (-Int32::MAX..-Int32::MAX).size.should eq(1) }
-        it { (-Int32::MAX...-Int32::MAX).size.should eq(0) }
-      end
+      it { (Int32::MAX..Int32::MAX).size.should eq(1) }
+      it { (Int32::MAX...Int32::MAX).size.should eq(0) }
+      it { (-Int32::MAX..-Int32::MAX).size.should eq(1) }
+      it { (-Int32::MAX...-Int32::MAX).size.should eq(0) }
 
-      describe "unsigned" do
-        it { (5_u8..12_u8).size.should eq(8) }
-        it { (5_u8...12_u8).size.should eq(7) }
-        it { (5_u8..4_u8).size.should eq(0) }
-        it { (0_u8..0_u8).size.should eq(1) }
-        it { (0_u8...0_u8).size.should eq(0) }
-        it { (1_u8..1_u8).size.should eq(1) }
-        it { (1_u8...1_u8).size.should eq(0) }
-        it { (UInt8::MAX..UInt8::MAX).size.should eq(1) }
-        it { (UInt8::MAX...UInt8::MAX).size.should eq(0) }
-      end
+      it { (5_u8..12_u8).size.should eq(8) }
+      it { (5_u8...12_u8).size.should eq(7) }
+      it { (5_u8..4_u8).size.should eq(0) }
+      it { (0_u8..0_u8).size.should eq(1) }
+      it { (0_u8...0_u8).size.should eq(0) }
+      it { (1_u8..1_u8).size.should eq(1) }
+      it { (1_u8...1_u8).size.should eq(0) }
+      it { (UInt8::MAX..UInt8::MAX).size.should eq(1) }
+      it { (UInt8::MAX...UInt8::MAX).size.should eq(0) }
+
+      it { (-32768_i16..254_u8).size.should eq(33023) }
+      it { (-128_i8..127_i8).size.should eq(256) }
+      it { (Int32::MIN..-127_i8).size.should eq(2_147_483_522) }
+      it { ((Int16::MIN.to_i32 - 1)..127_i16).size.should eq(32897) }
     end
 
     it "works for other types" do
--- src/range.cr
+++ src/range.cr
@@ -491,6 +491,12 @@ struct Range(B, E)
     if b.is_a?(Int) && e.is_a?(Int)
       return 0 if e < b
 
+      # Convert `e` to `Int32` in order to ensure that `e &- b` doesn't get
+      # truncated due to the smaller type of `e`.
+      if e.is_a?(UInt8 | Int8 | UInt16 | Int16)
+        e = e.to_i32!
+      end
+
       diff = (e &- b).to_i32.abs
       diff &+= 1 unless @exclusive
       diff
