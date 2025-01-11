d4d5f87b9a6af040b7f43495032302d6bc1aa6c8
kojix2 <2xijok@gmail.com>
2025-01-11 20:41:14 +0900

    Add system-level at_exit handlers for safe resource cleanup

--- src/crystal/at_exit_handlers.cr
+++ src/crystal/at_exit_handlers.cr
@@ -1,9 +1,14 @@
 # :nodoc:
 module Crystal::AtExitHandlers
   private class_getter(handlers) { [] of Int32, ::Exception? -> }
+  private class_getter(internal_handlers) { [] of Int32, ::Exception? -> }
 
   def self.add(handler)
     handlers << handler
+  end
+
+  def self.__crystal_add(handler)
+    internal_handlers << handler
   end
 
   def self.run(status, exception = nil)
@@ -14,6 +19,18 @@ module Crystal::AtExitHandlers
           handler.call status, exception
         rescue handler_ex
           Crystal::System.print_error "Error running at_exit handler: %s\n", handler_ex.message || ""
+          status = 1 if status.zero?
+        end
+      end
+    end
+
+    if internal_handlers = @@internal_handlers
+      # Run the registered internal handlers in reverse order
+      while handler = internal_handlers.pop?
+        begin
+          handler.call status, exception
+        rescue handler_ex
+          Crystal::System.print_error "Error running internal at_exit handler: %s\n", handler_ex.message || ""
           status = 1 if status.zero?
         end
       end
