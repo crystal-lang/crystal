2c1c20bcb27072954b97e1fb5b37bcaf30036c32
Julien Portalier <julien@portalier.com>
2025-01-14 10:14:01 +0100

    Fix: LLVM 14 and below need explicit pointer cast (i8* -> i1*)

--- src/compiler/crystal/codegen/once.cr
+++ src/compiler/crystal/codegen/once.cr
@@ -44,7 +44,9 @@ class Crystal::CodeGenVisitor
       end
 
       state = load(once_state_type, once_state_global)
-      args = [state, flag, initializer]
+      # cast Int8* to Bool* (required for LLVM 14 and below)
+      bool = bit_cast(flag, @llvm_context.int1.pointer)
+      args = [state, bool, initializer]
     end
 
     call once_fun, args
