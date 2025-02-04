2655a7ac22f89e6e85bafa017ef92e198064ce04
Julien Portalier <julien@portalier.com>
2025-02-04 11:56:29 +0100

    Move new_fake_fiber to spec/support/fibers + use it in fiber/list_spec

--- spec/std/fiber/execution_context/spec_helper.cr
+++ spec/std/fiber/execution_context/spec_helper.cr
@@ -1,24 +1,8 @@
 require "../../spec_helper"
+require "../../../support/fibers"
 require "crystal/system/thread_wait_group"
 require "fiber/execution_context/runnables"
 require "fiber/execution_context/global_queue"
-
-# Fake stack for `makecontext` to have somewhere to write in #initialize; We
-# don't actually run the fiber. The worst case is windows with ~300 bytes (with
-# shadow space and alignment taken into account). We allocate more to be safe.
-FAKE_FIBER_STACK = GC.malloc(512)
-
-def new_fake_fiber(name = nil)
-  stack = FAKE_FIBER_STACK
-  stack_bottom = FAKE_FIBER_STACK + 128
-
-  {% if flag?(:execution_context) %}
-    execution_context = Fiber::ExecutionContext.current
-    Fiber.new(name, stack, stack_bottom, execution_context) { }
-  {% else %}
-    Fiber.new(name, stack, stack_bottom) { }
-  {% end %}
-end
 
 module Fiber::ExecutionContext
   class FiberCounter
--- spec/support/fibers.cr
+++ spec/support/fibers.cr
@@ -14,3 +14,21 @@ def wait_until_finished(f : Fiber, timeout = 5.seconds)
     raise "Fiber failed to finish within #{timeout}" if (Time.monotonic - now) > timeout
   end
 end
+
+# Fake stack for `makecontext` to have somewhere to write in #initialize; We
+# don't actually run the fiber. The worst case is windows with ~300 bytes (with
+# shadow space and alignment taken into account). We allocate more to be safe.
+FAKE_FIBER_STACK = GC.malloc(512)
+
+def new_fake_fiber(name = nil)
+  stack = FAKE_FIBER_STACK
+  stack_bottom = FAKE_FIBER_STACK + 128
+
+  {% if flag?(:execution_context) %}
+    execution_context = Fiber::ExecutionContext.current
+    Fiber.new(name, stack, stack_bottom, execution_context) { }
+  {% else %}
+    Fiber.new(name, stack, stack_bottom) { }
+  {% end %}
+end
+
