e9bc307c05e2a1189c0c9911d73f1f384fe6c0dc
Johannes Müller <straightshoota@gmail.com>
2025-02-10 12:12:46 +0100

    Add `Call.new` overload without `obj` parameter

--- src/compiler/crystal/syntax/ast.cr
+++ src/compiler/crystal/syntax/ast.cr
@@ -662,12 +662,20 @@ module Crystal
       end
     end
 
-    def self.new(obj : ASTNode?, name : String, *args : ASTNode, global : Bool = false)
+    def self.new(obj : ASTNode?, name : String, *args : ASTNode, block : Block? = nil, block_arg : ASTNode? = nil, named_args : Array(NamedArgument)?  = nil, global : Bool = false)
       {% if compare_versions(Crystal::VERSION, "1.5.0") > 0 %}
-        new obj, name, [*args] of ASTNode, global: global
+        new obj, name, [*args] of ASTNode, block: block, block_arg: block_arg, named_args: named_args, global: global
       {% else %}
-        new obj, name, args.to_a(&.as(ASTNode)), global: global
+        new obj, name, args.to_a(&.as(ASTNode)), block: block, block_arg: block_arg, named_args: named_args, global: global
       {% end %}
+    end
+
+    def self.new(name : String, args : Array(ASTNode) = [] of ASTNode, block : Block? = nil, block_arg : ASTNode? = nil, named_args : Array(NamedArgument)?  = nil, global : Bool = false)
+      new(nil, name, args, block: block, block_arg: block_arg, named_args: named_args, global: global)
+    end
+
+    def self.new(name : String, *args : ASTNode, block : Block? = nil, block_arg : ASTNode? = nil, named_args : Array(NamedArgument)?  = nil, global : Bool = false)
+      new(nil, name, *args, block: block, block_arg: block_arg, named_args: named_args, global: global)
     end
 
     def self.global(name, *args : ASTNode)
