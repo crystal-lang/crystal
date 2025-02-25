0e826aa9f108c94295ada03e453108132742ec18
Johannes Müller <straightshoota@gmail.com>
2025-02-25 11:19:58 +0100

    Improve spec error handling

--- spec/primitives/external_command_spec.cr
+++ spec/primitives/external_command_spec.cr
@@ -10,7 +10,7 @@ describe "Crystal::Command" do
       with_tempfile "crystal-external.cr" do |source_file|
         File.write source_file, <<-CRYSTAL
           puts Process.find_executable("crystal")
-          puts ENV["CRYSTAL_EXEC_PATH"]
+          puts ENV["CRYSTAL_EXEC_PATH"]?
           puts PROGRAM_NAME
           puts ARGV
           CRYSTAL
@@ -20,16 +20,17 @@ describe "Crystal::Command" do
 
       File.exists?(command_path).should be_true
 
+      output = IO::Memory.new
       process = Process.new(compiler_path,
         ["external", "foo", "bar"],
-        output: :pipe,
+        output: output, error: output,
         env: {"PATH" => {ENV["PATH"], File.dirname(command_path)}.join(Process::PATH_DELIMITER)}
       )
-      lines = process.output.gets_to_end.lines
 
       status = process.wait
       status.success?.should be_true
 
+      lines = output.to_s.lines
       lines.should eq [
         compiler_path,
         File.dirname(compiler_path),
