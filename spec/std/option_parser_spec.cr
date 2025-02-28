bb88540924658f7210a9f419620177ec5a25610a
kojix2 <2xijok@gmail.com>
2025-02-28 23:13:29 +0900

    Silently clamp negative summary_width values to 0

--- spec/std/option_parser_spec.cr
+++ spec/std/option_parser_spec.cr
@@ -893,13 +893,20 @@ describe "OptionParser with summary_width and summary_indent" do
     output.should eq(expected_output)
   end
 
-  # it "raises an error for invalid summary_width or summary_indent" do
-  #   parser = OptionParser.new
-  #   expect_raises ArgumentError, "Invalid summary width: -10" do
-  #     parser.summary_width = -10
-  #   end
-  #   expect_raises ArgumentError, "Invalid summary indent: -5" do
-  #     parser.summary_indent = -5
-  #   end
-  # end
+  it "handles negative summary_width" do
+    parser = OptionParser.new
+    parser.summary_width = -10
+    parser.summary_indent = " " * 4
+
+    parser.on("--negative", "Test negative summary_width") { }
+
+    output = parser.to_s
+
+    expected_output = <<-USAGE
+        --negative
+         Test negative summary_width
+    USAGE
+
+    output.should eq(expected_output)
+  end
 end
