f24a948c10ad5f6c1edc1e382cc5365e8eaf09ed
Julien Portalier <julien@portalier.com>
2025-03-10 14:55:00 +0100

    Regex::PCRE2: set jit stack and match data as ThreadLocal

--- src/regex/lib_pcre2.cr
+++ src/regex/lib_pcre2.cr
@@ -268,6 +268,7 @@ lib LibPCRE2
   fun pattern_info = pcre2_pattern_info_8(code : Code*, what : UInt32, where : Void*) : Int
 
   fun match = pcre2_match_8(code : Code*, subject : UInt8*, length : LibC::SizeT, startoffset : LibC::SizeT, options : UInt32, match_data : MatchData*, mcontext : MatchContext*) : Int
+  fun match_data_create = pcre2_match_data_create_8(ovecsize : UInt32, gcontext : GeneralContext*) : MatchData*
   fun match_data_create_from_pattern = pcre2_match_data_create_from_pattern_8(code : Code*, gcontext : GeneralContext*) : MatchData*
   fun match_data_free = pcre2_match_data_free_8(match_data : MatchData*) : Void
 
