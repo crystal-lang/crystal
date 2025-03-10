7487cb513673600df1139f17682852696381a8e4
Julien Portalier <julien@portalier.com>
2025-03-10 19:19:43 +0100

    Windows: go back to TlsAlloc with custom destructor array

--- src/crystal/system/win32/thread_local.cr
+++ src/crystal/system/win32/thread_local.cr
@@ -1,30 +1,43 @@
 require "c/fibersapi"
 
 class Thread
+  protected class_getter(destructors : Array({LibC::DWORD, Proc(Void*, Nil)})) do
+    Array({LibC::DWORD, Proc(Void*, Nil)}).new
+  end
+
   struct Local(T)
     @key : LibC::DWORD
 
     def initialize
       {% raise "T must be a Reference or Pointer" unless T < Reference || T < Pointer %}
-      @key = LibC.FlsAlloc(nil)
-      raise RuntimeError.from_winerror("FlsAlloc: out of indexes") if @key == LibC::TLS_OUT_OF_INDEXES
+      @key = LibC.TlsAlloc()
+      raise RuntimeError.from_winerror("TlsAlloc: out of indexes") if @key == LibC::TLS_OUT_OF_INDEXES
     end
 
     def initialize(&destructor : Proc(T, Nil))
       {% raise "T must be a Reference or Pointer" unless T < Reference || T < Pointer %}
-      @key = LibC.FlsAlloc(destructor.unsafe_as(Proc(Void*, Nil)))
-      raise RuntimeError.from_winerror("FlsAlloc: out of indexes") if @key == LibC::TLS_OUT_OF_INDEXES
+      @key = LibC.TlsAlloc()
+      raise RuntimeError.from_winerror("TlsAlloc: out of indexes") if @key == LibC::TLS_OUT_OF_INDEXES
+      Thread.destructors << {@key, destructor.unsafe_as(Proc(Void*, Nil))}
     end
 
     def get? : T?
-      pointer = LibC.FlsGetValue(@key)
+      pointer = LibC.TlsGetValue(@key)
       pointer.as(T) if pointer
     end
 
     def set(value : T) : T
-      ret = LibC.FlsSetValue(@key, value.as(Void*))
-      raise RuntimeError.from_winerror("FlsSetValue") if ret == 0
+      ret = LibC.TlsSetValue(@key, value.as(Void*))
+      raise RuntimeError.from_winerror("TlsSetValue") if ret == 0
       value
     end
+  end
+
+  private def run_destructors
+    @@destructors.try(&.each do |(key, destructor)|
+      if pointer = LibC.TlsGetValue(key)
+        destructor.call(pointer) # rescue nil
+      end
+    end)
   end
 end
