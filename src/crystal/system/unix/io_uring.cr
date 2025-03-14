f953a124f78b16fc1a9f437156762788f8dc467b
Julien Portalier <julien@portalier.com>
2025-03-14 10:30:45 +0100

    Set io_uring_params.sq_thread_idle + support IORING_SETUP_NO_SQARRAY + fallback

--- src/crystal/system/unix/io_uring.cr
+++ src/crystal/system/unix/io_uring.cr
@@ -3,16 +3,28 @@ require "./syscall"
 require "./eventfd"
 
 class Crystal::System::IoUring
-  @@opcodes : Slice(LibC::IoUringProbeOp)?
+  @@no_sqarray = true
   @@features : UInt32?
+  @@opcodes : Slice(LibC::IoUringProbeOp)?
 
-  def self.supported? : Bool
+  class_getter?(supported : Bool) { check_kernel_support? }
+
+  def self.check_kernel_support? : Bool
+    # try with "no sqarray" flag first (available since linux 6.6)
     params = LibC::IoUringParams.new
     params.flags = LibC::IORING_SETUP_NO_SQARRAY
-
     fd = Syscall.io_uring_setup(1_u32, pointerof(params))
-    return false if fd == -LibC::ENOSYS
-    raise RuntimeError.from_os_error("io_uring_setup", Errno.new(-fd)) if fd < 0
+
+    if fd < 0
+      return false if fd == -LibC::ENOSYS
+      raise RuntimeError.from_os_error("io_uring_setup", Errno.new(-fd)) unless fd == -LibC::EINVAL
+
+      # try again without "no sqarray" flag
+      params = LibC::IoUringParams.new
+      fd = Syscall.io_uring_setup(1_u32, pointerof(params))
+      raise RuntimeError.from_os_error("io_uring_setup", Errno.new(-fd)) if fd < 0
+      @@no_sqarray = false
+    end
 
     @@features = params.features
 
@@ -22,8 +34,9 @@ class Crystal::System::IoUring
       raise RuntimeError.from_errno("malloc") unless probe
       LibIntrinsics.memset(probe, 0_u8, probe_sz, false)
 
-      ret = Crystal::System::Syscall.io_uring_register(fd, LibC::IORING_REGISTER_PROBE, probe.as(Void*), LibC::IORING_OP_LAST)
+      ret = Syscall.io_uring_register(fd, LibC::IORING_REGISTER_PROBE, probe.as(Void*), LibC::IORING_OP_LAST)
       raise RuntimeError.from_os_error("io_uring_register", Errno.new(-ret)) if ret < 0
+
       @@opcodes = Slice(LibC::IoUringProbeOp).new(probe.value.ops.to_unsafe, LibC::IORING_OP_LAST)
     ensure
       LibC.close(fd) # if fd > 0
@@ -42,17 +55,16 @@ class Crystal::System::IoUring
 
   @fd : Int32
   @flags : UInt32
-  @sqes_size : UInt32
   @sq_size : UInt32
   @cq_size : UInt32
+  @sqes_size : UInt32
   @sq : Void*
   @cq : Void*
 
-  # OPTIMIZE: leverage IORING_SETUP_NO_SQARRAY when available (linux 6.6+)
-
-  def initialize(sq_entries : UInt32, *, cq_entries : UInt32? = nil, sq_poll : Bool = false, wq : IoUring? = nil)
+  def initialize(sq_entries : UInt32, *, cq_entries : UInt32? = nil, sq_idle : Int32? = nil, wq : IoUring? = nil)
     # setup the ring
     params = LibC::IoUringParams.new
+    params.flags |= LibC::IORING_SETUP_NO_SQARRAY if @@no_sqarray
     # params.flags |= LibC::IORING_SETUP_COOP_TASKRUN
     # params.flags |= LibC::IORING_SETUP_TASKRUN_FLAG
 
@@ -61,7 +73,8 @@ class Crystal::System::IoUring
       params.flags |= LibC::IORING_SETUP_CQSIZE
     end
 
-    if sq_poll
+    if sq_idle
+      params.sq_thread_idle = sq_idle
       params.flags |= LibC::IORING_SETUP_SQPOLL
     end
 
@@ -70,22 +83,27 @@ class Crystal::System::IoUring
       params.flags |= LibC::IORING_SETUP_ATTACH_WQ
     end
 
-    @fd = Crystal::System::Syscall.io_uring_setup(sq_entries, pointerof(params))
+    @fd = Syscall.io_uring_setup(sq_entries, pointerof(params))
     raise RuntimeError.from_os_error("io_uring_setup", Errno.new(-@fd)) if @fd < 0
 
     @flags = params.flags
 
-    # determine the size of ring buffers
-    @sqes_size = params.sq_entries * sizeof(LibC::IoUringSqe)
-    @sq_size = params.@sq_off.array + params.sq_entries * sizeof(UInt32)
+    # determine buffer sizes
+    @sq_size = params.@sq_off.array
+    if (params.flags & LibC::IORING_SETUP_NO_SQARRAY) == 0
+      # legacy: account for SQARRAY ring
+      @sq_size += params.sq_entries * sizeof(UInt32)
+    end
     @cq_size = params.@cq_off.cqes + params.cq_entries * sizeof(LibC::IoUringCqe)
+    @sqes_size = params.sq_entries * sizeof(LibC::IoUringSqe)
 
     if (params.features & LibC::IORING_FEAT_SINGLE_MMAP) == LibC::IORING_FEAT_SINGLE_MMAP
+      # single mmap: keep the largest size
       @sq_size = @cq_size if @cq_size > @sq_size
       @cq_size = @sq_size
     end
 
-    # map the SQ array (ring indirection to SQE)
+    # map the SQ metadata + SQARRAY ring (legacy) + CQ metadata & ring buffer (modern)
     @sq = mmap(@sq_size, LibC::IORING_OFF_SQ_RING) do
       LibC.close(@fd)
     end
@@ -93,14 +111,14 @@ class Crystal::System::IoUring
     if (params.features & LibC::IORING_FEAT_SINGLE_MMAP) == LibC::IORING_FEAT_SINGLE_MMAP
       @cq = @sq
     else
-      # legacy: map the CQE ring buffer (Completion Queue Entries)
+      # legacy: must separately map the CQ metadata & ring buffer
       @cq = mmap(@cq_size, LibC::IORING_OFF_CQ_RING) do
         LibC.munmap(@sq, @sq_size)
         LibC.close(@fd)
       end
     end
 
-    # map the SQE ring buffer (Submission Queue Entries)
+    # map the SQE ring buffer
     @sqes = mmap(@cq_size, LibC::IORING_OFF_SQES) do
       LibC.munmap(@sq, @sq_size)
       LibC.munmap(@cq, @cq_size) unless @cq == @sq
@@ -113,13 +131,18 @@ class Crystal::System::IoUring
     @sq_mask    = (@sq + params.@sq_off.ring_mask).as(UInt32*)
     @sq_entries = (@sq + params.@sq_off.ring_entries).as(UInt32*)
     @sq_flags   = (@sq + params.@sq_off.flags).as(UInt32*)
-    @sq_array   = (@sq + params.@sq_off.array).as(UInt32*)
 
     @cq_head    = (@cq + params.@cq_off.head).as(UInt32*)
     @cq_tail    = (@cq + params.@cq_off.tail).as(UInt32*)
     @cq_mask    = (@cq + params.@cq_off.ring_mask).as(UInt32*)
     @cq_entries = (@cq + params.@cq_off.ring_entries).as(UInt32*)
     @cqes       = (@cq + params.@cq_off.cqes).as(LibC::IoUringCqe*)
+
+    if (params.flags & LibC::IORING_SETUP_NO_SQARRAY) == 0
+      # map SQARRAY to SQE indexes once and for all (no indirection)
+      sq_array = (@sq + params.@sq_off.array).as(UInt32*)
+      @sq_entries.value.times { |index| sq_array[index] = index }
+    end
   end
 
   def finalize
@@ -143,9 +166,9 @@ class Crystal::System::IoUring
     ptr
   end
 
-  def register(opcode : UInt32, arg : Pointer | Nil, arg_sz = 0) : Nil
+  def register(opcode : UInt32, arg : Pointer | Nil = nil, arg_sz = 0) : Nil
     argp = arg ? arg.as(Void*) : Pointer(Void).null
-    err = Crystal::System::Syscall.io_uring_register(@fd, opcode, argp, arg_sz.to_u32)
+    err = Syscall.io_uring_register(@fd, opcode, argp, arg_sz.to_u32)
     raise RuntimeError.from_os_error("io_uring_register", Errno.new(-err)) if err < 0
   end
 
@@ -192,9 +215,6 @@ class Crystal::System::IoUring
       sqe = @sqes.as(LibC::IoUringSqe*) + sq_index
       LibIntrinsics.memset(sqe, 0_u8, sizeof(LibC::IoUringSqe*), false)
       yield sqe
-
-      # set sqarray index (same index because we map 1:1 sqe and sqarray)
-      @sq_array[sq_index] = sq_index
 
       tail &+= 1_u32
       to_submit += 1_u32
