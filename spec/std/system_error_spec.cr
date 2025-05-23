55914741a03582eb4ec086a8379ff2b34053c85d
Johannes Müller <straightshoota@gmail.com>
2025-05-23 12:22:14 +0200

    Add specs

--- spec/std/system_error_spec.cr
+++ spec/std/system_error_spec.cr
@@ -3,9 +3,23 @@ require "spec"
 describe SystemError do
   describe ".from_os_error" do
     it "Can build an error from an errno" do
-      errno = Errno.new(2)
-      error = ::IO::Error.from_os_error(message: nil, os_error: errno)
+      errno = Errno::ENOENT
+      error = ::RuntimeError.from_os_error(message: nil, os_error: errno)
       error.message.should eq("No such file or directory")
+    end
+  end
+
+  describe ".from_errno" do
+    it "captures `Errno.value`" do
+      Errno.value = :ENOENT
+      error = ::RuntimeError.from_errno "foobar"
+      error.os_error.should eq Errno::ENOENT
+    end
+
+    it "avoid reset from message" do
+      Errno.value = :ENOENT
+      error = ::RuntimeError.from_errno(message: "foobar".tap{ Errno.value = :EPERM })
+      error.os_error.should eq Errno::EPERM # This should be ENOENT
     end
   end
 end
