53b9341356b958ee40334de98f3d60e1c07377e9
Fijxu <fijxu@nadeko.net>
2025-05-26 18:05:30 -0400

    Update src/file.cr
    Co-authored-by: Johannes Müller <straightshoota@gmail.com>

--- src/file.cr
+++ src/file.cr
@@ -523,7 +523,9 @@ class File < IO::FileDescriptor
     end
   end
 
-  # Returns the content of *filename* as a string. Raises `File::NotFoundError` if the file at filename does not exist.
+  # Returns the content of *filename* as a string.
+  # 
+  # Raises `File::NotFoundError` if the file at filename does not exist.
   #
   # ```
   # File.read("bar") # raises File::NotFoundError
