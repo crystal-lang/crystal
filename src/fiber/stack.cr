dea4cb059f1cd68cbbf8b269d21f22df4827e4f3
Julien Portalier <julien@portalier.com>
2025-06-02 20:34:45 +0200

    Add `Fiber::Stack#size`

--- src/fiber/stack.cr
+++ src/fiber/stack.cr
@@ -3,9 +3,19 @@ class Fiber
   struct Stack
     getter pointer : Void*
     getter bottom : Void*
+    getter size : Int32
     getter? reusable : Bool
 
-    def initialize(@pointer, @bottom, *, @reusable = false)
+    # Constructor for thread stacks (main fibers).
+    def initialize(@pointer : Void*, @bottom : Void*, *, @reusable = false)
+      # FIXME: sometimes gc/boehm reports weird stack limits on linux (over
+      # 2GB) at least, so we always cast to i32 without overflow checks.
+      @size = (@bottom - @pointer).to_i32!
+    end
+
+    # Constructor for fiber stacks.
+    def initialize(@pointer : Void*, @size : Int32, *, @reusable = false)
+      @bottom = @pointer + @size
     end
 
     def first_addressable_pointer : Void**
