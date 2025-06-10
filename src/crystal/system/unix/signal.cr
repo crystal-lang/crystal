39e8674c43e541b1486814289287ccccda3dc3ff
Johannes Müller <straightshoota@gmail.com>
2025-06-10 21:43:35 +0200

    Fix interpreter guard clauses for signal handling

--- src/crystal/system/unix/signal.cr
+++ src/crystal/system/unix/signal.cr
@@ -22,7 +22,7 @@ module Crystal::System::Signal
     @@mutex.synchronize do
       unless @@handlers[signal]?
         @@sigset << signal
-        {% if flag?(:interpreted) && Crystal::Interpreter.has_method?(:signal) %}
+        {% if flag?(:interpreted) && Crystal::Interpreter.class.has_method?(:signal) %}
           Crystal::Interpreter.signal(signal.value, 2)
         {% else %}
           action = LibC::Sigaction.new
@@ -66,7 +66,7 @@ module Crystal::System::Signal
     else
       @@mutex.synchronize do
         @@handlers.delete(signal)
-        {% if flag?(:interpreted) && Crystal::Interpreter.has_method?(:signal) %}
+        {% if flag?(:interpreted) && Crystal::Interpreter.class.has_method?(:signal) %}
           h = case handler
               when LibC::SIG_DFL then 0
               when LibC::SIG_IGN then 1
@@ -134,7 +134,7 @@ module Crystal::System::Signal
     ::Signal.each do |signal|
       next unless @@sigset.includes?(signal)
 
-      {% if flag?(:interpreted) && Crystal::Interpreter.has_method?(:signal) %}
+      {% if flag?(:interpreted) && Crystal::Interpreter.class.has_method?(:signal) %}
         Crystal::Interpreter.signal(signal.value, 0)
       {% else %}
         LibC.signal(signal, LibC::SIG_DFL)
