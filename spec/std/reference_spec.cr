72b89507d20f038f93eaa681d0a8a63c7787c343
Johannes Müller <straightshoota@gmail.com>
2025-06-11 14:01:00 +0200

    Group specs in `describe` blocks

--- spec/std/reference_spec.cr
+++ spec/std/reference_spec.cr
@@ -71,26 +71,30 @@ describe "Reference" do
     (!Reference.new).should be_false
   end
 
-  it "does inspect" do
-    r = ReferenceSpec::TestClass.new(1, "hello")
-    r.inspect.should eq(%(#<ReferenceSpec::TestClass:0x#{r.object_id.to_s(16)} @x=1, @y="hello">))
+  describe "#inspect" do
+    it "does inspect" do
+      r = ReferenceSpec::TestClass.new(1, "hello")
+      r.inspect.should eq(%(#<ReferenceSpec::TestClass:0x#{r.object_id.to_s(16)} @x=1, @y="hello">))
+    end
+
+    it "does inspect for class" do
+      String.inspect.should eq("String")
+    end
   end
 
-  it "does to_s" do
-    r = ReferenceSpec::TestClass.new(1, "hello")
-    r.to_s.should eq(%(#<ReferenceSpec::TestClass:0x#{r.object_id.to_s(16)}>))
-  end
+  describe "#to_s" do
+    it "does to_s" do
+      r = ReferenceSpec::TestClass.new(1, "hello")
+      r.to_s.should eq(%(#<ReferenceSpec::TestClass:0x#{r.object_id.to_s(16)}>))
+    end
 
-  it "does inspect for class" do
-    String.inspect.should eq("String")
-  end
+    it "does to_s for class" do
+      String.to_s.should eq("String")
+    end
 
-  it "does to_s for class" do
-    String.to_s.should eq("String")
-  end
-
-  it "does to_s for class if virtual" do
-    [ReferenceSpec::TestClassBase, ReferenceSpec::TestClassSubclass].to_s.should eq("[ReferenceSpec::TestClassBase, ReferenceSpec::TestClassSubclass]")
+    it "does to_s for class if virtual" do
+      [ReferenceSpec::TestClassBase, ReferenceSpec::TestClassSubclass].to_s.should eq("[ReferenceSpec::TestClassBase, ReferenceSpec::TestClassSubclass]")
+    end
   end
 
   it "returns itself" do
@@ -98,46 +102,50 @@ describe "Reference" do
     x.itself.should be(x)
   end
 
-  it "dups" do
-    original = ReferenceSpec::DupCloneClass.new
-    duplicate = original.dup
-    duplicate.should_not be(original)
-    duplicate.x.should eq(original.x)
-    duplicate.y.should be(original.y)
+  describe "#dup" do
+    it "dups" do
+      original = ReferenceSpec::DupCloneClass.new
+      duplicate = original.dup
+      duplicate.should_not be(original)
+      duplicate.x.should eq(original.x)
+      duplicate.y.should be(original.y)
+    end
+
+    it "can dup class that inherits abstract class" do
+      original = ReferenceSpec::Concrete.new(2).as(ReferenceSpec::Abstract)
+      duplicate = original.dup
+      duplicate.should be_a(ReferenceSpec::Concrete)
+      duplicate.should_not be(original)
+      duplicate.x.should eq(original.x)
+    end
+
+    it "calls #finalize on #dup'ed objects" do
+      obj = ReferenceSpec::TestClassWithFinalize.new
+      assert_finalizes("dup") { obj.dup }
+    end
   end
 
-  it "can dup class that inherits abstract class" do
-    original = ReferenceSpec::Concrete.new(2).as(ReferenceSpec::Abstract)
-    duplicate = original.dup
-    duplicate.should be_a(ReferenceSpec::Concrete)
-    duplicate.should_not be(original)
-    duplicate.x.should eq(original.x)
-  end
+  describe "#clone" do
+    it "clones with def_clone" do
+      original = ReferenceSpec::DupCloneClass.new
+      clone = original.clone
+      clone.should_not be(original)
+      clone.x.should eq(original.x)
+    end
 
-  it "clones with def_clone" do
-    original = ReferenceSpec::DupCloneClass.new
-    clone = original.clone
-    clone.should_not be(original)
-    clone.x.should eq(original.x)
-  end
-
-  it "clones with def_clone (recursive type)" do
-    original = ReferenceSpec::DupCloneRecursiveClass.new
-    clone = original.clone
-    clone.should_not be(original)
-    clone.x.should eq(original.x)
-    clone.y.should_not be(original.y)
-    clone.y.should eq(original.y)
-    clone.z.should be(clone)
+    it "clones with def_clone (recursive type)" do
+      original = ReferenceSpec::DupCloneRecursiveClass.new
+      clone = original.clone
+      clone.should_not be(original)
+      clone.x.should eq(original.x)
+      clone.y.should_not be(original.y)
+      clone.y.should eq(original.y)
+      clone.z.should be(clone)
+    end
   end
 
   it "pretty_print" do
     ReferenceSpec::TestClassBase.new.pretty_inspect.should match(/\A#<ReferenceSpec::TestClassBase:0x[0-9a-f]+>\Z/)
     ReferenceSpec::TestClass.new(42, "foo").pretty_inspect.should match(/\A#<ReferenceSpec::TestClass:0x[0-9a-f]+ @x=42, @y="foo">\Z/)
-  end
-
-  it "calls #finalize on #dup'ed objects" do
-    obj = ReferenceSpec::TestClassWithFinalize.new
-    assert_finalizes("dup") { obj.dup }
   end
 end
