c53c97dbc76b2880ecda84f1432c0c57c4a2c928
George Dietrich <dev@dietrich.pub>
2025-06-13 08:53:20 -0400

    Handle doc locations when file is in parent directory

---
+++ spec/compiler/crystal/tools/doc/relative_location_spec.cr
@@ -0,0 +1,32 @@
+require "../../../spec_helper"
+
+describe Crystal::Doc::RelativeLocation do
+  describe ".from" do
+    it "creates from same base directory" do
+      relative_location = Crystal::Doc::RelativeLocation.from(
+        Location.new("/base/dir/src/foo.cr", 1, 1),
+        "/base/dir/src"
+      ).should_not be_nil
+
+      relative_location.filename.should eq "foo.cr"
+    end
+
+    it "creates from a parent base directory" do
+      relative_location = Crystal::Doc::RelativeLocation.from(
+        Location.new("/base/dir/src/foo.cr", 1, 1),
+        "/base/dir"
+      ).should_not be_nil
+
+      relative_location.filename.should eq "src/foo.cr"
+    end
+
+    it "creates from a child base directory" do
+      relative_location = Crystal::Doc::RelativeLocation.from(
+        Location.new("/base/dir/src/foo.cr", 1, 1),
+        "/base/dir/src/app"
+      ).should_not be_nil
+
+      relative_location.filename.should eq "../foo.cr"
+    end
+  end
+end
