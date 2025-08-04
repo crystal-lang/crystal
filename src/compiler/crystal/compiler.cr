4adb09685769dd820fd4a4756eb7471b8aa461f9
kojix2 <2xijok@gmail.com>
2025-08-04 16:46:24 +0900

    Refactor must_compile? method for better performance and memory safety
    - Move expensive generate_bitcode call after early return conditions
    - Ensure memory_buffer.dispose is always called to prevent memory leaks

--- src/compiler/crystal/compiler.cr
+++ src/compiler/crystal/compiler.cr
@@ -995,26 +995,20 @@ module Crystal
       end
 
       private def must_compile?
+        return true unless compiler.emit_targets.none?
+        return true if @bc_flags_changed
+        return true unless File.exists?(bc_name)
+        return true unless File.exists?(object_name)
+
+        return true if File.size(object_name) == 0
+
         memory_buffer = generate_bitcode
+        memory_io = IO::Memory.new(memory_buffer.to_slice)
 
-        can_reuse_previous_compilation =
-          compiler.emit_targets.none? && !@bc_flags_changed && File.exists?(bc_name) && File.exists?(object_name)
+        changed = File.open(bc_name) { |bc_file| !IO.same_content?(bc_file, memory_io) }
+        memory_buffer.dispose
 
-        if can_reuse_previous_compilation
-          memory_io = IO::Memory.new(memory_buffer.to_slice)
-          changed = File.open(bc_name) { |bc_file| !IO.same_content?(bc_file, memory_io) }
-
-          # If the user cancelled a previous compilation
-          # it might be that the .o file is empty
-          if !changed && File.size(object_name) > 0
-            memory_buffer.dispose
-            return false
-          else
-            # We need to compile, so we'll write the memory buffer to file
-          end
-        end
-
-        true
+        changed
       end
 
       # Parse the previously generated bitcode into the LLVM module using a
