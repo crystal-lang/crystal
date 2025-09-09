01f20c726135b3411e0f54d3e1351a33ab263263
Julien Portalier <julien@portalier.com>
2025-09-09 18:16:02 +0200

    Simplify unix/cpucount + smoke spec

--- src/crystal/system/unix/cpucount.cr
+++ src/crystal/system/unix/cpucount.cr
@@ -3,10 +3,7 @@
 {% else %}
   require "c/unistd"
 {% end %}
-
-{% if flag?(:linux) %}
-  require "./syscall"
-{% end %}
+require "./syscall"
 
 module Crystal::System
   def self.cpu_count
@@ -14,7 +11,6 @@ module Crystal::System
       mib = Int32[LibC::CTL_HW, LibC::HW_NCPU]
       ncpus = 0
       size = sizeof(Int32).to_u64
-
       if LibC.sysctl(mib, 2, pointerof(ncpus), pointerof(size), nil, 0) == 0
         ncpus
       else
@@ -28,23 +24,16 @@ module Crystal::System
   def self.effective_cpu_count
     {% if flag?(:linux) %}
       # we use the syscall because it returns the number of bytes to check in
-      # the set while glibc always returns 0 and would thus require to zero the
-      # buffer and check at least every byte
+      # the set, while glibc always returns 0 and would require to zero the
+      # buffer and check every byte
       set = uninitialized UInt8[8192] # allows up to 65536 logical cpus
       byte_count = Syscall.sched_getaffinity(0, LibC::SizeT.new(8192), set.to_unsafe)
-      return -1 if byte_count < 0
-
-      count = 0
-      set.to_slice[0, byte_count].each do |byte|
-        until byte == 0
-          count &+= 1 unless (byte & 1) == 0
-          byte >>= 1
-        end
+      if byte_count > 0
+        count = set.to_slice[0, byte_count].sum(&.popcount)
+        return count if count > 0
       end
-
-      count > 0 ? count : -1
-    {% else %}
-      -1
     {% end %}
+
+    -1
   end
 end
