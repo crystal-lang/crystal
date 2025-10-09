ecb79a8824b4a4e2a1a09c785376677201fc7637
Julien Portalier <julien@portalier.com>
2025-10-09 14:52:41 +0200

    Apply suggestions from code review by @straight-shoota
    Co-authored-by: Johannes Müller <straightshoota@gmail.com>

--- src/compiler/crystal/interpreter/compiler.cr
+++ src/compiler/crystal/interpreter/compiler.cr
@@ -2928,18 +2928,17 @@ class Crystal::Repl::Compiler < Crystal::Visitor
   def visit(node : ProcPointer)
     target_def = node.call.target_def
 
-    if target_def.owner.is_a?(LibType)
-      # find or build a compiled_def
-      proc_type = node.type.as(ProcInstanceType)
-      symbol = @context.c_function(target_def.as(External).real_name)
-      compiled_def = @context.extern_proc_wrapper(proc_type, symbol)
+    raise "BUG: missing interpret for ProcPointer to non Lib fun" unless target_def.owner.is_a?(LibType)
+    
+    # find or build a compiled_def
+    proc_type = node.type.as(ProcInstanceType)
+    symbol = @context.c_function(target_def.as(External).real_name)
+    compiled_def = @context.extern_proc_wrapper(proc_type, symbol)
 
-      # push compiled_def to stack + no closure data (null pointer)
-      put_i64 compiled_def.object_id.to_i64!, node: node
-      put_i64 0, node: node
-    else
-      raise "BUG: missing interpret for ProcPointer to non Lib fun"
-    end
+    # push compiled_def to stack + no closure data (null pointer)
+    put_i64 compiled_def.object_id.to_i64!, node: node
+    put_i64 0, node: node
+    false
   end
 
   def visit(node : Break)
