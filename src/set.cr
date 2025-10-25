9c0e3005cc8465112bc44253f4f74ecde2b579db
Andrew Konchin <andry.konchin@gmail.com>
2025-10-26 02:13:00 +0300

    Add Set#map! method

--- src/set.cr
+++ src/set.cr
@@ -493,6 +493,13 @@ struct Set(T)
     @hash.same?(other.@hash)
   end
 
+  # Replaces every element of the set with a value returned from the block, and
+  # returns `self`.
+  def map!(& : T -> T) : self
+    @hash = @hash.transform_keys { |k, _| yield(k) }
+    self
+  end
+
   # Deletes every element of the set for which the block is falsey, and returns
   # `self`.
   def select!(& : T ->) : self
