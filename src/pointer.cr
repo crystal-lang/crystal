e36c936adfdf2e187a60fbbb6f3014e6141f1117
Johannes Müller <straightshoota@gmail.com>
2025-11-12 11:57:10 +0100

    Skip initialization of `Pointer.malloc` with zero value (#16333)
    Implements https://github.com/crystal-lang/crystal/issues/16203#issuecomment-3472156133
    
    `Pointer.malloc` initializes the allocated memory with zeros, so we can skip the explicit implementation if the representation of `value` is all zeroes.
    
    This should make `Pointer(T).malloc(size, value)` (almost) as efficient as `Pointer(T).malloc(size)` when `value` is all zeros.

--- src/pointer.cr
+++ src/pointer.cr
@@ -528,6 +528,13 @@ struct Pointer(T)
   # ```
   def self.malloc(size : Int, value : T) : Pointer(T)
     ptr = Pointer(T).malloc(size)
+
+    {% if Number::Primitive.union_types.includes?(T) %}
+      return ptr if value.zero?
+    {% elsif T < Pointer %}
+      return ptr if value.null?
+    {% end %}
+
     ptr.fill(size, value)
   end
 
