583192cd6d8cd2c4de061a39a70c80bee687c655
George Dietrich <dev@dietrich.pub>
2025-11-23 13:06:17 -0500

    Better handle free vars and unknown types in abstract defs

--- spec/compiler/semantic/abstract_def_spec.cr
+++ spec/compiler/semantic/abstract_def_spec.cr
@@ -1152,4 +1152,114 @@ describe "Semantic: abstract def" do
         CRYSTAL
     end
   end
+
+  describe "undefined types in abstract methods" do
+    it "reports undefined constant error for undefined type in abstract method" do
+      assert_error <<-CRYSTAL, "undefined constant Unknown"
+        module Foo
+          abstract def foo(x : Unknown)
+        end
+
+        class Bar
+          include Foo
+
+          def foo(x : Int32)
+          end
+        end
+        CRYSTAL
+    end
+
+    it "reports undefined constant error for undefined type in implementing method" do
+      assert_error <<-CRYSTAL, "undefined constant Unknown"
+        module Foo
+          abstract def foo(x : Int32)
+        end
+
+        class Bar
+          include Foo
+
+          def foo(x : Unknown)
+          end
+        end
+        CRYSTAL
+    end
+
+    it "allows forall type parameters in abstract methods (issue #10699)" do
+      assert_no_errors <<-CRYSTAL
+        module Foo
+          abstract def foo(x : U) : U forall U
+        end
+
+        class Bar
+          include Foo
+
+          def foo(x : U) : U forall U
+            x
+          end
+        end
+        CRYSTAL
+    end
+
+    it "allows generic type parameters from enclosing class" do
+      assert_no_errors <<-CRYSTAL
+        module Comparable(T)
+          abstract def <=>(other : T)
+        end
+
+        struct Slice(T)
+          include Comparable(Slice(T))
+
+          def <=>(other : Slice(T))
+            0
+          end
+        end
+        CRYSTAL
+    end
+
+    it "allows multiple generic type parameters" do
+      assert_no_errors <<-CRYSTAL
+        abstract class Container(K, V)
+          abstract def get(key : K) : V?
+          abstract def set(key : K, value : V) : Nil
+        end
+
+        class HashMap(K, V) < Container(K, V)
+          def get(key : K) : V?
+            nil
+          end
+
+          def set(key : K, value : V) : Nil
+          end
+        end
+        CRYSTAL
+    end
+
+    it "distinguishes between undefined types and valid type parameters" do
+      assert_error <<-CRYSTAL, "undefined constant UnknownType"
+        abstract class Container(T)
+          abstract def process(x : T, y : UnknownType)
+        end
+
+        class MyContainer(T) < Container(T)
+          def process(x : T, y : String)
+          end
+        end
+        CRYSTAL
+    end
+
+    it "reports undefined multi-segment paths" do
+      assert_error <<-CRYSTAL, "undefined constant Some::Path::Unknown"
+        module Foo
+          abstract def foo(x : Some::Path::Unknown)
+        end
+
+        class Bar
+          include Foo
+
+          def foo(x : Int32)
+          end
+        end
+        CRYSTAL
+    end
+  end
 end
