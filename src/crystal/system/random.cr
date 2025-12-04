813c0287afdba3dd139a1da8dd063ac246386ab6
Julien Portalier <julien@portalier.com>
2025-12-04 11:58:48 +0100

    Fix: always use getrandom on Linux and Android >= 28
    Crystal < 1.7 don't support calling the `has_method?` macro at the top
    level. We already assume the getrandom libc function to be defined on
    every linux targets with the exception of android 24 to 27 for which we
    now check the actual API version instead of the libc fun existence.

--- src/crystal/system/random.cr
+++ src/crystal/system/random.cr
@@ -12,15 +12,12 @@ end
 
 {% if flag?(:wasi) %}
   require "./wasi/random"
-{% elsif flag?(:linux) %}
-  require "c/sys/random"
-  \{% if LibC.has_method?(:getrandom) %}
-    require "./unix/getrandom"
-  \{% else %}
-    require "./unix/urandom"
-  \{% end %}
+{% elsif flag?(:android) && ANDROID_API >= 28 %}
+  require "./unix/getrandom"
 {% elsif flag?(:bsd) || flag?(:darwin) %}
   require "./unix/arc4random"
+{% elsif flag?(:linux) && !flag?(:android) %}
+  require "./unix/getrandom"
 {% elsif flag?(:unix) %}
   require "./unix/urandom"
 {% elsif flag?(:win32) %}
