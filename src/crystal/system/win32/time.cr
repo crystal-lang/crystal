2088323414ff47724d5438ffe609bff88b5d161d
Johannes Müller <straightshoota@gmail.com>
2025-12-09 19:40:24 +0100

    No need to cache `QueryPerformanceFrequency`, it is just a memory read

--- src/crystal/system/win32/time.cr
+++ src/crystal/system/win32/time.cr
@@ -53,7 +53,7 @@ module Crystal::System::Time
     ((filetime.dwHighDateTime.to_u64 << 32) | filetime.dwLowDateTime.to_u64).to_f64 / FILETIME_TICKS_PER_SECOND.to_f64
   end
 
-  private class_getter performance_frequency : Int64 do
+  private def performance_frequency
     LibC.QueryPerformanceFrequency(out frequency)
     frequency
   end
