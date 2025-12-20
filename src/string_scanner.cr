dd0105478d12518ae0ee90efdc5ac6c1bcccc24a
jneen <jneen@jneen.net>
2025-12-20 18:01:37 -0500

    add StringScanner#peek_behind

--- src/string_scanner.cr
+++ src/string_scanner.cr
@@ -41,8 +41,9 @@
 # * `#skip`
 # * `#skip_until`
 #
-# Methods that look ahead:
+# Methods that look ahead or behind:
 # * `#peek`
+# * `#peek_behind`
 # * `#check`
 # * `#check_until`
 #
@@ -424,6 +425,14 @@ class StringScanner
   def peek(len) : String
     byte_len = lookahead_byte_length(len) || @str.bytesize
     @str.byte_slice(@byte_offset, byte_len)
+  end
+
+  # Extracts a string by looking behind *len* characters, without moving the
+  # scan offset. The return value has at most *len* characters, but may have fewer
+  # if the scan head is close to the beginning of the string.
+  def peek_behind(len) : String
+    byte_len = lookbehind_byte_length(len) || @byte_offset
+    @str.byte_slice(@byte_offset - byte_len, byte_len)
   end
 
   # Returns the remainder of the string after the scan offset.
