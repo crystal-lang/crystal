bea15d26eaa2379b27c8e674603f13658478338a
Sijawusz Pur Rahnama <sija@sija.pl>
2026-01-02 02:24:54 +0100

    Add validation around filename passed through `#<loc:”…”>` pragma

--- src/compiler/crystal/syntax/lexer.cr
+++ src/compiler/crystal/syntax/lexer.cr
@@ -2378,6 +2378,10 @@ module Crystal
           end
         end
 
+        unless valid_filename?(filename)
+          raise "found invalid characters in filename: #{filename.inspect}"
+        end
+
         incr_column_number {{ %(#<loc:").size - 1 }}
 
         unless current_char == ','
@@ -2449,6 +2453,15 @@ module Crystal
       else
         raise %(expected #<loc:push>, #<loc:pop> or #<loc:"...">)
       end
+    end
+
+    private def valid_filename?(filename)
+      filename
+        .each_char
+        .none?(&.in?(
+          '\0', '\a', '\b', '\n', '\r', '\t', '\v', '\f', '\e',
+          *UNICODE_BIDI_CONTROL_CHARACTERS,
+        ))
     end
 
     private def consume_heredoc_start(start)
