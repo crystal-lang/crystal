d97f26d791c47bbea9c15fa51e5c58f6986ddef8
Julien Portalier <julien@portalier.com>
2026-01-12 11:49:59 +0100

    Add docs for Sync namespace

---
+++ src/sync/sync.cr
@@ -0,0 +1,17 @@
+# Synchronization primitives to build concurrent-safe and parallel-safe data
+# structures, so we can embrace concurrency and parallelism with more serenity.
+#
+# Communication through a `Channel` should be preferred whenever possible, but
+# sometimes we need to protect critical sections manually, for example to build
+# higher level constructs, or to protect a mutable global constant:
+#
+# - `Sync::Mutex` to protect critical sections using mutual exclusion.
+# - `Sync::RWLock` to protect critical sections using shared access and mutual
+#   exclusion.
+# - `Sync::ConditionVariable` to synchronize critical sections together.
+#
+# - `Sync::Exclusive(T)` to protect a value `T` using mutual exclusion.
+# - `Sync::Shared(T)` to protect a value `T` using a mix of shared access and
+#   mutual exclusion.
+module Sync
+end
