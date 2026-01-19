14169cbf3e6d92e1e27a73289cf1c0a38ced662e
Stephen von Takach <steve@place.technology>
2026-01-20 08:59:20 +1100

    add specs for debug and optimize

--- spec/compiler/codegen/target_annotation_spec.cr
+++ spec/compiler/codegen/target_annotation_spec.cr
@@ -1,9 +1,25 @@
 require "../spec_helper"
 
-describe "Code gen: Target annotation" do
+describe "Code gen: Target annotation", focus: true do
   it "errors if invalid Target argument provided" do
-    assert_error <<-CRYSTAL, "invalid Target argument 'invalid'. Valid arguments are features, cpu"
+    assert_error <<-CRYSTAL, "invalid Target argument 'invalid'. Valid arguments are features, optimize, debug, cpu"
       @[Target(invalid: "lorem ipsum")]
+      def foo
+      end
+      CRYSTAL
+  end
+
+  it "errors if invalid debug value provided" do
+    assert_error <<-CRYSTAL, "invalid value for debug argument. Must be true or false"
+      @[Target(debug: "all")]
+      def foo
+      end
+      CRYSTAL
+  end
+
+  it "errors if invalid optimize value provided" do
+    assert_error <<-CRYSTAL, "invalid value for optimize argument. Must be Size or None"
+      @[Target(optimize: Everything)]
       def foo
       end
       CRYSTAL
@@ -66,5 +82,38 @@ describe "Code gen: Target annotation" do
     {% else %}
       pending! "no CPU specified for this architecture"
     {% end %}
+  end
+
+  it "can disable debug metadata" do
+    run(<<-CRYSTAL).to_string.should contain("in '??'")
+      require "prelude"
+
+      @[Target(debug: false)]
+      def foo
+        raise "error"
+      end
+
+      begin
+        foo
+      rescue error
+        error.backtrace.first
+      end
+      CRYSTAL
+  end
+
+  it "can adjust optimizations" do
+    run(<<-CRYSTAL).to_b.should be_true
+      @[Target(optimize: :size)]
+      def size
+        true
+      end
+
+      @[Target(optimize: :none)]
+      def none
+        true
+      end
+
+      size && none
+      CRYSTAL
   end
 end
