37d25b037331f1053317ef4d9e4ae98491140170
Julien Portalier <julien@portalier.com>
2026-02-12 10:49:12 +0100

    Fix: OpenSSL sockets shouldn't flush on read (#16650)
    There's no reason autoflush the write buffer of the underlying IO object (e.g. TCP socket) when reading from the socket. Especially since we already disable the buffers in `OpenSSL::SSL::Socket#initialize`, so it ends up a no-op in practice.
    
    `Crystal::BIO#initialize` now always disables the buffers, so the logic is grouped together, instead of having `OpenSSL::SSL::Socket` do it.
    
    NOTE: this is potentially a breaking change if someone re-enabled buffers or expects the buffers to be buffered (in non SSL socket situations).
    
    See https://github.com/crystal-lang/crystal/pull/16640#discussion_r2770458991

--- src/openssl/bio.cr
+++ src/openssl/bio.cr
@@ -41,10 +41,6 @@ struct OpenSSL::BIO
   def self.read_ex(bio, buffer, len, readp)
     count = len > Int32::MAX ? Int32::MAX : len.to_i
     io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-
-    # FIXME: why flush (write) before reading?!
-    io.flush
-
     ret = io.read Slice.new(buffer, count)
     readp.value = LibC::SizeT.new(ret)
     1
@@ -52,10 +48,6 @@ struct OpenSSL::BIO
 
   def self.read(bio, buffer, len)
     io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-
-    # FIXME: why flush (write) before reading?!
-    io.flush
-
     io.read(Slice.new(buffer, len)).to_i
   end
 
@@ -92,6 +84,14 @@ struct OpenSSL::BIO
   @boxed_io : Void*
 
   def initialize(@io : IO)
+    if io.is_a?(IO::Buffered)
+      # Disable buffers of the underlying IO (e.g. TCP socket) so OpenSSL
+      # becomes responsible of what needs to be read/written on the wire;
+      # instead, buffers shall be on OpenSSL::SSL::Socket (for example).
+      io.sync = true
+      io.read_buffering = false
+    end
+
     @bio = LibCrypto.BIO_new(CRYSTAL_BIO)
 
     # We need to store a reference to the box because it's
