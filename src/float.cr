crystal-lang/crystal

commit ab304825ac8e637ab3056db23583c26e7c208e76

Fix `Float32#abs` for signed zeros (#15814)
Like #12424, but for `Float32`.

src/float.cr
@@ -194,6 +194,10 @@ struct Float32
   Number.expand_div [Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64, Int128, UInt128], Float32
   Number.expand_div [Float64], Float64
 
+  def abs
+    Math.copysign(self, 1)
+  end
+
   # Rounds towards positive infinity.
   def ceil : Float32
     LibM.ceil_f32(self)

spec/std/float_spec.cr
@@ -442,15 +442,26 @@ describe "Float" do
   end
 
   it "#abs" do
-    Math.copysign(1, 0.0.abs).should eq 1
-    Math.copysign(1, -0.0.abs).should eq 1
+    Math.copysign(1_f64, 0.0_f64.abs).should eq 1_f64
+    Math.copysign(1_f64, -0.0_f64.abs).should eq 1_f64
 
-    0.1.abs.should eq 0.1
-    -0.1.abs.should eq 0.1
+    0.1_f64.abs.should eq 0.1_f64
+    -0.1_f64.abs.should eq 0.1_f64
+
+    Math.copysign(1_f32, 0.0_f32.abs).should eq 1_f32
+    Math.copysign(1_f32, -0.0_f32.abs).should eq 1_f32
+
+    0.1_f32.abs.should eq 0.1_f32
+    -0.1_f32.abs.should eq 0.1_f32
 
     Float64::MAX.abs.should eq Float64::MAX
     Float64::MIN.abs.should eq -Float64::MIN
     Float64::INFINITY.abs.should eq Float64::INFINITY
     (-Float64::INFINITY).abs.should eq Float64::INFINITY
+
+    Float32::MAX.abs.should eq Float32::MAX
+    Float32::MIN.abs.should eq -Float32::MIN
+    Float32::INFINITY.abs.should eq Float32::INFINITY
+    (-Float32::INFINITY).abs.should eq Float32::INFINITY
   end
 end

spec/std/humanize_spec.cr
@@ -60,6 +60,9 @@ describe Number do
     it { assert_prints -0.0.format(decimal_places: 1), "-0.0" }
     it { assert_prints -0.0.format(decimal_places: 1, only_significant: true), "-0.0" }
 
+    it { assert_prints -0.0_f32.format(decimal_places: 1), "-0.0" }
+    it { assert_prints -0.0_f32.format(decimal_places: 1, only_significant: true), "-0.0" }
+
     it { assert_prints -0.01.format(decimal_places: 1), "-0.0" }
 
     it { assert_prints -123.45.format, "-123.45" }

commit 23b1aadbf79273a258f6e7ec84fcac11f3c114df

Fix `Float#abs` to return `self` (#16655)
This fixes a regression from #15814 which unintentionally changed the return type of `Float32#abs` to `Float64`. Previously, it returned `Float32`.

`Math.copysign(self, 1)` falls back to the overload without restrictions which converts both arguments to `Float64` and thus returns `Float64`.

This patch fixes that by explicitly targeting the `Math.copysign(Float32, Float32)` overload for `Float32#abs`.
`Float64#abs` is not broken, but still applies an unnecessary integer-to-float conversion when we should be using a float right away.

I discovered this type mismatch while researching #13389

src/float.cr
@@ -227,8 +227,8 @@ struct Float32
     (unsafe_as(UInt32) ^ 0x80000000_u32).unsafe_as(Float32)
   end
 
-  def abs
-    Math.copysign(self, 1)
+  def abs : Float32
+    Math.copysign(self, 1.0_f32)
   end
 
   # Returns `-1` if the sign bit of this float is set, `1` otherwise.
@@ -448,8 +448,8 @@ struct Float64
     (unsafe_as(UInt64) ^ 0x80000000_00000000_u64).unsafe_as(Float64)
   end
 
-  def abs
-    Math.copysign(self, 1)
+  def abs : Float64
+    Math.copysign(self, 1.0)
   end
 
   # Returns `-1` if the sign bit of this float is set, `1` otherwise.