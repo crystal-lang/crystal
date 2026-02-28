e13f4b3ae9870de478ed2685690b9f8f859c9f54
Julien Portalier <julien@portalier.com>
2026-02-28 19:21:02 +0100

    Add Kernel TLS support to custom `OpenSSL::BIO` (#16646)
    Co-authored-by: Johannes Müller <straightshoota@gmail.com>

---
+++ src/lib_c/x86_64-linux-musl/c/sys/uio.cr
@@ -0,0 +1,6 @@
+lib LibC
+  struct Iovec
+    iov_base : Void*
+    iov_len : SizeT
+  end
+end
--- src/openssl/bio.cr
+++ src/openssl/bio.cr
@@ -1,7 +1,8 @@
 require "./lib_crypto"
+require "./ktls"
 
 # :nodoc:
-struct OpenSSL::BIO
+class OpenSSL::BIO
   CRYSTAL_BIO = begin
     bio_id = LibCrypto.BIO_get_new_index
     raise OpenSSL::Error.new("BIO_get_new_index") if bio_id == -1
@@ -29,55 +30,115 @@ struct OpenSSL::BIO
     biom
   end
 
-  def self.write_ex(bio, data, len, writep)
-    count = len > Int32::MAX ? Int32::MAX : len.to_i
-    io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-    io.write Slice.new(data, count)
-    writep.value = LibC::SizeT.new(count)
-    1
-  end
+  def self.read_ex(b, buffer, len, readp)
+    size = len > Int32::MAX ? Int32::MAX : len.to_i
+    bio = Box(BIO).unbox(LibCrypto.BIO_get_data(b))
 
-  def self.write(bio, data, len)
-    io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-    io.write Slice.new(data, len)
-    len
-  end
+    {% if OpenSSL.has_constant?(:KTLS) %}
+      if bio.ktls_recv?
+        case ret = KTLS.read_record(bio.socket, buffer, size)
+        in Int32
+          readp.value = LibC::SizeT.new(ret)
+          return 1
+        in Errno
+          readp.value = LibC::SizeT.new(0)
+          return ret.value
+        end
+      end
+    {% end %}
 
-  def self.read_ex(bio, buffer, len, readp)
-    count = len > Int32::MAX ? Int32::MAX : len.to_i
-    io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-    ret = io.read Slice.new(buffer, count)
+    ret = bio.io.read(Slice.new(buffer, size))
     readp.value = LibC::SizeT.new(ret)
     1
   end
 
-  def self.read(bio, buffer, len)
-    io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-    io.read(Slice.new(buffer, len)).to_i
+  def self.read(b, buffer, len)
+    bio = Box(BIO).unbox(LibCrypto.BIO_get_data(b))
+    bio.io.read(Slice.new(buffer, len)).to_i
   end
 
-  def self.ctrl(bio, cmd, num, ptr)
-    io = Box(IO).unbox(LibCrypto.BIO_get_data(bio))
-    val = case cmd
-          when LibCrypto::CTRL_FLUSH
-            io.flush
-            1
-          when LibCrypto::CTRL_PUSH, LibCrypto::CTRL_POP, LibCrypto::CTRL_EOF
-            0
-          when LibCrypto::CTRL_SET_KTLS_SEND
-            0
-          when LibCrypto::CTRL_GET_KTLS_SEND, LibCrypto::CTRL_GET_KTLS_RECV
-            0
-          when LibCrypto::BIO_C_GET_FD
-            if io.is_a?(Socket) || io.is_a?(IO::FileDescriptor)
-              io.fd
+  def self.write_ex(b, data, len, writep)
+    size = len > Int32::MAX ? Int32::MAX : len.to_i
+    bio = Box(BIO).unbox(LibCrypto.BIO_get_data(b))
+
+    {% if OpenSSL.has_constant?(:KTLS) %}
+      if bio.ktls_send_ctrl_msg?
+        case ret = KTLS.send_ctrl_message(bio.socket, bio.ktls_record_type, data, size)
+        in Int32
+          LibCrypto.BIO_clear_flags(b, LibCrypto::BIO_FLAGS_KTLS_TX_CTRL_MSG)
+          writep.value = LibC::SizeT.new(ret)
+          return 1
+        in Errno
+          writep.value = LibC::SizeT.new(0)
+          return ret.value
+        end
+      end
+    {% end %}
+
+    bio.io.write Slice.new(data, size)
+    writep.value = LibC::SizeT.new(size)
+    1
+  end
+
+  def self.write(b, data, len)
+    bio = Box(BIO).unbox(LibCrypto.BIO_get_data(b))
+    bio.io.write Slice.new(data, len)
+    len
+  end
+
+  def self.ctrl(b, cmd, num, ptr)
+    bio = Box(BIO).unbox(LibCrypto.BIO_get_data(b))
+    val = {% begin %}
+            case cmd
+            when LibCrypto::CTRL_FLUSH
+              bio.io.flush
+              1
+            when LibCrypto::CTRL_PUSH, LibCrypto::CTRL_POP, LibCrypto::CTRL_EOF
+              0
+            {% if OpenSSL.has_constant?(:KTLS) %}
+              when LibCrypto::CTRL_SET_KTLS
+                socket = bio.socket
+                is_tx = num != 0
+                if KTLS.enable(socket) && KTLS.start(socket, ptr, is_tx)
+                  LibCrypto.BIO_set_flags(b, is_tx ? LibCrypto::BIO_FLAGS_KTLS_TX : LibCrypto::BIO_FLAGS_KTLS_RX)
+                  1
+                else
+                  0
+                end
+              when LibCrypto::CTRL_GET_KTLS_SEND
+                bio.ktls_send? ? 1 : 0
+              when LibCrypto::CTRL_GET_KTLS_RECV
+                bio.ktls_recv? ? 1 : 0
+              when LibCrypto::CTRL_SET_KTLS_TX_SEND_CTRL_MSG
+                LibCrypto.BIO_set_flags(b, LibCrypto::BIO_FLAGS_KTLS_TX_CTRL_MSG)
+                bio.ktls_record_type = num.to_u8
+                0
+              when LibCrypto::CTRL_CLEAR_KTLS_TX_CTRL_MSG
+                LibCrypto.BIO_clear_flags(b, LibCrypto::BIO_FLAGS_KTLS_TX_CTRL_MSG)
+                0
+              when LibCrypto::CTRL_SET_KTLS_TX_ZEROCOPY_SENDFILE
+                ret = KTLS.enable_tx_zerocopy_sendfile(bio.socket)
+                LibCrypto.BIO_set_flags(b, LibCrypto::BIO_FLAGS_KTLS_TX_ZEROCOPY_SENDFILE) if ret
+                ret ? 1 : 0
+            {% else %}
+              when LibCrypto::CTRL_SET_KTLS,
+                   LibCrypto::CTRL_GET_KTLS_SEND,
+                   LibCrypto::CTRL_GET_KTLS_RECV,
+                   LibCrypto::CTRL_SET_KTLS_TX_ZEROCOPY_SENDFILE
+                0
+            {% end %}
+            when LibCrypto::BIO_C_GET_FD
+              io = bio.io
+              if io.is_a?(Socket) || io.is_a?(IO::FileDescriptor)
+                io.fd
+              else
+                -1
+              end
             else
-              -1
+              STDERR.puts "WARNING: Unsupported BIO ctrl call (#{cmd})"
+              0
             end
-          else
-            STDERR.puts "WARNING: Unsupported BIO ctrl call (#{cmd})"
-            0
-          end
+          {% end %}
     LibCrypto::Long.new(val)
   end
 
@@ -93,21 +154,37 @@ struct OpenSSL::BIO
   end
 
   def initialize(@io : IO)
-    if io.is_a?(IO::Buffered)
-      # Disable buffers of the underlying IO (e.g. TCP socket) so OpenSSL
-      # becomes responsible of what needs to be read/written on the wire;
-      # instead, buffers shall be on OpenSSL::SSL::Socket (for example).
-      io.sync = true
-      io.read_buffering = false
-    end
-
     @bio = LibCrypto.BIO_new(CRYSTAL_BIO)
     raise OpenSSL::Error.new("BIO_new") if @bio.null?
 
-    LibCrypto.BIO_set_data(@bio, Box(IO).box(io))
+    LibCrypto.BIO_set_data(@bio, Box.box(self))
   end
 
-  getter io
+  getter io : IO
+
+  def socket
+    @io.as(Socket)
+  end
+
+  {% if OpenSSL.has_constant?(:KTLS) %}
+    property ktls_record_type : UInt8 = 0
+  {% end %}
+
+  def ktls_send?
+    @io.is_a?(Socket) && flag?(LibCrypto::BIO_FLAGS_KTLS_TX)
+  end
+
+  def ktls_send_ctrl_msg?
+    @io.is_a?(Socket) && flag?(LibCrypto::BIO_FLAGS_KTLS_TX_CTRL_MSG)
+  end
+
+  def ktls_recv?
+    @io.is_a?(Socket) && flag?(LibCrypto::BIO_FLAGS_KTLS_RX)
+  end
+
+  private def flag?(flag)
+    (LibCrypto.BIO_test_flags(@bio, flag) & flag) == flag
+  end
 
   def to_unsafe
     @bio
