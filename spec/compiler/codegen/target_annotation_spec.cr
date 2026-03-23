76bb31409608e8984512c8f14bfa2895987598d4
Julien Portalier <julien@portalier.com>
2026-03-23 14:56:20 +0100

    Fix: function must be called
    We must call the function for it to be compiled, otherwise
    it's dead code and will be skipped and always succeed.
    
    We must also specify the compilation target otherwise specs
    are failing once we call the function with a LLVM error.

--- spec/compiler/codegen/target_annotation_spec.cr
+++ spec/compiler/codegen/target_annotation_spec.cr
@@ -2,41 +2,43 @@ require "../spec_helper"
 
 describe "Code gen: TargetFeature annotation" do
   it "can target optional CPU features" do
-    compile(<<-CRYSTAL)
-      require "prelude"
+    {% if compare_versions(Crystal::LLVM_VERSION, "13.0.0") < 0 %} pending! "requires LLVM 13+" {% end %}
 
+    compile(<<-CRYSTAL, target: "aarch64-darwin")
       @[TargetFeature("+sve,+sve2")]
       def sve2_smoke_test : Nil
         asm("ext z0.b, { z1.b, z2.b }, #0" :::: "volatile")
         nil
       end
+
+      sve2_smoke_test
       CRYSTAL
   end
 
   it "can optimize code for a specific CPU" do
-    compile(<<-CRYSTAL)
-      require "prelude"
+    {% if compare_versions(Crystal::LLVM_VERSION, "13.0.0") < 0 %} pending! "requires LLVM 13+" {% end %}
 
-      {% if flag?(:aarch64) %}
-        @[TargetFeature(cpu: "apple-m1")]
-      {% elsif flag?(:x86_64) %}
-        @[TargetFeature(cpu: "x86-64-v3")]
-      {% end %}
+    compile(<<-CRYSTAL, prelude: "prelude", target: "x86_64-linux-gnu")
+      @[TargetFeature(cpu: "x86-64-v3")]
       def foo
         [1, 2, 3].sample
       end
+
+      foo
       CRYSTAL
   end
 
   it "can target optional CPU features and optimize code for a specific CPU" do
-    compile(<<-CRYSTAL)
-      require "prelude"
+    {% if compare_versions(Crystal::LLVM_VERSION, "13.0.0") < 0 %} pending! "requires LLVM 13+" {% end %}
 
+    compile(<<-CRYSTAL, target: "aarch64-darwin")
       @[TargetFeature("+sve,+sve2", cpu: "apple-m1")]
       def sve2_smoke_test : Nil
         asm("ext z0.b, { z1.b, z2.b }, #0" :::: "volatile")
         nil
       end
+
+      sve2_smoke_test
       CRYSTAL
   end
 end
