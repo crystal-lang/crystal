cc00527d8f0dd140af9b396a038277d2586427f4
Johannes Müller <straightshoota@gmail.com>
2026-04-18 13:54:11 +0200

    Exclude history reference to `File.writeable`

--- src/io/memory.cr
+++ src/io/memory.cr
@@ -40,7 +40,7 @@ class IO::Memory < IO
   #
   # ```
   # slice = Slice.new(6) { |i| ('a'.ord + i).to_u8 }
-  # io = IO::Memory.new slice, writable: false
+  # io = IO::Memory.new slice, writeable: false
   # io.pos            # => 0
   # io.read(slice)    # => 6
   # String.new(slice) # => "abcdef"
