2cbc95c3fdbc3ca2ac3b8170c5d01cf4ef7dba0a
Jamie Gaskins <jgaskins@hey.com>
2026-04-24 19:44:39 -0400

    Add Sync::MutexSet

---
+++ src/sync/mutex_set.cr
@@ -0,0 +1,29 @@
+module Sync
+  # The `MutexSet` is a collection of `Sync::Mutex`es that must all be locked in
+  # a deterministic order.
+  struct MutexSet
+    @mutexes : Array(Sync::Mutex)
+
+    def initialize(mutexes : Enumerable(Sync::Mutex))
+      @mutexes = mutexes.sort_by(&.object_id).uniq!
+    end
+
+    # Locks all mutexes in this `MutexSet` in a deterministic order for the duration of the block.
+    def synchronize(&)
+      lock
+      yield
+    ensure
+      unlock
+    end
+
+    # Locks all mutexes in this `MutexSet` in a deterministic order.
+    def lock
+      @mutexes.each(&.lock)
+    end
+
+    # Unlocks all mutexes in this `MutexSet` in a deterministic order.
+    def unlock
+      @mutexes.reverse_each(&.unlock)
+    end
+  end
+end
