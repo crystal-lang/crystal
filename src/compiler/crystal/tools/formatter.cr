222a39f474b3c7a8e36184367796bbbb30c3e9de
Johannes Müller <straightshoota@gmail.com>
2026-04-25 14:52:34 +0200

    Revert "Refactor `Formatter` code for simplification"
    This partially reverts commit 9546b9f49eec8c9a700f2dcc60c59290602b81db

--- src/compiler/crystal/tools/formatter.cr
+++ src/compiler/crystal/tools/formatter.cr
@@ -1258,21 +1258,37 @@ module Crystal
           break
         end
 
-        unless i == 0
-          skip_space_or_newline
-          write_token " ", :op_bar, " "
-          skip_space
-
-          if @token.type.newline?
-            write_line
-            write_indent(column)
-            next_token_skip_space_or_newline
-          end
-        end
-
         accept type
 
-        skip_space
+        last = last?(i, node.types)
+        if last
+          skip_space
+        else
+          skip_space_or_newline
+        end
+
+        while true
+          case @token.type
+          when .op_bar?
+            write " | "
+            next_token_skip_space
+            if @token.type.newline?
+              write_line
+              write_indent(column)
+              next_token_skip_space_or_newline
+            end
+          when .op_rparen?
+            if @paren_count > 0
+              @paren_count -= 1
+              write ")"
+              next_token_skip_space
+            else
+              break
+            end
+          else
+            break
+          end
+        end
       end
 
       check_close_paren
