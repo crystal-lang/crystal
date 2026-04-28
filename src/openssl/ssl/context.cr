06c29279d3db504d9f86de9309f08c57687d3f9f
Julien Portalier <julien@portalier.com>
2026-04-28 20:02:08 +0200

    fixup! OpenSSL Kernel TLS support is now opt-in

--- src/openssl/ssl/context.cr
+++ src/openssl/ssl/context.cr
@@ -259,7 +259,9 @@ abstract class OpenSSL::SSL::Context
       NO_SESSION_RESUMPTION_ON_RENEGOTIATION,
       NO_RENEGOTIATION,
     ))
-    remove_options(OpenSSL::SSL::Options::ENABLE_KTLS)
+    {% if OpenSSL.has_constant?(:KTLS) %}
+      remove_options(OpenSSL::SSL::Options::ENABLE_KTLS)
+    {% end %}
     add_modes(OpenSSL::SSL::Modes.flags(AUTO_RETRY, RELEASE_BUFFERS))
 
     # OpenSSL does not support reading from the system root certificate store on
