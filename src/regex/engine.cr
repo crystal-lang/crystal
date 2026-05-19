f97aa68676e0b13d18d26e03268915bc491ea700
Johannes Müller <straightshoota@gmail.com>
2026-05-19 13:53:19 +0200

    Accept `$USE_PCRE1` as alternative to compiler flag

--- src/regex/engine.cr
+++ src/regex/engine.cr
@@ -1,7 +1,7 @@
 # PCRE2 is the default regex engine and should always be preferred.
 # The legacy PCRE engine is still supported for compatibility reasons, but it is
-# opt-in via compiler flag `-Duse_pcre`.
-{% unless flag?(:use_pcre) %}
+# opt-in via compiler flag `-Duse_pcre` or env var `USE_PCRE1`.
+{% unless flag?(:use_pcre) || !(env("USE_PCRE1") || "").empty? %}
   require "./pcre2"
 
   # :nodoc:
