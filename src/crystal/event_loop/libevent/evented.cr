459c1e691abfda4db12403925de362254d59c01e
Julien Portalier <julien@portalier.com>
2026-05-31 17:24:30 +0200

    Fix: build without execution context

--- src/crystal/event_loop/libevent/evented.cr
+++ src/crystal/event_loop/libevent/evented.cr
@@ -14,7 +14,7 @@ module Crystal::EventLoop::LibEvent::Evented
     @read_timed_out.set(timed_out, :relaxed)
 
     if reader = @reader.swap(nil, :relaxed)
-      {% if !flag?(:without_mt) && EventLoop.has_constant?(:LibEvent) %}
+      {% if flag?(:execution_context) %}
         event_loop = EventLoop.current.as(LibEvent)
         event_loop.callback_enqueue(reader)
       {% else %}
@@ -28,7 +28,7 @@ module Crystal::EventLoop::LibEvent::Evented
     @write_timed_out.set(timed_out, :relaxed)
 
     if writer = @writer.swap(nil, :relaxed)
-      {% if !flag?(:without_mt) && EventLoop.has_constant?(:LibEvent) %}
+      {% if flag?(:execution_context) %}
         event_loop = EventLoop.current.as(LibEvent)
         event_loop.callback_enqueue(writer)
       {% else %}
