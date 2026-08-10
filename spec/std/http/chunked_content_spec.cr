d65b54286fabb8bd702c0a8bf521f4b170e3bdca
Johannes Müller <straightshoota@gmail.com>
2026-08-10 16:29:38 +0200

    Fail on invalid trailer key

--- spec/std/http/chunked_content_spec.cr
+++ spec/std/http/chunked_content_spec.cr
@@ -208,6 +208,15 @@ describe HTTP::ChunkedContent do
         chunked.gets
       end
     end
+
+    it "fails on invalid trailer key" do
+      mem = IO::Memory.new("0\r\nFoo\0: Bar\r\n\r\n")
+
+      chunked = HTTP::ChunkedContent.new(mem)
+      expect_raises ArgumentError, "Invalid header name" do
+        chunked.gets
+      end
+    end
   end
 
   describe "long trailer part" do
