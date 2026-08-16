af2a9a5b6b5ae6acaf9a1cf5b6ccabd9691ac33f
Teresa M. N. Egea <teresamnegea@proton.me>
2026-08-16 03:43:45 -0300

    Added a section on mkdir_p

--- src/dir.cr
+++ src/dir.cr
@@ -284,6 +284,7 @@ class Dir
   # Dir.mkdir("testdir")
   # Dir.exists?("testdir") # => true
   # ```
+  #
   # Throws if the directory already exists.
   def self.mkdir(path : Path | String, mode : Int32 = 0o777) : Nil
     Crystal::System::Dir.create(path.to_s, mode)
@@ -292,6 +293,8 @@ class Dir
   # Creates a new directory at the given path, including any non-existing
   # intermediate directories. The linux-style permission mode can be specified,
   # with a default of 777 (0o777).
+  #
+  # Does nothing if the directory already exists.
   def self.mkdir_p(path : Path | String, mode : Int32 = 0o777) : Nil
     return if Dir.exists?(path)
 
