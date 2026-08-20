e9fdda80f6fb26d73b8c3c5fd94901779e36ea6e
Johannes Müller <straightshoota@gmail.com>
2026-08-20 13:20:36 +0200

    Update markd 0.6.0

--- lib/markd/src/markd.cr
+++ lib/markd/src/markd.cr
@@ -8,11 +8,38 @@ require "./markd/parser"
 require "./markd/version"
 
 module Markd
-  def self.to_html(source : String, options = Options.new)
-    return "" if source.empty?
+  {% if @top_level.has_constant?("Tartrazine") %}
+    def self.to_html(
+      source : String,
+      options = Options.new,
+      *,
+      formatter : Tartrazine::Formatter | String = "catppuccin-macchiato",
+    )
+      return "" if source.empty?
 
-    document = Parser.parse(source, options)
-    renderer = HTMLRenderer.new(options)
-    renderer.render(document)
-  end
+      if formatter.is_a?(String)
+        formatter = Tartrazine::Html.new(
+          theme: Tartrazine.theme(formatter),
+          line_numbers: true,
+          standalone: true,
+        )
+      end
+
+      document = Parser.parse(source, options)
+      renderer = HTMLRenderer.new(options)
+      renderer.render(document, formatter)
+    end
+  {% else %}
+    def self.to_html(
+      source : String,
+      options = Options.new,
+      formatter = nil,
+    )
+      return "" if source.empty?
+
+      document = Parser.parse(source, options)
+      renderer = HTMLRenderer.new(options)
+      renderer.render(document, formatter)
+    end
+  {% end %}
 end
