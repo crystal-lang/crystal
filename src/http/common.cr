69049f550fc4593b8c5babf02cab9b7b35633226
Johannes Müller <straightshoota@gmail.com>
2026-08-28 13:36:26 +0200

    nodoc
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/http/common.cr
+++ src/http/common.cr
@@ -534,6 +534,7 @@ module HTTP
     end
   end
 
+  # :nodoc:
   def self.validate_resource(string : String) : String
     if string.empty? || string.to_slice.any? { |c| c < 0x21_u8 || c >= 0x7F_u8 }
       raise ArgumentError.new("Invalid HTTP resource: #{string.inspect}")
