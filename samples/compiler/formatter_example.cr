5343607471e1673873271bcc48305f2bbe425e54
Quinton Miller <nicetas.c@gmail.com>
2026-08-29 13:19:19 +0800

    Do not use relative requires for the standard library

--- samples/compiler/formatter_example.cr
+++ samples/compiler/formatter_example.cr
@@ -1,8 +1,7 @@
 # This is a small sample on how to use a Crystal's
 # formatter programmatically.
 
-# Use `require "compiler/crystal/formatter"` in your programs
-require "../../src/compiler/crystal/formatter"
+require "compiler/crystal/formatter"
 
 source = "[ 1 , 2 , 3].map { | x |  x.to_s  }"
 result = Crystal.format(source)
