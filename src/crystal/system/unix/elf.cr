f9add4986cee36341ac8418320adf81ecd5024f7
Julien Portalier <julien@portalier.com>
2026-08-31 11:13:10 +0200

    Fix: ASLR base address on musl-libc when linked with -static-pie

--- src/crystal/system/unix/elf.cr
+++ src/crystal/system/unix/elf.cr
@@ -16,6 +16,8 @@ class Crystal::System::ELF
     ENDIAN_LITTLE = 1
     ENDIAN_BIG    = 2
 
+    ET_DYN = 3
+
     struct Header
       ei_magic : UInt8[4]
       ei_class : UInt8
@@ -85,6 +87,10 @@ class Crystal::System::ELF
       header.value.ei_version == 1 &&
       header.value.e_version == 1 &&
       header.value.e_ehsize == sizeof(LibELF::Header)
+  end
+
+  def pie?
+    header.value.e_type == LibELF::ET_DYN
   end
 
   def section?(name : String, &)
