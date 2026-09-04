ae0e15d0d03f459693694f39e0759525d85cdf72
Johannes Müller <straightshoota@gmail.com>
2026-09-04 16:32:41 +0200

    Remove duplicate/unnecessary specs

--- spec/std/char/reader_spec.cr
+++ spec/std/char/reader_spec.cr
@@ -155,18 +155,6 @@ describe "Char::Reader" do
     end
   end
 
-  it "peeks next char" do
-    reader = Char::Reader.new("há日本語")
-    reader.peek_next_char.should eq('á')
-  end
-
-  it "sets pos" do
-    reader = Char::Reader.new("há日本語")
-    reader.pos = 1
-    reader.pos.should eq(1)
-    reader.current_char.should eq('á')
-  end
-
   describe "#each" do
     it "yields chars" do
       reader = Char::Reader.new("abc")
@@ -226,39 +214,6 @@ describe "Char::Reader" do
     reader.previous_char.should eq('á')
     reader.previous_char.should eq('h')
     reader.has_previous?.should be_false
-  end
-
-  it "#current_char?" do
-    reader = Char::Reader.new("há日本語")
-    reader.current_char?.should eq('h')
-    reader.next_char
-    reader.current_char?.should eq('á')
-    reader.next_char
-    reader.current_char?.should eq('日')
-    reader.next_char
-    reader.current_char?.should eq('本')
-    reader.next_char
-    reader.current_char?.should eq('語')
-    reader.next_char
-    reader.current_char?.should be_nil
-    reader.previous_char
-    reader.current_char?.should eq('語')
-  end
-
-  it "#next_char?" do
-    reader = Char::Reader.new("há日本語")
-    reader.next_char?.should eq('á')
-    reader.pos.should eq(1)
-    reader.next_char?.should eq('日')
-    reader.pos.should eq(3)
-    reader.next_char?.should eq('本')
-    reader.pos.should eq(6)
-    reader.next_char?.should eq('語')
-    reader.pos.should eq(9)
-    reader.next_char?.should be_nil
-    reader.pos.should eq(12)
-    reader.next_char?.should be_nil
-    reader.pos.should eq(12)
   end
 
   it "#previous_char?" do
