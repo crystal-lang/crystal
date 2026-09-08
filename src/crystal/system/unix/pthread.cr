684ac68d95b9b8ffdc02c44a19bf735d8b9ceef2
Julien Portalier <julien@portalier.com>
2026-09-08 18:14:04 +0200

    Fix: sem_wait may be interrupted by signal

--- src/crystal/system/unix/pthread.cr
+++ src/crystal/system/unix/pthread.cr
@@ -303,8 +303,12 @@ module Crystal::System::Thread
   end
 
   def wait : Nil
-    if LibC.sem_wait(semaphore) == -1
-      raise RuntimeError.from_errno("sem_wait")
+    while true
+      if LibC.sem_wait(semaphore) == -1
+        raise RuntimeError.from_errno("sem_wait") unless Errno.value == Errno::EINTR
+      else
+        break
+      end
     end
   end
 
