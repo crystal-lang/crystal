2b1eb9116929b1e7fc4bb9178ef163ac9fa84711
Johannes Müller <straightshoota@gmail.com>
2026-09-09 10:57:23 +0200

    Ensure `current_char_width == 0` at end

--- spec/std/char/reader_spec.cr
+++ spec/std/char/reader_spec.cr
@@ -40,6 +40,7 @@ private def assert_at_end(reader)
   reader.current_char.should eq '\0'
   reader.current_char?.should be_nil
   reader.pos.should eq reader.string.bytesize
+  reader.current_char_width.should eq(0)
 
   reader.has_next?.should be_false
 
@@ -136,7 +137,6 @@ describe "Char::Reader" do
       reader = Char::Reader.new("")
       reader.error.should be_nil
 
-      reader.current_char_width.should eq 0
       assert_at_end(reader)
       assert_at_start(reader)
 
@@ -150,7 +150,6 @@ describe "Char::Reader" do
       reader = Char::Reader.new(at_end: "")
       reader.error.should be_nil
 
-      reader.current_char_width.should eq 0
       assert_at_end(reader)
       assert_at_start(reader)
 
--- src/char/reader.cr
+++ src/char/reader.cr
@@ -132,11 +132,13 @@ struct Char
     # reader.current_char # => '\0'
     # ```
     def next_char? : Char?
-      next_pos = @pos + @current_char_width
-      if next_pos <= @string.bytesize
-        @pos = next_pos
+      @pos &+= @current_char_width
+      if has_next?
         decode_current_char
-        current_char?
+      else
+        @current_char_width = 0
+        @current_char = '\0'
+        nil
       end
     end
 
@@ -170,11 +172,13 @@ struct Char
     # reader.current_char   # => 'a'
     # ```
     def peek_next_char : Char
-      next_pos = @pos + @current_char_width
-
-      if next_pos > @string.bytesize
+      if @current_char_width.zero?
         raise IndexError.new
       end
+
+      next_pos = @pos &+ @current_char_width
+
+      return '\0' if next_pos == @string.bytesize
 
       decode_char_at(next_pos) do |code_point|
         code_point.unsafe_chr
