404f135e2aae8ae9f1784be7df39ad1e1d2a94a9
Julien Portalier <julien@portalier.com>
2026-09-11 11:06:51 +0200

    Fix: tracing with 128-bit integers (metadata)

--- src/crystal/tracing.cr
+++ src/crystal/tracing.cr
@@ -48,7 +48,7 @@ module Crystal
 
         def initialize
           @buf = uninitialized UInt8[N]
-          @int_buf = uninitialized UInt8[20] # max 64-bit integers
+          @int_buf = uninitialized UInt8[40] # max 128-bit integers
           @size = 0
         end
 
