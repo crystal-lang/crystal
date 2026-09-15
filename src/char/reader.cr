c71a0da8e531f27b74a3a7999c746a87c7bd6558
youdie006 <xncb135@korea.ac.kr>
2026-09-15 20:27:08 +0900

    Reuse `#pos=` in the constructor instead of repeating the guard
    Per review. `self.pos = pos.to_i` alone does not compile, because Crystal
    then sees no direct assignment to @pos and makes it nilable, so @pos is
    initialized first and the setter does the validation and the decode.

--- src/char/reader.cr
+++ src/char/reader.cr
@@ -81,13 +81,8 @@ struct Char
     # Creates a reader with the specified *string* positioned at
     # byte index *pos*.
     def initialize(@string : String, pos = 0)
-      pos = pos.to_i
-      unless 0 <= pos <= @string.bytesize
-        raise IndexError.new
-      end
-
-      @pos = pos
-      decode_current_char
+      @pos = 0
+      self.pos = pos.to_i
     end
 
     # Creates a reader that will be positioned at the last char
