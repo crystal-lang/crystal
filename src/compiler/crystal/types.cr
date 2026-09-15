ded4ca2e064a3d8ed56fc9c04d00a57e0ab1ada2
Quinton Miller <nicetas.c@gmail.com>
2026-09-16 05:35:06 +0800

    Add all enum types to `Enum`'s list of subclasses

--- src/compiler/crystal/types.cr
+++ src/compiler/crystal/types.cr
@@ -2862,10 +2862,15 @@ module Crystal
 
       add_def Def.new("value", [] of Arg, Primitive.new("enum_value", @base_type))
       metaclass.as(ModuleType).add_def Def.new("new", [Arg.new("value", restriction: Path.global(@base_type.to_s))], Primitive.new("enum_new", self))
+      program.enum.add_subclass self
     end
 
     def parents
       @parents ||= [program.enum] of Type
+    end
+
+    def superclass
+      program.enum
     end
 
     def add_constant(name, value)
