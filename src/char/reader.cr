5f2b9023e395d92dc3d90af67bc1903e935fbaaf
Johannes Müller <straightshoota@gmail.com>
2026-09-16 21:15:33 +0200

    Update documentation

--- src/char/reader.cr
+++ src/char/reader.cr
@@ -1,16 +1,17 @@
 struct Char
-  # A `Char::Reader` allows iterating a `String` by Chars.
+  # A `Char::Reader` allows iterating a string by character.
   #
-  # As soon as you instantiate a `Char::Reader` it will decode the first
-  # char in the `String`, which can be accessed by invoking `current_char`.
-  # At this point `pos`, the current position in the string, will equal zero.
-  # Successive calls to `next_char` return the next chars in the string,
-  # advancing `pos`.
+  # The string can be a `String` instance or a `Bytes` slice pointing to a UTF-8
+  # encoded string which does not need to be null-terminated.
   #
-  # NOTE: The null character `'\0'` will be returned in `current_char` when
-  # the end is reached (as well as when the string is empty). Thus, `has_next?`
-  # will return `false` only when `pos` is equal to the string's bytesize, in which
-  # case `current_char` will always be `'\0'`.
+  # `Char::Reader.new` decodes the first character (or last in case of `at_end`),
+  # which can be accessed by invoking `current_char`.  At this point, `pos`, the
+  # current position in the string, equals zero.  Successive calls to
+  # `next_char` return the next chars in the string, advancing `pos`.
+  #
+  # When the end is reached (i.e. `pos` equals the string's bytesize),
+  # `#current_char` returns the null character `'\0'` and `has_next?` returns
+  # `false`.
   #
   # NOTE: For performance reasons, `Char::Reader` has value semantics, so care
   # must be taken when a reader is declared as a local variable and passed to
@@ -36,7 +37,9 @@ struct Char
   struct Reader
     include Enumerable(Char)
 
-    # Returns the reader's String.
+    # Returns the reader's `String` value.
+    #
+    # Not available if the reader was constructed to operate on `Bytes`.
     getter! string : String
 
     # Returns the current character, or `'\0'` if the reader is at the end of
@@ -78,26 +81,26 @@ struct Char
     # out of bounds. Otherwise returns `nil`.
     getter error : UInt8?
 
-    # Creates a reader with the specified *string* positioned at
-    # byte index *pos*.
+    # Creates a reader with the specified *string* positioned at byte index
+    # *pos*.
     def initialize(@string : String, pos = 0)
       initialize(string.to_slice, pos.to_i)
     end
 
-    # Creates a reader that will be positioned at the last char
-    # of the given string.
+    # Creates a reader that will be positioned at the last char of the given
+    # string.
     def initialize(*, at_end @string : String)
       initialize(at_end: string.to_slice)
     end
 
     # Creates a reader with the specified *slice* of UTF-8 encoded string,
-    # positioned at# byte index *pos*.
+    # positioned at byte index *pos*.
     def initialize(@slice : Bytes, @pos : Int32 = 0)
       decode_current_char unless @slice.empty?
     end
 
-    # Creates a reader that will be positioned at the last char
-    # of the given slice of UTF-8 encoded string.
+    # Creates a reader that will be positioned at the last char of the given
+    # slice of UTF-8 encoded string.
     def initialize(*, at_end @slice : Bytes)
       @pos = @slice.size
       decode_previous_char unless @slice.empty?
